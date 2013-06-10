2cb76c5152c98f60e25bafe9ba7374107268d214
gtucker <gtucker@093548ea-dc74-4ebb-8037-4be9f23db00a>
2013-06-10 14:59:42 +0000

    Moved grid_schematic to model_grid_guide; added test/example script diffusion_with_model_grid.py. GT June 2013

---
+++ docs/model_grid_guide/diffusion_with_model_grid.py
@@ -0,0 +1,16 @@
+#! /usr/env/python
+"""
+
+2D numerical model of diffusion, implemented using ModelGrid.
+Provides a simple tutorial example of ModelGrid functionality.
+
+Last updated GT June 2013
+
+"""
+
+import numpy
+from landlab import model_grid
+
+
+
+
