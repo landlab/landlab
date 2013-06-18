1c9a66b2e437a438ab80e5ad49b6a7476647e95a
huttone <huttone@093548ea-dc74-4ebb-8037-4be9f23db00a>
2013-06-18 19:38:10 +0000

    * Added a main function that contains the unit test code.
    Added
    ~~~~~
    
    * New function *main* that contains the unit test code. This prevents the
      unit test from running when the module is imported.

--- landlab/tests/run_raster_model_grid_builtin_unit_test.py
+++ landlab/tests/run_raster_model_grid_builtin_unit_test.py
@@ -5,8 +5,11 @@ Runs RasterModelGrid's unit_test() method
 
 from landlab import model_grid
 
-reload(model_grid)
+def main():
+    reload(model_grid)
     
-mg = model_grid.RasterModelGrid()
+    mg = model_grid.RasterModelGrid()
+    mg.unit_test()
 
-mg.unit_test()
+if __name__ == '__main__':
+    main()
