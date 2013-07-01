a724575ea0aa7572f27b8ee20f153834851b62f7
gtucker <gtucker@093548ea-dc74-4ebb-8037-4be9f23db00a>
2013-07-01 21:49:44 +0000

    Fixed a minor error in the speed test script. It turns out that the new flux divergence algorithm is ~200x faster!

--- landlab/tests/speed_test_model_grid.py
+++ landlab/tests/speed_test_model_grid.py
@@ -8,7 +8,7 @@ import numpy
 
 mg = RasterModelGrid(20, 30, 1.0)
 
-nt = 100
+nt = 1000
 
 s = mg.create_node_dvector()
 g = numpy.zeros(mg.num_active_links)
@@ -19,28 +19,29 @@ for i in range(nt):
     
     g = mg.calculate_gradients_at_active_links(s, g)
     
-print('Elapsed time with fast gradient algo: '+str(time.time()-start_time))
-
-start_time = time.time()
+time1 = time.time()
 
 for i in range(nt):
     
     g = mg.calculate_gradients_at_active_links_slow(s, g)
     
-print('Elapsed time with slow gradient algo: '+str(time.time()-start_time))
+time2 = time.time()
 
 for i in range(nt):
     
     divg = mg.calculate_flux_divergence_at_nodes(g)
     
-print('Elapsed time with fast node-divergence algo: '+str(time.time()-start_time))
-
-start_time = time.time()
+time3 = time.time()
 
 for i in range(nt):
     
     divg = mg.calculate_flux_divergence_at_nodes_slow(g)
-    
-print('Elapsed time with slow node-divergence algo: '+str(time.time()-start_time))
+
+time4 = time.time()
+  
+print('Elapsed time with fast gradient algo: '+str(time1-start_time))
+print('Elapsed time with slow gradient algo: '+str(time2-time1))
+print('Elapsed time with fast node-divergence algo: '+str(time3-time2))
+print('Elapsed time with slow node-divergence algo: '+str(time4-time3))
 
    
