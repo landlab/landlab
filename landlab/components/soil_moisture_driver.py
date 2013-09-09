0ccff79abc39819bd4f8ad9dbe0b02432ad3011c
huttone <huttone@093548ea-dc74-4ebb-8037-4be9f23db00a>
2013-09-09 16:33:45 +0000

    * Changed soil moisture driver to not run on import.
    Changed
    =======
    
    * Wrapped the contents of soil_moisture_driver into a main function, which
      is run when soil_moisture_driver.py is run as a script. This prevents the
      code from running when the soil_moisture_driver is imported. To run from
      the Python command line:
        >>> import landlab.components.soil_moisture_driver as smd
        >>> smd.main()

--- landlab/components/soil_moisture_driver.py
+++ landlab/components/soil_moisture_driver.py
@@ -11,59 +11,64 @@
 
 from soil_moisture import *
 
-""" Create an Soil Moisture Object """
-SM = SoilMoisture()
-""" Initialize Soil Moisture """
-SM.initialize()
-SM.set_strms(50000)   # Example of using a set function
 
-SM._length = SM._iterate_storm
-""" Initializing arrays to store values of variables """
-Pa_ = zeros( SM._length, dtype = float )
-P_ = zeros( SM._length, dtype = float )
-ETA_ = zeros( SM._length, dtype = float )
-Sini_ = zeros( SM._length, dtype = float )
-S_ = zeros( SM._length, dtype = float )
-D_ = zeros( SM._length, dtype = float )
-Ro_ = zeros( SM._length, dtype = float )
+def main():
+    """ Create an Soil Moisture Object """
+    SM = SoilMoisture()
+    """ Initialize Soil Moisture """
+    SM.initialize()
+    SM.set_strms(50000)   # Example of using a set function
 
-""" Create an instance of Storm Class """
-PD = PrecipitationDistribution()
-""" Initialize Storm - Create first storm """
-PD.initialize()
+    SM._length = SM._iterate_storm
+    """ Initializing arrays to store values of variables """
+    Pa_ = zeros( SM._length, dtype = float )
+    P_ = zeros( SM._length, dtype = float )
+    ETA_ = zeros( SM._length, dtype = float )
+    Sini_ = zeros( SM._length, dtype = float )
+    S_ = zeros( SM._length, dtype = float )
+    D_ = zeros( SM._length, dtype = float )
+    Ro_ = zeros( SM._length, dtype = float )
 
-#######
+    """ Create an instance of Storm Class """
+    PD = PrecipitationDistribution()
+    """ Initialize Storm - Create first storm """
+    PD.initialize()
 
-""" Loop through the storms """
-for i in range( 0, SM._iterate_storm ):
-    if i != 0:
-       """ Create a new storm """ 
-       PD.update()
-    """ Soil moisture dynamics """
-    P = PD.storm_depth
-    Pin = PD.intensity
-    Tb = PD.interstorm_duration
-    Tr = PD.storm_duration
+    #######
 
-    """ Update Soil Moisture """
-    SM.update( P, Pin, Tb, Tr )
+    """ Loop through the storms """
+    for i in range( 0, SM._iterate_storm ):
+        if i != 0:
+           """ Create a new storm """ 
+           PD.update()
+        """ Soil moisture dynamics """
+        P = PD.storm_depth
+        Pin = PD.intensity
+        Tb = PD.interstorm_duration
+        Tr = PD.storm_duration
 
-    Pa_[i] = Pin
-    P_[i] = SM.get_Peff() # Example of using a get function
-    Sini_[i] = SM._Sini   # Example of using data stored in the object
-    S_[i] = SM.get_S()
-    ETA_[i] = SM._ETA     # Another example of using data stored in the object
-    Ro_[i] = SM.get_Ro()
-    D_[i] = SM.get_D()
-    
-#######
+        """ Update Soil Moisture """
+        SM.update( P, Pin, Tb, Tr )
 
-""" Calculate RHS of Water Balance """ 
-Total = (S_[SM._iterate_storm-1]-SM._si)*1000*SM._soil_pc*SM._zr+sum(ETA_)+sum(D_)+sum(Ro_)
-""" Calculate percentage error in LHS and RHS of water balance equation """
-Percent = (sum(P_)-Total)/(sum(P_))*100
-print '\nPercent Error: ', Percent, '\n'
+        Pa_[i] = Pin
+        P_[i] = SM.get_Peff() # Example of using a get function
+        Sini_[i] = SM._Sini   # Example of using data stored in the object
+        S_[i] = SM.get_S()
+        ETA_[i] = SM._ETA     # Another example of using data stored in the object
+        Ro_[i] = SM.get_Ro()
+        D_[i] = SM.get_D()
+        
+    #######
+
+    """ Calculate RHS of Water Balance """ 
+    Total = (S_[SM._iterate_storm-1]-SM._si)*1000*SM._soil_pc*SM._zr+sum(ETA_)+sum(D_)+sum(Ro_)
+    """ Calculate percentage error in LHS and RHS of water balance equation """
+    Percent = (sum(P_)-Total)/(sum(P_))*100
+    print '\nPercent Error: ', Percent, '\n'
 
 
-""" Plot output """
-SM.plott( P_, S_, ETA_, D_, Ro_ )
+    """ Plot output """
+    SM.plott( P_, S_, ETA_, D_, Ro_ )
+
+if __name__ == '__main__':
+    main()
