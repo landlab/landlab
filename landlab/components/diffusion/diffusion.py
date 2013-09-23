97c416787a82a5475ec798fa0a6d787b4c857993
gtucker <gtucker@093548ea-dc74-4ebb-8037-4be9f23db00a>
2013-09-23 21:35:04 +0000

    Edits to diffusion component, to compare "under the hood" versus "explicit"

--- landlab/components/diffusion/diffusion.py
+++ landlab/components/diffusion/diffusion.py
@@ -13,6 +13,9 @@ from landlab import create_and_initialize_grid
 
 _ALPHA = 0.1   # time-step stability factor
 
+_VERSION = 'make_all_data'
+#_VERSION = 'explicit'
+
 class DiffusionComponent():
     
     def __init__(self, grid=None, current_time=0.):
@@ -46,20 +49,58 @@ class DiffusionComponent():
         # Get a list of interior cells
         self.interior_cells = self.grid.get_active_cell_node_ids()
         
-        # Create data arrays for variables that won't (?) be shared with other
-        # components
-        self.g = self.grid.create_active_link_dvector()  # surface gradients
-        self.qs = self.grid.create_active_link_dvector()  # unit sediment flux
-        self.dqds = self.grid.create_node_dvector()  # sed flux derivative
+        # Here we're experimenting with different approaches: with 
+        # 'make_all_data', we create and manage all the data we need and embed
+        # it all in the grid. With 'explicit', we require the caller/user to 
+        # provide data.
+        if _VERSION=='make_all_data':
+            print 'creating internal data'
+            self.z = self.grid.create_node_array_zeros('landscape_surface__elevation')
+            self.g = self.grid.create_active_link_array_zeros('landscape_surface__gradient')  # surface gradients
+            self.qs = self.grid.create_active_link_array_zeros('unit_sediment_flux')  # unit sediment flux
+            self.dqds = self.grid.create_node_array_zeros('sediment_flux_divergence')  # sed flux derivative
+        elif _VERSION=='explicit':
+            pass
+        else:
+            # Create data arrays for variables that won't (?) be shared with other
+            # components
+            self.g = self.grid.create_active_link_array_zeros()  # surface gradients
+            self.qs = self.grid.create_active_link_array_zeros()  # unit sediment flux
+            self.dqds = self.grid.create_node_array_zeros()  # sed flux derivative
         
         
-    def run_one_step(self, z, delt):
+    def run_one_step_explicit(self, mg, z, g, qs, dqsds, dzdt, delt):
         
         # Take the smaller of delt or built-in time-step size self.dt
         dt = min(self.dt, delt)
         
         # Calculate the gradients and sediment fluxes
-        self.g = self.grid.calculate_gradients_at_active_links(z)
+        g = mg.calculate_gradients_at_active_links(z)
+        qs = -self.kd*g
+        
+        # Calculate the net deposition/erosion rate at each node
+        dqsds = mg.calculate_flux_divergence_at_nodes(qs)
+        
+        # Calculate the total rate of elevation change
+        dzdt = self.uplift_rate - dqsds
+        
+        # Update the elevations
+        z[self.interior_cells] = z[self.interior_cells] \
+                                 + dzdt[self.interior_cells] * dt
+                                 
+        # Update current time and return it
+        self.current_time += dt
+        
+        return z, g, qs, dqsds, dzdt
+        
+        
+    def run_one_step_internal(self, delt):
+        
+        # Take the smaller of delt or built-in time-step size self.dt
+        dt = min(self.dt, delt)
+        
+        # Calculate the gradients and sediment fluxes
+        self.g = self.grid.calculate_gradients_at_active_links(self.z)
         self.qs = -self.kd*self.g
         
         # Calculate the net deposition/erosion rate at each node
@@ -69,20 +110,28 @@ class DiffusionComponent():
         dzdt = self.uplift_rate - self.dqsds
         
         # Update the elevations
-        z[self.interior_cells] = z[self.interior_cells] \
-                                 + dzdt[self.interior_cells] * dt
+        self.z[self.interior_cells] += dzdt[self.interior_cells] * dt
                                  
         # Update current time and return it
         self.current_time += dt
         return self.current_time
         
         
-    def run_until(self, t, z):
+    def run_until_explicit(self, mg, t, z, g, qs, dqsds, dzdt):
         
         while self.current_time < t:
             remaining_time = t - self.current_time
-            self.run_one_step(z, remaining_time)
+            z, g, qs, dqsds, dzdt = self.run_one_step_explicit(mg, z, g, qs, dqsds, dzdt, remaining_time)
             
+        return z, g, qs, dqsds, dzdt
+        
+        
+    def run_until_internal(self, t):
+        
+        while self.current_time < t:
+            remaining_time = t - self.current_time
+            self.run_one_step_internal(remaining_time)
+                    
         
     def get_time_step(self):
         """
