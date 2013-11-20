d87ce75597b17953c58a9adaa5c0b0c468934ce0
nicgaspar <nicgaspar@093548ea-dc74-4ebb-8037-4be9f23db00a>
2013-11-20 20:55:22 +0000

    start of the driver for Jordan's AGU poster.

--- landlab/components/overland_flow/overland_flow_driver_springcreek.py
+++ landlab/components/overland_flow/overland_flow_driver_springcreek.py
@@ -6,11 +6,11 @@ This tests the overland flow and shear stress generator.
 """
 
 import landlab
-from landlab.components.dem_support.dem_boundary_conditions import WatershedBoundaryConditions
+#NG still trying to get the waterhsed boundary conditions working
+#from landlab.components.dem_support.dem_boundary_conditions import WatershedBoundaryConditions
 from landlab.components.overland_flow.overland_flow_generator import OverlandFlow
 from landlab.components.uniform_precip.generate_uniform_precip import PrecipitationDistribution
 from landlab.io import read_esri_ascii
-#import pylab as pl
 import pylab
 import numpy as np
 import os
@@ -18,67 +18,58 @@ import time
 
 def main():
     """
-    do some stuff
+    initialize DEM
+    Get storm info
+    Generate hydrograph
     """
     
     start_time = time.time()
     
-    dem_name = 'ExampleDEM/small_sc.asc'
+    dem_name = 'ExampleDEM/HalfFork.asc'
     
-#    outlet_row = 82
-#    outlet_column = 38
-#    next_to_outlet_row = 81
-#    next_to_outlet_column = 38
+
 
 
     # Create and initialize a raster model grid by reading a DEM
     DATA_FILE = os.path.join(os.path.dirname(__file__), dem_name)
     print('Reading data from "'+str(DATA_FILE)+'"')
     (rg, z) = read_esri_ascii(DATA_FILE)
+    nodata_val=-9999
+    
     print('DEM has '+str(rg.nrows)+' rows, '+str(rg.ncols)+ \
           ' columns, and cell size '+str(rg.dx))
-    #instantiate grid
-
-#    
-#    # Modify the grid DEM to set all nodata nodes to inactive boundaries
-#    nodata_val=0
-#    rg.set_nodata_nodes_to_inactive(z, nodata_val) # set nodata nodes to inactive bounds
-#
-#    # Set the open boundary (outlet) cell. We want to remember the ID of the 
-#    # outlet node and the ID of the interior node adjacent to it. We'll make
-#    # the outlet node an open boundary.
-#    outlet_node = rg.grid_coords_to_node_id(outlet_row, outlet_column)
-#    node_next_to_outlet = rg.grid_coords_to_node_id(next_to_outlet_row, 
-#                                                    next_to_outlet_column)
-#    rg.set_fixed_value_boundaries(outlet_node)
-#            
-#    #elevations  = nodata_val*np.ones( nnodes )    
-#    ##set-up interior elevations with random numbers
-#    ##for i in range(0, nnodes):
-#    ##    if rg.is_interior(i):
-#    ##        elevations[i]=random.random_sample()
-#    #
-#    ##set-up with prescribed elevations to test drainage area calcualtion
-#    #helper = [7,8,9,10,13,14,15,16]
-#    #elevations[helper]=2
-#    #helper = [19,20,21,22]
-#    #elevations[helper]=3        
-#    #elevations[7]=1    
-#    #
-#    # Get a 2D array version of the elevations
-#    elev_raster = rg.node_vector_to_raster(z,True)
-#    
-#    # Plot topography
-#    #pylab.figure(22)
-#    #pylab.subplot(121)
-#    #im = pylab.imshow(elev_raster, cmap=pylab.cm.RdBu, extent=[0, rg.ncols*rg.dx, 0, rg.nrows*rg.dx])
-#    #cb = pylab.colorbar(im)
-#    #cb.set_label('Elevation (m)', fontsize=12)
-#    #pylab.title('Topography')
-#    #
-#    #pylab.show()
-#    
-#    of=OverlandFlow('input_data.txt',rg,0)
+    
+    #Below for finding outlet, but not working. boo.
+    #wbc=WatershedBoundaryConditions()
+    #wbc.set_bc_find_outlet(rg, z, nodata_val)
+      
+    #Below points are actually the points of interest for plotting the hydrograph
+    #They do not have to be THE outlet of the watershed.
+    outlet_row = 238
+    outlet_column = 216
+    next_to_outlet_row = 239
+    next_to_outlet_column = 216
+          
+    outlet_node = rg.grid_coords_to_node_id(outlet_row, outlet_column)
+    node_next_to_outlet = rg.grid_coords_to_node_id(next_to_outlet_row, 
+                                                    next_to_outlet_column)
+                                                                                                       
+    # Get a 2D array version of the elevations for plotting purposes
+    elev_raster = rg.node_vector_to_raster(z,True)
+    
+    # Plot topography
+    #pylab.figure(22)
+    #pylab.subplot(121)
+    #im = pylab.imshow(elev_raster, cmap=pylab.cm.RdBu, extent=[0, rg.ncols*rg.dx, 0, rg.nrows*rg.dx])
+    #cb = pylab.colorbar(im)
+    #cb.set_label('Elevation (m)', fontsize=12)
+    #pylab.title('Topography')
+    #
+    #pylab.show()
+    
+    of=OverlandFlow('input_data.txt',rg,0)
+    
+    #Below for using rainfall distribution
 #    #rainfall = PrecipitationDistribution()
 #    #rainfall.initialize('input_data.txt')
 #    #rainfall.update
@@ -91,20 +82,11 @@ def main():
 #    #print "storm duration, hours ", rainfall.storm_duration
 #    #print "storm intensity ", storm_intensity
 #    
+
+
 #    #tau = of.run_one_step(rg,z,outlet_node,node_next_to_outlet,storm_duration,storm_intensity)
-#    of.run_one_step(rg,z,outlet_node,node_next_to_outlet,60*40,2.78e-05, 60*15)
-#    
-#    #imshow(elev_raster)
-#    #colorbar()
-#    #show()
-#    ##
-#    ### Create a shaded image
-#    ##pylab.close()  # clear any pre-existing plot
-#    ##image_extent = [0, 0.001*dx*nc, 0, 0.001*dx*nr] # in km
-#    ##im = pylab.imshow(elev_raster, cmap=pylab.cm.RdBu, extent=image_extent)
-#    ##pylab.xlabel('Distance (km)', fontsize=12)
-#    ##pylab.ylabel('Distance (km)', fontsize=12)
-#    
+    of.run_one_step(rg,z,outlet_node,node_next_to_outlet,60*15,1.39e-05, 60*15)
+
 #    print('Total run time = '+str(time.time()-start_time)+' seconds.')
     
 if __name__ == "__main__":
