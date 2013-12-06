9be22a9d3ae1a229cff05ef429611d8c232d4e9c
huttone <huttone@093548ea-dc74-4ebb-8037-4be9f23db00a>
2013-12-06 17:23:10 +0000

    * Added unit tests that check for return value for methods that add fields.
    == Added ==
    
    * New unit tests for methods that add fields to a RasterModelGrid. Check
      for return values for add_ones, add_zeros, and add_empty.

--- landlab/grid/tests/test_raster_grid/test_allocators.py
+++ landlab/grid/tests/test_raster_grid/test_allocators.py
@@ -1,6 +1,6 @@
 import numpy as np
 from numpy.testing import assert_array_equal
-from nose.tools import assert_equal
+from nose.tools import assert_equal, assert_is
 
 from landlab import RasterModelGrid
 
@@ -21,6 +21,20 @@ def generate_zeros_tests():
             yield _test
 
 
+def generate_add_zeros_tests():
+    for element in ELEMENTS:
+        for type in TYPES:
+            def _test():
+                rmg = RasterModelGrid(4, 5)
+                number_of_elements = rmg.number_of_elements(element)
+                rtn_values = rmg.add_zeros(element, 'name')
+                assert_is(rtn_values, rmg.field_values(element, 'name'))
+                assert_array_equal(
+                    rtn_values, np.zeros(number_of_elements, dtype=np.float))
+            _test.description = '%s.test_add_zeros_%s_%s'% (__name__, type, element)
+            yield _test
+
+
 def generate_ones_tests():
     for element in ELEMENTS:
         for type in TYPES:
@@ -30,6 +44,20 @@ def generate_ones_tests():
                 assert_array_equal(rmg.ones(centering=element),
                                    np.ones(number_of_elements, dtype=np.float))
             _test.description = '%s.test_zeros_%s_%s'% (__name__, type, element)
+            yield _test
+
+
+def generate_add_ones_tests():
+    for element in ELEMENTS:
+        for type in TYPES:
+            def _test():
+                rmg = RasterModelGrid(4, 5)
+                number_of_elements = rmg.number_of_elements(element)
+                rtn_values = rmg.add_ones(element, 'name')
+                assert_is(rtn_values, rmg.field_values(element, 'name'))
+                assert_array_equal(
+                    rtn_values, np.ones(number_of_elements, dtype=np.float))
+            _test.description = '%s.test_add_zeros_%s_%s'% (__name__, type, element)
             yield _test
 
 
@@ -49,3 +77,20 @@ def generate_empty_tests():
             _test.description = '%s.test_zeros_%s_%s'% (__name__, type, element)
             yield _test
 
+
+def generate_add_empty_tests():
+    elements = ['node', 'cell', 'link', 'face']
+    elements += ['active_' + name for name in elements]
+
+    types = ['float', 'int', 'bool']
+
+    for element in ELEMENTS:
+        for type in TYPES:
+            def _test():
+                rmg = RasterModelGrid(4, 5)
+                number_of_elements = rmg.number_of_elements(element)
+                rtn_values = rmg.add_empty(element, 'name')
+                assert_is(rtn_values, rmg.field_values(element, 'name'))
+                assert_array_equal(rtn_values.size, number_of_elements)
+            _test.description = '%s.test_zeros_%s_%s'% (__name__, type, element)
+            yield _test
