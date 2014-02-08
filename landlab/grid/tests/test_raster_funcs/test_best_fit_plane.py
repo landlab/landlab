7f553103037c8d520a4c12363de48b31c638266e
huttone <huttone@093548ea-dc74-4ebb-8037-4be9f23db00a>
2014-02-08 20:58:27 +0000

    * Removed test for points on a line as it wasn't correct.

--- landlab/grid/tests/test_raster_funcs/test_best_fit_plane.py
+++ landlab/grid/tests/test_raster_funcs/test_best_fit_plane.py
@@ -17,16 +17,6 @@ def test_best_fit_with_bad_args():
         rfuncs.calculate_slope_aspect_BFP(len_3, len_3, len_2)
 
 
-def test_best_fit_points_on_a_line():
-    (x, y, z) = (np.array([0., 1., 2.]),
-                 np.array([0., 0., 0.]),
-                 np.array([0., 0., 0.]), )
-    (slope, aspect) = rfuncs.calculate_slope_aspect_BFP(x, y, z)
-
-    assert_equal(slope, 0.)
-    assert_equal(aspect, 90.)
-
-
 def test_best_fit_plane_in_xy():
     (x, y, z) = (np.array([0., 1., 1.]),
                  np.array([0., 0., 1.]),
