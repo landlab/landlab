fcb96f0204ba2c7d5ade9fb3d35c03540082b61a
huttone <huttone@093548ea-dc74-4ebb-8037-4be9f23db00a>
2014-03-31 02:27:04 +0000

    Added new decorator, deprecated.
    Added
    =====
    
    * deprecated decorator marks functions a deprecated and issues a warning
      when they are used.

--- landlab/utils/decorators.py
+++ landlab/utils/decorators.py
@@ -1,3 +1,5 @@
+import warnings
+
 
 def make_return_array_immutable(func):
     def _wrapped(self, *args, **kwds):
@@ -5,4 +7,18 @@ def make_return_array_immutable(func):
         immutable_array = array.view()
         immutable_array.flags.writeable = False
         return immutable_array
+    return _wrapped
+
+
+def deprecated(func):
+    """Mark a function as deprecated
+    """
+    def _wrapped(*args, **kwargs):
+        warnings.warn("Call to deprecated function {}.".format(func.__name__),
+                      category=DeprecationWarning)
+        return func(*args, **kwargs)
+    _wrapped.__name__ = func.__name__
+    _wrapped.__doc__ = func.__doc__
+    _wrapped.__dict__.update(func.__dict__)
+
     return _wrapped
