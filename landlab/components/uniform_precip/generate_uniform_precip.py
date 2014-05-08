8c6999edcb2a54ed2c6aa3a4469afef1b8103251
jadams15 <jadams15@093548ea-dc74-4ebb-8037-4be9f23db00a>
2014-05-08 16:35:17 +0000

    update to doc strings

--- landlab/components/uniform_precip/generate_uniform_precip.py
+++ landlab/components/uniform_precip/generate_uniform_precip.py
@@ -1,4 +1,4 @@
-########## rainfall_driver.py ###############
+########## generate_uniform_precip.py ###############
 ##
 ## This component generates rainfall  
 ## events based on statistical distributions.
@@ -24,32 +24,31 @@ _DEFAULT_INPUT_FILE = os.path.join(os.path.dirname(__file__),
 class PrecipitationDistribution:
 
     def __init__(self):
-        '''All initial values are set to zero until initalized
+        """All initial values are set to zero until initalized
         using the initialize() method which reads in data using
         the ModelParameter Dictionary and sets the random variables
-        according to their respective distribution'''
+        according to their respective distribution"""
         self.mean_storm = 0.0
         self.mean_intensity = 0.0
         self.mean_interstorm = 0.0
         self.mean_storm_depth = 0.0
         
 
-        '''Storm_duration, interstorm_duration, storm_depth and intensity
+        """Storm_duration, interstorm_duration, storm_depth and intensity
         are not read in, rather, they are generated through initialize() and
-        updated using the update() method'''
+        updated using the update() method"""
         self.storm_duration = 0.0
         self.interstorm_duration = 0.0
         self.storm_depth = 0.0
         self.intensity = 0.0
         
-        '''TIME VARIABLES'''
+        """TIME VARIABLES"""
         self.storm_time_series =[]
 
 
     def initialize(self, input_file=None):
         MPD = ModelParameterDictionary()
-        '''
-        We imported methods from ModelParameterDictionary
+        """We imported methods from ModelParameterDictionary
         to read the parameters from the input file.
  
         Necessary parameters to run this code include:
@@ -62,34 +61,34 @@ class PrecipitationDistribution:
 
         The random variables of storm_duration, interstorm_duration, storm_depth
         and intensity are found using methods declared later in the class
-        '''
+        """
         if input_file is None:
             input_file = _DEFAULT_INPUT_FILE
         MPD.read_from_file(input_file)
         
         
         
-        self.mean_storm = MPD.read_float( 'MEAN_STORM' )
-        self.mean_intensity = MPD.read_float( 'MEAN_INTENSITY' )
-        self.mean_interstorm = MPD.read_float( 'MEAN_INTERSTORM' )
-        self.mean_storm_depth = self.mean_intensity * self.mean_storm
+        self.mean_storm = MPD.read_float( "MEAN_STORM" )
+        self.mean_interstorm = MPD.read_float( "MEAN_INTERSTORM" )
+        self.mean_storm_depth = MPD.read_float( "MEAN_DEPTH" )
+        self.mean_intensity = self.mean_storm_depth / self.mean_storm
 
         self.storm_duration = self.get_precipitation_event_duration()
         self.interstorm_duration = self.get_interstorm_event_duration()
         self.storm_depth = self.get_storm_depth()
         self.intensity = self.get_storm_intensity()
         
-        self.run_time = MPD.read_float('RUN_TIME')
+        self.run_time = MPD.read_float("RUN_TIME")
         try: #DEJH thinks this is redundant
-            self.delta_t = MPD.read_int('DELTA_T')
+            self.delta_t = MPD.read_int("DELTA_T")
         except:
             pass
 
 
     def update(self):
         
-        '''This method updates the variables of storm_duration, interstorm_duration,
-        storm_depth and intensity that were found in initialize() if needed.'''
+        """This method updates the variables of storm_duration, interstorm_duration,
+        storm_depth and intensity that were found in initialize() if needed."""
         
         self.storm_duration = self.get_precipitation_event_duration()
         self.interstorm_duration = self.get_interstorm_event_duration()
@@ -97,8 +96,7 @@ class PrecipitationDistribution:
         self.intensity = self.get_storm_intensity()
         
     def get_precipitation_event_duration(self):
-        '''
-    This method is the storm generator.
+        """This method is the storm generator.
     
     This method has one argument: the mean_storm parameter.
     (In Eagleson (1978), this parameter was called Tr.)
@@ -106,7 +104,7 @@ class PrecipitationDistribution:
     It finds a random storm_duration value
     based on the poisson distribution about the mean.
     This is accomplished using the expovariate function
-    from the 'random' standard library.
+    from the "random" standard library.
     Additionally, it is rounded to contain 4 significant figures, 
     for neatness. 
     
@@ -116,7 +114,9 @@ class PrecipitationDistribution:
     we return a storm duration IF it is greater than 0,
     otherwise, recursion is employed to re-call the storm
     generator function and get a new value.
-    '''
+    
+    :returns: storm_dration as a float
+    """
         storm = round(random.expovariate(1/self.mean_storm),2)
         while storm == 0: 
             storm = round(random.expovariate(1/self.mean_storm),2)
@@ -126,8 +126,7 @@ class PrecipitationDistribution:
     
     
     def get_interstorm_event_duration(self):
-        ''' 
-    This method is the interstorm duration generator
+        """ This method is the interstorm duration generator
     
     This method takes one argument, the mean_interstorm parameter.
     (In Eagleson (1978), this parameter was called Tb.)
@@ -137,7 +136,7 @@ class PrecipitationDistribution:
     This method finds a random value for interstorm_duration
     based on the poisson distribution about the mean.
     This is accomplished using the expovariate function
-    from the 'random' standard library.
+    from the "random" standard library.
     Additionally, it is rounded to contain 4 significant figures, for neatness. 
     
     The if-else statement is very important here. Values of 0
@@ -146,7 +145,9 @@ class PrecipitationDistribution:
     To avoid 0 hour interstorm durations, we return a 
     interstorm duration IF it is greater than 0,
     otherwise, recursion is employed to re-call the interstorm
-    duration generator function and get a new value. '''
+    duration generator function and get a new value.
+    
+    :returns: interstorm_duration as a float"""
     
         interstorm = round(random.expovariate(1/self.mean_interstorm),2)
         while interstorm == 0:
@@ -156,12 +157,11 @@ class PrecipitationDistribution:
      
                             
     def get_storm_depth(self):
-        '''  
-        This method is the storm depth generator.
+        """  This method is the storm depth generator.
     Storm depth is used to generate a realistic 
     intensity for different storm events.
     
-    (In Eagleson (1978) this parameter was called 'h')
+    (In Eagleson (1978) this parameter was called "h")
     
     This method requires storm_duration, mean_storm duration
     and the mean_storm_depth. Storm_duration is generated through
@@ -173,7 +173,9 @@ class PrecipitationDistribution:
     alpha (or the shape parameter), which is the generated over the mean event
     and beta (or the scale parameter), which is the mean value
     These are all arguments in the function, which returns storm depth.
-    '''
+    
+    :returns: storm_depth as a float
+    """
         
         shape_parameter = (self.storm_duration/self.mean_storm)
         scale_parameter = (self.mean_storm_depth)
@@ -182,22 +184,23 @@ class PrecipitationDistribution:
 
     
     def get_storm_intensity(self):
-        '''  
-        This method draws storm intensity out of the storm depth
+        """   This method draws storm intensity out of the storm depth
     generated by get_storm_depth. 
     
     This method requires the storm_depth and storm_duration
-    and is the same as the parameter ('i') in Eagleson (1978), but instead of
+    and is the same as the parameter ("i") in Eagleson (1978), but instead of
     being drawn from Poission, this is drawn from the Gamma distribution
-    of ('h'), as h = i*Tr. 
+    of ("h"), as h = i*Tr. 
     
-                            '''
+    :returns: storm_intensity as a float
+                            
+                            """
         self.intensity = self.storm_depth / self.storm_duration
         return self.intensity
 
 
     def get_storm_time_series(self):
-        '''
+        """
         This method creates a time series of storms based on storm_duration, and
         interstorm_duration. From these values it will calculate a complete
         time series.
@@ -206,24 +209,23 @@ class PrecipitationDistribution:
         sub-parts (e.g. [[x,y,z], [a,b,c]]) where x and a are the beginning times of a precipitation 
         event, y and b are the ending times of the precipitation event and z and c represent the
         average intensity (mm/hr) of the storm lasting from x to y and a to be, respectively. 
-        '''
+        :returns: array containing several sub-arrays of events [start, finish, intensity]"""
         
         
-        ''' This statement creates the first storm/interstorm pair and event.'''
+        """ This statement creates the first storm/interstorm pair and event."""
         storm = self.get_precipitation_event_duration()
         self.get_storm_depth()
         intensity = self.get_storm_intensity()
         self.storm_time_series.append([0, storm, intensity])
 
-        '''
-        There is a helper and an iterator. Open to suggestions on how to make this 
+        """There is a helper and an iterator. Open to suggestions on how to make this 
         sleeker. helper keeps track of storm events so that we can properly adjust the time for the 
         storm_time_series list. 
         
-        Storm iterator makes sure that the run goes through to the specified time, either read in as 'run_time' by
+        Storm iterator makes sure that the run goes through to the specified time, either read in as "run_time" by
         the ModelParameterDictionary or specified the fire time this method is called.
         
-        '''
+        """
         storm_helper = storm
         storm_iterator = storm
         while storm_iterator <= self.run_time:
