fcfa0cacdea28eb665152d54a1867d7df211e7cd
mcflugen <mcflugen@gmail.com>
2014-05-26 13:41:28 -0600

    Changed to use nose instead of unittest.

--- landlab/io/netcdf/tests/test_write_netcdf.py
+++ landlab/io/netcdf/tests/test_write_netcdf.py
@@ -3,10 +3,14 @@
 Unit tests for landlab.io.netcdf module.
 """
 
-import unittest
 import os
 import numpy as np
 from StringIO import StringIO
+from nose.tools import assert_equal, assert_true, assert_raises
+try:
+    from nose import assert_list_equal
+except ImportError:
+    from landlab.testing.tools import assert_list_equal
 
 from landlab import RasterModelGrid
 from landlab.io.netcdf import write_netcdf, NotRasterGridError
@@ -16,90 +20,81 @@ from landlab.io.netcdf.read import _get_raster_spacing
 _TEST_DATA_DIR = os.path.join(os.path.dirname(__file__), 'data')
 
 
-class TestWriteNetcdf(unittest.TestCase):
-    def test_write(self):
-        field = RasterModelGrid(4, 3)
-        #field.new_field_location('node', 12.)
-        values = np.arange(12.)
-        field.add_field('node', 'planet_surface__elevation', values)
+def test_netcdf_write():
+    field = RasterModelGrid(4, 3)
+    #field.new_field_location('node', 12.)
+    values = np.arange(12.)
+    field.add_field('node', 'planet_surface__elevation', values)
 
-        write_netcdf('test.nc', field)
+    write_netcdf('test.nc', field)
 
-        import netCDF4 as nc
+    import netCDF4 as nc
 
-        root = nc.Dataset('test.nc', 'r', format='NETCDF4')
-        self.assertEqual(set(root.dimensions), set(['ni', 'nj', 'nt']))
-        self.assertEqual(len(root.dimensions['ni']), 3)
-        self.assertEqual(len(root.dimensions['nj']), 4)
-        self.assertTrue(len(root.dimensions['nt']), 1)
-        self.assertTrue(root.dimensions['nt'].isunlimited())
+    root = nc.Dataset('test.nc', 'r', format='NETCDF4')
+    assert_equal(set(root.dimensions), set(['ni', 'nj', 'nt']))
+    assert_equal(len(root.dimensions['ni']), 3)
+    assert_equal(len(root.dimensions['nj']), 4)
+    assert_true(len(root.dimensions['nt']), 1)
+    assert_true(root.dimensions['nt'].isunlimited())
 
-        self.assertEqual(set(root.variables),
-                         set(['x', 'y', 'planet_surface__elevation']))
+    assert_equal(set(root.variables),
+                 set(['x', 'y', 'planet_surface__elevation']))
 
-        self.assertListEqual(list(root.variables['x'][:].flat),
-                             [0., 1., 2.,
-                              0., 1., 2.,
-                              0., 1., 2.,
-                              0., 1., 2., ])
-        self.assertListEqual(list(root.variables['y'][:].flat),
-                             [0., 0., 0.,
-                              1., 1., 1.,
-                              2., 2., 2.,
-                              3., 3., 3., ])
-        self.assertListEqual(
-            list(root.variables['planet_surface__elevation'][:].flat),
-            range(12))
-        root.close()
-
-
-class TestGetRasterSpacing2D(unittest.TestCase):
-    def test_unit_spacing(self):
-        (x, y) = np.meshgrid(np.arange(5.), np.arange(4.))
-
-        spacing = _get_raster_spacing((y, x))
-        self.assertEqual(spacing, 1.)
-
-    def test_non_unit_spacing(self):
-        (x, y) = np.meshgrid(np.arange(5.) * 2, np.arange(4.) * 2)
-
-        spacing = _get_raster_spacing((y, x))
-        self.assertEqual(spacing, 2.)
-
-    def test_uneven_spacing_axis_0(self):
-        (x, y) = np.meshgrid(np.logspace(0., 2., num=5), np.arange(4.))
-
-        with self.assertRaises(NotRasterGridError):
-            spacing = _get_raster_spacing((y, x))
-
-    def test_uneven_spacing_axis_1(self):
-        (x, y) = np.meshgrid(np.arange(4.), np.logspace(0., 2., num=5))
-
-        with self.assertRaises(NotRasterGridError):
-            spacing = _get_raster_spacing((y, x))
-
-    def test_switched_coords(self):
-        (x, y) = np.meshgrid(np.arange(5.), np.arange(4.))
-
-        spacing = _get_raster_spacing((x, y))
-        self.assertEqual(spacing, 0.)
+    assert_list_equal(list(root.variables['x'][:].flat),
+                      [0., 1., 2.,
+                       0., 1., 2.,
+                       0., 1., 2.,
+                       0., 1., 2., ])
+    assert_list_equal(list(root.variables['y'][:].flat),
+                      [0., 0., 0.,
+                       1., 1., 1.,
+                       2., 2., 2.,
+                       3., 3., 3., ])
+    assert_list_equal(
+        list(root.variables['planet_surface__elevation'][:].flat),
+        range(12))
+    root.close()
 
 
-class TestGetRasterSpacing1D(unittest.TestCase):
-    def test_unit_spacing(self):
-        spacing = _get_raster_spacing((np.arange(5.), ))
-        self.assertEqual(spacing, 1.)
+def test_2d_unit_spacing():
+    (x, y) = np.meshgrid(np.arange(5.), np.arange(4.))
 
-    def test_non_unit_spacing(self):
-        spacing = _get_raster_spacing((np.arange(5.) * 2, ))
-        self.assertEqual(spacing, 2.)
+    spacing = _get_raster_spacing((y, x))
+    assert_equal(spacing, 1.)
 
-    def test_uneven_spacing(self):
-        (x, y) = np.meshgrid(np.logspace(0., 2., num=5), np.arange(4.))
+def test_2d_non_unit_spacing():
+    (x, y) = np.meshgrid(np.arange(5.) * 2, np.arange(4.) * 2)
 
-        with self.assertRaises(NotRasterGridError):
-            spacing = _get_raster_spacing((np.logspace(0., 2., num=5), ))
+    spacing = _get_raster_spacing((y, x))
+    assert_equal(spacing, 2.)
+
+def test_2d_uneven_spacing_axis_0():
+    (x, y) = np.meshgrid(np.logspace(0., 2., num=5), np.arange(4.))
+
+    assert_raises(NotRasterGridError, _get_raster_spacing, (y, x))
+
+def test_2d_uneven_spacing_axis_1():
+    (x, y) = np.meshgrid(np.arange(4.), np.logspace(0., 2., num=5))
+
+    assert_raises(NotRasterGridError, _get_raster_spacing, (y, x))
+
+def test_2d_switched_coords():
+    (x, y) = np.meshgrid(np.arange(5.), np.arange(4.))
+
+    spacing = _get_raster_spacing((x, y))
+    assert_equal(spacing, 0.)
 
 
-if __name__ == '__main__':
-    unittest.main()
+def test_1d__unit_spacing():
+    spacing = _get_raster_spacing((np.arange(5.), ))
+    assert_equal(spacing, 1.)
+
+def test_1d_non_unit_spacing():
+    spacing = _get_raster_spacing((np.arange(5.) * 2, ))
+    assert_equal(spacing, 2.)
+
+def test_1d_uneven_spacing():
+    (x, y) = np.meshgrid(np.logspace(0., 2., num=5), np.arange(4.))
+
+    assert_raises(NotRasterGridError, _get_raster_spacing, 
+                  (np.logspace(0., 2., num=5), ))
