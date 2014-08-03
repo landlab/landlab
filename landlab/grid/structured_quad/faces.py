5bc1e8ff91c569d1d2c346c2b3104ed28bd42c22
mcflugen <mcflugen@gmail.com>
2014-08-03 12:50:46 -0600

    Added docstring for number_of_faces.

--- landlab/grid/structured_quad/faces.py
+++ landlab/grid/structured_quad/faces.py
@@ -2,4 +2,21 @@ from . import links
 
 
 def number_of_faces(shape):
+    """Number of faces in a structured quad grid.
+
+    Parameters
+    ----------
+    shape : tuple of int
+        Shape of grid of nodes.
+
+    Returns
+    -------
+    int :
+        Number of faces in grid.
+
+    Examples
+    --------
+    >>> number_of_faces((3, 4))
+    17
+    """
     return links.number_of_links(shape)
