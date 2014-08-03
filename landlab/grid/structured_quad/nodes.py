a793f77bf58680f6df08f3f9ca4bdc0d6c591cd6
mcflugen <mcflugen@gmail.com>
2014-08-02 18:41:11 -0600

    Added function to get corner nodes.

--- landlab/grid/structured_quad/nodes.py
+++ landlab/grid/structured_quad/nodes.py
@@ -20,3 +20,25 @@ def number_of_nodes(shape):
     12
     """
     return np.prod(shape)
+
+
+def corners(shape):
+    """IDs of corner nodes.
+
+    Parameters
+    ----------
+    shape : tuple of int
+        Shape of grid of nodes.
+
+    Returns
+    -------
+    (4, ) ndarray :
+        IDs of the corner nodes.
+
+    Examples
+    --------
+    >>> corners((3, 4))
+    array([ 0,  3,  8, 11])
+    """
+    node_count = number_of_nodes(shape)
+    return np.array([0, shape[1] - 1, node_count - shape[1], node_count - 1])
