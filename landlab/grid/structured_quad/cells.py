c649af1119c05fb376551b2e6b1cb5a8ac6b269e
mcflugen <mcflugen@gmail.com>
2014-08-03 11:23:33 -0600

    Added shape_of_cells and node_id_at_cells.

--- landlab/grid/structured_quad/cells.py
+++ landlab/grid/structured_quad/cells.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+from . import nodes
+
 
 def number_of_cells(shape):
     """Number of cells is a structured quad grid.
@@ -20,3 +22,46 @@ def number_of_cells(shape):
     2
     """
     return np.prod(np.array(shape) - 2)
+
+
+def shape_of_cells(shape):
+    """Shape of cells in a structured quad grid.
+
+    Parameters
+    ----------
+    shape : tuple of int
+        Shape of grid of nodes.
+
+    Returns
+    -------
+    int :
+        Shape of cells in grid.
+
+    Examples
+    --------
+    >>> shape_of_cells((3, 4))
+    (1, 2)
+    """
+    return tuple(np.array(shape) - 2)
+
+
+def node_id_at_cells(shape):
+    """Node ID at each cell.
+
+    Parameters
+    ----------
+    shape : tuple of int
+        Shape of grid of nodes.
+
+    Returns
+    -------
+    int :
+        ID of node associated with each cell.
+
+    Examples
+    --------
+    >>> node_id_at_cells((3, 4))
+    array([[5, 6]])
+    """
+    node_ids = nodes.node_ids(shape)
+    return node_ids[1:-1, 1:-1].copy().reshape(shape_of_cells(shape))
