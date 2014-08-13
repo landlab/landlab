4c8351ba5c64bd21dd65720ed8cffa77867d47af
gregtucker <gregteucro@yahoo.com>
2014-08-13 09:20:05 -0600

    Work on hillslope_ca example
    Partial implementation; still need to add motion rules and some
    demobilization rules

--- landlab/components/linkca/examples/hillslope_ca.py
+++ landlab/components/linkca/examples/hillslope_ca.py
@@ -12,6 +12,7 @@ GT, August 2014
 _DEBUG = False
 
 import time
+import numpy
 from landlab import RasterModelGrid
 from landlab.components.linkca.link_ca import LinkCellularAutomaton, Transition, CAPlotter
 from landlab.components.fracture_grid.fracture_grid import make_frac_grid
@@ -24,25 +25,63 @@ def setup_transition_list():
     
     The states and transitions are as follows (note: X-X means "horizontal"
     pair, X/X means "vertical" pair with first item beneath second):
+    
+    Node states
+    -----------
+    0 air
+    1 moving left
+    2 moving right
+    3 immobile
         
     Pair state      Transition to       Process     Rate
     ----------      -------------       -------     ----
-    0 (0-0)         (none)              -           -
-    1 (0-1)         3 (1-1)             weathering  1.0
-    2 (1-0)         3 (1-1)             weathering  1.0
-    3 (1-1)         (none)              -           -
-    4 (0/0)         (none)              -           -
-    5 (0/1)         7 (1/1)             weathering  1.0
-    6 (1/0)         (none)              -           -
-    7 (1/1)         7 (1/1)             weathering  1.0
+    0 (0-0)         
+    1 (0-1)
+    2 (0-2)
+    3 (0-3)         4 (1-0)             left ejection
+    4 (1-0)
+    5 (1-1)      
+    6 (1-2)
+    7 (1-3)
+    8 (2-0)
+    9 (2-1)
+    10 (2-2)
+    11 (2-3)        15 (3-3)            demobilization (right wall)
+    12 (3-0)        2 (0-2)             right ejection
+    13 (3-1)        15 (3-3)            demobilization (left wall)
+    14 (3-2)
+    15 (3-3)
+    16 (0/0)      
+    17 (0/1) 
+    18 (0/2)             -           
+    19 (0/3)        20 (1/0)            downward ejection, left
+                    24 (2/0)            downward ejection, right
+    20 (1/0)
+    21 (1/1)          
+    22 (1/2)
+    23 (1/3)
+    24 (2/0)
+    25 (2/1)
+    26 (2/2)
+    27 (2/3)
+    28 (3/0)        17 (0/1)            upward ejection, left
+                    18 (0/2)            upward ejection, right
+    29 (3/1)
+    30 (3/2)
+    31 (3/3)
     
     """
     xn_list = []
     
-    xn_list.append( Transition(1, 3, 1., 'weathering') ) # rock-sap to sap-sap
-    xn_list.append( Transition(2, 3, 1., 'weathering') ) # sap-rock to sap-sap
-    xn_list.append( Transition(5, 3, 1., 'weathering') ) # rock/sap to sap/sap
-    xn_list.append( Transition(6, 3, 1., 'weathering') ) # sap/rock to sap/sap
+    xn_list.append( Transition(3, 4, 1., 'left ejection') )
+    xn_list.append( Transition(12, 2, 1., 'right ejection') )
+    xn_list.append( Transition(19, 20, 1., 'downward ejection, left') )
+    xn_list.append( Transition(19, 24, 1., 'downward ejection, right') )
+    xn_list.append( Transition(28, 17, 1., 'upward ejection, left') )
+    xn_list.append( Transition(28, 18, 1., 'upward ejection, right') )
+    xn_list.append( Transition(11, 15, 10., 'demobilization (right wall)') )
+    xn_list.append( Transition(13, 15, 10., 'demobilization (left wall)') )
+    # still to add: motion l, r, u, d; demob for vertical pairs
         
     if _DEBUG:
         print
@@ -61,7 +100,7 @@ def main():
     nr = 128
     nc = 128
     plot_interval = 0.25
-    run_duration = 4.0
+    run_duration = 8.0
     report_interval = 5.0  # report interval, in real-time seconds
     
     # Initialize real time
@@ -76,12 +115,13 @@ def main():
     # represented by nodes with state 1. Node pairs (links) with 0-1 or 1-0
     # can undergo a transition to 1-1, representing chemical weathering of the
     # rock.
-    ns_dict = { 0 : 'rock', 1 : 'saprolite' }
+    ns_dict = { 0 : 'air', 1 : 'mobile left', 2 : 'mobile right', 3 : 'immobile' }
     xn_list = setup_transition_list()
 
-    # The initial grid
+    # Create the node-state map and attach it to the grid
     node_state_grid = mg.add_zeros('node', 'node_state_map', dtype=int)
-    node_state_grid[:] = make_frac_grid(10, model_grid=mg)    
+    (lower_half,) = numpy.where(mg.node_y<nr/2)
+    node_state_grid[lower_half] = 3
     
     # Create the CA model
     ca = LinkCellularAutomaton(mg, ns_dict, xn_list, node_state_grid)
