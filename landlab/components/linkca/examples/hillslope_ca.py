b0faabe70c80d275b556752f8d4d4f5ce323a922
gregtucker <gregteucro@yahoo.com>
2014-08-25 09:02:44 -0600

    Playing with hillslope_ca.py
    Just tweaking params

--- landlab/components/linkca/examples/hillslope_ca.py
+++ landlab/components/linkca/examples/hillslope_ca.py
@@ -9,7 +9,7 @@ of a hillslope by disturbance-driven soil creep.
 GT, August 2014
 """
 
-_DEBUG = True
+_DEBUG = False
 
 import time
 import numpy
@@ -73,22 +73,22 @@ def setup_transition_list():
     """
     xn_list = []
     
-    xn_list.append( Transition(3, 4, 0.01, 'left ejection') )
-    xn_list.append( Transition(12, 2, 0.01, 'right ejection') )
-    xn_list.append( Transition(19, 20, 0.01, 'downward ejection, left') )
-    xn_list.append( Transition(19, 24, 0.01, 'downward ejection, right') )
-    xn_list.append( Transition(28, 17, 0.01, 'upward ejection, left') )
-    xn_list.append( Transition(28, 18, 0.01, 'upward ejection, right') )
-    xn_list.append( Transition(11, 15, 100., 'demobilization (right wall)') )
-    xn_list.append( Transition(13, 15, 100., 'demobilization (left wall)') )
-    xn_list.append( Transition(29, 31, 0.1, 'demobilization (friction)') )
-    xn_list.append( Transition(30, 31, 0.1, 'demobilization (friction)') )
-    xn_list.append( Transition(1, 4, 1.0, 'leftward motion') )
-    xn_list.append( Transition(8, 2, 1.0, 'rightward motion') )
-    xn_list.append( Transition(20, 17, 0.1, 'upward motion') )
-    xn_list.append( Transition(24, 18, 0.1, 'upward motion') )
-    xn_list.append( Transition(18, 24, 10.0, 'downward motion') )
-    xn_list.append( Transition(17, 20, 10.0, 'downward motion') )
+    xn_list.append( Transition(3, 4, 2., 'left ejection') )
+    xn_list.append( Transition(12, 2, 2., 'right ejection') )
+    xn_list.append( Transition(19, 20, 2.e8, 'downward ejection, left') )
+    xn_list.append( Transition(19, 24, 2.e8, 'downward ejection, right') )
+    xn_list.append( Transition(28, 17, 1., 'upward ejection, left') )
+    xn_list.append( Transition(28, 18, 1., 'upward ejection, right') )
+    xn_list.append( Transition(11, 15, 3.0e7, 'demobilization (right wall)') )
+    xn_list.append( Transition(13, 15, 3.0e7, 'demobilization (left wall)') )
+    xn_list.append( Transition(29, 31, 2.0e6, 'demobilization (friction)') )
+    xn_list.append( Transition(30, 31, 2.0e6, 'demobilization (friction)') )
+    xn_list.append( Transition(1, 4, 3.0e7, 'leftward motion') )
+    xn_list.append( Transition(8, 2, 3.0e7, 'rightward motion') )
+    xn_list.append( Transition(20, 17, 2.0e6, 'upward motion') )
+    xn_list.append( Transition(24, 18, 2.0e6, 'upward motion') )
+    xn_list.append( Transition(18, 24, 2.0e8, 'downward motion') )
+    xn_list.append( Transition(17, 20, 2.0e8, 'downward motion') )
         
     if _DEBUG:
         print
@@ -99,24 +99,47 @@ def setup_transition_list():
     return xn_list
     
     
+def extract_hillslope_profile(node_matrix):
+    """
+    Extracts the hillslope profile by finding, for each column, the highest row
+    that contains a non-air node.
+    """
+    ncols = numpy.size(node_matrix, 1)
+    z = numpy.zeros(ncols)
+    for col in range(ncols):
+        dirt = numpy.where(node_matrix[:,col]!=0)[0]
+        if len(dirt)>0:
+            z[col] = numpy.amax(dirt)
+    return z
+    
+    
 def main():
     
     # INITIALIZE
 
     # User-defined parameters
-    nr = 9
-    nc = 9
-    plot_interval = 1.0
-    run_duration = 100.0
+    nr = 65
+    nc = 129
+    plot_interval = 10
+    run_duration = 63*10
     report_interval = 5.0  # report interval, in real-time seconds
-    uplift_interval = 1
+    baselevel_lowering_interval = 10
+    initial_hill_height = nr-3  # must be < nr-1
     
     # Initialize real time
     current_real_time = time.time()
     next_report = current_real_time + report_interval
     
-    # Initialize next uplift time
-    next_uplift = uplift_interval
+    # Initialize information for baselevel lowering
+    next_bl_drop = baselevel_lowering_interval
+    left_edge_alinks = (nc-2)*(nr-1)+(nc-1)*numpy.arange(nr-2)
+    #print left_edge_alinks
+    right_edge_alinks = left_edge_alinks + (nc-2)
+    #print right_edge_alinks
+    bl_height = initial_hill_height
+    bl_left_alink = left_edge_alinks[bl_height-1]
+    bl_right_alink = right_edge_alinks[bl_height-1]
+    #print 'bllal',bl_left_alink,'blral',bl_right_alink
 
     # Create grid and set up boundaries
     mg = RasterModelGrid(nr, nc, 1.0)
@@ -131,12 +154,18 @@ def main():
 
     # Create the node-state map and attach it to the grid
     node_state_grid = mg.add_zeros('node', 'node_state', dtype=int)
-    (lower_half,) = numpy.where(mg.node_y<nr/2)
-    node_state_grid[lower_half] = 3
+    (initial_hill,) = numpy.where(mg.node_y<=initial_hill_height)
+    node_state_grid[initial_hill] = 3
     
     # Set the left and right boundary conditions
-    node_state_grid[mg.left_edge_node_ids()] = 0
-    node_state_grid[mg.right_edge_node_ids()] = 0
+    left_side_ids = mg.left_edge_node_ids()
+    right_side_ids = mg.right_edge_node_ids()
+    #node_state_grid[mg.left_edge_node_ids()] = 0
+    #node_state_grid[mg.right_edge_node_ids()] = 0
+    
+    # Remember the IDs of the lower row of nodes, so we can keep them set to
+    # state 3 when we do uplift
+    #bottom_row = mg.bottom_edge_node_ids()
     
     # Create the CA model
     ca = LinkCellularAutomaton(mg, ns_dict, xn_list, node_state_grid)
@@ -172,10 +201,28 @@ def main():
         ca_plotter.update_plot()
         
         # Uplift
-        if current_time >= next_uplift:
-            print 'doing uplift at time', current_time
-            next_uplift += uplift_interval
-            ca.grid.roll_nodes_ud('node_state', 1, interior_only=True)
+        if current_time >= next_bl_drop and bl_height>0:
+            #print 'doing bl drop at time', current_time
+            next_bl_drop += baselevel_lowering_interval
+            ca.node_state[left_side_ids[bl_height]] = 0  # turn the former baselevel to air
+            ca.node_state[right_side_ids[bl_height]] = 0  # turn the former baselevel to air
+            ca.update_link_state(bl_left_alink, 0, current_time)
+            ca.update_link_state(bl_right_alink, 0, current_time)
+            bl_height -= 1
+            #ca.node_state[left_side_ids[bl_height]] = 3  # turn the new baselevel to regolith
+            #ca.node_state[right_side_ids[bl_height]] = 3  # turn the new baselevel to regolith
+            bl_left_alink = left_edge_alinks[bl_height-1]
+            bl_right_alink = right_edge_alinks[bl_height-1]
+            #ca.update_link_state(bl_left_alink, 0, current_time)
+            #ca.update_link_state(bl_right_alink, 0, current_time)
+            
+            #ca.node_state[left_side_ids[:bl_height]] = 3
+            #ca.node_state[left_side_ids[bl_height:]] = 0
+            #ca.node_state[right_side_ids[:bl_height]] = 3
+            #ca.node_state[right_side_ids[bl_height:]] = 0           
+            #next_uplift += uplift_interval
+            #ca.grid.roll_nodes_ud('node_state', 1, interior_only=True)
+            #ca.node_state[bottom_row] = 3
             #print 'after roll:',ca.node_state
 
         # for debugging        
@@ -192,7 +239,10 @@ def main():
     
     # Plot
     ca_plotter.finalize()
-        
+    z = extract_hillslope_profile(mg.node_vector_to_raster(ca.node_state))
+    numpy.savetxt('h0822-01-u125.txt',z)
+    print 'PEAK ELEV = ',numpy.amax(z)
 
 if __name__ == "__main__":
-    main()
+    import profile
+    profile.run('main()')
