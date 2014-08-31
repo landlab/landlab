ceb779c2b5ef64e59ac7250b6355d221307bf19f
mcflugen <mcflugen@gmail.com>
2014-08-31 11:00:11 -0600

    Added some LinkGrid and CellGrid methods.

--- landlab/grid/unstructured/base.py
+++ landlab/grid/unstructured/base.py
@@ -20,12 +20,16 @@ def _default_axis_units(n_dims):
 class BaseGrid(object):
     def __init__(self, nodes, axis_name=None, axis_units=None, node_status=None,
                  links=None, cells=None):
-        """__init__([coord0, coord1, ...])
+        """__init__([coord0, coord1, ...], axis_name=None, axis_units=None)
 
         Parameters
         ----------
         coord0, coord1, ... : sequence of array-like
             Coordinates of grid nodes
+        axis_name : sequence of strings, optional
+            Names of coordinate axes
+        axis_units : sequence of strings, optional
+            Units of coordinate axes
 
         Returns
         -------
@@ -43,6 +47,22 @@ class BaseGrid(object):
         0.0
         >>> ngrid.point_at_node[2]
         array([ 1.,  0.])
+
+        >>> cells = ([0, 1, 2, 1, 3, 2], [3, 3])
+        >>> ngrid = BaseGrid(([0, 0, 1, 1], [0, 1, 0, 1]), cells=cells)
+        >>> ngrid.number_of_cells
+        2
+        >>> ngrid.nodes_at_cell(0)
+        array([0, 1, 2])
+
+        >>> links = [(0, 2), (1, 3), (0, 1), (1, 2), (0, 3)]
+        >>> ngrid = BaseGrid(([0, 0, 1, 1], [0, 1, 0, 1]), links=zip(*links))
+        >>> ngrid.number_of_links
+        5
+        >>> ngrid.links_leaving_at_node(0)
+        array([0, 2, 4])
+        >>> ngrid.links_entering_at_node(0)
+        array([], dtype=int64)
         """
         self._axis_name = tuple(axis_name or _default_axis_names(self.ndim))
         self._axis_units = tuple(axis_units or _default_axis_units(self.ndim))
@@ -50,7 +70,7 @@ class BaseGrid(object):
         self._node_grid = NodeGrid(nodes)
 
         if cells is not None:
-            self._cell_grid = CellGrid(cells)
+            self._cell_grid = CellGrid(*cells)
         if links is not None:
             self._link_grid = LinkGrid(links, self.number_of_nodes)
         if node_status is not None:
@@ -104,6 +124,12 @@ class BaseGrid(object):
         return self._axis_name
 
     @property
+    def number_of_links(self):
+        """Number of links.
+        """
+        return self._link_grid.number_of_links
+
+    @property
     def number_of_cells(self):
         """Number of cells.
         """
@@ -130,6 +156,15 @@ class BaseGrid(object):
     @property
     def point_at_node(self):
         return self._node_grid.point
+
+    def nodes_at_cell(self, cell):
+        return self._cell_grid.nodes_at_cell(cell)
+
+    def links_leaving_at_node(self, node):
+        return self._link_grid.out_link_at_node(node)
+
+    def links_entering_at_node(self, node):
+        return self._link_grid.in_link_at_node(node)
 
     @property
     def node_id_at_cells(self):
