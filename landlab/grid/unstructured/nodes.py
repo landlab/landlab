6e0f54d5b62c665ad6b4bfe4d117342a8a9ea17f
mcflugen <mcflugen@gmail.com>
2014-08-31 10:20:55 -0600

    Added n-D support for NodeGrid.

--- landlab/grid/unstructured/nodes.py
+++ landlab/grid/unstructured/nodes.py
@@ -19,31 +19,116 @@ class NodeGrid(object):
         Examples
         --------
         >>> ngrid = NodeGrid(([0, 0, 1, 1], [0, 1, 0, 1]))
+        >>> ngrid.ndim
+        2
         >>> ngrid.number_of_nodes
         4
         >>> ngrid.x
         array([ 0.,  1.,  0.,  1.])
         >>> ngrid.y
         array([ 0.,  0.,  1.,  1.])
-        """
-        if len(nodes) > 2:
-            raise ValueError('only 2D grids supported')
 
-        self._coords = np.vstack((np.array(nodes[0], dtype=float),
-                                  np.array(nodes[1], dtype=float)))
+        Create a 1D grid.
+
+        >>> ngrid = NodeGrid(((0, 1, 3), ))
+        >>> ngrid.ndim
+        1
+        >>> ngrid.number_of_nodes
+        3
+        >>> ngrid.x
+        array([ 0.,  1.,  3.])
+        >>> ngrid.y
+        Traceback (most recent call last):
+        AttributeError: Grid has no y-coordinate
+        """
+        self._coords = np.vstack(
+            [np.array(coord, dtype=float) for coord in nodes]
+        )
+        self._coords.flags['WRITEABLE'] = False
 
         self._number_of_nodes = len(nodes[0])
 
     @property
+    def ndim(self):
+        return self._coords.shape[0]
+
+    @property
     def number_of_nodes(self):
+        """
+        Examples
+        --------
+        >>> ngrid = NodeGrid(([0, 0, 1], [0, 1, 0]))
+        >>> ngrid.number_of_nodes
+        3
+        """
         return self._number_of_nodes
 
     @property
     def x(self):
+        """Node coordinates of the "fast" dimension.
+
+        Examples
+        --------
+        >>> ngrid = NodeGrid(([0, 0, 1], [0, 1, 0]))
+        >>> ngrid.x
+        array([ 0.,  1.,  0.])
+        """
         return self._coords[-1]
 
     @property
     def y(self):
-        return self._coords[-2]
+        """Node y-coordinates.
 
+        Examples
+        --------
+        >>> ngrid = NodeGrid(([0, 0, 1], [0, 1, 0]))
+        >>> ngrid.y
+        array([ 0.,  0.,  1.])
+        """
+        try:
+            return self._coords[-2]
+        except IndexError:
+            raise AttributeError('Grid has no y-coordinate')
 
+    @property
+    def z(self):
+        """Node z-coordinates.
+
+        Examples
+        --------
+        >>> ngrid = NodeGrid(([0, 0, 1], [0, 1, 0]))
+        >>> ngrid.y
+        array([ 0.,  0.,  1.])
+        """
+        try:
+            return self._coords[-2]
+        except IndexError:
+            raise AttributeError('Grid has no z-coordinate')
+
+    @property
+    def coord(self):
+        """
+        Examples
+        --------
+        >>> ngrid = NodeGrid(([0, 0, 1], [0, 1, 0]))
+        >>> ngrid.coord[0]
+        array([ 0.,  0.,  1.])
+        >>> ngrid.coord[1]
+        array([ 0.,  1.,  0.])
+        """
+        return self._coords
+
+    @property
+    def point(self):
+        """
+        Examples
+        --------
+        >>> ngrid = NodeGrid(([0, 0, 1], [0, 1, 0]))
+        >>> ngrid.point
+        array([[ 0.,  0.],
+               [ 0.,  1.],
+               [ 1.,  0.]])
+        >>> ngrid.point[1]
+        array([ 0.,  1.])
+        """
+        return self._coords.T
