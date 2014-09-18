19742eefa4597971b66665c5f9fb2352a13fbb74
mcflugen <mcflugen@gmail.com>
2014-09-18 14:51:26 -0600

    Added active_link_grid and more methods to BaseGrid.

--- landlab/grid/unstructured/base.py
+++ landlab/grid/unstructured/base.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 from .status import StatusGrid
-from .links import link_is_active, LinkGrid
+from .links import link_is_active, find_active_links, LinkGrid, _split_link_ends
 from .cells import CellGrid
 from .nodes import NodeGrid
 
@@ -47,13 +47,18 @@ class BaseGrid(object):
         0.0
         >>> ngrid.point_at_node[2]
         array([ 1.,  0.])
+        >>> ngrid.coord_at_node[:, [2, 3]]
+        array([[ 1.,  1.],
+               [ 0.,  1.]])
+        >>> ngrid.distance_between_nodes([0, 1, 2, 3], 0)
+        array([ 0.        ,  1.        ,  1.        ,  1.41421356])
 
-        >>> cells = ([0, 1, 2, 1, 3, 2], [3, 3])
+        >>> cells = ([0, 1, 2, 1, 3, 2], [3, 3], [0, 1])
         >>> ngrid = BaseGrid(([0, 0, 1, 1], [0, 1, 0, 1]), cells=cells)
         >>> ngrid.number_of_cells
         2
-        >>> ngrid.nodes_at_cell(0)
-        array([0, 1, 2])
+        >>> ngrid.node_at_cell
+        array([0, 1])
 
         >>> links = [(0, 2), (1, 3), (0, 1), (1, 2), (0, 3)]
         >>> ngrid = BaseGrid(([0, 0, 1, 1], [0, 1, 0, 1]), links=zip(*links))
@@ -63,22 +68,44 @@ class BaseGrid(object):
         array([0, 2, 4])
         >>> ngrid.links_entering_at_node(0)
         array([], dtype=int64)
+
+        >>> grid = BaseGrid(([0, 0, 1, 1], [0, 1, 0, 1]),
+        ...     node_status=[0, 0, 0, 4], links=zip(*links))
+        >>> grid.status_at_node
+        array([0, 0, 0, 4])
+        >>> grid.active_links_entering_at_node(0)
+        array([], dtype=int64)
+        >>> grid.active_links_leaving_at_node(0)
+        array([0, 2])
         """
+        self._node_grid = NodeGrid(nodes)
+
         self._axis_name = tuple(axis_name or _default_axis_names(self.ndim))
         self._axis_units = tuple(axis_units or _default_axis_units(self.ndim))
 
-        self._node_grid = NodeGrid(nodes)
-
         if cells is not None:
             self._cell_grid = CellGrid(*cells)
+
         if links is not None:
             self._link_grid = LinkGrid(links, self.number_of_nodes)
+
         if node_status is not None:
             self._status_grid = StatusGrid(node_status)
 
+        if links is not None and node_status is not None:
+            links = _split_link_ends(links)
+            self._active_link_grid = BaseGrid.create_active_link_grid(
+                self.status_at_node, links, self.number_of_nodes)
+
+    @staticmethod
+    def create_active_link_grid(node_status, links, number_of_nodes):
+        active_link_ids = find_active_links(node_status, links)
+        return LinkGrid((links[0][active_link_ids], links[1][active_link_ids]),
+                        number_of_nodes, link_ids=active_link_ids)
+
     @property
     def ndim(self):
-        return 2
+        return self._node_grid.ndim
 
     @property
     def axis_units(self):
@@ -157,37 +184,74 @@ class BaseGrid(object):
     def point_at_node(self):
         return self._node_grid.point
 
-    def nodes_at_cell(self, cell):
-        return self._cell_grid.nodes_at_cell(cell)
-
     def links_leaving_at_node(self, node):
         return self._link_grid.out_link_at_node(node)
 
     def links_entering_at_node(self, node):
         return self._link_grid.in_link_at_node(node)
 
+    def active_links_leaving_at_node(self, node):
+        return self._active_link_grid.out_link_at_node(node)
+
+    def active_links_entering_at_node(self, node):
+        return self._active_link_grid.in_link_at_node(node)
+
     @property
-    def node_id_at_cells(self):
-        return self._node_id_at_cells
+    def node_at_link_start(self):
+        return self._link_grid.node_at_link_start
 
-    def _reset_list_of_active_links(self):
-        """
-        Creates or resets a list of active links. We do this by sweeping
-        through the given lists of from and to nodes, and checking the status
-        of these as given in the node_status list. A link is active if both its
-        nodes are active interior points, or if one is an active interior and
-        the other is an active boundary.
-        """
-        status_at_link_ends = (self.node_status[self._node_id_at_link_start],
-                               self.node_status[self._node_id_at_link_end])
+    @property
+    def node_at_link_end(self):
+        return self._link_grid.node_at_link_end
 
-        (self._active_link_ids, ) = np.where(
-            link_is_active(status_at_link_ends))
+    @property
+    def node_at_cell(self):
+        return self._cell_grid.node_at_cell
 
-        self._number_of_active_links = len(self._active_link_ids)
-        self._number_of_active_faces = len(self._active_link_ids)
-        self._node_id_at_active_link_start = self._node_id_at_link_start[self._active_link_ids]
-        self._node_id_at_active_link_end = self._node_id_at_link_end[self._active_link_ids]
+    @property
+    def cell_at_node(self):
+        return self._cell_grid.cell_at_node
 
-        # Set up active inlink and outlink matrices
-        self._setup_active_inlink_and_outlink_matrices()
+    def core_cells(self):
+        return self.cell_at_node[self.core_nodes]
+
+    @property
+    def status_at_node(self):
+        return self._status_grid.node_status
+
+    @status_at_node.setter
+    def status_at_node(self, status):
+        self._status_grid.node_status = status
+        self._active_link_grid = BaseGrid.create_active_link_grid(
+            self.status_at_node, (self.node_at_link_start,
+                                  self.node_at_link_end), self.number_of_nodes)
+
+    def active_nodes(self):
+        return self._status_grid.active_nodes()
+
+    def core_nodes(self):
+        return self._status_grid.core_nodes()
+
+    def boundary_nodes(self):
+        return self._status_grid.boundary_nodes()
+
+    def closed_boundary_nodes(self):
+        return self._status_grid.closed_boundary_nodes()
+
+    def fixed_gradient_boundary_nodes(self):
+        return self._status_grid.fixed_gradient_boundary_nodes()
+
+    def fixed_value_boundary_nodes(self):
+        return self._status_grid.fixed_value_boundary_nodes()
+
+    def active_links(self):
+        return self._active_link_grid.link_id
+
+    def link_length(self, link):
+        return self.distance_between_nodes(self.node_at_link_start[link],
+                                           self.node_at_link_end[link])
+
+    def distance_between_nodes(self, node0, node1):
+        node0, node1 = np.broadcast_arrays(node0, node1)
+        return np.sqrt(np.sum((self.coord_at_node[:, node1] -
+                               self.coord_at_node[:, node0]) ** 2, axis=0))
