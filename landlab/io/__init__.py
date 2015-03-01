401d4e0db7c7da262e4ed8fe1e259cd01c47590f
mcflugen <mcflugen@gmail.com>
2015-03-01 10:14:25 -0700

    Added write_esri_ascii.

--- landlab/io/__init__.py
+++ landlab/io/__init__.py
@@ -1,6 +1,9 @@
 """
 Modules that read/write ModelGrids from various file formats.
 """
-from esri_ascii import (read_esri_ascii, read_asc_header)
+from esri_ascii import (read_esri_ascii, read_asc_header, write_esri_ascii)
 from esri_ascii import (MissingRequiredKeyError, KeyTypeError,
                         DataSizeError)
+
+__all__ = ['read_esri_ascii', 'read_asc_header', 'write_esri_ascii',
+           'MissingRequiredKeyError', 'KeyTypeError', 'DataSizeError']
