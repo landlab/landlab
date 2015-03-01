0dcdcaa0c5b74dcaee243bc3045973d4a8cf701a
mcflugen <mcflugen@gmail.com>
2015-03-01 14:17:07 -0700

    More unit tests for RasterModelGrid.save.

--- landlab/grid/tests/test_raster_grid/test_save.py
+++ landlab/grid/tests/test_raster_grid/test_save.py
@@ -10,11 +10,55 @@ except ImportError:
     from landlab.testing.tools import assert_list_equal
 
 from landlab.testing.tools import cdtemp
-from landlab.io import write_esri_ascii, read_esri_ascii
+from landlab.io import read_esri_ascii
+from landlab.io.netcdf import read_netcdf
 from landlab import RasterModelGrid
 
-def test_save():
+def test_save_esri_ascii():
     grid = RasterModelGrid(4, 5, 2.)
     grid.add_field('node', 'air__temperature', np.arange(20.))
+
+    with cdtemp() as _:
+        grid.save('test.asc', format='esri-ascii')
+        assert_true(os.path.isfile('test.asc'))
+
+
+def test_add_extension():
+    grid = RasterModelGrid(4, 5, 2.)
+    grid.add_field('node', 'air__temperature', np.arange(20.))
+
+    with cdtemp() as _:
+        grid.save('test', format='esri-ascii')
+        assert_true(os.path.isfile('test.asc'))
+
+    with cdtemp() as _:
+        grid.save('test', format='netcdf')
+        assert_true(os.path.isfile('test.nc'))
+
+
+def test_replace_extension():
+    grid = RasterModelGrid(4, 5, 2.)
+    grid.add_field('node', 'air__temperature', np.arange(20.))
+
+    with cdtemp() as _:
+        grid.save('test.nc', format='esri-ascii')
+        assert_true(os.path.isfile('test.asc'))
+
+    with cdtemp() as _:
+        grid.save('test.asc', format='netcdf')
+        assert_true(os.path.isfile('test.nc'))
+
+
+def test_guess_format():
+    grid = RasterModelGrid(4, 5, 2.)
+    grid.add_field('node', 'air__temperature', np.arange(20.))
+
     with cdtemp() as _:
         grid.save('test.asc')
+        assert_true(os.path.isfile('test.asc'))
+        read_esri_ascii('test.asc')
+
+    with cdtemp() as _:
+        grid.save('test.nc')
+        assert_true(os.path.isfile('test.nc'))
+        read_netcdf('test.nc')
