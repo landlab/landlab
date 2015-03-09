91498555695105691849ba3765e6876af43bc507
mcflugen <mcflugen@gmail.com>
2015-03-08 21:22:05 -0600

    Minor style fixes.

--- landlab/utils/structured_grid.py
+++ landlab/utils/structured_grid.py
@@ -2,13 +2,9 @@
 import numpy as np
 import itertools
 
-from ..grid.base import (CORE_NODE, FIXED_VALUE_BOUNDARY,
-                         FIXED_GRADIENT_BOUNDARY, TRACKS_CELL_BOUNDARY,
-                         CLOSED_BOUNDARY,BAD_INDEX_VALUE)
-
-#BAD_INDEX_VALUE = np.iinfo(np.int).max
-
-#from landlab.utils import count_repeated_values
+from ..grid.base import (CORE_NODE, FIXED_GRADIENT_BOUNDARY,
+                         TRACKS_CELL_BOUNDARY, CLOSED_BOUNDARY,
+                         BAD_INDEX_VALUE)
 
 
 def node_count(shape):
@@ -1157,8 +1153,9 @@ def neighbor_node_ids(shape, inactive=BAD_INDEX_VALUE):
 
 
 def linked_neighbor_node_ids(shape, closed_boundary_nodes,
-                             open_boundary_nodes=[],
+                             open_boundary_nodes=None,
                              inactive=BAD_INDEX_VALUE):
+    open_boundary_nodes = open_boundary_nodes or []
     ids_with_halo = node_index_with_halo(shape, halo_indices=inactive)
 
     # Everything that touches a closed boundary is inactive
@@ -1368,24 +1365,24 @@ def has_boundary_neighbor(neighbors, diagonals,
 
 
 def has_boundary_neighbor_slow(neighbors, diagonals, out_of_bounds=BAD_INDEX_VALUE):
-        #nbr_nodes=self.get_neighbor_list(id)
-        #diag_nbrs=self.get_diagonal_list(id)
+    # nbr_nodes=self.get_neighbor_list(id)
+    # diag_nbrs=self.get_diagonal_list(id)
 
-        i=0
-        while i < 4 and neighbors[i] != out_of_bounds:
-            i += 1
+    i=0
+    while i < 4 and neighbors[i] != out_of_bounds:
+        i += 1
 
-        if i < 4:
-            return True
-        else:
-            r = 0
-            while r < 4 and diagonals[r] != out_of_bounds:
-                r += 1
+    if i < 4:
+        return True
+    else:
+        r = 0
+        while r < 4 and diagonals[r] != out_of_bounds:
+            r += 1
 
-        if r < 4 :
-            return True
-        else:
-            return False
+    if r < 4 :
+        return True
+    else:
+        return False
 
 
 def reshape_array(shape, u, flip_vertically=False, copy=False):
@@ -1457,7 +1454,7 @@ def nodes_around_points_on_unit_grid(shape, coords, mode='raise'):
         return np.ravel_multi_index(((rows, rows + 1, rows + 1, rows),
                                      (cols, cols, cols + 1, cols + 1)),
                                     shape, mode=mode).T
-    except ValueError as e:
+    except ValueError:
         raise
 
 def nodes_around_points(shape, coords, spacing=(1., 1.),
@@ -1483,7 +1480,7 @@ def nodes_around_points(shape, coords, spacing=(1., 1.),
             shape,
             ((coords[0] - origin[0]) / spacing[0],
              (coords[1] - origin[1]) / spacing[1]))
-    except ValueError as e:
+    except ValueError:
         raise
 
 def nodes_around_point(shape, coords, spacing=(1., 1.)):
