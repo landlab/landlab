67ade5f748b9762e45825897e560c709bce6b282
mcflugen <mcflugen@gmail.com>
2015-05-29 21:22:19 -0600

    Changed to use print function from six.

--- landlab/testing/decorators.py
+++ landlab/testing/decorators.py
@@ -1,13 +1,17 @@
 from functools import wraps
 
+import six
+
 
 def track_this_method(func):
     @wraps(func)
     def _wrapped(self, *args, **kwds):
         if self._DEBUG_TRACK_METHODS:
-            print 'Entering: %s.%s' % (self.__class__.__name__, func.__name__)
+            six.print('Entering: %s.%s' % (self.__class__.__name__,
+                                           func.__name__))
         ans = func(self, *args, **kwds)
         if self._DEBUG_TRACK_METHODS:
-            print 'Exiting: %s.%s' % (self.__class__.__name__, func.__name__)
+            six.print('Exiting: %s.%s' % (self.__class__.__name__,
+                                          func.__name__))
         return ans
     return _wrapped
