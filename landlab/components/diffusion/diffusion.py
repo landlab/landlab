489c4fb4029ba6e6900243661b27e6ef009898e1
SiccarPoint <dan.hobley@gmail.com>
2015-07-29 14:22:00 -0600

    Bug in linear_diffuser
    If you specify a timestep already smaller than the Von Neumann
    condition, timestep was set wrong and solution wrong. Now corrected.

--- landlab/components/diffusion/diffusion.py
+++ landlab/components/diffusion/diffusion.py
@@ -28,67 +28,67 @@ class LinearDiffuser(Component):
     """
     This component implements linear diffusion of a field in the supplied
     ModelGrid.
-    
+
     This components requires the following parameters be set in the input file,
     *input_stream*, set in the component initialization:
-        
+
         'linear_diffusivity', the diffusivity to use
-    
+
     Optional inputs are:
-    
+
         'uplift_rate', if you want this component to include the uplift
             internally
         'dt', the model timestep (assumed constant)
         'values_to_diffuse', a string giving the name of the grid field
         containing the data to diffuse.
-    
-    Supply *dt* to the diffuser through the diffuse() argument. 
+
+    Supply *dt* to the diffuser through the diffuse() argument.
     This allows you to set a dynamic timestep for this class.
-    If 'values_to_diffuse' is not provided, defaults to 
+    If 'values_to_diffuse' is not provided, defaults to
     'topographic__elevation'.
-    
+
     No particular units are necessary where they are not specified, as long as
     all units are internally consistent.
-    
+
     The component takes *grid*, the ModelGrid object, and (optionally)
     *current_time* and *input_stream*. If *current_time* is not set, it defaults
     to 0.0. If *input_stream* is not set in instantiation of the class,
     :func:`initialize` with *input_stream* as in input must be called instead.
-    *Input_stream* is the filename of (& optionally, path to) the parameter 
+    *Input_stream* is the filename of (& optionally, path to) the parameter
     file.
-    
+
     At the moment, this diffuser can only work with constant diffusivity.
     Spatially variable diffusivity hopefully coming soon.
-    
+
     The primary method of this class is :func:`diffuse`.
     """
-    
+
     _name = 'LinearDiffuser'
-    
+
     _input_var_names = set(['topographic__elevation',
                             ])
-    
+
 #############################UPDATE ME
     _output_var_names = set(['topographic__elevation',
                              'surface_gradient',
                              'unit_flux',
                              ])
-                             
+
     _var_units = {'topographic__elevation' : 'm',
                   'surface_gradient' : '-',
                   'unit_flux' : 'm**3/s',
                   }
-    
+
     _var_mapping = {'topographic__elevation' : 'node',
                     'surface_gradient' : 'link',
                     'unit_flux' : 'link',
                     }
-    
+
     _var_defs = {'topographic__elevation' : 'Land surface topographic elevation; can be overwritten in initialization',
                  'surface_gradient' : 'Gradient of surface, on links',
                  'unit_flux' : 'Volume flux per unit width along links',
                   }
-    
+
     def __init__(self, grid, input_stream=None, current_time=0.):
         self._grid = grid
         self.current_time = current_time
@@ -97,15 +97,15 @@ class LinearDiffuser(Component):
         else:
             print('Ensure you call the initialize(input_stream) method before '
                   'running the model!')
-        
+
     def initialize(self, input_stream):
-        
+
         # Create a ModelParameterDictionary for the inputs
         if type(input_stream)==ModelParameterDictionary:
             inputs = input_stream
         else:
             inputs = ModelParameterDictionary(input_stream)
-        
+
         # Read input/configuration parameters
         self.kd = inputs.read_float('linear_diffusivity')
         try:
@@ -123,20 +123,20 @@ class LinearDiffuser(Component):
                 mysets.add(self.values_to_diffuse)
             for mydicts in (self._var_units, self._var_mapping, self._var_defs):
                 mydicts[self.values_to_diffuse] = mydicts.pop('topographic__elevation')
-            
+
         try:
-            self.timestep_in = inputs.read_float('dt')  
+            self.timestep_in = inputs.read_float('dt')
         except MissingKeyError:
             pass
 
-        
+
         # Create grid if one doesn't already exist
         if self._grid is None:
             self._grid = create_and_initialize_grid(input_stream)
-            
+
         # Set internal time step
         # ..todo:
-        #   implement mechanism to compute time-steps dynamically if grid is 
+        #   implement mechanism to compute time-steps dynamically if grid is
         #   adaptive/changing
         dx = self._grid.min_active_link_length()  # smallest active link length
         self.dt = _ALPHA*dx*dx/self.kd  # CFL condition
@@ -144,14 +144,14 @@ class LinearDiffuser(Component):
             self.tstep_ratio = self.timestep_in/self.dt
         except AttributeError:
             pass
-        
+
         # Get a list of interior cells
         self.interior_cells = self._grid.get_core_cell_node_ids()
-  
-##DEJH bites the bullet and forces the 2015 style with fields      
-#        # Here we're experimenting with different approaches: with 
+
+##DEJH bites the bullet and forces the 2015 style with fields
+#        # Here we're experimenting with different approaches: with
 #        # 'make_all_data', we create and manage all the data we need and embed
-#        # it all in the grid. With 'explicit', we require the caller/user to 
+#        # it all in the grid. With 'explicit', we require the caller/user to
 #        # provide data.
 #        if _VERSION=='make_all_data':
 #            #print('creating internal data')
@@ -180,8 +180,8 @@ class LinearDiffuser(Component):
         except FieldError:
             pass
         #note all these terms are deliberately loose, as we won't always be dealing with topo
-    
-    
+
+
     def input_timestep(self, timestep_in):
         """
         Allows the user to set a dynamic (evolving) timestep manually as part of
@@ -189,74 +189,74 @@ class LinearDiffuser(Component):
         """
         self.timestep_in = timestep_in
         self.tstep_ratio = timestep_in/self.dt
-        
-        
+
+
     def run_one_step_explicit(self, mg, z, g, qs, dqsds, dzdt, delt):
-        
+
         # Take the smaller of delt or built-in time-step size self.dt
         dt = min(self.dt, delt)
-        
+
         # Calculate the gradients and sediment fluxes
         g = mg.calculate_gradients_at_active_links(z)
         qs = -self.kd*g
-        
+
         # Calculate the net deposition/erosion rate at each node
         dqsds = mg.calculate_flux_divergence_at_nodes(qs)
-        
+
         # Calculate the total rate of elevation change
         dzdt = self.uplift_rate - dqsds
-        
+
         # Update the elevations
         z[self.interior_cells] = z[self.interior_cells] \
                                  + dzdt[self.interior_cells] * dt
-                                 
+
         # Update current time and return it
         self.current_time += dt
-        
+
         return z, g, qs, dqsds, dzdt
-        
-        
+
+
     def run_one_step_internal(self, delt):
-        
+
         # Take the smaller of delt or built-in time-step size self.dt
         dt = min(self.dt, delt)
-        
+
         # Calculate the gradients and sediment fluxes
         self.g = self._grid.calculate_gradients_at_active_links(self.z)
         self.qs = -self.kd*self.g
-        
+
         # Calculate the net deposition/erosion rate at each node
         self.dqsds = self._grid.calculate_flux_divergence_at_nodes(self.qs)
-        
+
         # Calculate the total rate of elevation change
         dzdt = self.uplift_rate - self.dqsds
-        
+
         # Update the elevations
         self.z[self.interior_cells] += dzdt[self.interior_cells] * dt
-                                 
+
         # Update current time and return it
         self.current_time += dt
         return self.current_time
-    
+
     def diffuse(self, dt, internal_uplift=False, num_uplift_implicit_comps = 1):
         """
         This is the primary method of the class. Call it to perform an iteration
         of the model. Takes *dt*, the current timestep.
-        
+
         The modelgrid must contain the field to diffuse, which defaults to
-        'topographic__elevation'. This can be overridden with the 
+        'topographic__elevation'. This can be overridden with the
         values_to_diffuse property in the input file.
-        
+
         See the class docstring for a list of the other properties necessary
         in the input file for this component to run.
-        
+
         To improve stability, this component can incorporate uplift into its
         internal mechanism. To use this, set *internal_uplift* to True, and . If you only have one module that requires this, do not add
-        uplift manually in your loop; this method will include uplift 
+        uplift manually in your loop; this method will include uplift
         automatically. If more than one of your components has this requirement,
         set *num_uplift_implicit_comps* to the total number of components that
         do.
-        
+
         You can suppress this behaviour by setting *internal_uplift* to False.
 
         """
@@ -265,32 +265,35 @@ class LinearDiffuser(Component):
         repeats = int(self.tstep_ratio//1.)
         extra_time = self.tstep_ratio-repeats
         z = self._grid.at_node[self.values_to_diffuse]
-        
+
         core_nodes = self._grid.get_core_cell_node_ids()
-        
+
         for i in xrange(repeats+1):
+            print(repeats, extra_time)
             # Calculate the gradients and sediment fluxes
             self.g[self._grid.active_links] = self._grid.calculate_gradients_at_active_links(z)
             self.qs[self._grid.active_links] = -self.kd*self.g[self._grid.active_links]
-        
+
             # Calculate the net deposition/erosion rate at each node
-            self.dqsds = self._grid.calculate_flux_divergence_at_nodes(self.qs[self._grid.active_links])    
-            
+            self.dqsds = self._grid.calculate_flux_divergence_at_nodes(self.qs[self._grid.active_links])
+
             # Calculate the total rate of elevation change
             #dzdt = self.uplift_rate - self.dqsds
             dzdt = - self.dqsds
-            
+
             # Update the elevations
+            timestep = self.dt
             if i == (repeats):
-                timestep = extra_time
+                timestep *= extra_time
             else:
-                timestep = self.dt
+                pass
             if internal_uplift:
                 add_uplift = self.uplift_rate/num_uplift_implicit_comps
             else:
                 add_uplift = 0.
             self._grid.at_node[self.values_to_diffuse][core_nodes] += add_uplift + dzdt[core_nodes] * timestep
-            
+            print(timestep)
+
             #check the BCs, update if fixed gradient
             if self._grid.fixed_gradient_boundary_nodes:
                 self._grid.at_node[self.values_to_diffuse][self._grid.fixed_gradient_node_properties['boundary_node_IDs']] = self._grid.at_node[self.values_to_diffuse][self._grid.fixed_gradient_node_properties['anchor_node_IDs']] + self._grid.fixed_gradient_node_properties['values_to_add']
@@ -298,36 +301,36 @@ class LinearDiffuser(Component):
         #return the grid
         return self._grid
 
-        
+
     def run_until_explicit(self, mg, t, z, g, qs, dqsds, dzdt):
-        
+
         while self.current_time < t:
             remaining_time = t - self.current_time
             z, g, qs, dqsds, dzdt = self.run_one_step_explicit(mg, z, g, qs, dqsds, dzdt, remaining_time)
-            
+
         return z, g, qs, dqsds, dzdt
-        
-        
+
+
     def run_until_internal(self, t):
-        
+
         while self.current_time < t:
             remaining_time = t - self.current_time
             self.run_one_step_internal(remaining_time)
-                    
-        
+
+
     def run_until(self, t):  # this is just a temporary duplicate
-        
+
         while self.current_time < t:
             remaining_time = t - self.current_time
             self.run_one_step_internal(remaining_time)
-                    
-        
+
+
     def get_time_step(self):
         """
         Returns time-step size.
         """
         return self.dt
-    
+
     @property
     def time_step(self):
         """
