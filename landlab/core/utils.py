b8e7c66bf2ea2e6d7e8da35ea29c46766fb49536
mcflugen <mcflugen@gmail.com>
2015-08-26 23:17:26 -0600

    Functions that dynamically add methods to classes.

---
+++ landlab/core/utils.py
@@ -0,0 +1,36 @@
+#! /usr/bin/env python
+
+
+def get_functions_from_module(mod, pattern=None):
+    import inspect, re
+
+    funcs = {}
+    for name, func in inspect.getmembers(mod, inspect.isroutine):
+        if pattern is None or re.match(pattern, name):
+            funcs[name] = func
+    return funcs
+
+
+def add_functions_to_class(cls, funcs):
+    for name, func in funcs.items():
+        setattr(cls, name, func)
+
+
+def add_module_functions_to_class(cls, module, pattern=None):
+    import inspect, imp, os
+
+    caller = inspect.stack()[1]
+    path = os.path.join(os.path.dirname(caller[1]), os.path.dirname(module))
+
+    (module, _) = os.path.splitext(os.path.basename(module))
+
+    try:
+        mod = imp.load_module(module, *imp.find_module(module, [path]))
+    except ImportError:
+        print path, module
+        raise
+
+    funcs = get_functions_from_module(mod, pattern=pattern)
+    add_functions_to_class(cls, funcs)
+
+
