774e6b9f308f67cf6f5975999e141d73d564689c
mcflugen <mcflugen@gmail.com>
2015-09-19 19:28:38 -0600

    Fixed minor style issues.

--- landlab/io/netcdf/__init__.py
+++ landlab/io/netcdf/__init__.py
@@ -1,5 +1,9 @@
 import os
 
+from .read import read_netcdf
+from .write import write_netcdf
+from .errors import NotRasterGridError
+
 try:
     import netCDF4 as nc4
 except ImportError:
@@ -14,12 +18,6 @@ NETCDF4_EXAMPLE_FILE = os.path.join(os.path.dirname(__file__), 'tests', 'data',
 NETCDF3_64BIT_EXAMPLE_FILE = os.path.join(os.path.dirname(__file__), 'tests',
                                           'data', 'test-netcdf3-64bit.nc')
 
-
-from .read import read_netcdf
-from .write import write_netcdf
-from .errors import NotRasterGridError
-
-
-__all__ = ['read_netcdf', 'write_netcdf', 'NotRasterGridError',
+__all__ = ('read_netcdf', 'write_netcdf', 'NotRasterGridError',
            'WITH_NETCDF4', 'NETCDF4_EXAMPLE_FILE',
-           'NETCDF3_64BIT_EXAMPLE_FILE']
+           'NETCDF3_64BIT_EXAMPLE_FILE')
