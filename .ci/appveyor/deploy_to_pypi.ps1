6233d3485708f8c693956be5fee7bb22f6d6b3c8
mcflugen <mcflugen@gmail.com>
2015-10-11 11:57:07 -0600

    Call twine command without full path.

--- .ci/appveyor/deploy_to_pypi.ps1
+++ .ci/appveyor/deploy_to_pypi.ps1
@@ -1,3 +1,3 @@
 if (($env:appveyor_repo_tag -eq "True") -and ($env:appveyor_repo_branch.StartsWith("v"))) {
-    Invoke-Expression "$env:PYTHON/Scripts/twine upload -u landlab -p $env:PYPI_PASS dist/*"
+    Invoke-Expression "twine upload -u landlab -p $env:PYPI_PASS dist/*"
 }
