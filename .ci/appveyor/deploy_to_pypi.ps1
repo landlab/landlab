84b8645c8d4d0a929e98f28f8c41658ea3cb61ea
mcflugen <mcflugen@gmail.com>
2015-10-11 12:31:47 -0600

    Updated PyPI username/password.

--- .ci/appveyor/deploy_to_pypi.ps1
+++ .ci/appveyor/deploy_to_pypi.ps1
@@ -1,3 +1,3 @@
 if (($env:appveyor_repo_tag -eq "True") -and ($env:appveyor_repo_branch.StartsWith("v"))) {
-    Invoke-Expression "twine upload -u landlab -p $env:PYPI_PASS dist/*"
+    Invoke-Expression "twine upload -u mcflugen -p $env:PYPI_PASS dist/*"
 }
