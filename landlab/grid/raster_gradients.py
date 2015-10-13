fc67315f044aca646ae953811828fe049a6ef6f4
mcflugen <mcflugen@gmail.com>
2015-10-13 08:54:32 -0600

    Added better docstrings/doctests.

--- landlab/grid/raster_gradients.py
+++ landlab/grid/raster_gradients.py
@@ -7,6 +7,37 @@ from landlab.grid import gradients
 def calculate_gradients_at_links(grid, node_values, out=None):
     """Calculate gradients over links.
 
+    Parameters
+    ----------
+    grid : RasterModelGrid
+        A grid.
+    node_values : array_like
+        Values at nodes.
+    out : ndarray, optional
+        Buffer to hold result. If `None`, create a new array.
+
+    Returns
+    -------
+    ndarray
+        Gradients of the nodes values for each link.
+
+    Examples
+    --------
+    >>> from landlab import RasterModelGrid
+    >>> from landlab.grid.raster_gradients import (
+    ...     calculate_gradients_at_links)
+    >>> grid = RasterModelGrid((3, 3))
+    >>> node_values = [0., 0., 0., 1., 3., 1., 2., 2., 2.]
+    >>> calculate_gradients_at_links(grid, node_values)
+    array([ 1.,  3.,  1.,  1., -1.,  1.,  0.,  0.,  2., -2.,  0.,  0.])
+
+    >>> out = np.empty(grid.number_of_links, dtype=float)
+    >>> rtn = calculate_gradients_at_links(grid, node_values, out=out)
+    >>> rtn is out
+    True
+    >>> out
+    array([ 1.,  3.,  1.,  1., -1.,  1.,  0.,  0.,  2., -2.,  0.,  0.])
+
     .. deprecated:: 0.1
         Use :func:`calculate_gradient_across_cell_faces`
                 or :func:`calculate_gradient_across_cell_corners` instead
@@ -30,19 +61,20 @@ def calculate_gradients_at_active_links(grid, node_values, out=None):
     Note that a negative gradient corresponds to a lower node in the
     direction of the link.
 
+    Returns
+    -------
+    ndarray
+        Gradients of the nodes values for each link.
+
     Examples
     --------
     >>> import numpy as np
     >>> from landlab import RasterModelGrid
     >>> rmg = RasterModelGrid(4, 5, 1.0)
     >>> u = [0., 1., 2., 3., 0.,
-    ...     1., 2., 3., 2., 3.,
-    ...     0., 1., 2., 1., 2.,
-    ...     0., 0., 2., 2., 0.]
-    >>> u = np.array(u)
-    >>> u
-    array([ 0.,  1.,  2.,  3.,  0.,  1.,  2.,  3.,  2.,  3.,  0.,  1.,  2.,
-            1.,  2.,  0.,  0.,  2.,  2.,  0.])
+    ...      1., 2., 3., 2., 3.,
+    ...      0., 1., 2., 1., 2.,
+    ...      0., 0., 2., 2., 0.]
     >>> grad = rmg.calculate_gradients_at_active_links(u)
     >>> grad
     array([ 1.,  1., -1., -1., -1., -1., -1.,  0.,  1.,  1.,  1., -1.,  1.,
@@ -51,12 +83,13 @@ def calculate_gradients_at_active_links(grid, node_values, out=None):
     For greater speed, sending a pre-created numpy array as an argument
     avoids having to create a new one with each call:
 
-    >>> grad = np.zeros(rmg.number_of_active_links)
-    >>> u = u*10
-    >>> grad = rmg.calculate_gradients_at_active_links(u, grad)
+    >>> grad = np.empty(rmg.number_of_active_links)
+    >>> rtn = rmg.calculate_gradients_at_active_links(u, out=grad)
     >>> grad
-    array([ 10.,  10., -10., -10., -10., -10., -10.,   0.,  10.,  10.,  10.,
-           -10.,  10.,  10.,  10., -10.,  10.])
+    array([ 1.,  1., -1., -1., -1., -1., -1.,   0.,  1.,  1.,  1.,
+           -1.,  1.,  1.,  1., -1.,  1.])
+    >>> rtn is grad
+    True
     """
     diffs = gradients.calculate_diff_at_active_links(grid, node_values,
                                                      out=out)
