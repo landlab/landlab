b0e1ff75aa32715b510b3c04790c009098765852
mcflugen <mcflugen@gmail.com>
2015-10-20 21:41:39 -0600

    Only deploy wheel on Mac.

--- .ci/travis/deploy_to_pypi.sh
+++ .ci/travis/deploy_to_pypi.sh
@@ -1,5 +1,9 @@
 if [[ "$TRAVIS_TAG" == v* ]]; then
   pip install twine wheel
-  python setup.py bdist_wheel
+  if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
+    python setup.py sdist
+  else
+    python setup.py bdist_wheel
+  fi
   twine upload -u mcflugen -p$PYPI_PASS dist/*
 fi
