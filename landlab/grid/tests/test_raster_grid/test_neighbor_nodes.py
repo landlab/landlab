6a2811b86dc17570c67132c97d7701e264a8d52c
mcflugen <mcflugen@gmail.com>
2015-11-06 08:37:17 -0700

    Added unit tests for neighbors_at_node.

--- landlab/grid/tests/test_raster_grid/test_neighbor_nodes.py
+++ landlab/grid/tests/test_raster_grid/test_neighbor_nodes.py
@@ -3,12 +3,11 @@ from numpy.testing import assert_array_equal
 from nose.tools import with_setup
 
 from landlab import RasterModelGrid
-from landlab.grid.base import BAD_INDEX_VALUE
+from landlab.grid.base import BAD_INDEX_VALUE as X
 
 
-def test_all_neighbors():
+def test_all_active_neighbors():
     rmg = RasterModelGrid(5, 4)
-    X = BAD_INDEX_VALUE
     expected = np.array([
         [X, X, X, X], [X, 5, X, X], [X, 6, X, X], [X, X, X, X],
         [5, X, X, X], [6, 9, 4, 1], [7, 10, 5, 2], [X, X, 6, X],
@@ -16,34 +15,58 @@ def test_all_neighbors():
         [13, X, X, X], [14, 17, 12, 9], [15, 18, 13, 10], [X, X, 14, X],
         [X, X, X, X], [X, X, X, 13], [X, X, X, 14], [X, X, X, X],
     ])
-    assert_array_equal(rmg.get_neighbor_list(), expected)
+    assert_array_equal(rmg.get_active_neighbors_at_node(), expected)
+
+
+def test_all_neighbors():
+    rmg = RasterModelGrid((5, 4))
+    expected = np.array([
+        [ 1,  4, X,  X], [ 2,  5,  0,  X], [ 3,  6,  1,  X], [X,  7,  2,  X],
+        [ 5,  8, X,  0], [ 6,  9,  4,  1], [ 7, 10,  5,  2], [X, 11,  6,  3],
+        [ 9, 12, X,  4], [10, 13,  8,  5], [11, 14,  9,  6], [X, 15, 10,  7],
+        [13, 16, X,  8], [14, 17, 12,  9], [15, 18, 13, 10], [X, 19, 14, 11],
+        [17,  X, X, 12], [18,  X, 16, 13], [19,  X, 17, 14], [X,  X, 18, 15],
+    ])
+    assert_array_equal(rmg.neighbors_at_node, expected)
+
+
+def test_active_neighbor_list_with_scalar_arg():
+    rmg = RasterModelGrid(5, 4)
+
+    assert_array_equal(rmg.get_active_neighbors_at_node(6),
+                       np.array([7, 10, 5, 2]))
+    assert_array_equal(rmg.get_active_neighbors_at_node(-1),
+                       np.array([X, X, X, X]))
+    assert_array_equal(rmg.get_active_neighbors_at_node(-2),
+                       np.array([X, X, X, 14]))
 
 
 def test_neighbor_list_with_scalar_arg():
-    X = BAD_INDEX_VALUE
+    rmg = RasterModelGrid((5, 4))
+
+    assert_array_equal(rmg.neighbors_at_node[6], np.array([7, 10, 5, 2]))
+    assert_array_equal(rmg.neighbors_at_node[-1], np.array([X, X, 18, 15]))
+    assert_array_equal(rmg.neighbors_at_node[-2], np.array([19, X, 17, 14]))
+
+
+def test_active_neighbor_list_with_array_arg():
     rmg = RasterModelGrid(5, 4)
-
-    assert_array_equal(rmg.get_neighbor_list(6), np.array([7, 10, 5, 2]))
-    assert_array_equal(rmg.get_neighbor_list(-1), np.array([X, X, X, X]))
-    assert_array_equal(rmg.get_neighbor_list(-2), np.array([X, X, X, 14]))
-
-
-def test_neighbor_list_with_array_arg():
-    X = BAD_INDEX_VALUE
-    rmg = RasterModelGrid(5, 4)
-    assert_array_equal(rmg.get_neighbor_list([6, -1]),
+    assert_array_equal(rmg.get_active_neighbors_at_node([6, -1]),
                        np.array([[7, 10, 5, 2], [X, X, X, X]]))
 
 
+def test_neighbor_list_with_array_arg():
+    rmg = RasterModelGrid(5, 4)
+    assert_array_equal(rmg.neighbors_at_node[(6, -1), :],
+                       np.array([[7, 10, 5, 2], [X, X, 18, 15]]))
+
+
 def test_neighbor_list_boundary():
-    """
-    All of the neighbor IDs for a boundary cell are -1.
-    """
-    X = BAD_INDEX_VALUE
+    """All of the neighbor IDs for a boundary cell are -1."""
     rmg = RasterModelGrid(5, 4)
     import landlab.utils.structured_grid as sgrid
     rmg.set_closed_nodes([0, 1, 2, 3, 4, 7, 8, 11, 12, 15, 16, 17, 18, 19])
 
     for node_id in sgrid.boundary_iter(rmg.shape):
-        assert_array_equal(rmg.get_neighbor_list(node_id),
+        assert_array_equal(rmg.get_active_neighbors_at_node(node_id),
                            np.array([X, X, X, X]))
