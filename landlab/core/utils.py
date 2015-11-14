e9a8043c462803706865eab3fc0ce4e07aea6354
Greg Tucker <gtucker@colorado.edu>
2015-11-14 09:24:54 -0700

    implemented argsort by x,y in utils.py; cleaned up failing doctests in hex_lattice_tectonicizer

--- landlab/core/utils.py
+++ landlab/core/utils.py
@@ -191,6 +191,34 @@ def add_module_functions_to_class(cls, module, pattern=None):
     add_functions_to_class(cls, funcs)
 
 
+def argsort_points_by_x_then_y(pts):
+    """Sort points by coordinates, first x then y, returning sorted indices.
+    
+    Parameters
+    ----------
+    pts : Nx2 NumPy array of float
+        (x,y) points to be sorted
+
+    Returns
+    -------
+    indices : Nx1 NumPy array of int
+        indices of sorted (x,y) points
+    
+    Examples
+    --------
+    >>> import numpy as np
+    >>> from landlab.core.utils import argsort_points_by_x_then_y
+    >>> pts = np.zeros((10, 2))
+    >>> pts[:,0] = np.array([0., 0., 0., 1., 1., 1., 1., 2., 2., 2.])
+    >>> pts[:,1] = np.array([0., 1., 2., -0.5, 0.5, 1.5, 2.5, 0., 1., 2.])
+    >>> idx = argsort_points_by_x_then_y(pts)
+    >>> idx
+    array([3, 0, 7, 4, 1, 8, 5, 2, 9, 6])
+    """
+    a = pts[:,0].argsort(kind='mergesort')
+    b = pts[a,1].argsort(kind='mergesort')
+    return a[b]
+
 def sort_points_by_x_then_y(pts):
     """Sort points by coordinates, first x then y.
     
@@ -224,8 +252,11 @@ def sort_points_by_x_then_y(pts):
            [ 2. ,  2. ],
            [ 1. ,  2.5]])
     """
-    a = pts[:,0].argsort(kind='mergesort')
-    b = pts[a,1].argsort(kind='mergesort')
-    pts[:,0] = pts[a[b],0]
-    pts[:,1] = pts[a[b],1]
+    indices = argsort_points_by_x_then_y(pts)
+    pts[:,0] = pts[indices,0]
+    pts[:,1] = pts[indices,1]
     return pts
+
+if __name__=='__main__':
+    import doctest
+    doctest.testmod()
