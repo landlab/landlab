a3a53f303c9d0ef30a16fa6b85d8f3fbe9cd3baf
mcflugen <mcflugen@gmail.com>
2015-12-06 15:55:11 -0700

    Import component user interface.

---
+++ landlab/components/stream_power/__init__.py
@@ -0,0 +1,6 @@
+from .stream_power import StreamPowerEroder
+from .fastscape_stream_power import SPEroder
+from .sed_flux_dep_incision import SedDepEroder
+
+
+__all__ = ['StreamPowerEroder', 'SPEroder', 'SedDepEroder', ]
