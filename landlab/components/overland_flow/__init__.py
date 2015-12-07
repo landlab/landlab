19c8f4451ad793e3d19d9687c1ae1acd666600a5
mcflugen <mcflugen@gmail.com>
2015-12-07 00:05:43 -0700

    Import modules with sub-package.

---
+++ landlab/components/overland_flow/__init__.py
@@ -0,0 +1,6 @@
+from . import generate_overland_flow_Bates
+from . import generate_overland_flow_deAlmeida
+
+
+__all__ = ['generate_overland_flow_Bates',
+           'generate_overland_flow_deAlmeida', ]
