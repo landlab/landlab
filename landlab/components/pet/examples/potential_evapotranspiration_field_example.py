0fff9b5b224f6e25565dda6d0afe0624206dcb24
mcflugen <mcflugen@gmail.com>
2015-12-07 00:11:27 -0700

    Import PotentialEvapotranspiration into pet.

--- landlab/components/pet/examples/potential_evapotranspiration_field_example.py
+++ landlab/components/pet/examples/potential_evapotranspiration_field_example.py
@@ -4,8 +4,7 @@
 #import landlab
 from landlab import RasterModelGrid
 from landlab.components.radiation.radiation_field import Radiation
-from landlab.components.pet.potential_evapotranspiration_field import (
-    PotentialEvapotranspiration)
+from landlab.components.pet import PotentialEvapotranspiration
 import numpy as np
 import matplotlib.pyplot as plt
 from landlab.plot.imshow import imshow_grid
