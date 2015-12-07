a75287fba478d0f94fb924762a97bafa8be27fcd
mcflugen <mcflugen@gmail.com>
2015-12-07 00:26:55 -0700

    Import PowerLawIncision into sub-package.

--- landlab/components/simple_power_law_incision/power_law_incision_driver.py
+++ landlab/components/simple_power_law_incision/power_law_incision_driver.py
@@ -4,7 +4,7 @@ from landlab import RasterModelGrid, CLOSED_BOUNDARY
 from landlab.plot.imshow import imshow_grid
 from landlab.components.dem_support import WatershedBoundaryConditions
 from random import uniform
-from landlab.components.simple_power_law_incision.power_law_fluvial_eroder import PowerLawIncision
+from landlab.components.simple_power_law_incision import PowerLawIncision
 from landlab.components.flow_routing import RouteFlowD8
 from landlab.components.flow_accum import AccumFlow
 import matplotlib.pyplot as plt
--- landlab/components/simple_power_law_incision/power_law_incision_driver_side_outlet.py
+++ landlab/components/simple_power_law_incision/power_law_incision_driver_side_outlet.py
@@ -4,7 +4,7 @@ import numpy as np
 import pylab
 from landlab import RasterModelGrid, CLOSED_BOUNDARY
 from random import uniform
-from landlab.components.simple_power_law_incision.power_law_fluvial_eroder import PowerLawIncision
+from landlab.components.simple_power_law_incision import PowerLawIncision
 from landlab.components.flow_routing import RouteFlowD8
 from landlab.components.flow_accum import AccumFlow
 import matplotlib.pyplot as plt
