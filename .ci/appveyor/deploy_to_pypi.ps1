5e813ee4589a736e1dbbc7ce62456a540a995caa
mcflugen <mcflugen@gmail.com>
2015-12-13 10:15:02 -0700

    Print tag and version information.

--- .ci/appveyor/deploy_to_pypi.ps1
+++ .ci/appveyor/deploy_to_pypi.ps1
@@ -1,3 +1,9 @@
 if (($env:appveyor_repo_tag -eq "True") -and ($env:appveyor_repo_branch.StartsWith("v"))) {
+    write-output "Deploying to PyPI..."
     Invoke-Expression "twine upload -u mcflugen -p $env:PYPI_PASS dist/*"
+    write-output "OK"
+} else {
+    write-output "Not deploying."
+    write-output "Repo tag is $env:appveyor_repo_tag"
+    write-output "Branch name $env:appveyor_repo_branch"
 }
