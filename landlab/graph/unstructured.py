afc324d412ed43449a9d1e0387c9a7a0e552088c
mcflugen <mcflugen@gmail.com>
2015-12-15 21:37:39 -0700

    Added more docstrings.

--- landlab/graph/unstructured.py
+++ landlab/graph/unstructured.py
@@ -1,174 +1,70 @@
+"""Define a graph of nodes-links-patches.
+
+Examples
+--------
+>>> from landlab.graph import Graph
+
+>>> node_x, node_y = [0, 1, 2, 0, 1, 2], [0, 0, 0, 1, 1, 1]
+>>> graph = Graph((node_y, node_x))
+>>> graph.x_of_node
+array([0, 1, 2, 0, 1, 2])
+>>> graph.y_of_node
+array([0, 0, 0, 1, 1, 1])
+
+>>> links = ((0, 1), (1, 2),
+...          (0, 3), (1, 4), (2, 5),
+...          (3, 4), (4, 5),
+...          (3, 6), (4, 7), (5, 8),
+...          (6, 7), (7, 8))
+>>> graph = Graph((node_y, node_x), links=links)
+>>> graph.nodes_at_link # doctest: +NORMALIZE_WHITESPACE
+array([[0, 1], [1, 2],
+       [0, 3], [1, 4], [2, 5],
+       [3, 4], [4, 5],
+       [3, 6], [4, 7], [5, 8],
+       [6, 7], [7, 8]])
+>>> graph.node_at_link_head
+array([1, 2, 3, 4, 5, 4, 5, 6, 7, 8, 7, 8])
+>>> graph.node_at_link_tail
+array([0, 1, 0, 1, 2, 3, 4, 3, 4, 5, 6, 7])
+
+>>> graph.links_at_node # doctest: +NORMALIZE_WHITESPACE
+array([[ 0,  2, -1, -1], [ 0,  1,  3, -1], [ 1,  4, -1, -1],
+       [ 2,  5,  7, -1], [ 3,  5,  6,  8], [ 4,  6,  9, -1]])
+
+>>> graph.link_dirs_at_node # doctest: +NORMALIZE_WHITESPACE
+array([[-1, -1,  0,  0], [ 1, -1, -1,  0], [ 1, -1,  0,  0],
+       [ 1, -1, -1,  0], [ 1,  1, -1, -1], [ 1,  1, -1,  0]])
+
+>>> patches = ((0, 3, 5, 2), (1, 4, 6, 3))
+>>> graph = Graph((node_y, node_x), links=links, patches=patches)
+>>> graph.links_at_patch
+array([[0, 3, 5, 2],
+       [1, 4, 6, 3]])
+>>> graph.nodes_at_patch
+array([[0, 1, 3, 4],
+       [1, 2, 4, 5]])
+"""
 from six.moves import range
 
 import numpy as np
 
 
-def _find_links_at_node(node, nodes_at_link):
-    """Find links and link directions at a node.
-
-    Examples
-    --------
-    >>> nodes_at_link = ((0, 1), (1, 2),
-    ...                  (0, 3), (1, 4), (2, 5),
-    ...                  (3, 4), (4, 5),
-    ...                  (3, 6), (4, 7), (5, 8),
-    ...                  (6, 7), (7, 8))
-
-    The first node has only two links, both of which are directed outward.
-
-    >>> (links, dirs) = _find_links_at_node(0, nodes_at_link)
-    >>> links
-    array([0, 2])
-    >>> dirs
-    array([-1, -1])
-
-    >>> (links, dirs) = _find_links_at_node(4, nodes_at_link)
-    >>> links
-    array([3, 5, 6, 8])
-    >>> dirs
-    array([ 1,  1, -1, -1])
-    """
-    nodes_at_link = np.asarray(nodes_at_link)
-    (links_at_node, is_inward) = np.where(nodes_at_link == node)
-
-    link_dirs_at_node = np.choose(is_inward, (-1, 1))
-
-    return links_at_node, link_dirs_at_node
-
-
-def _setup_links_at_node(nodes_at_link, number_of_nodes=None):
-    """Set up data structures for node-to-link connectivity.
-
-    Parameters
-    ----------
-    nodes_at_link: ndarray of int
-        Nodes at either end of a link (tail node, then head node).
-    number_of_nodes: int, optional
-        The total number of nodes. If not given, use the largest node in
-        *nodes_at_link*.
-
-    Returns
-    -------
-    tuple of ndarray
-        Tuple of *links_at_node* and *link_dirs_at_node*.
-    """
-    node_count = np.bincount(nodes_at_link.flat)
-    number_of_nodes = number_of_nodes or len(node_count)
-
-    max_node_count = np.max(node_count)
-
-    link_dirs_at_node = np.full((number_of_nodes, max_node_count), 0,
-                                dtype=int)
-    links_at_node = np.full((number_of_nodes, max_node_count), -1, dtype=int)
-
-    for node in range(number_of_nodes):
-        links, dirs = _find_links_at_node(node, nodes_at_link)
-
-        # (link, is_outward) = np.where(nodes_at_link == node)
-        # is_outward[is_outward == 1] = -1
-        # is_outward[is_outward == 0] = 1
-
-        link_dirs_at_node[node, :len(dirs)] = dirs
-        links_at_node[node, :len(links)] = links
-
-    return links_at_node, link_dirs_at_node
-
-
-def _setup_links_at_patch(patches):
-    """Set up data structure that describes link-patch connectivity.
-
-    Parameters
-    ----------
-    patches: iterable of iterables
-        List of links for each patch.
-
-    Returns
-    -------
-    ndarray
-        Links for each patch.
-    """
-    max_links_per_patch = np.max([len(links) for links in patches])
-    links_at_patch = np.full((len(patches), max_links_per_patch), -1,
-                             dtype=int)
-
-    for patch, links in enumerate(patches):
-        links_at_patch[patch, :len(links)] = links
-
-    return links_at_patch
-
-
-def _setup_nodes_at_patch(links_at_patch, nodes_at_link):
-    """Set up data structure that describes node-patch connectivity.
-
-    Parameters
-    ----------
-    links_at_patch: ndarray
-        Links that define each patch.
-    nodes_at_link: ndarray
-        Nodes that define each link.
-
-    Returns
-    -------
-    ndarray
-        Nodes that define each patch.
-    """
-    nodes_at_patch = np.full(links_at_patch.shape, -1, dtype=int)
-
-    for patch, links in enumerate(links_at_patch):
-        unique_nodes = np.unique(nodes_at_link[links])
-        nodes_at_patch[patch, :len(unique_nodes)] = unique_nodes
-
-    return nodes_at_patch
-
-
 class Graph(object):
+
+    """Define the connectivity of a graph of nodes, links, and patches."""
+
     def __init__(self, nodes, links=None, patches=None):
         """Define a graph of connected nodes.
 
-        Examples
-        --------
-        >>> from landlab.graph import Graph
-
-        >>> node_x, node_y = [0, 1, 2, 0, 1, 2], [0, 0, 0, 1, 1, 1]
-        >>> graph = Graph((node_y, node_x))
-        >>> graph.x_of_node
-        array([0, 1, 2, 0, 1, 2])
-        >>> graph.y_of_node
-        array([0, 0, 0, 1, 1, 1])
-
-        >>> links = ((0, 1), (1, 2),
-        ...          (0, 3), (1, 4), (2, 5),
-        ...          (3, 4), (4, 5),
-        ...          (3, 6), (4, 7), (5, 8),
-        ...          (6, 7), (7, 8))
-        >>> graph = Graph((node_y, node_x), links=links)
-        >>> graph.nodes_at_link # doctest: +NORMALIZE_WHITESPACE
-        array([[0, 1], [1, 2],
-               [0, 3], [1, 4], [2, 5],
-               [3, 4], [4, 5],
-               [3, 6], [4, 7], [5, 8],
-               [6, 7], [7, 8]])
-        >>> graph.node_at_link_head
-        array([1, 2, 3, 4, 5, 4, 5, 6, 7, 8, 7, 8])
-        >>> graph.node_at_link_tail
-        array([0, 1, 0, 1, 2, 3, 4, 3, 4, 5, 6, 7])
-
-        >>> graph.links_at_node # doctest: +NORMALIZE_WHITESPACE
-        array([[ 0,  2, -1, -1], [ 0,  1,  3, -1], [ 1,  4, -1, -1],
-               [ 2,  5,  7, -1], [ 3,  5,  6,  8], [ 4,  6,  9, -1]])
-
-        >>> graph.link_dirs_at_node # doctest: +NORMALIZE_WHITESPACE
-        array([[-1, -1,  0,  0], [ 1, -1, -1,  0], [ 1, -1,  0,  0],
-               [ 1, -1, -1,  0], [ 1,  1, -1, -1], [ 1,  1, -1,  0]])
-
-        >>> patches = ((0, 3, 5, 2), (1, 4, 6, 3))
-        >>> graph = Graph((node_y, node_x), links=links, patches=patches)
-        >>> graph.links_at_patch
-        array([[0, 3, 5, 2],
-               [1, 4, 6, 3]])
-        >>> graph.nodes_at_patch
-        array([[0, 1, 3, 4],
-               [1, 2, 4, 5]])
+        Parameters
+        ----------
+        nodes : tuple of array_like
+            Coordinates of nodes as (*y*, *x*).
+        links : array_like of tuple
+            Tail node and head node for each link in the graph.
+        patches : array_like of tuple
+            Links that define each patch.
         """
         if len(nodes[0]) != len(nodes[1]):
             raise ValueError('length mismatch in node coordinates')
@@ -432,3 +328,120 @@ class Graph(object):
                [ 1, -1, -1,  0], [ 1,  1, -1, -1], [ 1,  1, -1,  0]])
         """
         return self._link_dirs_at_node
+
+
+def _find_links_at_node(node, nodes_at_link):
+    """Find links and link directions at a node.
+
+    Examples
+    --------
+    >>> nodes_at_link = ((0, 1), (1, 2),
+    ...                  (0, 3), (1, 4), (2, 5),
+    ...                  (3, 4), (4, 5),
+    ...                  (3, 6), (4, 7), (5, 8),
+    ...                  (6, 7), (7, 8))
+
+    The first node has only two links, both of which are directed outward.
+
+    >>> (links, dirs) = _find_links_at_node(0, nodes_at_link)
+    >>> links
+    array([0, 2])
+    >>> dirs
+    array([-1, -1])
+
+    The fourth node has two links entering and two link leaving.
+
+    >>> (links, dirs) = _find_links_at_node(4, nodes_at_link)
+    >>> links
+    array([3, 5, 6, 8])
+    >>> dirs
+    array([ 1,  1, -1, -1])
+    """
+    nodes_at_link = np.asarray(nodes_at_link)
+    (links_at_node, is_inward) = np.where(nodes_at_link == node)
+
+    link_dirs_at_node = np.choose(is_inward, (-1, 1))
+
+    return links_at_node, link_dirs_at_node
+
+
+def _setup_links_at_node(nodes_at_link, number_of_nodes=None):
+    """Set up data structures for node-to-link connectivity.
+
+    Parameters
+    ----------
+    nodes_at_link: ndarray of int
+        Nodes at either end of a link (tail node, then head node).
+    number_of_nodes: int, optional
+        The total number of nodes. If not given, use the largest node in
+        *nodes_at_link*.
+
+    Returns
+    -------
+    tuple of ndarray
+        Tuple of *links_at_node* and *link_dirs_at_node*.
+    """
+    node_count = np.bincount(nodes_at_link.flat)
+    number_of_nodes = number_of_nodes or len(node_count)
+
+    max_node_count = np.max(node_count)
+
+    link_dirs_at_node = np.full((number_of_nodes, max_node_count), 0,
+                                dtype=int)
+    links_at_node = np.full((number_of_nodes, max_node_count), -1, dtype=int)
+
+    for node in range(number_of_nodes):
+        links, dirs = _find_links_at_node(node, nodes_at_link)
+        link_dirs_at_node[node, :len(dirs)] = dirs
+        links_at_node[node, :len(links)] = links
+
+    return links_at_node, link_dirs_at_node
+
+
+def _setup_links_at_patch(patches):
+    """Set up data structure that describes link-patch connectivity.
+
+    Parameters
+    ----------
+    patches: iterable of iterables
+        List of links for each patch.
+
+    Returns
+    -------
+    ndarray
+        Links for each patch.
+    """
+    max_links_per_patch = np.max([len(links) for links in patches])
+    links_at_patch = np.full((len(patches), max_links_per_patch), -1,
+                             dtype=int)
+
+    for patch, links in enumerate(patches):
+        links_at_patch[patch, :len(links)] = links
+
+    return links_at_patch
+
+
+def _setup_nodes_at_patch(links_at_patch, nodes_at_link):
+    """Set up data structure that describes node-patch connectivity.
+
+    Parameters
+    ----------
+    links_at_patch: ndarray
+        Links that define each patch.
+    nodes_at_link: ndarray
+        Nodes that define each link.
+
+    Returns
+    -------
+    ndarray
+        Nodes that define each patch.
+    """
+    nodes_at_patch = np.full(links_at_patch.shape, -1, dtype=int)
+
+    for patch, links in enumerate(links_at_patch):
+        unique_nodes = np.unique(nodes_at_link[links])
+        nodes_at_patch[patch, :len(unique_nodes)] = unique_nodes
+
+    return nodes_at_patch
+
+
