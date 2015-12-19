ef4c5aae3e2181676d80ffa68ae4814a69ca3463
mcflugen <mcflugen@gmail.com>
2015-12-19 13:36:46 -0700

    Added GroupSizeError.

--- landlab/field/grouped.py
+++ landlab/field/grouped.py
@@ -22,6 +22,22 @@ class GroupError(Error, KeyError):
         return self._group
 
 
+class GroupSizeError(Error, KeyError):
+
+    """Raise this error if a group has changed sizes."""
+
+    def __init__(self, group, old_size, new_size):
+        self._group = group
+        self._old_size = old_size
+        self._new_size = new_size
+
+    def __str__(self):
+        return ("number of {group} elements has changed. "
+                "(was = {was}, now={now})".format(group=self._group,
+                                                  was=self._old_size,
+                                                  now=self._new_size))
+
+
 class ModelDataFields(object):
 
     """Collection of grouped data-fields.
