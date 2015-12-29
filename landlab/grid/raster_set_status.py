5d44cc0eaf7eec1dcabacc52405dc1d214a04106
mcflugen <mcflugen@gmail.com>
2015-12-29 09:01:26 -0700

    Added keywords for the status of each edge.

--- landlab/grid/raster_set_status.py
+++ landlab/grid/raster_set_status.py
@@ -1,13 +1,19 @@
-def set_status_at_node_on_edges(grid, status_at_edge=None):
+def set_status_at_node_on_edges(grid, right=None, top=None, left=None,
+                                bottom=None):
     """Set node status on grid edges.
 
     Parameters
     ----------
     grid : RasterModelGrid
         A grid.
-    status_at_edge : dict_like
-        Status of grid edges. This can be either a dict (or an OrderedDict)
-        or a list of tuples.
+    right : int, optional
+        Node status along right edge.
+    top : int, optional
+        Node status along top edge.
+    left : int, optional
+        Node status along left edge.
+    bottom : int, optional
+        Node status along bottom edge.
 
     Examples
     --------
@@ -19,8 +25,7 @@ def set_status_at_node_on_edges(grid, status_at_edge=None):
            1, 0, 0, 1,
            1, 1, 1, 1], dtype=int8)
 
-    >>> grid.set_status_at_node_on_edges(
-    ...     status_at_edge=dict(right=CLOSED_BOUNDARY))
+    >>> grid.set_status_at_node_on_edges(right=CLOSED_BOUNDARY)
     >>> grid.status_at_node # doctest: +NORMALIZE_WHITESPACE
     array([1, 1, 1, 4,
            1, 0, 0, 4,
@@ -29,36 +34,44 @@ def set_status_at_node_on_edges(grid, status_at_edge=None):
     >>> from landlab import FIXED_GRADIENT_BOUNDARY
     >>> grid = RasterModelGrid((3, 4))
 
-    The statuses are set in the order they are provided in the *status_at_edge*
-    keyword. This is useful for status values on corners. If two edges
-    share a corner, it is the value of the last edge set that gives the value
-    of the corner node.
+    The status of a corner is set along with its clockwise edge. That is,
+    if setting the status for the top and right edges, the upper-right corner
+    has the status of the right edge.
 
-    >>> status_at_edge = [('left', CLOSED_BOUNDARY),
-    ...                   ('bottom', FIXED_GRADIENT_BOUNDARY)]
-    >>> grid.set_status_at_node_on_edges(status_at_edge=status_at_edge)
+    >>> grid.set_status_at_node_on_edges(top=CLOSED_BOUNDARY,
+    ...     right=FIXED_GRADIENT_BOUNDARY)
     >>> grid.status_at_node # doctest: +NORMALIZE_WHITESPACE
-    array([2, 2, 2, 2,
-           4, 0, 0, 1,
-           4, 1, 1, 1], dtype=int8)
+    array([1, 1, 1, 2,
+           1, 0, 0, 2,
+           4, 4, 4, 2], dtype=int8)
 
-    In this case we set the left edge last, so the bottom left corner is
-    assigned the value for the left edge.
+    In the above example, if you wanted the corner to have the status of the
+    top edge, you need to make two calls to `set_status_at_node_on_edges`,
 
-    >>> status_at_edge = [('bottom', FIXED_GRADIENT_BOUNDARY),
-    ...                   ('left', CLOSED_BOUNDARY)]
-    >>> grid.set_status_at_node_on_edges(status_at_edge=status_at_edge)
+    >>> grid = RasterModelGrid((3, 4))
+    >>> grid.set_status_at_node_on_edges(right=FIXED_GRADIENT_BOUNDARY)
+    >>> grid.set_status_at_node_on_edges(top=CLOSED_BOUNDARY)
     >>> grid.status_at_node # doctest: +NORMALIZE_WHITESPACE
-    array([4, 2, 2, 2,
-           4, 0, 0, 1,
-           4, 1, 1, 1], dtype=int8)
+    array([1, 1, 1, 2,
+           1, 0, 0, 2,
+           4, 4, 4, 4], dtype=int8)
+
+    An example that sets all of the edges shows how corners are set.
+
+    >>> grid.set_status_at_node_on_edges(right=1, top=2, left=3, bottom=4)
+    >>> grid.status_at_node # doctest: +NORMALIZE_WHITESPACE
+    array([3, 4, 4, 4,
+           3, 0, 0, 1,
+           2, 2, 2, 1], dtype=int8)
     """
-    status_at_edge = status_at_edge or []
-    try:
-        status_at_edge = status_at_edge.items()
-    except AttributeError:
-        pass
+    status_at_edge = (('bottom', bottom), ('left', left), ('top', top),
+                      ('right', right), )
 
     for edge, val in status_at_edge:
-        nodes = grid.nodes_at_edge(edge)
-        grid.status_at_node[nodes] = val
+        if val is not None:
+            nodes = grid.nodes_at_edge(edge)
+            grid.status_at_node[nodes] = val
+
+    if right is not None and bottom is not None:
+        lr = grid.nodes_at_right_edge[0]
+        grid.status_at_node[lr] = bottom
