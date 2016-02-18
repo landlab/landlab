621ecfd310b0cb2c081ed46130230a61a0b0c3a2
mcflugen <mcflugen@gmail.com>
2016-02-18 15:16:07 -0700

    Removed usused import_landlab_component function.

--- landlab/bmi/components.py
+++ landlab/bmi/components.py
@@ -1,23 +1,8 @@
 import sys
-import importlib
 import warnings
 
 from .bmi_bridge import wrap_as_bmi
 from ..components import COMPONENTS
-
-
-def import_landlab_component(mod_name, cls_name):
-    try:
-        landlab_module = importlib.import_module(mod_name)
-    except ImportError:
-        warnings.warn('unable to import {mod}'.format(mod=mod_name))
-        raise
-    else:
-        obj = landlab_module.__dict__[cls_name]
-        as_bmi = wrap_as_bmi(obj)
-        return as_bmi.__name__, as_bmi
-        # setattr(sys.modules[__name__], as_bmi.__name__, as_bmi)
-        # return as_bmi.__name__
 
 
 __all__ = []
