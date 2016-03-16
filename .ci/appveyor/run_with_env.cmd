66220eb3963d4edc3248cd4c3023a240281b46a5
mcflugen <mcflugen@gmail.com>
2016-03-16 12:47:55 -0600

    Changed SDK version to 7.0.

--- .ci/appveyor/run_with_env.cmd
+++ .ci/appveyor/run_with_env.cmd
@@ -6,21 +6,12 @@
 :: More details at:
 :: https://github.com/cython/cython/wiki/64BitCythonExtensionsOnWindows
 ::
-IF %MAJOR_PYTHON_VERSION% == "2" (
-    SET WINDOWS_SDK_VERSION="v7.0"
-) ELSE IF %MAJOR_PYTHON_VERSION% == "3" (
-    SET WINDOWS_SDK_VERSION="v7.1"
-) ELSE (
-    ECHO Unsupported Python version: "%MAJOR_PYTHON_VERSION%"
-    EXIT 1
-)
-
 IF "%DISTUTILS_USE_SDK%"=="1" (
     IF "%PLATFORM%"=="x64" (
         ECHO Configuring environment to build with MSVC on a 64bit architecture
-        ECHO Using Windows SDK %WINDOWS_SDK_VERSION%
-        "C:\Program Files\Microsoft SDKs\Windows\%WINDOWS_SDK_VERSION%\Setup\WindowsSdkVer.exe" -q -version:%WINDOWS_SDK_VERSION%
-        CALL "C:\Program Files\Microsoft SDKs\Windows\%WINDOWS_SDK_VERSION%\Bin\SetEnv.cmd" /x64 /release
+        ECHO Using Windows SDK v7.0
+        "C:\Program Files\Microsoft SDKs\Windows\v7.0\Setup\WindowsSdkVer.exe" -q -version:v7.0
+        CALL "C:\Program Files\Microsoft SDKs\Windows\v7.0\Bin\SetEnv.cmd" /x64 /release
         SET MSSdk=1
         REM Need the following to allow tox to see the SDK compiler
         SET TOX_TESTENV_PASSENV=DISTUTILS_USE_SDK MSSdk INCLUDE LIB
