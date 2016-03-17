b4406fdaf48cc018422837ef21fb4fdc0b6ab8ac
mcflugen <mcflugen@gmail.com>
2016-03-17 08:47:41 -0600

    Changed to separate run command for each case.

--- .ci/appveyor/run_with_env.cmd
+++ .ci/appveyor/run_with_env.cmd
@@ -22,16 +22,15 @@ IF "%DISTUTILS_USE_SDK%"=="1" (
         SET MSSdk=1
         "%WIN_SDK_ROOT%\%WINDOWS_SDK_VERSION%\Setup\WindowsSdkVer.exe" -q -version:%WINDOWS_SDK_VERSION%
         "%WIN_SDK_ROOT%\%WINDOWS_SDK_VERSION%\Bin\SetEnv.cmd" /x64 /release
-        :: ECHO Executing: %COMMAND_TO_RUN%
-        :: call %COMMAND_TO_RUN% || EXIT 1
+        ECHO Executing: %COMMAND_TO_RUN%
+        call %COMMAND_TO_RUN% || EXIT 1
     ) ELSE (
         ECHO Using default MSVC build environment for 32 bit architecture
-        :: ECHO Executing: %COMMAND_TO_RUN%
-        :: call %COMMAND_TO_RUN% || EXIT 1
+        ECHO Executing: %COMMAND_TO_RUN%
+        call %COMMAND_TO_RUN% || EXIT 1
     )
 ) ELSE (
     ECHO Using default MSVC build environment
+    ECHO Executing: %COMMAND_TO_RUN%
+    call %COMMAND_TO_RUN% || EXIT 1
 )
-
-ECHO Executing: %COMMAND_TO_RUN%
-call %COMMAND_TO_RUN% || EXIT 1
