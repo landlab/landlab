fa7a5faec005123ada522e08d5ad40ab2e02878a
mcflugen <mcflugen@gmail.com>
2016-04-19 13:28:16 -0600

    Added get_area_of_patch function.

--- landlab/graph/quantity/of_patch.py
+++ landlab/graph/quantity/of_patch.py
@@ -14,3 +14,13 @@ def get_center_of_patch(graph, out=None):
 
     return out
 
+
+def get_area_of_patch(graph, out=None):
+    from .ext.of_patch import calc_area_of_patch
+
+    if out is None:
+        out = np.empty(n_patches, dtype=float)
+
+    calc_area_of_patch(graph.nodes_at_patch, out)
+
+    return out
