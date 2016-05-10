method kwd; correct sign error
1. Add method={‘patch_mean’, ‘Horn’} to mg.calc_slope_of_node().
2. Correct a sign error in the slope direction where specified; it
should correspond the gradient, not the surface dip direction.
3. Better docs.

landlab/grid/base.py
@@ -2480,7 +2480,7 @@ class ModelGrid(ModelDataFieldsMixIn):
         >>> mg = RasterModelGrid((4, 5))
         >>> z = mg.node_y
         >>> (x_grad, y_grad) = mg.calc_grad_of_patch(elevs=z)
-        >>> np.allclose(y_grad, -np.pi/4.)
+        >>> np.allclose(y_grad, np.pi/4.)
         True
         >>> np.allclose(x_grad, 0.)
         True
@@ -2496,14 +2496,15 @@ class ModelGrid(ModelDataFieldsMixIn):
         else:
             slopes_at_patch = self.calc_slope_of_patch(elevs=elevs,
                                                        unit_normal=nhat)
-        theta = numpy.arctan2(nhat[:, 1], nhat[:, 0])
+        theta = numpy.arctan2(-nhat[:, 1], -nhat[:, 0])
         x_slope_patches = numpy.cos(theta)*slopes_at_patch
         y_slope_patches = numpy.sin(theta)*slopes_at_patch
 
         return (x_slope_patches, y_slope_patches)
 
     def calc_slope_of_node(self, elevs='topographic__elevation',
-                           return_components=False):
+                           method='patch_mean',
+                           return_components=False, **kwds):
         """Array of slopes at nodes, averaged over neighboring patches.
 
         Produces a value for node slope (i.e., mean gradient magnitude)
@@ -2523,6 +2524,9 @@ class ModelGrid(ModelDataFieldsMixIn):
         ----------
         elevs : str or ndarray, optional
             Field name or array of node values.
+        method : {'patch_mean', 'Horn'}
+            By equivalence to the raster version, 'patch_mean' returns a scalar
+            mean on the patches; 'Horn' returns a vector mean on the patches.
         return_components : bool
             If True, return a tuple, (array_of_magnitude,
             (array_of_slope_x_radians, array_of_slope_y_radians)).
@@ -2545,7 +2549,7 @@ class ModelGrid(ModelDataFieldsMixIn):
         >>> numpy.allclose(slopes, 45./180.*numpy.pi)
         True
         >>> mg = RasterModelGrid((4, 5), 1.)
-        >>> z = mg.node_y
+        >>> z = -mg.node_y
         >>> slope_mag, cmp = mg.calc_slope_of_node(elevs=z,
         ...                                        return_components=True)
         >>> numpy.allclose(slope_mag, numpy.pi/4.)
@@ -2570,6 +2574,7 @@ class ModelGrid(ModelDataFieldsMixIn):
         >>> numpy.allclose(mean_ring_slope, target_mean_ring_slope)
         True
         """
+        assert method in ('patch_mean', 'Horn')
         try:
             patches_at_node = self.patches_at_node()
         except TypeError:  # was a property, not a fn (=> new style)
@@ -2586,7 +2591,7 @@ class ModelGrid(ModelDataFieldsMixIn):
                                             mask=patches_at_node.mask)
         slope_mag = np.mean(slopes_at_node_masked, axis=1).data
 
-        if return_components:
+        if return_components or method == 'Horn':
             (x_slope_patches, y_slope_patches) = self.calc_grad_of_patch(
                 elevs=elevs, unit_normal=nhat,
                 slope_magnitude=slopes_at_patch)
@@ -2601,13 +2606,18 @@ class ModelGrid(ModelDataFieldsMixIn):
             mean_grad_x = x_slope
             mean_grad_y = y_slope
 
-            return slope_mag, (mean_grad_x, mean_grad_y)
+            if method == 'Horn':
+                slope_mag = np.arctan(numpy.sqrt(numpy.tan(y_slope_masked)**2 +
+                                                 numpy.tan(x_slope_masked)**2))
+                return slope_mag
+            else:
+                return slope_mag, (mean_grad_x, mean_grad_y)
 
         else:
             return slope_mag
 
     def calc_aspect_of_node(self, slope_component_tuple=None,
-                    elevs='topographic__elevation', unit='degrees'):
+                            elevs='topographic__elevation', unit='degrees'):
         """Get array of aspect of a surface.
 
         Calculates at returns the aspect of a surface. Aspect is returned as
@@ -2615,12 +2625,16 @@ class ModelGrid(ModelDataFieldsMixIn):
         'degrees'.
 
         If slope_component_tuple is provided, i.e., (slope_x, slope_y), the
-        aspect will be calculated from these data.
+        aspect will be calculated from these data. Remember, aspect points
+        180 degrees away from the slope direction as under Landlab conventions,
+        slope is defined as UPSLOPE direction (i.e., positive gradient),
+        but aspect is the surface dip direction.
 
         If it is not, it will be derived from elevation data at the nodes,
         which can either be a string referring to a grid field (default:
         'topographic__elevation'), or an nnodes-long numpy array of the
-        values themselves.
+        values themselves. The slope will be taken as the mean gradient of the
+        surrounding patches.
 
         Parameters
         ----------
@@ -2674,7 +2688,7 @@ class ModelGrid(ModelDataFieldsMixIn):
             _, slope_component_tuple = self.calc_slope_of_node(
                 elevs=elev_array, return_components=True)
         angle_from_x_ccw = numpy.arctan2(
-            slope_component_tuple[1], slope_component_tuple[0])
+            -slope_component_tuple[1], -slope_component_tuple[0])
         # angle_from_N_cw = ((angle_from_x_ccw + numpy.pi / 2.) % (
         #     2 * numpy.pi))
         angle_from_N_cw = (5.*numpy.pi/2. - angle_from_x_ccw) % (2.*numpy.pi)
@@ -2686,7 +2700,7 @@ class ModelGrid(ModelDataFieldsMixIn):
             raise TypeError("unit must be 'degrees' or 'radians'")
 
     def calc_hillshade_of_node(self, alt=45., az=315., slp=None, asp=None,
-                       unit='degrees', elevs='topographic__elevation'):
+                               unit='degrees', elevs='topographic__elevation'):
         """Get array of hillshade.
 
         .. codeauthor:: Katy Barnhart <katherine.barnhart@colorado.edu>
@@ -2696,7 +2710,7 @@ class ModelGrid(ModelDataFieldsMixIn):
         alt : float
             Sun altitude (from horizon) - defaults to 45 degrees
         az : float
-            Sun azimuth (from north) - defaults to 315 degrees
+            Sun azimuth (CW from north) - defaults to 315 degrees
         slp : float
             slope of cells at surface - optional
         asp : float
@@ -2729,13 +2743,14 @@ class ModelGrid(ModelDataFieldsMixIn):
 
         Examples
         --------
+        >>> import numpy as np
         >>> from landlab import RasterModelGrid
         >>> mg = RasterModelGrid((5, 5), 1.)
         >>> z = 6. - ((mg.node_x-2.)**2 + (mg.node_y-2.)**2)
-        >>> mg.calc_hillshade_of_node(elevs=z) # doctest: +NORMALIZE_WHITESPACE
-        array([ 0.16222142,  0.03572257, -0.26353058, -0.4766685 , -0.52602578,
-                0.33996228,  0.25232065, -0.13335582, -0.4082354 , -0.4766685 ,
-                0.68993201,  0.76230631,  0.2256741 , -0.13335582, -0.26353058,
+        >>> mg.calc_hillshade_of_node(elevs=z)
+        array([ 0.16222142,  0.03572257,  0.        ,  0.        ,  0.        ,
+                0.33996228,  0.25232065,  0.        ,  0.        ,  0.        ,
+                0.68993201,  0.76230631,  0.95597085,  0.        ,  0.        ,
                 0.85235335,  0.9128767 ,  0.76230631,  0.25232065,  0.03572257,
                 0.85046862,  0.85235335,  0.68993201,  0.33996228,  0.16222142])
         """
@@ -2763,7 +2778,7 @@ class ModelGrid(ModelDataFieldsMixIn):
             slp, slp_comps = self.calc_slope_of_node(
                 elevs, return_components=True)
             asp = self.calc_aspect_of_node(slope_component_tuple=slp_comps,
-                                   unit='radians')
+                                           unit='radians')
         else:
             raise TypeError('Either both slp and asp must be set, or neither!')
 
@@ -2772,7 +2787,7 @@ class ModelGrid(ModelDataFieldsMixIn):
             numpy.cos(alt) * numpy.sin(slp) * numpy.cos(az - asp)
         )
 
-        return shaded
+        return shaded.clip(0.)
 
     @deprecated(use='calc_flux_div_at_node', version=1.0)
     def calculate_flux_divergence_at_core_nodes(self, active_link_flux,

landlab/grid/raster.py
@@ -3930,7 +3930,7 @@ XXXXXXeric should be killing this with graphs.
         Returns
         -------
         slopes_at_patch : n_patches-long array
-            The slope (positive gradient magnitude) of each patch.
+            The slope (positive gradient magnitude) of each patch, in radians.
 
         Examples
         --------
@@ -3977,7 +3977,8 @@ XXXXXXeric should be killing this with graphs.
                            slope_magnitude=None):
         """Calculate the components of the gradient of each raster patch.
 
-        Returns the mean gradient of the four possible patch subtriangles.
+        Returns the mean gradient of the four possible patch subtriangles,
+        in radians.
 
         Parameters
         ----------
@@ -3994,7 +3995,7 @@ XXXXXXeric should be killing this with graphs.
         -------
         gradient_tuple : (x_component_at_patch, y_component_at_patch)
             Len-2 tuple of arrays giving components of gradient in the x and y
-            directions, in the units of *units*.
+            directions, in the units of *radians*.
 
         Examples
         --------
@@ -4003,7 +4004,7 @@ XXXXXXeric should be killing this with graphs.
         >>> mg = RasterModelGrid((4, 5))
         >>> z = mg.node_y
         >>> (x_grad, y_grad) = mg.calc_grad_of_patch(elevs=z)
-        >>> np.allclose(y_grad, -np.pi/4.)
+        >>> np.allclose(y_grad, np.pi/4.)
         True
         >>> np.allclose(x_grad, 0.)
         True
@@ -4027,28 +4028,29 @@ XXXXXXeric should be killing this with graphs.
 
         n_sum_x = n_TR[:, 0] + n_TL[:, 0] + n_BL[:, 0] + n_BR[:, 0]
         n_sum_y = n_TR[:, 1] + n_TL[:, 1] + n_BL[:, 1] + n_BR[:, 1]
-        theta_sum = np.arctan2(n_sum_y, n_sum_x)
+        theta_sum = np.arctan2(-n_sum_y, -n_sum_x)
         x_slope_patches = np.cos(theta_sum)*slopes_at_patch
         y_slope_patches = np.sin(theta_sum)*slopes_at_patch
 
         return (x_slope_patches, y_slope_patches)
 
     def calc_slope_of_node(self, elevs='topographic__elevation',
-                           return_components=False):
+                           method='patch_mean', return_components=False):
         """Array of slopes at nodes, averaged over neighboring patches.
 
         Produces a value for node slope (i.e., mean gradient magnitude)
         at each node in a manner analogous to a GIS-style slope map.
-        It averages the gradient on each of the
-        patches surrounding the node, creating a value for node slope that
-        better incorporates nonlocal elevation information. Directional
-        information can still be returned through use of the return_components
-        keyword.
+        If method=='patch_mean', it averages the gradient on each of the
+        patches surrounding the node; if method=='Horn', it returns the
+        resolved slope direction. Directional information can still be
+        returned through use of the return_components keyword.
+        All values are returned in radians, including the components;
+        take the tan to recover the rise/run.
 
         Note that under these definitions, it is not always true that::
 
             mag, cmp = mg.calc_slope_of_node(z)
-            mag**2 == cmp[0]**2 + cmp[1]**2  # not always true
+            mag**2 == cmp[0]**2 + cmp[1]**2  # only if method=='Horn'
 
         This is a verion of this code specialized for a raster. It subdivides
         the four square patches around each node into subtriangles,
@@ -4059,6 +4061,12 @@ XXXXXXeric should be killing this with graphs.
         ----------
         elevs : str or ndarray, optional
             Field name or array of node values.
+        method : {'patch_mean', 'Horn'}
+            Controls the slope algorithm. Current options are 'patch_mean',
+            which takes the mean slope of each pf the four neighboring
+            square patches, and 'Horn', which is the standard ArcGIS slope
+            algorithm. These produce very similar solutions; the Horn method
+            gives a vector mean and the patch_mean gives a scalar mean.
         return_components : bool
             If True, return a tuple, (array_of_magnitude,
             (array_of_slope_x_radians, array_of_slope_y_radians)).
@@ -4081,7 +4089,7 @@ XXXXXXeric should be killing this with graphs.
         >>> numpy.allclose(slopes, numpy.pi/4.)
         True
         >>> mg = RasterModelGrid((4, 5), 2.)
-        >>> z = mg.node_y
+        >>> z = -mg.node_y
         >>> slope_mag, cmp = mg.calc_slope_of_node(elevs=z,
         ...                                        return_components=True)
         >>> numpy.allclose(slope_mag, np.pi/4.)
@@ -4102,38 +4110,89 @@ XXXXXXeric should be killing this with graphs.
         ...             cmp[1].reshape((4, 4))[0, :])  # test radial symmetry
         True
         """
+        assert method in {'patch_mean', 'Horn'}
         try:
             patches_at_node = self.patches_at_node()
         except TypeError:  # was a property, not a fn (=> new style)
             patches_at_node = np.ma.masked_where(
-                self.patches_at_node == -1, self.patches_at_node, copy=False)
-        n_TR, n_TL, n_BL, n_BR = \
-            self.calc_unit_normals_of_patch_subtriangles(elevs)
+                self.patches_at_node == -1, self.patches_at_node,
+                copy=False)
+        # now, we also want to mask any "closed" patches (any node closed)
+        closed_patches = (self.status_at_node[self.nodes_at_patch] ==
+                          CLOSED_BOUNDARY).sum(axis=1) > 0
+        closed_patch_mask = np.logical_or(
+            patches_at_node.mask, closed_patches[patches_at_node.data])
 
-        mean_slope_at_patches = self.calc_slope_of_patch(
-            elevs=elevs, subtriangle_unit_normals=(n_TR, n_TL, n_BL, n_BR))
+        if method == 'patch_mean':
+            n_TR, n_TL, n_BL, n_BR = \
+                self.calc_unit_normals_of_patch_subtriangles(elevs)
 
-        # now CAREFUL - patches_at_node is MASKED
-        slopes_at_node_unmasked = mean_slope_at_patches[patches_at_node]
-        slopes_at_node_masked = np.ma.array(slopes_at_node_unmasked,
-                                            mask=patches_at_node.mask)
-        slope_mag = np.mean(slopes_at_node_masked, axis=1).data
+            mean_slope_at_patches = self.calc_slope_of_patch(
+                elevs=elevs, subtriangle_unit_normals=(n_TR, n_TL, n_BL, n_BR))
+
+            # now CAREFUL - patches_at_node is MASKED
+            slopes_at_node_unmasked = mean_slope_at_patches[patches_at_node]
+            slopes_at_node_masked = np.ma.array(slopes_at_node_unmasked,
+                                                mask=closed_patch_mask)
+            slope_mag = np.mean(slopes_at_node_masked, axis=1).data
+            if return_components:
+                (x_slope_patches, y_slope_patches) = self.calc_grad_of_patch(
+                    elevs=elevs, subtriangle_unit_normals=(
+                        n_TR, n_TL, n_BL, n_BR),
+                    slope_magnitude=mean_slope_at_patches)
+                x_slope_unmasked = x_slope_patches[patches_at_node]
+                x_slope_masked = np.ma.array(x_slope_unmasked,
+                                             mask=closed_patch_mask)
+                x_slope = np.mean(x_slope_masked, axis=1).data
+                y_slope_unmasked = y_slope_patches[patches_at_node]
+                y_slope_masked = np.ma.array(y_slope_unmasked,
+                                             mask=closed_patch_mask)
+                y_slope = np.mean(y_slope_masked, axis=1).data
+                mean_grad_x = x_slope
+                mean_grad_y = y_slope
+        elif method == 'Horn':
+            z = np.empty(self.number_of_nodes + 1, dtype=float)
+            mean_grad_x = self.empty('node', dtype=float)
+            mean_grad_y = self.empty('node', dtype=float)
+            z[-1] = 0.
+            try:
+                z[:-1] = self.at_node[elevs]
+            except TypeError:
+                z[:-1] = elevs
+            # proof code for bad indexing:
+            diags = self.diagonal_neighbors_at_node.copy()  # LL order
+            orthos = self.neighbors_at_node.copy()
+            # these have closed node neighbors...
+            for dirs in (diags, orthos):
+                dirs[dirs == BAD_INDEX_VALUE] = -1  # indexing to work
+            # now make an array like patches_at_node to store the interim calcs
+            patch_slopes_x = np.ma.zeros(patches_at_node.shape, dtype=float)
+            patch_slopes_y = np.ma.zeros(patches_at_node.shape, dtype=float)
+            diff_E = z[orthos[:, 0]] - z[:-1]
+            diff_W = z[:-1] - z[orthos[:, 2]]
+            diff_N = z[orthos[:, 1]] - z[:-1]
+            diff_S = z[:-1] - z[orthos[:, 3]]
+            patch_slopes_x[:, 0] = z[diags[:, 0]] - z[orthos[:, 1]] + diff_E
+            patch_slopes_x[:, 1] = z[orthos[:, 1]] - z[diags[:, 1]] + diff_W
+            patch_slopes_x[:, 2] = z[orthos[:, 3]] - z[diags[:, 2]] + diff_W
+            patch_slopes_x[:, 3] = z[diags[:, 3]] - z[orthos[:, 3]] + diff_E
+            patch_slopes_y[:, 0] = z[diags[:, 0]] - z[orthos[:, 0]] + diff_N
+            patch_slopes_y[:, 1] = z[diags[:, 1]] - z[orthos[:, 2]] + diff_N
+            patch_slopes_y[:, 2] = z[orthos[:, 2]] - z[diags[:, 2]] + diff_S
+            patch_slopes_y[:, 3] = z[orthos[:, 0]] - z[diags[:, 3]] + diff_S
+            patch_slopes_x /= (2. * self.dx)
+            patch_slopes_y /= (2. * self.dy)
+            patch_slopes_x.mask = closed_patch_mask
+            patch_slopes_y.mask = closed_patch_mask
+            mean_grad_x = patch_slopes_x.mean(axis=1).data
+            mean_grad_y = patch_slopes_y.mean(axis=1).data
+            slope_mag = np.arctan(np.sqrt(np.square(mean_grad_x) +
+                                          np.square(mean_grad_y)))
+            if return_components:
+                mean_grad_x = np.arctan(mean_grad_x)
+                mean_grad_y = np.arctan(mean_grad_y)
+
         if return_components:
-            (x_slope_patches, y_slope_patches) = self.calc_grad_of_patch(
-                elevs=elevs, subtriangle_unit_normals=(
-                    n_TR, n_TL, n_BL, n_BR),
-                slope_magnitude=mean_slope_at_patches)
-            x_slope_unmasked = x_slope_patches[patches_at_node]
-            x_slope_masked = np.ma.array(x_slope_unmasked,
-                                         mask=patches_at_node.mask)
-            x_slope = np.mean(x_slope_masked, axis=1).data
-            y_slope_unmasked = y_slope_patches[patches_at_node]
-            y_slope_masked = np.ma.array(y_slope_unmasked,
-                                         mask=patches_at_node.mask)
-            y_slope = np.mean(y_slope_masked, axis=1).data
-            mean_grad_x = x_slope
-            mean_grad_y = y_slope
-
             return slope_mag, (mean_grad_x, mean_grad_y)
 
         else: