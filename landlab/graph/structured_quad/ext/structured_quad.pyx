b5f246bf96d8fd7272f8821f2a648d33d6cdf46f
mcflugen <mcflugen@gmail.com>
2016-05-12 00:07:02 -0600

    Added get_nodes_at_face function.

--- landlab/graph/structured_quad/ext/structured_quad.pyx
+++ landlab/graph/structured_quad/ext/structured_quad.pyx
@@ -31,3 +31,42 @@ def get_node_at_cell(shape, np.ndarray[DTYPE_t, ndim=1] node_at_cell):
             node_at_cell[cell] = row_offset + col
             cell += 1
         row_offset += node_cols
+
+
+@cython.boundscheck(False)
+def get_nodes_at_face(shape, np.ndarray[DTYPE_t, ndim=2] nodes_at_face):
+    """Get nodes on either side of a face.
+
+    Parameters
+    ----------
+    shape : tuple of int
+        Shape of the grid as `(n_rows, n_cols)`.
+    nodes_at_face : ndarray of int, shape `(n_faces, 2)`
+        Buffer into which to place node identifiers.
+    """
+    cdef int face
+    cdef int node
+    cdef int row
+    cdef int col
+    cdef int n_rows = shape[0]
+    cdef int n_cols = shape[1]
+
+    # Horizontal faces first
+    face = 0
+    for row in range(n_rows - 1):
+        for col in range(1, n_cols - 1):
+            node = row * n_cols + col
+            nodes_at_face[face, 0] = node
+            nodes_at_face[face, 1] = node + n_cols
+            face += 1
+        face += n_cols - 1
+
+    # Vertical faces next
+    face = n_cols - 2
+    for row in range(1, n_rows - 1):
+        for col in range(n_cols - 1):
+            node = row * n_cols + col
+            nodes_at_face[face, 0] = node
+            nodes_at_face[face, 1] = node + 1
+            face += 1
+        face += n_cols - 2
