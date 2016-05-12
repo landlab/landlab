31f6d9f77780e6d0a8973b22a61d7fa41ca19375
mcflugen <mcflugen@gmail.com>
2016-05-11 23:55:35 -0600

    Added hex and radial graphs to import.

--- landlab/graph/__init__.py
+++ landlab/graph/__init__.py
@@ -1,9 +1,12 @@
 from .graph import Graph
 from .structured_quad import *
 from .voronoi import *
+from .hex import *
+from .radial import *
 
 
 __all__ = ['Graph', 'StructuredQuadGraph', 'RectilinearGraph',
            'UniformRectilinearGraph', 'DualUniformRectilinearGraph',
            'DualRectilinearGraph', 'DualStructuredQuadGraph', 'VoronoiGraph',
-           'DualVoronoiGraph', ]
+           'DualVoronoiGraph', 'HexGraph', 'DualHexGraph', 'RadialGraph',
+           'DualRadialGraph', ]
