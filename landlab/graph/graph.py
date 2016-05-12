e4392bbfda8cf6197203de0fa8666f0f8df4454d
mcflugen <mcflugen@gmail.com>
2016-05-11 23:56:37 -0600

    Changed to sort graph at the end of __init__.

--- landlab/graph/graph.py
+++ landlab/graph/graph.py
@@ -47,10 +47,10 @@ array([[ 5,  3,  0,  2],
        [10,  8,  5,  7],
        [11,  9,  6,  8]])
 >>> graph.nodes_at_patch
-array([[3, 4, 1, 0],
-       [4, 5, 2, 1],
-       [6, 7, 4, 3],
-       [7, 8, 5, 4]])
+array([[4, 1, 0, 3],
+       [5, 2, 1, 4],
+       [7, 4, 3, 6],
+       [8, 5, 4, 7]])
 """
 from six.moves import range
 
@@ -58,18 +58,20 @@ import numpy as np
 
 from ..core.utils import as_id_array, argsort_points_by_x_then_y
 from ..utils.jaggedarray import flatten_jagged_array
-from .sort import sort_graph
+from .sort import sort_graph, reindex_by_xy, reorder_links_at_patch
 from .object.at_node import get_links_at_node
 from .object.at_patch import get_nodes_at_patch
 from .quantity.of_link import get_angle_of_link
+
+from .sort.sort import reverse_one_to_many, reorient_link_dirs
 
 
 class Graph(object):
 
     """Define the connectivity of a graph of nodes, links, and patches."""
 
-    def __init__(self, nodes, links=None, patches=None, xy_sort=False,
-                 rot_sort=False):
+    def __init__(self, nodes, links=None, patches=None, xy_sort=True,
+                 rot_sort=True, sort_opts=None):
         """Define a graph of connected nodes.
 
         Parameters
@@ -90,18 +92,21 @@ class Graph(object):
         self._rot_sort = rot_sort
 
         nodes = [np.asarray(coord, dtype=float) for coord in nodes]
-        if links is not None:
-            links = np.asarray(links, dtype=int)
+        # if links is not None:
+        #     links = np.asarray(links, dtype=int)
 
         if len(nodes[0]) != len(nodes[1]):
             raise ValueError('length mismatch in node coordinates')
 
-        if xy_sort:
-            nodes, links, patches = sort_graph(nodes, links=links,
-                                               patches=patches)
-        else:
-            if patches is not None:
+        # if xy_sort:
+        #     nodes, links, patches = sort_graph(nodes, links=links,
+        #                                        patches=patches)
+        # else:
+        if patches is not None:
+            if len(patches) > 0:
                 patches = flatten_jagged_array(patches, dtype=int)
+            else:
+                patches = None
 
         self._y_of_node = nodes[0]
         self._x_of_node = nodes[1]
@@ -114,6 +119,15 @@ class Graph(object):
         if patches is not None:
             self._setup_patches(patches)
 
+        if xy_sort:
+            reindex_by_xy(self)
+
+        if rot_sort and patches is not None:
+            reorder_links_at_patch(self)
+
+        if links is not None:
+            reorient_link_dirs(self)
+
     def _setup_links(self, links):
         """Set up node-link data structures."""
         self._nodes_at_link = np.asarray(links, dtype=np.int)
@@ -124,11 +138,11 @@ class Graph(object):
         from .quantity.ext.of_link import calc_midpoint_of_link
         from .matrix.at_patch import links_at_patch
 
-        if self._rot_sort:
-            xy_of_link = np.empty((self.number_of_links, 2), dtype=float)
-            calc_midpoint_of_link(self.nodes_at_link, self.x_of_node,
-                                  self.y_of_node, xy_of_link)
-            reorder_links_at_patch(patches[0], patches[1], xy_of_link)
+        # if self._rot_sort:
+        #     xy_of_link = np.empty((self.number_of_links, 2), dtype=float)
+        #     calc_midpoint_of_link(self.nodes_at_link, self.x_of_node,
+        #                           self.y_of_node, xy_of_link)
+        #     reorder_links_at_patch(patches[0], patches[1], xy_of_link)
 
         self._links_at_patch = links_at_patch(patches,
                                               nodes_at_link=self.nodes_at_link)
@@ -318,14 +332,30 @@ class Graph(object):
         >>> patches = ((0, 3, 5, 2), (1, 4, 6, 3))
         >>> graph = Graph((node_y, node_x), links=links, patches=patches)
         >>> graph.nodes_at_patch
-        array([[0, 1, 4, 3],
-               [1, 2, 5, 4]])
+        array([[1, 4, 3, 0],
+               [2, 5, 4, 1]])
         """
         try:
             return self._nodes_at_patch
         except AttributeError:
             self._nodes_at_patch = get_nodes_at_patch(self)
             return self._nodes_at_patch
+
+    @property
+    def patches_at_node(self):
+        try:
+            return self._patches_at_node
+        except AttributeError:
+            self._patches_at_node = reverse_one_to_many(self._nodes_at_patch)
+            return self._patches_at_node
+
+    @property
+    def patches_at_link(self):
+        try:
+            return self._patches_at_link
+        except AttributeError:
+            self._patches_at_link = reverse_one_to_many(self._links_at_patch)
+            return self._patches_at_link
 
     @property
     def number_of_patches(self):
