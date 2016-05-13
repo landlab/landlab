423752f72b6d605fd68867a3150176e1e33e40d7
mcflugen <mcflugen@gmail.com>
2016-05-13 09:59:21 -0600

    Fixed minor PEP8 formatting.

--- landlab/components/radiation/radiation.py
+++ landlab/components/radiation/radiation.py
@@ -77,6 +77,7 @@ from landlab import Component
 from ...utils.decorators import use_file_name_or_kwds
 import numpy as np
 
+
 _VALID_METHODS = set(['Grid'])
 
 
@@ -86,7 +87,9 @@ def _assert_method_is_valid(method):
 
 
 class Radiation(Component):
-    """
+
+    """Compute 1D and 2D total incident shortwave radiation.
+
     Landlab component that computes 1D and 2D total incident shortwave
     radiation. This code also computes relative incidence shortwave radiation
     compared to a flat surface.
@@ -122,8 +125,8 @@ class Radiation(Component):
     >>> from landlab.components import Radiation
     >>> import numpy as np
 
-    >>> grid = RasterModelGrid( (5, 4), spacing=(0.2,0.2))
-    >>> rad = Radiation( grid )
+    >>> grid = RasterModelGrid((5, 4), spacing=(0.2, 0.2))
+    >>> rad = Radiation(grid)
     >>> rad.name
     'Radiation'
     >>> rad.input_var_names
@@ -252,7 +255,7 @@ class Radiation(Component):
 
         self._nodal_values = self.grid['node']
         self._cell_values = self.grid['cell']
-        self._slope, self._aspect = \
+        self._slope, self._aspect = 
             grid.calculate_slope_aspect_at_nodes_burrough(
                 vals='topographic__elevation')
 #        self._slope = grid.calc_slope_of_node( \
@@ -262,8 +265,7 @@ class Radiation(Component):
         self._cell_values['Aspect'] = self._aspect
 
     def update(self, current_time, hour=12., **kwds):
-        """
-        Update fields with current loading conditions.
+        """Update fields with current loading conditions.
 
         Parameters
         ----------
@@ -277,46 +279,46 @@ class Radiation(Component):
         self._Rs = self._cell_values['radiation__incoming_shortwave']
         self._Rnet = self._cell_values['radiation__net_shortwave']
 
-        self._julian = np.floor((current_time - np.floor(current_time)) *
-                                365.25)                     # Julian day
+        self._julian = np.floor(
+            (current_time - np.floor(current_time)) * 365.25) # Julian day
 
-        self._phi = np.radians(self._latitude)           # Latitude in Radians
+        self._phi = np.radians(self._latitude) # Latitude in Radians
 
-        self._delta = 23.45 * np.radians(np.cos(2*np.pi/365 * (
-            172 - self._julian)))                         # Declination angle
+        self._delta = 23.45 * np.radians(
+            np.cos(2*np.pi / 365 * (172 - self._julian))) # Declination angle
 
-        self._tau = (self._t + 12.0) * np.pi/12.0                # Hour angle
+        self._tau = (self._t + 12.0) * np.pi / 12.0 # Hour angle
 
         self._alpha = np.arcsin(np.sin(self._delta) * np.sin(self._phi) +
                                 np.cos(self._delta) * np.cos(self._phi) *
-                                np.cos(self._tau))           # Solar Altitude
+                                np.cos(self._tau)) # Solar Altitude
 
-        if self._alpha <= 0.25 * np.pi/180.0:         # If altitude is -ve,
-            self._alpha = 0.25 * np.pi/180.0       # sun is beyond the horizon
+        if self._alpha <= 0.25 * np.pi / 180.0: # If altitude is -ve,
+            self._alpha = 0.25 * np.pi / 180.0 # sun is beyond the horizon
 
-        self._Rgl = (self._Io*np.exp((-1) * self._n * (
-            0.128 - 0.054 * np.log10(1/np.sin(self._alpha)))*(
-                1/np.sin(self._alpha))))
+        self._Rgl = (self._Io * np.exp((-1) * self._n * (
+            0.128 - 0.054 * np.log10(1. / np.sin(self._alpha)))*(
+                1. / np.sin(self._alpha))))
         # Counting for Albedo, Cloudiness and Atmospheric turbidity
 
-        self._phisun = (np.arctan(-np.sin(self._tau)/(np.tan(self._delta) *
+        self._phisun = (np.arctan(- np.sin(self._tau) / (np.tan(self._delta) *
                         np.cos(self._phi) - np.sin(self._phi) *
-                        np.cos(self._tau))))  # Sun's Azhimuth
+                        np.cos(self._tau)))) # Sun's Azhimuth
 
-        if (self._phisun >= 0 and -np.sin(self._tau) <= 0):
+        if (self._phisun >= 0 and - np.sin(self._tau) <= 0):
             self._phisun = self._phisun + np.pi
 
-        elif (self._phisun <= 0 and -np.sin(self._tau) >= 0):
+        elif (self._phisun <= 0 and - np.sin(self._tau) >= 0):
             self._phisun = self._phisun + np.pi
 
         self._flat = (np.cos(np.arctan(0)) * np.sin(self._alpha) +
                       np.sin(np.arctan(0)) * np.cos(self._alpha) *
-                      np.cos(self._phisun - 0))  # flat surface reference
+                      np.cos(self._phisun - 0)) # flat surface reference
 
         self._Rsflat = self._Rgl * self._flat
         # flat surface total incoming shortwave radiation
 
-        self._Rnetflat = ((1 - self._A) * (1 - 0.65 * (self._N**2)) *
+        self._Rnetflat = ((1 - self._A) * (1 - 0.65 * (self._N ** 2)) *
                           self._Rsflat)
         # flat surface Net incoming shortwave radiation
 
@@ -324,7 +326,7 @@ class Radiation(Component):
                         np.sin(self._slope) * np.cos(self._alpha) *
                         np.cos(self._phisun - self._aspect))
 
-        self._radf = self._sloped/self._flat
+        self._radf = self._sloped / self._flat
 
         self._radf[self._radf <= 0.] = 0.
         self._radf[self._radf > 6.] = 6.
