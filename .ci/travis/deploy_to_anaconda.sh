829d4a7d22bc841cee9ea21fd12c35b070f193c0
mcflugen <mcflugen@gmail.com>
2016-05-16 00:15:54 -0600

    Added anaconda deploy scripts for Travis.

---
+++ .ci/travis/deploy_to_anaconda.sh
@@ -0,0 +1,17 @@
+if [[ "$TRAVIS_TAG" == v* ]]; then
+  echo "Building release version"
+  unset BUILD_STR
+  export CHANNEL="main"
+else
+  echo "Building dev version"
+  export BUILD_STR="dev"
+  export CHANNEL="dev"
+fi
+
+echo "Building package for Python $PYTHON_VERSION, numpy $NUMPY_VERSION"
+conda config --set anaconda_upload no
+
+echo "Uploading to $CHANNEL"
+anaconda -t $ANACONDA_TOKEN upload --force --user landlab --channel $CHANNEL $HOME/miniconda/conda-bld/**/landlab*bz2
+
+echo "Done."
