a62e91a03bb12eb78a7d363214562dcbcacafec8
mcflugen <mcflugen@gmail.com>
2016-05-16 22:35:49 -0600

    Join command args together.

--- .ci/appveyor/anaconda_upload.py
+++ .ci/appveyor/anaconda_upload.py
@@ -14,7 +14,7 @@ print('Using python: {prefix}'.format(prefix=sys.prefix))
 
 repo_tag = os.environ.get('APPVEYOR_REPO_TAG', 'false')
 tag_name = os.environ.get('APPVEYOR_REPO_TAG_NAME', '')
-token = os.environ.get('ANACONDA_TOKEN', '')
+token = os.environ.get('ANACONDA_TOKEN', 'NOT_A_TOKEN')
 
 if repo_tag == 'true' and tag_name.startswith('v'):
     channel = 'main'
@@ -25,8 +25,8 @@ else:
 print('Uploading to {channel} channel'.format(channel=channel))
 
 try:
-    resp = subprocess.check_output(['conda', 'build', '--output',
-                                    '.conda-recipe'])
+    cmd = ' '.join(['conda', 'build', '--output', '.conda-recipe'])
+    resp = subprocess.check_output(cmd, shell=True)
 except subprocess.CalledProcessError:
     traceback.print_exc()
 else:
@@ -38,8 +38,8 @@ if not os.path.isfile(file_to_upload):
     raise RuntimeError('{name}: not a file'.format(name=file_to_upload))
 
 try:
-    subprocess.check_call(['anaconda', '-t', token, 'upload', '--force',
-                           '--user', 'landlab', '--channel', channel,
-                           file_to_upload])
+    cmd = ' '.join(['anaconda', '-t', token, 'upload', '--force', '--user',
+                    'landlab', '--channel', channel, file_to_upload])
+    subprocess.check_call(cmd, shell=True)
 except subprocess.CalledProcessError:
     traceback.print_exc()
