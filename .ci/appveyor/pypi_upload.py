a3b9187ae208ced611ba2720ac2f1669351d1743
mcflugen <mcflugen@gmail.com>
2016-05-18 08:53:34 -0600

    Fixed typo.

--- .ci/appveyor/pypi_upload.py
+++ .ci/appveyor/pypi_upload.py
@@ -14,18 +14,15 @@ tag_name = os.environ.get('APPVEYOR_REPO_TAG_NAME', '')
 token = os.environ.get('PYPI_PASS', 'NOT_A_TOKEN')
 
 if repo_tag == 'true' and tag_name.startswith('v'):
-    pass
+    print('Uploading to PyPI')
+
+    try:
+        cmd = ' '.join(['twine', 'upload', '-u', 'mcflugen', '-p', token,
+                        'dist/*'])
+        resp = subprocess.check_output(cmd, shell=True)
+    except subprocess.CalledProcessError:
+        traceback.print_exc()
+    else:
+        print('OK')
 else:
     print('Not a tagged release. Not deploying to PyPI.')
-    return
-
-print('Uploading to PyPI')
-
-try:
-    cmd = ' '.join(['twine', 'upload', '-u', 'mcflugen', '-p', token,
-                    'dist/*'])
-    resp = subprocess.check_output(cmd, shell=True)
-except subprocess.CalledProcessError:
-    traceback.print_exc()
-else:
-    print('OK')
