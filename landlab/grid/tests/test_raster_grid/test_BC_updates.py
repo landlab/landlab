e7db54a4dad422d945c4fe9ff1b82dea9eb4d7d8
SiccarPoint <dan.hobley@gmail.com>
2016-06-23 11:46:13 -0600

    bc_set_code as an int not float
    `bc_set_code` now implements an incremented counter (i.e., as int), not
    a time float. Rest of the functionality remains the same.

--- landlab/grid/tests/test_raster_grid/test_BC_updates.py
+++ landlab/grid/tests/test_raster_grid/test_BC_updates.py
@@ -29,10 +29,10 @@ def test_link_update_with_nodes_fixed_grad():
 
 @with_setup(setup_grid)
 def test_bc_set_code_init():
-    assert_equal(rmg.bc_set_code, 0.)
+    assert_equal(rmg.bc_set_code, 0)
 
 
 @with_setup(setup_grid)
 def test_bc_set_code_change():
     rmg.status_at_node[rmg.nodes_at_bottom_edge] = CLOSED_BOUNDARY
-    assert_not_equal(rmg.bc_set_code, 0.)
+    assert_not_equal(rmg.bc_set_code, 0)
