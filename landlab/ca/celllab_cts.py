3c612e4f7267810db8819903fdab0429e4269c0e
Greg Tucker <gtucker@colorado.edu>
2016-07-03 09:18:11 -0600

    cythonize ca update_node_states

--- landlab/ca/celllab_cts.py
+++ landlab/ca/celllab_cts.py
@@ -133,6 +133,8 @@ from .cfuncs import update_node_states_cython
 
 _NEVER = 1e50
 
+_USE_CYTHON = True
+
 _DEBUG = False
 
 _TEST = False
@@ -847,28 +849,27 @@ class CellLabCTSModel(object):
             Flags indicating whether the tail node and head node, respectively,
             have changed state
         """
+        if _USE_CYTHON:
+            return update_node_states_cython(self.node_state,
+                              self.grid.status_at_node,
+                              tail_node,
+                              head_node,
+                              new_link_state,
+                              self.node_pair)
+        else:
+			# Remember the previous state of each node so we can detect whether the
+			# state has changed
+			old_tail_node_state = self.node_state[tail_node]
+			old_head_node_state = self.node_state[head_node]
+ 
+			# Change to the new states
+			if self.grid.status_at_node[tail_node] == _CORE:
+				self.node_state[tail_node] = self.node_pair[new_link_state][0]
+			if self.grid.status_at_node[head_node] == _CORE:
+				self.node_state[head_node] = self.node_pair[new_link_state][1]
 
-        # Remember the previous state of each node so we can detect whether the
-        # state has changed
-        old_tail_node_state = self.node_state[tail_node]
-        old_head_node_state = self.node_state[head_node]
-
-        # Change to the new states
-        if self.grid.status_at_node[tail_node] == _CORE:
-            self.node_state[tail_node] = self.node_pair[new_link_state][0]
-        # landlab.grid.base.CORE_NODE:
-        if self.grid.status_at_node[head_node] == _CORE:
-            self.node_state[head_node] = self.node_pair[new_link_state][1]
-
-        if _DEBUG:
-            print('update_node_states() for', tail_node, 'and', head_node)
-            print('  tail_node was', old_tail_node_state,
-                  'and is now', self.node_state[tail_node])
-            print('  head_node was', old_head_node_state,
-                  'and is now', self.node_state[head_node])
-
-        return self.node_state[tail_node] != old_tail_node_state, \
-            self.node_state[head_node] != old_head_node_state
+			return self.node_state[tail_node] != old_tail_node_state, \
+				   self.node_state[head_node] != old_head_node_state
 
     def update_link_state(self, link, new_link_state, current_time):
         """
--- landlab/ca/cfuncs.pyx
+++ landlab/ca/cfuncs.pyx
@@ -7,28 +7,43 @@ Created on Thu Jun 30 12:40:39 2016
 import numpy as np
 cimport numpy as np
 cimport cython
+from landlab import CORE_NODE as _CORE
+
+DTYPE_INT = np.int
+ctypedef np.int_t DTYPE_INT_t
+
+DTYPE_INT8 = np.int8
+ctypedef np.int8_t DTYPE_INT8_t
 
 
-def update_node_states_cython():
-    
-	# Remember the previous state of each node so we can detect whether the
-	# state has changed
-	old_tail_node_state = self.node_state[tail_node]
-	old_head_node_state = self.node_state[head_node]
+# def froggo(a, b):
+#     
+#     c = a + b
 
-	# Change to the new states
-	if self.grid.status_at_node[tail_node] == _CORE:
-		self.node_state[tail_node] = self.node_pair[new_link_state][0]
-	# landlab.grid.base.CORE_NODE:
-	if self.grid.status_at_node[head_node] == _CORE:
-		self.node_state[head_node] = self.node_pair[new_link_state][1]
+#def update_node_states_cython(a, b):
+#
+#    cdef int frog
+#
+#    frog = a + b
 
-	if _DEBUG:
-		print('update_node_states() for', tail_node, 'and', head_node)
-		print('  tail_node was', old_tail_node_state,
-			  'and is now', self.node_state[tail_node])
-		print('  head_node was', old_head_node_state,
-			  'and is now', self.node_state[head_node])
 
-	return self.node_state[tail_node] != old_tail_node_state, \
-		self.node_state[head_node] != old_head_node_state
+def update_node_states_cython(np.ndarray[DTYPE_INT_t, ndim=1] node_state,
+                              np.ndarray[DTYPE_INT8_t, ndim=1] status_at_node,
+                              DTYPE_INT_t tail_node, 
+                              DTYPE_INT_t head_node,
+                              DTYPE_INT_t new_link_state,
+                              node_pair):
+
+    # Remember the previous state of each node so we can detect whether the
+    # state has changed
+    old_tail_node_state = node_state[tail_node]
+    old_head_node_state = node_state[head_node]
+
+    # Change to the new states
+    if status_at_node[tail_node] == _CORE:
+        node_state[tail_node] = node_pair[new_link_state][0]
+    if status_at_node[head_node] == _CORE:
+        node_state[head_node] = node_pair[new_link_state][1]
+
+    return node_state[tail_node] != old_tail_node_state, \
+           node_state[head_node] != old_head_node_state
