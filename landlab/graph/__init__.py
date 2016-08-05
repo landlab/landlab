908741fab410e38500f5411bbed8b245b37a6469
mcflugen <mcflugen@gmail.com>
2016-08-05 17:50:44 -0600

    Added DualGraph to import.

--- landlab/graph/__init__.py
+++ landlab/graph/__init__.py
@@ -1,11 +1,12 @@
 from .graph import Graph
+from .dual import DualGraph
 from .structured_quad import *
 from .voronoi import *
 from .hex import *
 from .radial import *
 
 
-__all__ = ['Graph', 'StructuredQuadGraph', 'RectilinearGraph',
+__all__ = ['Graph', 'DualGraph', 'StructuredQuadGraph', 'RectilinearGraph',
            'UniformRectilinearGraph', 'DualUniformRectilinearGraph',
            'DualRectilinearGraph', 'DualStructuredQuadGraph', 'VoronoiGraph',
            'DualVoronoiGraph', 'HexGraph', 'DualHexGraph', 'RadialGraph',
