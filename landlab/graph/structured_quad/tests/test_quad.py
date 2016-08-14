d9d2d24f9b02461bffcd99b4758791c48dcb570f
mcflugen <mcflugen@gmail.com>
2016-08-14 13:35:59 -0600

    Removed unused Voronoi import.

--- landlab/graph/structured_quad/tests/test_quad.py
+++ landlab/graph/structured_quad/tests/test_quad.py
@@ -2,7 +2,6 @@
 from nose.tools import (assert_true, assert_false, assert_equal,
                         assert_almost_equal)
 from numpy.testing import assert_array_equal, assert_array_almost_equal
-from scipy.spatial import Voronoi
 import numpy as np
 
 from landlab.graph import StructuredQuadGraph
