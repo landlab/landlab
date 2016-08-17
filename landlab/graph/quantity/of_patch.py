88554fb8678cf9a7d03749b36186eff53525cbac
mcflugen <mcflugen@gmail.com>
2016-08-17 17:05:07 -0600

    Removed unused get_center_of_patch.

--- landlab/graph/quantity/of_patch.py
+++ landlab/graph/quantity/of_patch.py
@@ -1,20 +1,6 @@
 import numpy as np
 
 
-def get_center_of_patch(graph, out=None):
-    from .ext.remap_element import calc_center_of_patch
-
-    n_patches = graph.number_of_patches
-
-    if out is None:
-        out = np.empty((n_patches, 2), dtype=float)
-
-    calc_center_of_patch(links_at_patch, offset_to_patch,
-                         xy_of_link, out)
-
-    return out
-
-
 def get_area_of_patch(graph, out=None):
     from .ext.of_patch import calc_area_at_patch
 
