95f26e53f60a97131a7ed1d5864ca62b6bf8a964
Michael S. Steckler <michael@engr2-2-89-51-dhcp.int.colorado.edu>
2016-09-15 12:15:35 -0600

    calculate diffusion coefficients

--- landlab/components/submarine_diffusion/submarine.py
+++ landlab/components/submarine_diffusion/submarine.py
@@ -2,14 +2,19 @@
 import numpy as np
 
 from .. import LinearDiffuser
-
+from .utils import find_shoreline
 
 class SubmarineDiffuser(LinearDiffuser):
 
-    def __init__(self, grid, k0=1., sea_level=0., k_land=1., **kwds):
-        self._k0 = k0
+    def __init__(self, grid, shore, ksh=100., sea_level=0., wavebase=60.,
+                 hgt = 15., alpha = 1/2000., sl_sh = .001, load = .3, **kwds):
+        self._ksh = ksh
         self._sea_level = sea_level
-        self._k_land = k_land
+        self._wavebase = wavebase
+        self._hgt = hgt
+        self._alpha = alpha
+        self._sl_sh = sl_sh
+        self._load = load
         kwds['linear_diffusivity'] = 'kd'
 
         grid.add_zeros('kd', at='node')
@@ -22,7 +27,7 @@ class SubmarineDiffuser(LinearDiffuser):
 
     @property
     def k_land(self):
-        return self._k_land
+        return self._ksh
 
     @property
     def sea_level(self):
@@ -32,24 +37,33 @@ class SubmarineDiffuser(LinearDiffuser):
     def sea_level(self, sea_level):
         self._sea_level = sea_level
 
-    def calc_diffusion_coef(self):
+    def calc_diffusion_coef(self,shore):
         water_depth = (self.sea_level -
                        self._grid.at_node['topographic__elevation'])
+        wavebase = self._wavebase
         under_water = water_depth > 0.
+        deep_water = water_depth > wavebase
 
         k = self._grid.at_node['kd']
+    
 
-        x = self._grid.x_of_node[under_water]
-        x_sh = 0.
-        a = b = 1e-6
-        big_z = 1.
-
-        k[under_water] = self.k0 * (1. /
-                                    (water_depth[under_water] + b)) * big_z
-        k[~ under_water] = self.k_land
+        x = self._grid.x_of_node
+        x_sh = shore
+        a = dx = self.grid.dx
+        b = (self._hgt*self._alpha + self._sl_sh)*dx
+        
+        k_land = self._load/.0008 #self._sl_sh
+        k[under_water] = self._load * ((x[under_water]-x_sh) +a) / (
+                                    water_depth[under_water] + b) 
+        k[deep_water] = k[deep_water] * np.exp(-1*(water_depth[deep_water]-
+                                                wavebase)/wavebase)
+        k[~ under_water] = k_land
 
         return k
 
     def run_one_step(self, dt):
-        self.calc_diffusion_coef()
+        z = self._grid.at_node['topographic__elevation']
+        shore = find_shoreline(self.grid.x_of_node[self.grid.core_nodes], 
+                               z[self.grid.core_nodes], self.sea_level)
+        self.calc_diffusion_coef(shore)
         super(SubmarineDiffuser, self).run_one_step(dt)
