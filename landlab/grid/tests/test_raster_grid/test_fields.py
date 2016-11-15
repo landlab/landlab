1e172cd6a11c8e32a653acb31460a9792dec7120
kbarnhart <katy.barnhart@gmail.com>
2016-11-15 14:12:08 -0700

    added two tests to the grid to verify at_grid fields

--- landlab/grid/tests/test_raster_grid/test_fields.py
+++ landlab/grid/tests/test_raster_grid/test_fields.py
@@ -25,3 +25,14 @@ def test_add_field_without_at():
     """Test raises error with wrong size array."""
     grid = RasterModelGrid((4, 5))
     assert_raises(ValueError, grid.add_field, 'z', np.arange(21))
+
+def test_add_field_at_grid_value_error():
+    """Test raises error with wrong size array for adding a at_grid field."""
+    grid = RasterModelGrid((4, 5))
+    assert_raises(ValueError, grid.add_field, 'value', [0,1], at='grid')
+
+def test_add_field_at_grid():
+    """Test add field at grid."""
+    grid = RasterModelGrid((4, 5))
+    grid.at_grid['value']=1
+    assert_array_equal(1, grid.at_grid['value'].size)
