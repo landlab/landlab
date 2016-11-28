5509a06f86f8af1f9541fa10e88d4c6113c25e10
Greg Tucker <gtucker@colorado.edu>
2016-11-28 08:28:30 -0700

    add doctests and newton() fn

--- landlab/components/stream_power/stream_power_smooth_threshold.py
+++ landlab/components/stream_power/stream_power_smooth_threshold.py
@@ -21,8 +21,68 @@ if __name__ == '__main__':
 else:
     from .fastscape_stream_power import FastscapeEroder
 import numpy as np
+from scipy.optimize import newton
 
 UNDEFINED_INDEX = -1
+
+
+
+
+def new_elev(x, a, b, c, d):
+    """Equation for elevation of a node at timestep t+1.
+    
+    Parameters
+    ----------
+    x : float
+        Value of new elevation
+    a : float
+        Parameter = K A^m dt / L
+    b : float
+        Parameter = K A^m / wc L
+    c : float
+        Elevation of downstream node, z_j
+    d : float
+        z(t) + a z_j + wc
+    """
+    return x * (1.0 + a) + np.exp(-b * (x - c)) - d
+
+
+def new_elev_prime(x, a, b, c, d=0.0):
+    """Equation for elevation of a node at timestep t+1.
+    
+    Parameters
+    ----------
+    x : float
+        Value of new elevation
+    a : float
+        Parameter = K A^m dt / L
+    b : float
+        Parameter = K A^m / wc L
+    c : float
+        Elevation of downstream node, z_j
+    d : n/a
+        Placeholder; not used
+    """
+    return (1.0 + a) - b * np.exp(-b * (x - c))
+
+
+def new_elev_prime2(x, a, b, c, d=0.0):
+    """Equation for elevation of a node at timestep t+1.
+    
+    Parameters
+    ----------
+    x : float
+        Value of new elevation
+    a : n/a
+        Placeholder; not used
+    b : float
+        Parameter = K A^m / wc L
+    c : float
+        Elevation of downstream node, z_j
+    d : n/a
+        Placeholder; not used
+    """
+    return b * b * np.exp(-b * (x - c))
 
 
 
@@ -48,17 +108,15 @@ class StreamPowerSmoothThresholdEroder(FastscapeEroder):
                  rainfall_intensity=1., **kwargs):
         """Initialize StreamPowerSmoothThresholdEroder."""
         
-        print('*****SPSTE*****')
-        print(K_sp)
-        
         # Call base-class init
         super(StreamPowerSmoothThresholdEroder, 
-              self).__init__(grid, K_sp, m_sp, n_sp, threshold_sp, 
-                             rainfall_intensity, **kwargs)
+              self).__init__(grid, K_sp=K_sp, m_sp=m_sp, n_sp=n_sp, 
+                             threshold_sp=threshold_sp,
+                             rainfall_intensity=rainfall_intensity, **kwargs)
 
         # Arrays with parameters for use in implicit solver
         self.beta = grid.empty(at='node')
-        self.gamma = grid.empty(at='node')
+        self.delta = grid.empty(at='node')
 
 
     def run_one_step(self, dt, flooded_nodes=None, runoff_rate=None, **kwds):
@@ -77,9 +135,21 @@ class StreamPowerSmoothThresholdEroder(FastscapeEroder):
         --------
         >>> from landlab import RasterModelGrid
         >>> rg = RasterModelGrid((3, 3), 1.0)
-        >>> rg.set_closed_boundaries_on_grid_edges(False, True, True, True)
+        >>> rg.set_closed_boundaries_at_grid_edges(False, True, True, True)
         >>> z = rg.add_zeros('node', 'topographic__elevation')
         >>> z[4] = 1.0
+        >>> from landlab.components import FlowRouter
+        >>> fr = FlowRouter(rg, method='D4')
+        >>> fr.run_one_step()
+        >>> from landlab.components import StreamPowerSmoothThresholdEroder
+        >>> sp = StreamPowerSmoothThresholdEroder(rg, K_sp=1.0)
+        >>> sp.run_one_step(dt=1.0)
+        >>> sp.alpha
+        array([ 0.,  0.,  0.,  0.,  1.,  0.,  0.,  0.,  0.])
+        >>> sp.beta
+        array([ 0.,  0.,  0.,  0.,  1.,  0.,  0.,  0.,  0.])
+        >>> sp.gamma
+        array([ 0.,  0.,  0.,  0.,  2.,  0.,  0.,  0.,  0.])
         """
         
         # Set up needed arrays
@@ -117,17 +187,38 @@ class StreamPowerSmoothThresholdEroder(FastscapeEroder):
             / (self.thresholds * flow_link_lengths))
 
         #   Gamma
-        self.gamma[defined_flow_receivers] = (z[defined_flow_receivers]
+        self.delta[defined_flow_receivers] = (z[defined_flow_receivers]
             + (self.alpha[defined_flow_receivers]
                * z[flow_receivers[defined_flow_receivers]]) 
             + self.thresholds)
 
         # Iterate over nodes from downstream to upstream, using scipy's
         # 'newton' function to find new elevation at each node in turn.
+        for node in upstream_order_IDs:
+            print(node)
+            if defined_flow_receivers[node]:
+                z[node] = newton(new_elev, z[node],
+                                 fprime=new_elev_prime,
+                                 args=(self.alpha[node], self.beta[node],
+                                       z[flow_receivers[node]],
+                                       self.delta[node]), 
+                                 fprime2=new_elev_prime2)
+                print(node, z[node])
+
+        # TODO: handle case self.thresholds = 0
+
+
 
 
 if __name__ == '__main__':
     from landlab import RasterModelGrid
-    rg = RasterModelGrid((3, 3), 1.0)
+    rg = RasterModelGrid((3, 4), 1.0)
     z = rg.add_zeros('node', 'topographic__elevation')
+    rg.set_closed_boundaries_at_grid_edges(False, True, True, True)
+    z[5] = 2.0
+    z[6] = 1.0
+    from landlab.components import FlowRouter
+    fr = FlowRouter(rg, method='D4')
+    fr.run_one_step()
     sp = StreamPowerSmoothThresholdEroder(rg, K_sp=1.0)
+    sp.run_one_step(dt=1.0)
