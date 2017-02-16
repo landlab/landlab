38048d45a2a7e183e4a2c039590df2337f96418d
Katy Barnhart <katy.barnhart@gmail.com>
2017-02-15 18:12:39 -0700

    Minor punctuation changes to documentation

--- landlab/components/flow_director/flow_director_steepest.py
+++ landlab/components/flow_director/flow_director_steepest.py
@@ -184,7 +184,7 @@ class FlowDirectorSteepest(_FlowDirectorToOne):
         slopes on links, finds baselevel nodes based on the status at node,
         calculates flow directions, and saves results to the grid.
 
-        an alternative to direct_flow() is direct_flow() which does the same
+        An alternative to direct_flow() is direct_flow() which does the same
         things but also returns the receiver nodes not return values.
         """
         self.direct_flow()
@@ -199,7 +199,7 @@ class FlowDirectorSteepest(_FlowDirectorToOne):
         at-node array  of receiver nodes. This array is stored in the grid at:
         grid['node']['flow__receiver_node']
 
-        an alternative to direct_flow() is run_one_step() which does the same
+        An alternative to direct_flow() is run_one_step() which does the same
         things but also returns a at-node array  of receiver nodes. This array
         is stored in the grid at:
         grid['node']['flow__receiver_node']
