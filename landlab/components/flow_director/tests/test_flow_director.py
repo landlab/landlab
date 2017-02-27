788f58c24aff0dd31e37b1d73b8f266d86546ce0
Katy Barnhart <katy.barnhart@gmail.com>
2017-02-26 19:40:40 -0700

    Finalizing test of director field creation

--- landlab/components/flow_director/tests/test_flow_director.py
+++ landlab/components/flow_director/tests/test_flow_director.py
@@ -64,13 +64,18 @@ def test_grid_type_testing():
     assert_raises(NotImplementedError, FlowDirectorDINF, hmg)
     
 def test_check_fields():
-    """Check to make sure the right fields have been created."""
+    """Check to make sure the right fields have been created.
+    
+    Check that the sizes of at least one are also correct (they are at node
+    so if one is the right size, then they all should be) 
+    """
     
     mg0 = RasterModelGrid((10,10), spacing=(1, 1))
     z0 = mg0.add_field('topographic__elevation', mg0.node_x**2 + mg0.node_y**2, at = 'node')
     fd0 = _FlowDirector(mg0, 'topographic__elevation')
     assert_equal(list(mg0.at_node.keys()), ['topographic__elevation'])
-    
+    assert_equal(np.size(mg0.at_node['topographic__elevation']), mg0.number_of_nodes)
+
     mg1 = RasterModelGrid((10,10), spacing=(1, 1))
     z1 = mg1.add_field('topographic__elevation', mg1.node_x**2 + mg1.node_y**2, at = 'node')
     fd1 = _FlowDirectorToMany(mg1, 'topographic__elevation')
@@ -79,7 +84,8 @@ def test_check_fields():
                                                      'flow__sink_flag',
                                                      'topographic__elevation',
                                                      'topographic__steepest_slope'])
-    
+    assert_equal(np.size(mg1.at_node['topographic__elevation']), mg1.number_of_nodes)
+
     mg2 = RasterModelGrid((10,10), spacing=(1, 1))
     z2 = mg2.add_field('topographic__elevation', mg2.node_x**2 + mg2.node_y**2, at = 'node')
     fd2 = _FlowDirectorToOne(mg2, 'topographic__elevation')
@@ -88,6 +94,8 @@ def test_check_fields():
                                                     'flow__sink_flag',
                                                     'topographic__elevation',
                                                     'topographic__steepest_slope'])
+    assert_equal(np.size(mg2.at_node['topographic__elevation']), mg2.number_of_nodes)
+
     
     mg3 = RasterModelGrid((10,10), spacing=(1, 1))
     z3 = mg3.add_field('topographic__elevation', mg3.node_x**2 + mg3.node_y**2, at = 'node')
@@ -100,6 +108,9 @@ def test_check_fields():
                                                      'flow__sink_flag',
                                                      'topographic__elevation',
                                                      'topographic__steepest_slope'])
+    assert_equal(np.size(mg3.at_node['topographic__elevation']), mg3.number_of_nodes)
+
+    
     
     mg4 = RasterModelGrid((10,10), spacing=(1, 1))
     z4 = mg4.add_field('topographic__elevation', mg4.node_x**2 + mg4.node_y**2, at = 'node')
@@ -112,7 +123,8 @@ def test_check_fields():
                                                      'flow__sink_flag',
                                                      'topographic__elevation',
                                                      'topographic__steepest_slope'])
-    
+    assert_equal(np.size(mg4.at_node['topographic__elevation']), mg4.number_of_nodes)
+
     mg5 = RasterModelGrid((10,10), spacing=(1, 1))
     z5 = mg5.add_field('topographic__elevation', mg5.node_x**2 + mg5.node_y**2, at = 'node')
     fd5 = FlowDirectorSteepest(mg5, 'topographic__elevation')
@@ -121,7 +133,9 @@ def test_check_fields():
                                                     'flow__sink_flag',
                                                     'topographic__elevation',
                                                     'topographic__steepest_slope'])
-    
+    assert_equal(np.size(mg5.at_node['topographic__elevation']), mg5.number_of_nodes)
+
+
     mg6 = RasterModelGrid((10,10), spacing=(1, 1))
     z6 = mg6.add_field('topographic__elevation', mg6.node_x**2 + mg6.node_y**2, at = 'node')
     fd6 = FlowDirectorD8(mg6, 'topographic__elevation')
@@ -130,4 +144,4 @@ def test_check_fields():
                                                     'flow__sink_flag',
                                                     'topographic__elevation',
                                                     'topographic__steepest_slope'])
-     
+    assert_equal(np.size(mg6.at_node['topographic__elevation']), mg6.number_of_nodes)
