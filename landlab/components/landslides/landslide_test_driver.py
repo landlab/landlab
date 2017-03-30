c70e47d8fffe3e40b9296223a9cf9ace8c39fa50
RondaStrauch <rstrauch@uw.edu>
2017-03-30 10:45:37 -0700

    replaced_test_driver
    Replaced older test driver with updated driver.

--- landlab/components/landslides/landslide_test_driver.py
+++ landlab/components/landslides/landslide_test_driver.py
@@ -19,23 +19,24 @@ from landlab.components.landslides import LandslideProbability
 
 grid = RasterModelGrid((5, 4), spacing=(0.2, 0.2))
 # Add required fields for component.
-grid['node']['slope'] = np.random.rand(5,4)
-scatter_dat = np.random.random_integers(1, 10, (5,4))
-grid['node']['contributing_area']= \
-         np.sort(np.random.random_integers(30, 900, (5,4)))
-grid['node']['soil_transmissivity']= \
-         np.sort(np.random.random_integers(5, 20, (5,4)),-1)
-grid['node']['combined_cohesion__mode']= \
-         np.sort(np.random.random_integers(30, 900, (5,4)))
-grid['node']['combined_cohesion__minimum']= \
-         grid.at_node['combined_cohesion__mode'] - scatter_dat
-grid['node']['combined_cohesion__maximum']= \
-         grid.at_node['combined_cohesion__mode'] + scatter_dat
-grid['node']['soil_internal_angle_friction__mode']= \
-         np.sort(np.random.random_integers(26, 40, (5,4)))
-grid['node']['soil_thickness__mode']= \
-         np.sort(np.random.random_integers(1, 10, (5,4)))
-grid['node']['soil_density']= \
+gridnum = grid.number_of_nodes
+grid['node']['topographic__slope'] = np.random.rand(gridnum)
+scatter_dat = np.random.randint(1, 10, gridnum)
+grid['node']['topographic__specific_contributing_area']= \
+         np.sort(np.random.randint(30, 900, gridnum))
+grid['node']['soil__transmissivity']= \
+         np.sort(np.random.randint(5, 20, gridnum),-1)
+grid['node']['soil__mode_total_cohesion']= \
+         np.sort(np.random.randint(30, 900, gridnum))
+grid['node']['soil__minimum_total_cohesion']= \
+         grid.at_node['soil__mode_total_cohesion'] - scatter_dat
+grid['node']['soil__maximum_total_cohesion']= \
+         grid.at_node['soil__mode_total_cohesion'] + scatter_dat
+grid['node']['soil__internal_friction_angle']= \
+         np.sort(np.random.randint(26, 40, gridnum))
+grid['node']['soil__thickness']= \
+         np.sort(np.random.randint(1, 10, gridnum))
+grid['node']['soil__density']= \
          2000. * np.ones(grid.number_of_nodes)
          
 grid_size = grid.number_of_core_nodes
@@ -44,56 +45,57 @@ n = 10
 
 # Recharge options
 #Option 1 - uniform distribution
-distribution = 'uniform'
+distribution1 = 'uniform'
 Remin_value = 20.
 Remax_value = 120.
 #Opton 2 - Lognormal-Uniform
-distribution = 'lognormal_uniform'
+distribution2 = 'lognormal_uniform'
 Remean = 4.
 Restandard_deviation = 0.25
 #Option 3 - Lognormal-Spatial
-distribution = 'lognormal_spatial'
-Remean = np.random.random_integers(2,7,grid_size)
-Restandard_deviation = np.random.rand(grid_size)
+distribution3 = 'lognormal_spatial'
+Remean3 = np.random.randint(2,7,grid_size)
+Restandard_deviation3 = np.random.rand(grid_size)
 #Option 4 - Fully distributed
 VIC_dict = {}
 vic_id_dict = {}
 fract_dict = {}
 # VIC id and recharge array
 for vkey in range(2,8):
-    VIC_dict[vkey] = np.random.random_integers(20,120,10)
+    VIC_dict[vkey] = np.random.randint(20,120,10)
 # node id and vic grid ids
 for ckey in grid.core_nodes:
-    vic_id_dict[ckey] = np.random.random_integers(2,8,2)
+    vic_id_dict[ckey] = np.random.randint(2,8,2)
 # node id and vic grid fraction
 for ckey in grid.core_nodes:
     fract_dict[ckey] =  np.random.rand(2)
-distribution = 'VIC'
+distribution4 = 'VIC'
 vic_inputs = [VIC_dict,vic_id_dict, fract_dict]
 
 # Instantiate the 'LandslideProbability' component to work on this grid,
 # and run it.
 #Uniform
-LS_prob1 = LandslideProbability(grid,number_of_simulations=n,
-    groudwater__recharge_distribution=distribution,
-    groundwater__recharge_min_value=Remin_value,
-    groundwater__recharge_max_value=Remax_value)
-LS_prob1.calculate_landslide_probability()
+#LS_prob1 = LandslideProbability(grid,number_of_iterations=n,
+#    groundwater__recharge_distribution=distribution1,
+#    groundwater__recharge_min_value=Remin_value,
+#    groundwater__recharge_max_value=Remax_value)
+#LS_prob1.calculate_landslide_probability()
 #lognormal_uniform
-LS_prob2 = LandslideProbability(grid,number_of_simulations=n,
-    groudwater__recharge_distribution=distribution,
-    groundwater__recharge_mean=Remean,
-    groundwater__recharge_standard_deviation=Restandard_deviation)
-LS_prob2.calculate_landslide_probability()
+#LS_prob2 = LandslideProbability(grid,number_of_iterations=n,
+#    groundwater__recharge_distribution=distribution2,
+#    groundwater__recharge_mean=Remean,
+#    groundwater__recharge_standard_deviation=Restandard_deviation)
+#LS_prob2.calculate_landslide_probability()
 #lognormal_spatial
-LS_prob3 = LandslideProbability(grid,number_of_simulations=n,
-    groudwater__recharge_distribution=distribution,
-    groundwater__recharge_mean=Remean,
-    groundwater__recharge_standard_deviation=Restandard_deviation)
-LS_prob3.calculate_landslide_probability()
+#LS_prob3 = LandslideProbability(grid,number_of_iterations=n,
+#    groundwater__recharge_distribution=distribution3,
+#    groundwater__recharge_mean=Remean3,
+#    groundwater__recharge_standard_deviation=Restandard_deviation3)
+#LS_prob3.calculate_landslide_probability()
 #VIC
-LS_prob4 = LandslideProbability(grid,number_of_simulations=n,
-    groudwater__recharge_vic_inputs=vic_inputs)
+LS_prob4 = LandslideProbability(grid,number_of_iterations=n,
+    groundwater__recharge_distribution=distribution4,
+    groundwater__recharge_vic_inputs=vic_inputs)
 LS_prob4.calculate_landslide_probability()
 
 
