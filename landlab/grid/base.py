a495b3324c109498f8a5e11bafb3e8b3363c00cb
Jordan Adams <jadams15@tulane.edu>
2017-04-18 12:36:17 -0500

    small change to update fixed link/node status in base.py

--- landlab/grid/base.py
+++ landlab/grid/base.py
@@ -3295,10 +3295,8 @@ class ModelGrid(ModelDataFieldsMixIn):
         try:
             self._fixed_grad_links_created
         except AttributeError:
-            pass
-        else:
-            self._gradient_boundary_node_links()
             self._create_fixed_gradient_boundary_node_anchor_node()
+            self._create_fixed_gradient_boundary_node_links()
         try:
             self._patches_created
             self._reset_patch_status()
