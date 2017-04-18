b44da8fc94bb74f95a98d257bb495fe2b03bb14b
saisiddu <saisiddu@uw.edu>
2017-04-17 23:06:26 -0700

    Packaging Source Tracking Algorithm
    Packaging Source Tracking Algorithm as a Landlab utility and a test
    driver for this utility.

--- landlab/components/landslides/SourceTrackingAlgorithm.py
+++ landlab/components/landslides/SourceTrackingAlgorithm.py
@@ -13,38 +13,16 @@ their fractions of contribution.
 # %%
 # Import required libraries
 import numpy as np
-from landlab.io.esri_ascii import read_esri_ascii
 import copy
 from collections import Counter
-from datetime import datetime
-import cPickle as pickle
-
-# Start the clock
-startTime = datetime.now()
-
-# %%
-# Import input files. Create a RasterModelGrid object 'grid' and assign
-# input data as fields or assigned to a variable
-grid, z = read_esri_ascii('./Input_files/elevation.txt',
-                          name='topographic__elevation')
-grid.set_nodata_nodes_to_closed(grid['node']['topographic__elevation'], -9999.)
-grid, flow_dir_arc = read_esri_ascii('./Input_files/flow_direction.txt',
-                                     name='flow_dir', grid=grid)
-grid, hsd_ids = read_esri_ascii('./Input_files/vic_idsnoca.txt',
-                                name='hsd_id', grid=grid)
-grid, slp_g16 = read_esri_ascii('./Input_files/slp_g16msk.txt',
-                                name='slp_g16', grid=grid)
-hsd_ids = hsd_ids.astype(int)
-# Define model domain
-grid.set_closed_boundaries_at_grid_edges(True, True, True, True)
-grid.set_nodata_nodes_to_closed(grid['node']['flow_dir'], -9999.)
-grid.set_nodata_nodes_to_closed(grid['node']['slp_g16'], -9999.)
 
 # %%
 """
  Convert Arc flow_directions file to be represented in node ids
  gives receiver of each node starting at right and going clockwise.
 """
+
+
 def convert_arc_flow_directions_to_landlab_node_ids(grid, flow_dir_arc):
     r_arc_raw = np.log2(flow_dir_arc)
     r_arc_raw = r_arc_raw.astype('int')
@@ -61,14 +39,16 @@ def convert_arc_flow_directions_to_landlab_node_ids(grid, flow_dir_arc):
     r = np.zeros(grid.number_of_nodes, dtype=int)
     r[grid.core_nodes] = np.choose(r_arc_raw[grid.core_nodes],
                                    np.transpose(neighbors[grid.core_nodes]))
-    return r
+    return (r)
+
 
 # %%
 # Source Routing Algorithm
 # Note 1: This algorithm works on core nodes only because core nodes
 # have neighbors that are real values and not -1s.
 # Note 2: Nodes in the following comments in this section refer to core nodes.
-def track_source():
+def track_source(grid, r, hsd_ids):
+    z = grid.at_node['topographic__elevation']
     core_nodes = grid.core_nodes
     core_elev = z[core_nodes]
     # Sort all nodes in the descending order of elevation
@@ -76,7 +56,6 @@ def track_source():
     # Create a list to record all nodes that have been visited
     # To store nodes that have already been counted
     alr_counted = []
-    r_core = r[core_nodes]
     flow_accum = np.zeros(grid.number_of_nodes, dtype=int)
     hsd_upstr = {}
     # Loop through all nodes
@@ -95,7 +74,7 @@ def track_source():
         # belongs to a stream segment. Hence, all the nodes in the stream will
         # have to betraversed.
         # stream_buffer is a list that will hold the upstream contributing
-        # node information for that particular segment until reaching the outlet.
+        # node information for that particular segment until reaching outlet.
         stream_buffer = []
         j = i
         switch_i = True
@@ -123,21 +102,22 @@ def track_source():
                 hsd_upstr[j] += copy.copy(stream_buffer)
             # If the node has been already visited, then the upstream segment
             # that was not accounted for in the main stem, would be added to
-            # all the downstream nodes, one by one, until the outlet is reached.
+            # all downstream nodes, one by one, until the outlet is reached.
             else:
                 hsd_upstr.update({j: copy.copy(stream_buffer)})
             # If the outlet is reached, the 'while' loop will be exited.
             if r[j] == j:
                 break
-            # This will be executed only for the first node of the stream segment.
+            # This will be executed only for the first node of the
+            # stream segment.
             if switch_i:
                 switch_i = False
-    
-    # OrderingTime = datetime.now() - startTime
+            return (hsd_upstr, flow_accum)
+
 
 # %%
 # Algorithm to calculate coefficients of each upstream HSD ID
-def find_unique_upstream_hsd_ids_and_fractions():
+def find_unique_upstream_hsd_ids_and_fractions(hsd_upstr):
     uniq_ids = {}  # Holds unique upstream HSD ids
     C = {}  # Holds corresponding total numbers
     coeff = {}  # Holds corresponding coefficients of contribution
@@ -152,16 +132,4 @@ def find_unique_upstream_hsd_ids_and_fractions():
         C.update({ke: buf})
         e = [s/float(sum(buf)) for s in buf]
         coeff.update({ke: e})
-
-    # TotalTime = datetime.now() - startTime
-    
-    # %%
-    # Plot or/and Save files
-    sim = 'source_tracking_'
-    # Saving dict{MD id: unique HSD ids}
-    pickle.dump(uniq_ids, open("dict_uniq_ids.p", "wb"))
-    # Saving dict{MD id: Fraction of unique HSD ids
-    # contributing to this MD id}
-    pickle.dump(coeff, open("dict_coeff.p", "wb"))
-    np.save(sim + 'OrderingTime', OrderingTime)
-    np.save(sim + 'TotalTime', TotalTime)
+    return (uniq_ids, coeff)
---
+++ landlab/components/landslides/Source_tracking_driver.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+
+"""
+Created on Mon Apr 17 22:47:22 2017
+
+@author: saisiddu
+"""
+from landlab.io.esri_ascii import read_esri_ascii
+import SourceTrackingAlgorithm as STA
+import cPickle as pickle
+
+
+# %%
+# Import input files. Create a RasterModelGrid object 'grid' and assign
+# input data as fields or assigned to a variable
+grid, z = read_esri_ascii('./Input_files/elevation.txt',
+                          name='topographic__elevation')
+grid.set_nodata_nodes_to_closed(grid['node']['topographic__elevation'], -9999.)
+grid, flow_dir_arc = read_esri_ascii('./Input_files/flow_direction.txt',
+                                     name='flow_dir', grid=grid)
+grid, hsd_ids = read_esri_ascii('./Input_files/vic_idsnoca.txt',
+                                name='hsd_id', grid=grid)
+grid, slp_g16 = read_esri_ascii('./Input_files/slp_g16msk.txt',
+                                name='slp_g16', grid=grid)
+hsd_ids = hsd_ids.astype(int)
+# Define model domain
+grid.set_closed_boundaries_at_grid_edges(True, True, True, True)
+grid.set_nodata_nodes_to_closed(grid['node']['flow_dir'], -9999.)
+grid.set_nodata_nodes_to_closed(grid['node']['slp_g16'], -9999.)
+
+
+# %%
+r = STA.convert_arc_flow_directions_to_landlab_node_ids(grid, flow_dir_arc)
+(hsd_upstr, flow_accum) = STA.track_source(grid, r)
+(uniq_ids, coeff) = STA.find_unique_upstream_hsd_ids_and_fractions(hsd_upstr)
+
+
+# %%
+# Plot or/and Save files
+# Saving dict{MD id: unique HSD ids}
+pickle.dump(uniq_ids, open("dict_uniq_ids.p", "wb"))
+# Saving dict{MD id: Fraction of unique HSD ids
+# contributing to this MD id}
+pickle.dump(coeff, open("dict_coeff.p", "wb"))
