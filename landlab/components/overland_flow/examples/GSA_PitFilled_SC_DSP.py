d39218e38d859f8cba1d275a485c9589201dc413
Jordan Adams <jadams15@tulane.edu>
2017-06-05 15:56:26 -0500

    Merge branch 'master' into jadams15/kinwave-rengers-fix

---
+++ landlab/components/overland_flow/examples/GSA_PitFilled_SC_DSP.py
@@ -0,0 +1,321 @@
+#! /usr/env/python
+"""
+
+GSA_PitFilled_SC.py
+
+This is a driver file to run sample storms over the D4 pitfilled Spring Creek
+DEM.
+
+"""
+
+
+from __future__ import print_function
+
+from landlab.components import OverlandFlow, DetachmentLtdErosion, SoilInfiltrationGreenAmpt
+from landlab.components.detachment_ltd_erosion.generate_erosion_by_depth_slope import DepthSlopeProductErosion
+from landlab.utils.depth_dependent_roughness import depth_dependent_mannings_n
+from landlab.io import read_esri_ascii, write_esri_ascii
+from landlab.grid.raster_mappers import map_max_of_inlinks_to_node , map_max_of_outlinks_to_node
+import time
+import numpy as np
+from matplotlib import pyplot as plt
+from landlab.plot import imshow_grid
+from matplotlib import cm
+import matplotlib as mpl
+def cmap_discretize(cmap, N):
+    """Return a discrete colormap from the continuous colormap cmap.
+
+   4         cmap: colormap instance, eg. cm.jet.
+   5         N: number of colors.
+   6
+   7     Example
+   8         x = resize(arange(100), (5,100))
+   9         djet = cmap_discretize(cm.jet, 5)
+  10         imshow(x, cmap=djet)
+  11     """
+    if type(cmap) == str:
+        cmap = cm.get_cmap(cmap)
+    colors_i = np.concatenate((np.linspace(0, 1., N), (0.,0.,0.,0.)))
+    colors_rgba = cmap(colors_i)
+    indices = np.linspace(0, 1., N+1)
+    cdict = {}
+    for ki,key in enumerate(('red','green','blue')):
+        cdict[key] = [ (indices[i], colors_rgba[i-1,ki], colors_rgba[i,ki]) for i in xrange(N+1) ]
+     # Return colormap object.
+    return mpl.colors.LinearSegmentedColormap(cmap.name + "_%d"%N, cdict, 1024)
+start_time = time.time()
+
+
+DAs = [19405, 39016]
+links= [110756] # 117782
+
+filename_input = 'SC_30m_Mar17.asc'#SpringCreek_PitsFilled_30m.asc'
+
+(rmg, z) = read_esri_ascii(filename_input)
+
+elapsed_time = 0.0
+
+## Model Run Time in seconds
+model_run_time = 43200.0
+
+## Lists for saving data
+discharge_at_outlet = []
+gage_hydrographs = []
+midstream_node = []
+midstream_incision =[]
+hydrograph_time_sec = []
+hydrograph_time_hrs = []
+outlet_incisograph = []
+
+drainage_area = 'Pitfilled_SC_DrainageArea.txt'
+drainage_area = np.loadtxt(drainage_area)
+unique_da = np.unique(drainage_area)
+
+
+## Setting initial fields...
+rmg['node']['topographic__elevation'] = z
+rmg['node']['water_surface__slope'] = np.zeros(rmg.number_of_nodes)
+rmg['node']['total_incision_depth'] = np.zeros(rmg.number_of_nodes)
+rmg['node']['soil_water_infiltration__depth'] = np.zeros(rmg.number_of_nodes)
+rmg['node']['soil_water_infiltration__depth'] += 10**-3
+rmg.set_watershed_boundary_condition(z, -9999)
+outlet_id = np.where(rmg.status_at_node == 1)[0]
+link_to_sample = 110756
+total_incision_depth = np.zeros(rmg.number_of_nodes)
+
+
+storm_flag = 'LargeStorm'
+save_flag = 'False'
+
+if storm_flag == 'LargeStorm':
+    rainfall_mmhr = 80.
+    rainfall_ms = rainfall_mmhr * 2.77778 * (10**-7)
+    storm_duration = 3600.
+
+if storm_flag == 'MeanStorm':
+    rainfall_mmhr = 22.0
+    rainfall_ms = rainfall_mmhr * 2.77778 * (10**-7)
+    storm_duration = 3600.
+
+if storm_flag == 'SmallStorm':
+    rainfall_mmhr =10.0
+    rainfall_ms = rainfall_mmhr * 2.77778 * (10**-7)
+    storm_duration = 3600.
+
+if storm_flag == 'UngagedStorm':
+    rainfall_mmhr = 50.0
+    rainfall_ms = rainfall_mmhr * 2.77778 * (10**-7)
+    storm_duration = 7200.
+
+    
+rmg['link']['mannings_n'] = np.ones(rmg.number_of_links)
+rmg['link']['mannings_n'] *= 0.055
+conductivity1 = 2.7778*(10**-7)
+lowest_3_conductivity = 0.00000083333# 3 mm/hr
+low_6_conductivity = 0.000001666666667 # 6 mm/hr
+mid_9_conductivity = 0.0000025 # 9 mm/hr
+high_conductivity =  0.0000033333 # 12 mm/hr
+
+#mid_conductivity = (1.94*10**-6)#0.00001944444444444
+#high_conductivity = 0.00002222222222
+sampling_sites = [43644, 50755, 54856, 58180, 59700]
+low_soil_moisture = 0.05
+default_soil_moisture=0.15
+mid_soil_moisture = 0.2
+high_soil_moisture = 0.35
+
+of = OverlandFlow(rmg, steep_slopes=True, alpha=0.15, mannings_n='mannings_n', h_init=0.00001)
+dle = DepthSlopeProductErosion(rmg, k_e = (4.0 * (10**-9)), tau_crit=10.0)#.1)#232#704)
+siga = SoilInfiltrationGreenAmpt(rmg, surface_water_minimum_depth=of.h_init,
+                                 hydraulic_conductivity=mid_9_conductivity, 
+                                 initial_soil_moisture_content=0.35, 
+                                 soil_bulk_density=1700., rock_density=2700., 
+                                 soil_type ='sand',    
+                                 volume_fraction_coarse_fragments=0.6)
+peak_I = np.zeros(rmg.number_of_nodes)
+filenameS_template = r'/Users/Jordan/Desktop/Movies/WSS_movie_22mmhr_K6_SM35/{0:01d}.png'
+filenameT_template = r'/Users/Jordan/Desktop/Movies/Tau_movie_22mmhr_K6_SM35/{0:01d}.png'
+y= 0
+outlet_tau_list = []
+upstream_tau_list=[]
+print(rainfall_mmhr, 'Infil K = ', siga._Ks ,'n = 0.05', 'soil moisture =', round((0.37) - siga._Md, 2))
+## Running the overland flow component.
+while elapsed_time < model_run_time:
+    
+    of.dt = of.calc_time_step()
+    
+    #if (elapsed_time < storm_duration) and ((elapsed_time + of.dt) > storm_duration):
+       # of.dt = storm_duration - (elapsed_time - 0.1)
+    ## The storm starts when the model starts. While the elapsed time is less
+    ## than the storm duration, we add water to the system as rainfall.
+
+    if elapsed_time < (storm_duration):
+
+        of.rainfall_intensity =  rainfall_ms
+
+    ## Then the elapsed time exceeds the storm duration, rainfall ceases.
+    else:
+
+        of.rainfall_intensity = 0.0
+        
+    depth_dependent_mannings_n(rmg, min_mannings_n=0.055, index_flow_depth=0.05)
+    
+    rmg['link']['mannings_n'] = rmg.map_min_of_link_nodes_to_link('mannings_n')
+
+    of.overland_flow(dt=of.dt)
+
+    node_slope = ((of.water_surface_slope[rmg.links_at_node] *
+                                rmg.active_link_dirs_at_node))
+    
+    incision_Q = np.abs(of.q * rmg.dx)[rmg.links_at_node]
+
+    rmg['node']['surface_water__discharge'] = (incision_Q[np.arange(len(
+                                node_slope)), np.argmax(node_slope, axis=1)])
+
+    node_slope = node_slope.max(axis=1)
+   
+    rmg['node']['water_surface__slope'] = node_slope
+
+    siga.run_one_step(of.dt)
+    dle.erode(of.dt, slope = 'water_surface__slope')
+   # outlet_tau_list.append(dle.tau[sampling_sites])
+    
+#==============================================================================
+#     if y%20 == 0:
+#         WSS = rmg['node']['water_surface__slope']
+#         WSS[np.where(z == -9999)] = None
+#         plt.figure('Water surface slope plots')
+#         plt.subplot2grid((3,2), (0,0), colspan=2, rowspan=2)
+#         DJET = cmap_discretize(cm.ocean_r, N=25)
+#         fig = plt.imshow(WSS.reshape(rmg.shape), origin='left', cmap=DJET, vmin=0, vmax=0.25)
+#         fig.axes.get_xaxis().set_visible(False)
+#         fig.axes.get_yaxis().set_visible(False)
+#         cb = plt.colorbar()
+#     
+#         #cb.ax.tick_params(**hfont)
+#         cb.set_label('Discharge (cms)', rotation=270, labelpad=15)#, **hfont)
+#         plt.subplot2grid((3,2), (2, 0), colspan=2)
+#         discharge = np.array(np.abs(discharge_at_outlet)) * rmg.dx
+#         hr = [x / 3600. for x in hydrograph_time_sec]
+#         plt.plot(hr, discharge, 'k.')
+#         plt.ylim(0, 40)
+#         plt.ylabel('Discharge (cms)')#, **hfont)
+#         plt.xlabel('Time (hrs)')#, **hfont)
+#         plt.xlim(0, 8)
+#         plt.show()
+#     
+#         plt.savefig(filenameS_template.format(y), format='png', dpi=100)
+#         plt.close()
+#         
+# #        plt.figure('Tau lots')
+# #        tau = dle.tau
+# #        tau[np.where(z == -9999)] = None
+# #        plt.figure(1)
+# #        plt.subplot2grid((3,2), (0,0), colspan=2, rowspan=2)
+# #        DJET = cmap_discretize(cm.viridis, N=25)
+# #        fig = plt.imshow(tau.reshape(rmg.shape), origin='left', cmap=DJET, vmin=0, vmax=150)
+# #        fig.axes.get_xaxis().set_visible(False)
+# #        fig.axes.get_yaxis().set_visible(False)
+# #        cb = plt.colorbar()
+# #    
+# #        #cb.ax.tick_params(**hfont)
+# #        cb.set_label('Discharge (cms)', rotation=270, labelpad=15)#, **hfont)
+# #        plt.subplot2grid((3,2), (2, 0), colspan=2)
+# #        discharge = np.array(np.abs(discharge_at_outlet)) * rmg.dx
+# #        hr = [x / 3600. for x in hydrograph_time_sec]
+# #        plt.plot(hr, discharge, 'k.')
+# #        plt.ylim(0, 40)
+# #        plt.ylabel('Discharge (cms)')#, **hfont)
+# #        plt.xlabel('Time (hrs)')#, **hfont)
+# #        plt.xlim(0, 8)
+# #        plt.show()
+# #    
+# #        plt.savefig(filenameT_template.format(y), format='png', dpi=100)
+#         plt.close()
+#     
+#     
+#==============================================================================
+
+    
+   # dle.erode(of.dt, slope = 'water_surface__slope')
+    peak_I = np.maximum(np.abs(dle.E), peak_I)
+    z[outlet_id] -= np.min(dle.dz[rmg.neighbors_at_node[outlet_id]])
+    outlet_tau_list.append(dle.tau[sampling_sites])
+    upstream_tau_list.append(dle.tau[DAs])
+    hydrograph_time_sec.append(elapsed_time)
+    discharge_at_outlet.append(of.q[link_to_sample])
+    midstream_node.append(rmg.at_node['surface_water__discharge'][DAs])
+    midstream_incision.append(np.abs(dle.E[DAs]))
+    outlet_incisograph.append(dle.E[55744])
+    total_incision_depth += (np.abs(dle.dz))
+
+    y+=1
+    elapsed_time += of.dt
+
+sq_dx = rmg.dx ** 2
+volume_at_cells = total_incision_depth * sq_dx
+print('total incision volume: ', sum(volume_at_cells))
+
+rmg['node']['total_incision_depth'] = total_incision_depth
+
+calc_water_mass = round(np.abs((np.trapz(hydrograph_time_sec, (np.abs(
+                    discharge_at_outlet) * rmg.dx)))), 2)
+theoretical_water_mass = round(((max(drainage_area)) *
+                    (rainfall_ms) * storm_duration), 2)
+percent_error = round(((np.abs(calc_water_mass) - theoretical_water_mass) /
+                    theoretical_water_mass * 100), 2)
+
+print('Percent error: ', percent_error)
+endtime = time.time()
+print('\n', 'Overall model run time: ', round(endtime - start_time, 2), ' seconds, for alpha = ', of.alpha)
+
+
+plt.figure(1)
+#plt.title(rainfall_mmhr, '  ', siga._Ks, '   ', round(0.35 - siga._Md, 2))
+hr = [x / 3600. for x in hydrograph_time_sec]
+plt.plot(hr, np.abs(discharge_at_outlet)*rmg.dx)
+
+plt.title('Outlet Hydrograph')
+plt.xlim(0, 8)
+
+
+print(max(np.abs(discharge_at_outlet)*rmg.dx))
+#print(max(np.abs(discharge_at_outlet)*rmg.dx))
+q_midstream = np.array(midstream_node)
+i_midstream = np.array(midstream_incision)
+Ks = str(int(siga._Ks / (2.7778*(10**-7))) + 1)
+SM = str(int((round((0.37) - siga._Md, 2))*100.))
+string_for_save = str(int(rainfall_mmhr)) + 'mmhr_K' + Ks + '_SM' + SM 
+
+rmg.at_node['peak_I'] = peak_I
+
+tau_flag = 'TauCrit' # 'TauCrit'
+
+#write_esri_ascii('/Users/Jordan/Desktop/Chapter 2 - Buffalo Creek/IncisionResults/I22mmhr/' + tau_flag + '/Q' + string_for_save + '_max_erosion_rate_grid.asc', rmg, 'peak_I')
+#np.savetxt('/Users/Jordan/Desktop/Chapter 2 - Buffalo Creek/IncisionResults/I22mmhr/' + tau_flag + '/Q' + string_for_save + '_outlet_incisograph.txt', outlet_incisograph)
+#np.savetxt('/Users/Jordan/Desktop/Chapter 2 - Buffalo Creek/IncisionResults/I22mmhr/' + tau_flag + '/Q' + string_for_save + '_midstream_incisograph.txt', i_midstream[:,1])
+#np.savetxt('/Users/Jordan/Desktop/Chapter 2 - Buffalo Creek/IncisionResults/I22mmhr/' + tau_flag + '/Q' + string_for_save + '_upstream_incisograph.txt', i_midstream[:,0])
+#write_esri_ascii('/Users/Jordan/Desktop/Chapter 2 - Buffalo Creek/IncisionResults/I22mmhr/' + tau_flag + '/Q' + string_for_save + '_total_eroded_depth_grid.asc', rmg, 'total_incision_depth')
+#np.savetxt('/Users/Jordan/Desktop/Chapter 2 - Buffalo Creek/IncisionResults/I22mmhr/' + tau_flag + '/Q' + string_for_save + '_total_sed_yield.txt', volume_at_cells)
+upstream_tau_list = np.array(upstream_tau_list)
+#np.savetxt('/Users/Jordan/Desktop/Chapter 2 - Buffalo Creek/IncisionResults/I22mmhr/' + tau_flag + '/Q' + string_for_save + '_tau_graph.txt', outlet_tau_list)
+#np.savetxt('/Users/Jordan/Desktop/Chapter 2 - Buffalo Creek/IncisionResults/I80mmhr/' + tau_flag + '/Q' + string_for_save + '_tau_time.txt', hr)
+#np.savetxt('/Users/Jordan/Desktop/Chapter 2 - Buffalo Creek/IncisionResults/I22mmhr/' + tau_flag + '/Q' + string_for_save + '_tau_graph_upstream.txt', upstream_tau_list[:,0])
+#np.savetxt('/Users/Jordan/Desktop/Chapter 2 - Buffalo Creek/IncisionResults/I22mmhr/' + tau_flag + '/Q' + string_for_save + '_tau_graph_midstream.txt', upstream_tau_list[:,1])
+
+
+
+
+
+
+
+
+#np.savetxt('/Users/Jordan/Desktop/Chapter 2 - Buffalo Creek/InfiltrationTest/I10mmhr_1hr/K1mmhr_5cm/Q10mmhr_60m_ddn5cm_SIGA_35.txt', np.abs(discharge_at_outlet)*rmg.dx)
+#np.savetxt('/Users/Jordan/Desktop/Chapter 2 - Buffalo Creek/InfiltrationTest/I10mmhr_1hr/K1mmhr_5cm/T10mmhr_60m_ddn5cm_SIGA_35.txt', hr)
+#np.savetxt('/Users/Jordan/Desktop/Chapter 2 - Buffalo Creek/InfiltrationTest/I10mmhr_1hr/K1mmhr_5cm/Q10mmhr_60m_ddn5cm_SIGA_35_upstream.txt', q_midstream[:,0])
+#np.savetxt('/Users/Jordan/Desktop/Chapter 2 - Buffalo Creek/InfiltrationTest/I10mmhr_1hr/K1mmhr_5cm/Q10mmhr_60m_ddn5cm_SIGA_35_midstream.txt', q_midstream[:,1])
+###
+#np.savetxt('/Users/Jordan/Desktop/Chapter 2 - Buffalo Creek/InfiltrationTest/I80mmhr_1hr/K1mmhr_5cm/Q22mmhr_60m_ddn5cm_NOSIGA.txt', np.abs(discharge_at_outlet)*rmg.dx)
+#np.savetxt('/Users/Jordan/Desktop/Chapter 2 - Buffalo Creek/InfiltrationTest/I80mmhr_1hr/K1mmhr_5cm/T22mmhr_60m_ddn5cm_NOSIGA.txt', hr)
+#np.savetxt('/Users/Jordan/Desktop/Chapter 2 - Buffalo Creek/InfiltrationTest/I80mmhr_1hr/K1mmhr_5cm/Q22mmhr_60m_ddn5cm_NOSIGA_upstream.txt', q_midstream[:,0])
+#np.savetxt('/Users/Jordan/Desktop/Chapter 2 - Buffalo Creek/InfiltrationTest/I80mmhr_1hr/K1mmhr_5cm/Q22mmhr_60m_ddn5cm_NOSIGA_midstream.txt', q_midstream[:,1])
--- landlab/graph/graph.py
+++ landlab/graph/graph.py
@@ -52,9 +52,12 @@ array([[4, 3, 0, 1],
        [7, 6, 3, 4],
        [8, 7, 4, 5]])
 """
+import six
 from six.moves import range
 
 import numpy as np
+import xarray as xr
+import json
 
 from ..core.utils import as_id_array, argsort_points_by_x_then_y
 from ..utils.jaggedarray import flatten_jagged_array
@@ -67,6 +70,8 @@ from .quantity.of_link import (get_angle_of_link, get_length_of_link,
 from .quantity.of_patch import get_centroid_of_patch, get_area_of_patch
 
 from .sort.sort import reverse_one_to_many, reorient_link_dirs
+
+from .ugrid import update_nodes_at_patch, ugrid_from_unstructured
 
 
 def _parse_sorting_opt(sorting):
@@ -105,75 +110,158 @@ def find_perimeter_nodes(graph):
     return as_id_array(hull.vertices)
 
 
+class thawed(object):
+    def __init__(self, graph):
+        self._graph = graph
+        self._initially_frozen = graph.frozen
+
+    def __enter__(self):
+        self._graph.thaw()
+
+    def __exit__(self, ex_type, ex_value, traceback):
+        if self._initially_frozen:
+            self._graph.freeze()
+
+
 class Graph(object):
 
     """Define the connectivity of a graph of nodes, links, and patches."""
 
-    def __init__(self, nodes, links=None, patches=None, sorting=True):
+    def __init__(self, mesh, **kwds):
         """Define a graph of connected nodes.
 
         Parameters
         ----------
-        nodes : tuple of array_like
-            Coordinates of nodes as (*y*, *x*).
-        links : array_like of tuple
-            Tail node and head node for each link in the graph.
-        patches : array_like of tuple
-            Links that define each patch.
-        sort : bool, optional
-            Sort elements by their *x* and then *y* coordinate and use
-            counter-clockwise element ordering when ordering one set
-            of elements around another.
+        mesh : Dataset
+            xarray Dataset that defines the topology in ugrid format.
         """
-        sorting = _parse_sorting_opt(sorting)
-        if sorting is None:
-            raise ValueError('bad argument for sorting keyword')
-        if len(nodes[0]) != len(nodes[1]):
-            raise ValueError('length mismatch in node coordinates')
+        if not isinstance(mesh, xr.Dataset):
+            node_y_and_x = mesh
+            links = kwds.get('links', None)
+            patches = kwds.get('patches', None)
+            mesh = ugrid_from_unstructured(node_y_and_x, links=links,
+                                           patches=patches)
+        self._ds = mesh
 
-        self._sorting = sorting
+        self._frozen = False
+        self.freeze()
 
-        nodes = [np.asarray(coord, dtype=float) for coord in nodes]
-
-        if patches is not None:
-            if len(patches) > 0:
-                patches = flatten_jagged_array(patches, dtype=int)
-            else:
-                patches = None
-
-        self._xy_of_node = np.stack((nodes[1], nodes[0])).T.copy()
-        # self._y_of_node, self._x_of_node = nodes[0], nodes[1]
-        self._nodes = np.arange(len(nodes[0]), dtype=int)
-
-        self._create_nodes_at_link(links)
-        self._create_links_at_patch(patches)
-
-        not sorting['ne'] or reorient_link_dirs(self)
-        not sorting['xy'] or reindex_by_xy(self)
-        not sorting['ccw'] or reorder_links_at_patch(self)
+        if kwds.get('sort', True):
+            Graph.sort(self)
 
         self._origin = (0., 0.)
 
-    def _create_nodes_at_link(self, links):
-        """Set up node-link data structures."""
-        if links is not None:
-            self._nodes_at_link = np.asarray(links, dtype=np.int)
-            return self._nodes_at_link
+    @property
+    def frozen(self):
+        return self._frozen
 
-    def _create_links_at_patch(self, patches):
-        """Set up patch data structures."""
-        from .matrix.at_patch import links_at_patch
+    def thawed(self):
+        return thawed(self)
 
-        if patches is not None:
-            self._links_at_patch = links_at_patch(
-                patches, nodes_at_link=self.nodes_at_link)
-            return self._links_at_patch
+    def sort(self):
+        with self.thawed():
+            reorient_link_dirs(self)
+            sorted_nodes, sorted_links, sorted_patches = reindex_by_xy(self)
+            reorder_links_at_patch(self)
+
+        return sorted_nodes, sorted_links, sorted_patches
+
+    def freeze(self):
+        """Freeze the graph by making arrays read-only."""
+        for var in self.ds:
+            self.ds[var].values.flags.writeable = False
+        self._frozen = True
+
+    def thaw(self):
+        """Thaw the graph by making arrays writable."""
+        for var in self.ds:
+            self.ds[var].values.flags.writeable = True
+        self._frozen = False
+
+    def _add_variable(self, name, var, dims=None, attrs=None):
+        kwds = dict(data=var, dims=dims, attrs=attrs)
+        self.ds.update({name: xr.DataArray(**kwds)})
+        if self._frozen:
+            self.freeze()
+
+    @property
+    def ds(self):
+        return self._ds
+
+    def to_dict(self):
+        return self.ds.to_dict()
+
+    def to_json(self):
+        return json.dumps(self.ds.to_dict())
+
+    def to_netcdf(self, *args, **kwds):
+        """Write graph contents to a netCDF file.
+
+        See xarray.Dataset.to_netcdf for a complete list of parameters.
+        Below are only the most common.
+
+        Parameters
+        ----------
+        path : str, optional
+            Path to which to save this graph.
+        mode : {'w', 'a'}, optional
+            Write ('w') or append ('a') mode. If mode='w', any
+            existing file at this location will be overwritten.
+        format : {'NETCDF4', 'NETCDF4_CLASSIC', 'NETCDF3_64BIT', 'NETCDF3_CLASSIC'}, optional
+            File format for the resulting netCDF file:
+
+            * NETCDF4: Data is stored in an HDF5 file, using netCDF4 API
+              features.
+            * NETCDF4_CLASSIC: Data is stored in an HDF5 file, using only
+              netCDF 3 compatible API features.
+            * NETCDF3_64BIT: 64-bit offset version of the netCDF 3 file format,
+              which fully supports 2+ GB files, but is only compatible with
+              clients linked against netCDF version 3.6.0 or later.
+            * NETCDF3_CLASSIC: The classic netCDF 3 file format. It does not
+              handle 2+ GB files very well.
+
+            All formats are supported by the netCDF4-python library.
+            scipy.io.netcdf only supports the last two formats.
+
+            The default format is NETCDF4 if you are saving a file to disk and
+            have the netCDF4-python library available. Otherwise, xarray falls
+            back to using scipy to write netCDF files and defaults to the
+            NETCDF3_64BIT format (scipy does not support netCDF4).
+        """
+        self.ds.to_netcdf(*args, **kwds)
+
+    @classmethod
+    def from_netcdf(cls, fname):
+        return cls.from_dataset(xr.open_dataset(fname))
+
+    @classmethod
+    def from_dict(cls, meta):
+        return cls((meta['y_of_node'], meta['x_of_node']),
+                   links=meta.get('nodes_at_link', None),
+                   patches=meta.get('links_at_patch', None))
+
+    @classmethod
+    def load(cls, source):
+        if isinstance(source, six.string_types):
+            return cls.from_netcdf(source)
+        elif isinstance(source, (dict, xr.Dataset)):
+            return cls.from_dict(source)
+        else:
+            raise ValueError('source must be dict-like or NetCDF ({type})'.format(type=type(source)))
+
+    def __str__(self):
+        return str(self.ds)
+
+    def __repr__(self):
+        return repr(self.ds)
 
     @property
     def ndim(self):
         return 2
 
     @property
+    @store_result_in_grid()
+    @read_only_array
     def xy_of_node(self):
         """Get x and y-coordinates of node.
 
@@ -187,7 +275,7 @@ class Graph(object):
         >>> graph.xy_of_node[:, 1]
         array([ 0.,  0.,  0.,  1.,  1.,  1.])
         """
-        return self._xy_of_node
+        return np.stack((self.x_of_node, self.y_of_node)).T.copy()
 
     @property
     def x_of_node(self):
@@ -201,8 +289,7 @@ class Graph(object):
         >>> graph.x_of_node
         array([ 0.,  1.,  2.,  0.,  1.,  2.])
         """
-        return self._xy_of_node[:, 0]
-        # return self._x_of_node
+        return self.ds['x_of_node'].values
 
     @property
     def y_of_node(self):
@@ -216,8 +303,7 @@ class Graph(object):
         >>> graph.y_of_node
         array([ 0.,  0.,  0.,  1.,  1.,  1.])
         """
-        return self._xy_of_node[:, 1]
-        # return self._y_of_node
+        return self.ds['y_of_node'].values
 
     @property
     def nodes(self):
@@ -231,10 +317,11 @@ class Graph(object):
         >>> graph.nodes
         array([0, 1, 2, 3, 4, 5])
         """
-        return self._nodes
+        return self.ds['node'].values
 
     @property
     @store_result_in_grid()
+    @read_only_array
     def perimeter_nodes(self):
         return find_perimeter_nodes(self)
 
@@ -247,10 +334,10 @@ class Graph(object):
         >>> from landlab.graph import Graph
         >>> node_x, node_y = [0, 1, 2, 0, 1, 2], [0, 0, 0, 1, 1, 1]
         >>> graph = Graph((node_y, node_x))
-        >>> graph.number_of_nodes
-        6
+        >>> graph.number_of_nodes == 6
+        True
         """
-        return self._nodes.size
+        return self.ds.dims['node']
 
     @property
     def nodes_at_link(self):
@@ -273,7 +360,7 @@ class Graph(object):
                [3, 6], [4, 7], [5, 8],
                [6, 7], [7, 8]])
         """
-        return self._nodes_at_link
+        return self.ds['nodes_at_link'].values
 
     @property
     def node_at_link_tail(self):
@@ -292,7 +379,7 @@ class Graph(object):
         >>> graph.node_at_link_tail
         array([0, 1, 0, 1, 2, 3, 4, 3, 4, 5, 6, 7])
         """
-        return self._nodes_at_link[:, 0]
+        return self.nodes_at_link[:, 0]
 
     @property
     def node_at_link_head(self):
@@ -311,7 +398,7 @@ class Graph(object):
         >>> graph.node_at_link_head
         array([1, 2, 3, 4, 5, 4, 5, 6, 7, 8, 7, 8])
         """
-        return self._nodes_at_link[:, 1]
+        return self.nodes_at_link[:, 1]
 
     @property
     def number_of_links(self):
@@ -327,12 +414,12 @@ class Graph(object):
         ...          (3, 6), (4, 7), (5, 8),
         ...          (6, 7), (7, 8))
         >>> graph = Graph((node_y, node_x), links=links)
-        >>> graph.number_of_links
-        12
+        >>> graph.number_of_links == 12
+        True
         """
         try:
-            return len(self._nodes_at_link)
-        except AttributeError:
+            return self.ds.dims['link']
+        except KeyError:
             return 0
 
     @property
@@ -354,9 +441,11 @@ class Graph(object):
         array([[3, 5, 2, 0],
                [4, 6, 3, 1]])
         """
-        return self._links_at_patch
+        return self.ds['links_at_patch'].values
 
     @property
+    # @store_result_in_grid()
+    @read_only_array
     def nodes_at_patch(self):
         """Get the nodes that define a patch.
 
@@ -376,13 +465,11 @@ class Graph(object):
         array([[4, 3, 0, 1],
                [5, 4, 1, 2]])
         """
-        try:
-            return self._nodes_at_patch
-        except AttributeError:
-            self._nodes_at_patch = get_nodes_at_patch(self)
-            return self._nodes_at_patch
+        return get_nodes_at_patch(self)
 
     @property
+    @store_result_in_grid()
+    @read_only_array
     def patches_at_node(self):
         """Get the patches that touch each node.
 
@@ -400,11 +487,7 @@ class Graph(object):
         array([[ 0, -1], [ 0,  1], [ 1, -1],
                [ 0, -1], [ 0,  1], [ 1, -1]])
         """
-        try:
-            return self._patches_at_node
-        except AttributeError:
-            self._patches_at_node = reverse_one_to_many(self.nodes_at_patch)
-            return self._patches_at_node
+        return reverse_one_to_many(self.nodes_at_patch)
 
     @property
     @store_result_in_grid()
@@ -427,7 +510,18 @@ class Graph(object):
                [ 0, -1], [ 0,  1], [ 1, -1],
                [ 0, -1], [ 1, -1]])
         """
-        return reverse_one_to_many(self._links_at_patch)
+        return reverse_one_to_many(self.links_at_patch, min_counts=2)
+        try:
+            return self.ds['patches_at_link'].values
+        except KeyError:
+            patches_at_link = xr.DataArray(
+                data=reverse_one_to_many(self.links_at_patch, min_counts=2),
+                dims=('link', 'Two'),
+                attrs={'cf_role': 'edge_node_connectivity',
+                       'long_name': 'patches on either side of a link',
+                       'start_index': 0})
+            self.ds.update({'patches_at_link': patches_at_link})
+            return self.ds['patches_at_link'].values
 
     @property
     def number_of_patches(self):
@@ -444,12 +538,12 @@ class Graph(object):
         ...          (6, 7), (7, 8))
         >>> patches = ((0, 3, 5, 2), (1, 4, 6, 3))
         >>> graph = Graph((node_y, node_x), links=links, patches=patches)
-        >>> graph.number_of_patches
-        2
+        >>> graph.number_of_patches == 2
+        True
         """
         try:
-            return len(self._links_at_patch)
-        except AttributeError:
+            return self.ds.dims['patch']
+        except KeyError:
             return 0
 
     @property
@@ -480,7 +574,8 @@ class Graph(object):
             return self._links_at_node
 
     def _create_links_and_dirs_at_node(self):
-        return get_links_at_node(self, sort=self._sorting['ccw'])
+        return get_links_at_node(self, sort=True)
+        # return get_links_at_node(self, sort=self._sorting['ccw'])
 
     @property
     def link_dirs_at_node(self):
