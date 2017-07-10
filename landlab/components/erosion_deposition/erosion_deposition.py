2c529a963b44c2172e511cc124e786968167b2e0
cmshobe <cmshobe@email.wm.edu>
2017-07-10 11:00:21 -0600

    docstring for class

--- landlab/components/erosion_deposition/erosion_deposition.py
+++ landlab/components/erosion_deposition/erosion_deposition.py
@@ -1,9 +1,92 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Jun 19 09:40:26 2017
+import numpy as np
+from landlab import Component
 
-@author: Charlie Shobe
-
-Landlab erosion-deposition component, in the style of Davy and Lague (2009)
-"""
-
+class ErosionDeposition(Component):
+    """
+    Erosion-Deposition model in the style of Davy and Lague (2009)
+    
+    Component written by C. Shobe, begun July 2016.
+    """
+    
+    _name= 'ErosionDeposition'
+    
+    _input_var_names = (
+        'flow__receiver_node',
+        'flow__upstream_node_order',
+        'topographic__steepest_slope',
+        'drainage_area',
+    )
+    
+    _output_var_names = (
+        'topographic__elevation'
+    )
+    
+    _var_units = {
+        'flow__receiver_node': '-',
+        'flow__upstream_node_order': '-',
+        'topographic__steepest_slope': '-',
+        'drainage_area': 'm**2',
+        'topographic__elevation': 'm',
+    }
+    
+    _var_mapping = {
+        'flow__receiver_node': 'node',
+        'flow__upstream_node_order': 'node',
+        'topographic__steepest_slope': 'node',
+        'drainage_area': 'node',
+        'topographic__elevation': 'node',
+    }
+    
+    _var_doc = {
+        'flow__receiver_node':
+            'Node array of receivers (node that receives flow from current '
+            'node)',
+        'flow__upstream_node_order':
+            'Node array containing downstream-to-upstream ordered list of '
+            'node IDs',
+        'topographic__steepest_slope':
+            'Topographic slope at each node',
+        'drainage_area':
+            "Upstream accumulated surface area contributing to the node's "
+            "discharge",
+        'topographic__elevation': 
+            'Land surface topographic elevation',
+    }
+    
+    def __init__(self, grid, K=None, phi=None, v_s=None, 
+                 m_sp=None, n_sp=None, sp_crit=None, 
+                 method=None, discharge_method=None, 
+                 area_field=None, discharge_field=None, **kwds):
+        """Initialize the HybridAlluvium model.
+        
+        Parameters
+        ----------
+        grid : ModelGrid
+            Landlab ModelGrid object
+        K : float
+            Erodibility constant for sediment (units vary).
+        phi : float
+            Sediment porosity [-].
+        v_s : float
+            Effective settling velocity for chosen grain size metric [L/T].
+        m_sp : float
+            Drainage area exponent (units vary)
+        n_sp : float
+            Slope exponent (units vary)
+        sp_crit : float
+            Critical stream power to erode sediment [E/(TL^2)]
+        method : string
+            Either "simple_stream_power", "threshold_stream_power", or
+            "stochastic_hydrology". Method for calculating sediment
+            and bedrock entrainment/erosion.
+        discharge_method : string
+            Either "area_field" or "discharge_field". If using stochastic
+            hydrology, determines whether component is supplied with
+            drainage area or discharge.
+        area_field : string or array
+            Used if discharge_method = 'area_field'. Either field name or
+            array of length(number_of_nodes) containing drainage areas [L^2].
+        discharge_field : string or array
+            Used if discharge_method = 'discharge_field'.Either field name or
+            array of length(number_of_nodes) containing drainage areas [L^2/T].
+        """
