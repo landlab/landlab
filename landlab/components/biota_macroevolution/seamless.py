c659c559619c148470fb58091cf4dafc58d60432
nathanlyons <nathan.j.lyons@gmail.com>
2017-10-01 19:50:54 -0500

    species range is now tracked in species class

--- landlab/components/biota_macroevolution/seamless.py
+++ landlab/components/biota_macroevolution/seamless.py
@@ -4,8 +4,7 @@ Component written by Nathan Lyons beginning August 20, 2017.
 """
 
 from copy import deepcopy
-from itertools import groupby
-from landlab import Component, FieldError
+from landlab import Component
 from matplotlib.lines import Line2D
 import matplotlib.pyplot as plt
 import numpy as np
@@ -37,17 +36,15 @@ class CladeDiversifier(Component):
     
     _name = 'CladeDiversifier'
 
-    _input_var_names = ('region__id')
+    _input_var_names = ()
 
     _output_var_names = ()
 
-    _var_units = {'region__id': '-'}
+    _var_units = {}
 
-    _var_mapping = {'region__id': 'node'}
+    _var_mapping = {}
 
-    _var_doc = {
-        'region__id': 'the identifier of the region'
-    }
+    _var_doc = {}
     
     def __init__(self, grid, minimum_capture_area=0,
                  extinction_area_threshold=0, **kwds):
@@ -68,27 +65,10 @@ class CladeDiversifier(Component):
         self._minimum_capture_area = minimum_capture_area
         self._extinction_area_threshold = extinction_area_threshold
         self.timestep = -1
-        self.time = {}
+        self.at_timestep = {'time': [], 'captured_area': []}
         self.species = []
         
-        # Create fields.
-        if 'region__id' in grid.at_node:
-            self.grab_region_snapshot()
-        else:
-            self._grid.add_zeros('node', 'region__id')
-            self._grid.at_node['region__id'][:] = -1
-#            raise FieldError(
-#                "A 'region__id' field is required as a component input.")
-            
-    @property
-    def region_ids(self):
-        """Get the region ids of the current timestep."""
-        return self._unique(self._grid.at_node['region__id'])
-    
-    @property
-    def prior_region_ids(self):
-        """Get the region ids of the prior timestep."""
-        return self._unique(self._prior_region)
+        
     
     @property
     def species_ids(self):
@@ -98,108 +78,86 @@ class CladeDiversifier(Component):
             ids.append(species.identifier)
         return ids
     
+    @property
+    def timesteps(self):
+        number_of_timesteps = len(self.at_timestep['time'])
+        return range(number_of_timesteps)
+    
     def run_one_step(self, current_time):
-        """Evalute region for captures and adjust species for the time,
-        'current_time'.
-        """
-        mg = self._grid
-        
         self.timestep += 1
-        self.time[self.timestep] = current_time
         
-        # Update regions.
-        
-        reg_update = {}
-        nodes_above_area_threshold = (mg.at_node['drainage_area'] >= 
-                                      self._minimum_capture_area)
-
-        for reg_id in self.region_ids: 
-            
-            reg_mg = mg.at_node['region__id'] == reg_id
-
-            reg_update[reg_id] = {}
-            
-            # Mark region for extinction if area is below extinction parameter.
-            A = mg.at_node['drainage_area'][reg_mg]
-            A_extinct_thresh = self._extinction_area_threshold
-            reg_update[reg_id]['extinction'] = max(A) < A_extinct_thresh
-
-            for prior_reg_id in self.prior_region_ids:
-                if prior_reg_id not in reg_update.keys():
-                    reg_update[prior_reg_id] = {}
-                reg_update[prior_reg_id].setdefault('parent_region_ids', [])
-                
-                # Skip loop if comparing the same region.
-                if prior_reg_id == reg_id:
-                    continue
-                
-                # Identify nodes intersecting regions and those above the
-                # drainage area threshold.
-                prior_reg_mg = self._prior_region == prior_reg_id
-                intersecting_nodes = np.all([prior_reg_mg, reg_mg,
-                                             nodes_above_area_threshold], 0)
-                
-                regions_intersected = any(intersecting_nodes)
-                
-                if regions_intersected:                    
-                    print(33333,reg_id)
-                    reg_update[prior_reg_id]['parent_region_ids'].append(reg_id)
-
-        self.grab_region_snapshot()
-        
-        pprint(reg_update)
-        
-        # Update species.
+        mg = self._grid
+        A = mg.at_node['drainage_area']
+        AE = self._extinction_area_threshold
+        stream_nodes = A >= self._minimum_capture_area
         
         new_species = []
         
-        if self.timestep < 1:
-            prior_timestep = 0
+        if self.timestep == 0:
+            prior_species = self.species
         else:
-            prior_timestep = self.timestep - 1
-        print(0,prior_timestep)
-        for species in self.species:
-            if prior_timestep in species.phylogeny.keys():
-                print(1,species.phylogeny.keys())
-                species_regions = species.phylogeny[prior_timestep]['regions']
-                timestep_regions = []
-                
-                for region in species_regions:
-                    print(2,region)
-                    parent_regions = reg_update[region]['parent_region_ids']
-
-                    # Reassign region to species or add new species based upon
-                    # region history.
-                    if reg_update[reg_id]['extinction']:
-                        continue
-                    elif len(parent_regions) == 0:
-                        timestep_regions.append(region)
-                    else:
-                        for pr in parent_regions:
-                            new_species.append(Species(self.timestep, [pr],
-                                                       species.identifier))
-                            new_species.append(Species(self.timestep, [region],
-                                                       species.identifier))
-                    
-                if len(timestep_regions) > 0:
-                    species.record_timestep(self.timestep, timestep_regions)
-                
-        for species in new_species:
-            self.species.append(species)
+            prior_species = self.species_at_timestep(self.timestep - 1)
         
+        captured_nodes = []
+        
+        for species in prior_species:
+            prior_range = deepcopy(species.nodes)
+            species.update_range(mg, self._minimum_capture_area)
+            updated_range = species.nodes
+            persistent_range = np.all([prior_range, updated_range, stream_nodes], 0)
+            
+            if len(np.where(persistent_range)[0]) == 0:
+                persistent_range_max_A = 0
+            else:
+                persistent_range_max_A = max(A[persistent_range])
+    
+            new_range = np.all([prior_range, np.invert(updated_range)], 0)
+            captured_nodes.extend(np.where(new_range)[0])
+            
+            barrier_created = any(persistent_range) and any(new_range)
+
+            if barrier_created:
+                # Add new species.
+                parent_id = species.identifier
+
+#                if max(A[new_range]) >= AE:
+                species = Species(self.timestep, new_range, parent_id)
+                new_species.append(species)
+            
+#                if persistent_range_max_A >= AE:
+                species = Species(self.timestep, updated_range, parent_id)
+                new_species.append(species)
+
+            else:
+                # Update existing species.
+                if persistent_range_max_A <= AE:
+                    # Extinction.
+                    print(999)
+                    species.nodes = prior_range  
+                else:
+                    # No changes so species persists.
+                    species.timesteps_existed.append(self.timestep)
+                    
+#        print(len(self.species), self.species_at_timestep(self.timestep - 1), current_time)
+        self.species.extend(new_species)
+        
+        self.at_timestep['time'].append(current_time)
+        captured_area = len(np.unique(captured_nodes)) * mg.dx * mg.dy
+        self.at_timestep['captured_area'].append(captured_area)
+    
     # Species methods
     
-    def add_new_species(self, timestep, regions, parent_species_id=-1):
-        species = Species(timestep, regions, parent_species_id=-1)
+    def add_new_species(self, timestep, nodes, parent_species_id=-1):
+        species = Species(timestep, nodes, parent_species_id)
         self.species.append(species)
         return species
-        
-    def remove_species_from_region(self, timestep, species, region):
-        updated_regions = deepcopy(species.phylogeny[timestep - 1]['regions'])
-        updated_regions.remove(region)
-
-        if len(updated_regions) > 0:
-            species.record_timestep(timestep, updated_regions)
+    
+    def species_at_timestep(self, step):
+        species = []
+        for s in self.species:
+            if step in s.timesteps_existed:
+                species.append(s)
+        return species
     
     def species_at_nodes_at_current_timestep(self, nodes):
         species = self.species_at_nodes(self.regions, nodes)
@@ -216,14 +174,6 @@ class CladeDiversifier(Component):
                 species.append(region.species)
         return species    
     
-    def species_in_region(self, timestep, region):
-        region_species = []
-        for species in self.species:
-            if species.exists_at_timestep(timestep):
-                if region in species.phylogeny[timestep]['regions']:
-                    region_species.append(species)
-        return region_species
-    
     def species_with_id(self, identifier):
         return self._object_with_identifier(self.species, identifier)
     
@@ -231,24 +181,20 @@ class CladeDiversifier(Component):
         
         tree = {}
         
-        steps = list(reversed(list(self.time.keys())))
+        steps = list(reversed(self.timesteps))
 
         for step in steps:
-            time = self.time[step]            
+            time = self.at_timestep['time'][step]           
 
             # Get species that existed in step.
             tree[time] = {}
-            step_species = []
             for species in self.species:
-                if step in species.phylogeny.keys():
-                    step_species.append(species)      
-                    
-            # Group species by parent.
-            groups = groupby(step_species, lambda x: x.parent_species_id)
-            for parent_id, group in groups:             
-                
-                group_species = [species for species in group]
-                tree[time][parent_id] = group_species
+                if step in species.timesteps_existed:
+                    key = species.parent_species_id
+                        
+                    if key not in tree[time].keys():
+                        tree[time][key] = []
+                    tree[time][key].append(species)
                 
         return tree
     
@@ -264,20 +210,18 @@ class CladeDiversifier(Component):
         tree = self.get_tree()
         pprint(tree)
     
-    # Region methods.
-    
-    def grab_region_snapshot(self):
-        self._prior_region = deepcopy(self._grid.at_node['region__id'])
-    
     # Plotting methods.
         
     def plot_number_of_species(self):
+        
+        time_in_ky = np.multiply(self.at_timestep['time'], 1e-3)
+        
         plt.figure('Number of species')
 
         time_species_existed = np.array([])
         for species in self.species:
             time_species_existed = np.append(time_species_existed, 
-                                             list(species.phylogeny.keys()))
+                                             species.timesteps_existed)
             
         time_species_existed = time_species_existed.flatten()
                 
@@ -299,14 +243,14 @@ class CladeDiversifier(Component):
         plt.figure('Phylogeny', facecolor='white')
         ax = plt.axes(frameon=False)
         
-        y_interval = 1
+        y_spacing = 1
        
         species_position = {}
         times = list(tree.keys())
-        
-        # Construct tree beginning at last time.
+
+        # Construct tree beginning at final time.
         for i, time in enumerate(times):
-                        
+
             if time == max(times):
                 prior_time = time
                 next_time = times[i + 1]
@@ -318,57 +262,56 @@ class CladeDiversifier(Component):
                 next_time = times[i + 1]
 
             x_max = (time + (prior_time - time) * 0.5) * x_multiplier
-            x_mid = time * x_multiplier
             x_min = (time - (time - next_time) * 0.5) * x_multiplier
+            x_mid = np.mean([x_min, x_max])
 
-            y = 0
-            y_initial = 0
+            y_min = deepcopy(y_spacing)
                   
             for parent_id, species_list in tree[time].items():
-                group_only = False
-                
-                y_min = deepcopy(y)
-                
+                species_group = False
+
+                y_species = deepcopy(y_min)
+
                 for species in species_list:
                     
+                    no_parent = species.parent_species_id == -1
+                    
+                    existed_prior_step = (len(times) - i - 2) in species.timesteps_existed
+
                     if species.identifier in species_position.keys():
-                        species_position[species.identifier]
+                        y_species = species_position[species.identifier]
+
+                    # Draw line when a species continues across timesteps.
+                    if existed_prior_step or no_parent:
+                        x = x_min
                     else:
-                        species_position[species.identifier] = y_initial
-                        y_initial += y_interval
+                        x = x_mid
+                        species_group = True
                     
-                    y = species_position[species.identifier]
-                    
-                    existed_prior_step = (len(times) - i - 2) in species.phylogeny.keys()
-                    if existed_prior_step:
-                        # Draw line when a species continues across timesteps.
-                        plt.plot([x_max, x_min], [y, y], 'k')
-                        y_min += y_interval * 0.5
+                    plt.plot([x, x_max], [y_species, y_species], 'k')
                         
-                    else:   
-                        # Draw the base of species line.
-                        plt.plot([x_max, x_mid], [y, y], 'b')
-                        
-                        # Draw line that connects species at a timestep.
-                        if time != 0:
-                            y_max = y
-                            plt.plot([x_mid, x_mid], [y_min, y_max], 'c')
-                            group_only = True
-                        
-                    y += y_interval
-                    y_max = y - y_interval
-                    y_mid = (y_max - y_min) * 0.5 + y_min
-                    
-                    species_position[parent_id] = y_mid
+                    y_mid = np.mean([y_min, y_species])
+
+                    if parent_id not in species_position.keys():
+                        species_position[parent_id] = y_mid
                 
                 # Plot trunk
-                if group_only:
-                    y_mid = (y_max - y_min) * 0.5 + y_min
-                    plt.plot([x_mid, x_min], [y_mid, y_mid], 'r')
-        
+                if species_group:
+                    
+                    # Draw line that connects species at a timestep.
+                    if time != 0:
+                        plt.plot([x_mid, x_mid], [y_min, y_species], 'r')
+                    
+                    # Draw the base line of a species group.
+                    plt.plot([x_min, x_mid], [y_mid, y_mid], 'c')
+                    
+                    species_position[parent_id] = y_mid
+                                                            
+                y_min = deepcopy(y_species + y_spacing)
+                        
         # Format figure.
         plt.xlim(xmin=0, xmax=max(times) * x_multiplier)
-        plt.ylim(ymin=y_interval * 0.5)
+        plt.ylim(ymin=y_spacing * 0.5)
         ax.get_xaxis().tick_bottom()
         ax.axes.get_yaxis().set_visible(False)
         xmin, xmax = ax.get_xaxis().get_view_interval()
@@ -398,13 +341,24 @@ class CladeDiversifier(Component):
         delta_area = []
         number_of_species = []
         for t in timesteps:
-            delta_area.append(self.region_data[t + 1]['stream_area'] - self.region_data[t]['stream_area'])
+            delta_area.append(self.region_data[t + 1]['stream_area'] - 
+                              self.region_data[t]['stream_area'])
             number_of_species.append(self.region_data[t+1]['number_of_species'])
         
         plt.figure('Number of species')
         plt.plot(delta_area, number_of_species, 'k.')
         plt.xlabel('Delta stream area ($m^2$)')
         plt.ylabel('Number of species')
+    
+    def plot_captured_area(self):
+        
+        time_in_ky = np.multiply(self.at_timestep['time'], 1e-3)
+        area_in_km = np.multiply(self.at_timestep['captured_area'], 1e-6)
+
+        plt.figure('Captured area')
+        plt.plot(time_in_ky, area_in_km, 'k')
+        plt.xlabel('Time (ky)')
+        plt.ylabel('Area captured ($km^2$)')
     
     # Convenience methods.
         
--- landlab/components/biota_macroevolution/species.py
+++ landlab/components/biota_macroevolution/species.py
@@ -1,29 +1,75 @@
 """TODO: Description.
 """
 
+from copy import deepcopy
+from landlab import FieldError
+import numpy as np
 from uuid import uuid4
+from watershed import get_watershed_array
 
 class Species(object):
     
-    def __init__(self, timestep, regions, parent_species_id=-1):
+    def __init__(self, timestep, nodes, parent_species_id=-1):
         """Initialize Species.
         """
         
-        self.identifier = uuid4()
-        
-        self.phylogeny = {}  
-        self.record_timestep(timestep, regions)
-        
+        if timestep == 0:
+            self.timesteps_existed = []
+        else:
+            self.timesteps_existed = [timestep]
+        self.nodes = nodes
         self.parent_species_id = parent_species_id
+        self.identifier = uuid4()
     
-    @property
-    def phylogeny_timesteps(self):
-        """Get the timesteps when the species existed."""
-        return self.phylogeny.keys()
-    
-    def record_timestep(self, timestep, regions):
-        self.phylogeny[timestep] = {'regions': regions}
+    def update_range(self, grid, min_stream_drainage_area):
         
-    def exists_at_timestep(self, timestep):
-        return timestep in self.phylogeny.keys()
-    
+        if 'drainage_area' not in grid.at_node.keys():
+            raise FieldError('A grid "drainage_area" field is required to '
+                             'update a species range.')
+        
+        if 'flow__receiver_node' not in grid.at_node.keys():
+            raise FieldError('A grid "flow__receiver_node" field is required '
+                             'to update a species range.')
+        
+        A = grid.at_node['drainage_area']
+        stream_nodes = A >= min_stream_drainage_area
+        receiver_at_node = grid.at_node['flow__receiver_node']
+        
+        # The while loops continues until all nodes are moved from
+        # unresolved to updated. 
+        unresolved_nodes = self.nodes
+        updated_nodes = np.zeros(grid.number_of_nodes, dtype=bool)
+        
+        while any(unresolved_nodes):
+        
+            # Each loop processes the largest watershed of unresolved nodes.
+            max_A = max(A[unresolved_nodes])
+            max_A_array = np.all([A == max_A, unresolved_nodes], 0)
+            
+            # Find the outlet of the watershed that contains the species.
+            prior_outlet = np.where(max_A_array)[0][0]
+            giver_node = prior_outlet
+            receiver_node = receiver_at_node[prior_outlet]
+
+            if prior_outlet == receiver_node:
+                updated_outlet = receiver_node
+            else:
+                outlet_not_found = True
+                while outlet_not_found:
+                    if np.any([grid.node_is_boundary(receiver_node),
+                               receiver_node == receiver_at_node[receiver_node]]):
+                        outlet_not_found = False
+                        updated_outlet = giver_node
+                    else:
+                        giver_node = deepcopy(receiver_node)
+                        receiver_node = receiver_at_node[receiver_node]
+
+            new_range = get_watershed_array(grid, updated_outlet)
+            new_range_streams = np.all([new_range, stream_nodes], 0)
+            updated_nodes = np.any([updated_nodes, new_range_streams], 0)
+            
+            # Identify species nodes not yet updated.
+            inverted_updated_nodes = np.invert(updated_nodes)
+            unresolved_nodes = np.all([inverted_updated_nodes, self.nodes, stream_nodes], 0)
+            
+        self.nodes = updated_nodes
