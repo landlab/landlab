061db8b6bdad1da13d38010c37b91cc53a569f65
mcflugen <mcflugen@gmail.com>
2017-10-03 21:54:00 -0600

    Add a subsidence landlab component.

---
+++ landlab/components/submarine_diffusion/subsidence.py
@@ -0,0 +1,68 @@
+#! /usr/bin/env python
+import numpy as np
+from scipy.interpolate import interp1d
+from landlab import RasterModelGrid, Component
+
+
+class SubsidenceTimeSeries(Component):
+
+    _name = 'Subsider'
+
+    _time_units = 'y'
+
+    _input_var_names = ()
+
+    _output_var_names = (
+        'bedrock_surface__increment_of_elevation',
+        'bedrock_surface__elevation',
+    )
+
+    _var_units = {
+        'bedrock_surface__increment_of_elevation': 'm',
+        'bedrock_surface__elevation': 'm',
+    }
+
+    _var_mapping = {
+        'bedrock_surface__increment_of_elevation': 'node',
+        'bedrock_surface__elevation': 'node',
+    }
+
+    _var_doc = {
+        'bedrock_surface__increment_of_elevation': 'Increment of elevation',
+        'bedrock_surface__elevation': 'Surface elevation',
+    }
+
+    def __init__(self, grid, filepath=None, kind='linear', **kwds):
+        """Generate subsidence rates.
+
+        Parameters
+        ----------
+        grid: RasterModelGrid
+            A landlab grid.
+        filepath: str
+            Name of csv-formatted subsidence file.
+        kind: str, optional
+            Kind of interpolation as a string (one of 'linear',
+            'nearest', 'zero', 'slinear', 'quadratic', 'cubic').
+            Default is 'linear'.
+        """
+        super(SubsidenceTimeSeries, self).__init__(grid, **kwds)
+
+        data = np.loadtxt(filepath, delimiter=',')
+        subsidence = interp1d(data[:, 0], data[:, 1], kind=kind,
+                              copy=True, assume_sorted=True,
+                              bounds_error=True)
+        inc = self.grid.add_empty('bedrock_surface__increment_of_elevation',
+                                  at='node').reshape(self.grid.shape)
+        inc[:] = subsidence(self.grid.x_of_node[self.grid.nodes_at_bottom_edge])
+
+        self._time = 0.
+
+    @property
+    def time(self):
+        return self._time
+
+    def run_one_step(self, dt):
+        self.grid.at_node['bedrock_surface__elevation'] += (
+            self.grid.at_node['bedrock_surface__increment_of_elevation'] * dt)
+        self._time += dt
