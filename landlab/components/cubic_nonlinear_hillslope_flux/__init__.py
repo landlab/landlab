4c1e31fa49ad89dde78107f2d777b9a94dcc9691
Katy Barnhart <katy.barnhart@gmail.com>
2017-11-06 10:34:05 -0700

    changing component names, __init__ files, and component file names

--- landlab/components/cubic_nonlinear_hillslope_flux/__init__.py
+++
@@ -1,4 +0,0 @@
-from .cubic_nonlinear_hillslope_flux import CubicNonLinearDiffuser
-
-__all__ = ['CubicNonLinearDiffuser', ]
-
--- landlab/components/depth_dependent_cubic_soil_creep/__init__.py
+++
@@ -1,4 +0,0 @@
-from .hillslope_depth_dependent_cubic_flux import DepthDependentCubicDiffuser
-
-__all__ = ['DepthDependentCubicDiffuser', ]
-
