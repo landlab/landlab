7e8b64aa53bb38b0c751486f4b736cefacb315a9
mcflugen <mcflugen@gmail.com>
2017-11-09 13:09:52 -0700

    Add doctest for adjacent_nodes_at_node.

--- landlab/grid/voronoi.py
+++ landlab/grid/voronoi.py
@@ -116,6 +116,19 @@ class VoronoiDelaunayGrid(ModelGrid):
             1.,  1.,  1.,
             2.,  2.,  2.,
             3.,  3.,  3.])
+    >>> vmg.adjacent_nodes_at_node
+    array([[ 1,  3, -1, -1, -1, -1],
+           [ 2,  4,  3,  0, -1, -1],
+           [ 5,  4,  1, -1, -1, -1],
+           [ 4,  6,  0,  1, -1, -1],
+           [ 5,  7,  6,  3,  1,  2],
+           [ 8,  7,  4,  2, -1, -1],
+           [ 7,  9,  3,  4, -1, -1],
+           [ 8, 10,  9,  6,  4,  5],
+           [11, 10,  7,  5, -1, -1],
+           [10,  6,  7, -1, -1, -1],
+           [11,  9,  7,  8, -1, -1],
+           [10,  8, -1, -1, -1, -1]])
     """
 
     def __init__(self, x=None, y=None, reorient_links=True, **kwds):
@@ -237,7 +250,7 @@ class VoronoiDelaunayGrid(ModelGrid):
         # create link x, y:
         self._create_link_face_coords()
 
-        self._create_neighbors()
+        # self._create_neighbors()
 
     @property
     def number_of_patches(self):
