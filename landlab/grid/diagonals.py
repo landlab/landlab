a642d58b00b531eb8d90c3f4515088ea09aaa5c8
mcflugen <mcflugen@gmail.com>
2017-11-09 20:19:36 -0700

    Add doctests for diagonals_at_node, d8_at_node.

--- landlab/grid/diagonals.py
+++ landlab/grid/diagonals.py
@@ -306,7 +306,29 @@ class DiagonalsMixIn(object):
 
     @property
     @cache_result_in_object()
+    @make_return_array_immutable
     def diagonals_at_node(self):
+        """Diagonals attached to nodes.
+
+        Returns
+        -------
+        ndarray of int, shape `(n_nodes, 4)`
+            Diagonals at each node.
+
+        Examples
+        --------
+        >>> from landlab import RasterModelGrid
+        >>> grid = RasterModelGrid((4, 3))
+        >>> grid.diagonals_at_node.shape == (grid.number_of_nodes, 4)
+        True
+        >>> grid.diagonals_at_node
+        array([[ 0, -1, -1, -1], [ 2,  1, -1, -1], [-1,  3, -1, -1],
+               [ 4, -1, -1,  1], [ 6,  5,  0,  3], [-1,  7,  2, -1],
+               [ 8, -1, -1,  5], [10,  9,  4,  7], [-1, 11,  6, -1],
+               [-1, -1, -1,  9], [-1, -1,  8, 11], [-1, -1, 10, -1]])
+
+        LLCATS: NINF LINF CONN
+        """
         return create_diagonals_at_node(self.shape)
 
     @property
@@ -332,7 +354,7 @@ class DiagonalsMixIn(object):
         --------
         >>> from landlab import RasterModelGrid
         >>> grid = RasterModelGrid((4, 3))
-        >>> diagonals = grid.diagonal_adjacent_nodes_at_node
+        >>> grid.diagonal_adjacent_nodes_at_node
         array([[ 4, -1, -1, -1], [ 5,  3, -1, -1], [-1,  4, -1, -1],
                [ 7, -1, -1,  1], [ 8,  6,  0,  2], [-1,  7,  1, -1],
                [10, -1, -1,  4], [11,  9,  3,  5], [-1, 10,  4, -1],
@@ -387,7 +409,46 @@ class DiagonalsMixIn(object):
 
     @property
     @cache_result_in_object()
+    @make_return_array_immutable
     def d8_at_node(self):
+        """Links and diagonals attached to nodes.
+
+        Returns
+        -------
+        ndarray of int, shape `(n_nodes, 8)`
+            Links and diagonals at each node.
+
+        Examples
+        --------
+        >>> import numpy as np
+        >>> from landlab import RasterModelGrid
+
+        >>> grid = RasterModelGrid((3, 4))
+        >>> grid.d8_at_node.shape == (grid.number_of_nodes, 8)
+        True
+        >>> grid.d8_at_node
+        array([[ 0,  3, -1, -1, 17, -1, -1, -1],
+               [ 1,  4,  0, -1, 19, 18, -1, -1],
+               [ 2,  5,  1, -1, 21, 20, -1, -1],
+               [-1,  6,  2, -1, -1, 22, -1, -1],
+               [ 7, 10, -1,  3, 23, -1, -1, 18],
+               [ 8, 11,  7,  4, 25, 24, 17, 20],
+               [ 9, 12,  8,  5, 27, 26, 19, 22],
+               [-1, 13,  9,  6, -1, 28, 21, -1],
+               [14, -1, -1, 10, -1, -1, -1, 24],
+               [15, -1, 14, 11, -1, -1, 23, 26],
+               [16, -1, 15, 12, -1, -1, 25, 28],
+               [-1, -1, 16, 13, -1, -1, 27, -1]])
+        >>> np.all(grid.d8_at_node[:, :4] == grid.links_at_node)
+        True
+
+        >>> diagonals_at_node = grid.d8_at_node[:, 4:] - grid.number_of_links
+        >>> diagonals_at_node[grid.d8_at_node[:, 4:] == -1] = -1
+        >>> np.all(diagonals_at_node == grid.diagonals_at_node)
+        True
+
+        LLCATS: NINF LINF CONN
+        """
         diagonals_at_node = self.diagonals_at_node.copy()
         diagonals_at_node[diagonals_at_node >= 0] += self.number_of_links
         return np.hstack((super(DiagonalsMixIn, self).links_at_node,
