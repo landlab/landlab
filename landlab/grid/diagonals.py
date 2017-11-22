da057b8eb49b33582f3f73a1e602ddfdc8b8ef61
mcflugen <mcflugen@gmail.com>
2017-11-22 15:35:14 -0700

    Add docstrings; Set returned arrays to read-only.

--- landlab/grid/diagonals.py
+++ landlab/grid/diagonals.py
@@ -52,6 +52,10 @@ def create_nodes_at_diagonal(shape, out=None):
 def create_diagonals_at_node(shape, out=None):
     """Create array of diagonals at node.
 
+    Create an array that gives the diagonal touching each node
+    for a structured grid of quadrilaterals. For each node, links
+    are ordered clockwise from axis=1.
+
     Parameters
     ----------
     shape : tuple of *(n_rows, n_cols)*
@@ -158,6 +162,15 @@ class DiagonalsMixIn(object):
     @property
     @cache_result_in_object()
     def number_of_diagonals(self):
+        """Number of diagonals in the grid.
+
+        Examples
+        --------
+        >>> from landlab import RasterModelGrid
+        >>> grid = RasterModelGrid((4, 3))
+        >>> grid.number_of_diagonals
+        12
+        """
         return 2 * np.prod(np.asarray(self.shape) - 1)
 
     @property
@@ -189,7 +202,41 @@ class DiagonalsMixIn(object):
 
     @property
     @cache_result_in_object()
+    @make_return_array_immutable
     def diagonal_dirs_at_node(self):
+        """Directions of diagonals attached to nodes.
+
+        Array of diagonal directions relative to each node. If the
+        diagonal is directed away from the node the direction is -1, if the
+        diagonal is directed toward the node its direction is 1. Each
+        node is assumed to have exactly four diagonals attached to it.
+        However, perimeter nodes will have few diagonals (corner ndoes
+        will only have one diagonal and edge nodes two). In such cases,
+        placeholders of 0 are used.
+
+        Examples
+        --------
+        >>> from landlab import RasterModelGrid
+        >>> grid = RasterModelGrid((4, 3))
+        >>> grid.diagonal_dirs_at_node.shape == (grid.number_of_nodes, 4)
+        True
+        >>> grid.diagonal_dirs_at_node
+        array([[-1,  0,  0,  0], [-1, -1,  0,  0], [ 0, -1,  0,  0],
+               [-1,  0,  0,  1], [-1, -1,  1,  1], [ 0, -1,  1,  0],
+               [-1,  0,  0,  1], [-1, -1,  1,  1], [ 0, -1,  1,  0],
+               [ 0,  0,  0,  1], [ 0,  0,  1,  1], [ 0,  0,  1,  0]],
+               dtype=int8)
+
+        The lower-right corner node only has one diagonal.
+
+        >>> grid.diagonal_dirs_at_node[2]
+        array([ 0, -1,  0,  0], dtype=int8)
+
+        A node on one of the edges has two diagonals.
+
+        >>> grid.diagonal_dirs_at_node[3]
+        array([-1,  0,  0,  1], dtype=int8)
+        """
         diagonals_at_node = self.diagonals_at_node
         dirs_at_node = np.zeros_like(diagonals_at_node, dtype=np.int8)
         dirs_at_node[diagonals_at_node >= 0] = 1
@@ -216,7 +263,7 @@ class DiagonalsMixIn(object):
                [10, -1, -1,  4], [11,  9,  3,  5], [-1, 10,  4, -1],
                [-1, -1, -1,  7], [-1, -1,  6,  8], [-1, -1,  7, -1]])
 
-        LLCATS: DEPR NINF CONN
+        LLCATS: NINF CONN
         """
         node_is_at_tail = np.choose(self.diagonal_dirs_at_node + 1,
                                     np.array((1, -1, 0), dtype=np.int8))
@@ -227,23 +274,58 @@ class DiagonalsMixIn(object):
 
     @property
     @cache_result_in_object()
+    @make_return_array_immutable
     def d8_adjacent_nodes_at_node(self):
         return np.vstack((super(DiagonalsMixIn, self).adjacent_nodes_at_node,
                           self.diagonal_adjacent_nodes_at_node))
 
     @property
     @cache_result_in_object()
+    @make_return_array_immutable
     def nodes_at_diagonal(self):
+        """Nodes at diagonal tail and head.
+
+        Examples
+        --------
+        >>> from landlab import RasterModelGrid
+        >>> grid = RasterModelGrid((4, 3))
+        >>> grid.nodes_at_diagonal.shape == (grid.number_of_diagonals, 2)
+        True
+        >>> grid.nodes_at_diagonal
+        array([[ 0,  4], [ 1,  3], [ 1,  5], [ 2,  4],
+               [ 3,  7], [ 4,  6], [ 4,  8], [ 5,  7],
+               [ 6, 10], [ 7,  9], [ 7, 11], [ 8, 10]])
+
+        The first column is diagonal tail and the second the head.
+
+        >>> grid.diagonals_at_node[3]
+        array([ 4, -1, -1,  1])
+        >>> grid.nodes_at_diagonal[(4, 1), ]
+        array([[3, 7],
+               [1, 3]])
+        >>> grid.diagonal_dirs_at_node[3]
+        array([-1,  0,  0,  1], dtype=int8)
+        """
         return create_nodes_at_diagonal(self.shape)
 
     @property
     @cache_result_in_object()
     def number_of_d8(self):
+        """Number of links and diagonals.
+
+        Examples
+        --------
+        >>> from landlab import RasterModelGrid
+        >>> grid = RasterModelGrid((4, 3))
+        >>> grid.number_of_d8
+        29
+        """
         return (super(DiagonalsMixIn, self).number_of_links +
                 self.number_of_diagonals)
 
     @property
     @cache_result_in_object()
+    @make_return_array_immutable
     def nodes_at_d8(self):
         return np.vstack((self.nodes_at_link, self.nodes_at_diagonal))
 
@@ -297,17 +379,20 @@ class DiagonalsMixIn(object):
 
     @property
     @cache_result_in_object()
+    @make_return_array_immutable
     def d8_dirs_at_node(self):
         return np.hstack((super(DiagonalsMixIn, self).link_dirs_at_node,
                           self.diagonal_dirs_at_node))
 
     @property
     # @cache_result_in_object()
+    @make_return_array_immutable
     def d8_status_at_node(self):
         return self.status_at_d8[self.d8_at_node]
 
     @property
     @cache_result_in_object()
+    @make_return_array_immutable
     def length_of_diagonal(self):
         return np.sqrt(
             np.power(np.diff(self.xy_of_node[self.nodes_at_diagonal], axis=1),
@@ -315,6 +400,7 @@ class DiagonalsMixIn(object):
 
     @property
     @cache_result_in_object()
+    @make_return_array_immutable
     def length_of_d8(self):
         """Length of links and diagonals.
 
@@ -345,36 +431,91 @@ class DiagonalsMixIn(object):
         return np.hstack((super(DiagonalsMixIn, self).length_of_link,
                           self.length_of_diagonal))
 
+    def reset_status_at_node(self):
+        super(DiagonalsMixIn, self).reset_status_at_node()
+        attrs = ['_status_at_diagonal', '_diagonal_status_at_node',
+                 '_active_diagonals', '_active_diagonal_dirs_at_node',
+                 '_status_at_d8', '_active_d8', '_active_d8_dirs_at_node']
+
+        for attr in attrs:
+            try:
+                del self.__dict__[attr]
+            except KeyError:
+                pass
+
     @property
     @cache_result_in_object()
+    @make_return_array_immutable
     def status_at_diagonal(self):
+        """Status at diagonals.
+
+        Examples
+        --------
+        >>> from landlab import RasterModelGrid
+        >>> grid = RasterModelGrid((4, 3))
+
+        An inactive link (or diagonal) is one that joins two
+        boundary nodes or has one end that is a closed boundary.
+
+        >>> grid.status_at_node
+        array([1, 1, 1,
+               1, 0, 1,
+               1, 0, 1,
+               1, 1, 1], dtype=uint8)
+        >>> grid.BC_NODE_IS_CORE, grid.BC_NODE_IS_FIXED_VALUE
+        (0, 1)
+
+        Diagonals that connect two boundary nodes are inactive.
+
+        >>> grid.status_at_diagonal
+        array([0, 4, 4, 0,
+               0, 0, 0, 0,
+               4, 0, 0, 4], dtype=uint8)
+        >>> grid.BC_LINK_IS_ACTIVE, grid.BC_LINK_IS_INACTIVE
+        (0, 4)
+
+        By setting a node to closed that wasn't before, a new link
+        become inactive.
+
+        >>> grid.status_at_node[5] = grid.BC_NODE_IS_CLOSED
+        >>> grid.status_at_diagonal
+        array([0, 4, 4, 0,
+               0, 0, 0, 4,
+               4, 0, 0, 4], dtype=uint8)
+        """
         return set_status_at_link(self.status_at_node[self.nodes_at_diagonal])
 
     @property
-    # @cache_result_in_object()
+    @cache_result_in_object()
+    @make_return_array_immutable
     def diagonal_status_at_node(self):
         return self.status_at_diagonal[self.diagonals_at_node]
 
     @property
+    @make_return_array_immutable
     def active_diagonals(self):
         return np.where(self.status_at_diagonal == ACTIVE_LINK)[0]
 
     @property
+    @make_return_array_immutable
     def active_diagonal_dirs_at_node(self):
         return np.choose(self.diagonal_status_at_node == ACTIVE_LINK,
                          (0, self.diagonal_dirs_at_node))
 
     @property
     @cache_result_in_object()
+    @make_return_array_immutable
     def status_at_d8(self):
         return np.hstack((super(DiagonalsMixIn, self).status_at_link,
                           self.status_at_diagonal))
 
     @property
+    @make_return_array_immutable
     def active_d8(self):
         return np.where(self.status_at_d8 == ACTIVE_LINK)[0]
 
     @property
+    @make_return_array_immutable
     def active_d8_dirs_at_node(self):
         return np.choose(self.d8_status_at_node == ACTIVE_LINK,
                          (0, self.d8_dirs_at_node))
