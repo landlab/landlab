9281932fda158f1354ecad72603ad9ef4e123f84
nathanlyons <nathan.j.lyons@gmail.com>
2017-12-21 23:10:11 -0600

    removed whitespace

--- landlab/plot/field_profile.py
+++ landlab/plot/field_profile.py
@@ -17,11 +17,11 @@ class FieldProfiler:
     def __init__(self, grid, field, start, end):
         """
         Create profiles of model grid field values.
-        
+
         The profile extends from start to end. The grid field is sampled at the
         resolution of the grid. Profile endpoints and samples are snapped to
         node centers.
-    
+
         Parameters
         ----------
         grid : RasterModelGrid
@@ -33,14 +33,14 @@ class FieldProfiler:
         end : tuple or integer
             The end of the profile
         """
-        
+
         if field not in grid.at_node.keys():
             raise FieldError('A grid field, {} is required to '
                              'update a stream species range.'.format(field))
-        
+
         self._grid = grid
         self._field = field
-        
+
         # Handle inputs that can be integers or tuples for the profile
         # endpoints.
         start_node, x0, y0 = self._get_node_x_y(start)
@@ -55,12 +55,12 @@ class FieldProfiler:
         self.nodes = []
         self.distance = []
         self.field_value = []
-        
+
         for xi, yi in zip(x_samples, y_samples):
             node = self._grid.find_nearest_node((xi, yi))
-            
+
             if node not in self.nodes and not grid.node_is_boundary(node):
-                
+
                 if len(self.nodes) == 0:
                     di = 0
                 else:
@@ -68,7 +68,7 @@ class FieldProfiler:
                     xp = grid.x_of_node[prior_node]
                     yp = grid.y_of_node[prior_node]
                     di = np.hypot(xi - xp, yi - yp) + self.distance[-1]
-                    
+     
                 zi = self._grid.at_node[self._field][node]
                 self.nodes.append(node)
                 self.distance.append(di)
@@ -83,19 +83,19 @@ class FieldProfiler:
             node = self._grid.find_nearest_node((point[0], point[1]))
             x = point[0]
             y = point[1]
-        
+
         return node, x, y
-            
+
     def plot(self, distance_unit_label, field_y_axis_label, **kwds):
         """
         Create a figure of the grid field profile.
-        
+
         The figure has two subplots:
             1. The trace of the profile plotted on the grid shaded by the
                field values.
             2. The profile illustrating the profile samples (distance vs the
                field value).
-    
+
         Parameters
         ----------
         distance_unit_label : string
@@ -106,9 +106,9 @@ class FieldProfiler:
             The same parameters in :func:`imshow_grid_at_node` can be used
             in this method.
         """
-        
+
         mg = self._grid
-        
+
         self.fig = plt.subplots(nrows=2, ncols=1)
 
         # Plot field values as grid.
@@ -116,26 +116,25 @@ class FieldProfiler:
         z = mg.at_node[self._field]
         z_min = min(z[z != -9999])
         z_max = max(z[z != -9999])
-        
+
         # Set colorbar label to the profile y axis label unless specified in
         # kwds.
         if 'colorbar_label' not in kwds:
             kwds['colorbar_label'] = field_y_axis_label     
-        
+
         imshow_grid(self._grid, self._field, limits=(z_min, z_max), **kwds)
-        
+
         # Plot profile trace on grid.
         x1 = mg.x_of_node[self.nodes[0]]
         y1 = mg.y_of_node[self.nodes[0]]
         x2 = mg.x_of_node[self.nodes[-1]]
         y2 = mg.y_of_node[self.nodes[-1]]
         plt.plot((x1, x2), (y1, y2), 'co-')
-        
+
         # Plot profile.
         plt.subplot(2 ,1, 2)
         plt.plot(self.distance, self.field_value, 'k')
         plt.xlabel('Distance ({})'.format(distance_unit_label))
         plt.ylabel(field_y_axis_label)
-        
+
         plt.tight_layout()
-        
