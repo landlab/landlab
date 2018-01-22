bbdb15d866799c1e54657eb50ee55a4c3f20280a
mcflugen <mcflugen@gmail.com>
2018-01-21 20:12:41 -0700

    Use grid.BC_* for node/link status.

--- landlab/grid/tests/test_raster_grid/test_bc_updates.py
+++ landlab/grid/tests/test_raster_grid/test_bc_updates.py
@@ -2,8 +2,6 @@ import numpy as np
 from numpy.testing import assert_array_equal
 from nose.tools import with_setup, assert_true, assert_equal, assert_not_equal
 
-from landlab import FIXED_GRADIENT_BOUNDARY, CLOSED_BOUNDARY, INACTIVE_LINK, \
-    FIXED_LINK
 from landlab import RasterModelGrid
 
 
@@ -46,24 +44,24 @@ def test_issue_428_b():
 
 @with_setup(setup_grid)
 def test_link_update_with_nodes_closed():
-    rmg.status_at_node[rmg.nodes_at_bottom_edge] = CLOSED_BOUNDARY
-    inactive_array = np.array([INACTIVE_LINK, ] * 5)
+    rmg.status_at_node[rmg.nodes_at_bottom_edge] = rmg.BC_NODE_IS_CLOSED
+    inactive_array = np.array([rmg.BC_LINK_IS_INACTIVE, ] * 5)
     assert_array_equal(rmg.status_at_link[4:9], inactive_array)
 
 
 @with_setup(setup_grid)
 def test_link_update_with_nodes_fixed_grad():
-    rmg.status_at_node[rmg.nodes_at_bottom_edge] = FIXED_GRADIENT_BOUNDARY
-    fixed_array = np.array([FIXED_LINK, ] * 3)
+    rmg.status_at_node[rmg.nodes_at_bottom_edge] = rmg.BC_NODE_IS_FIXED_GRADIENT
+    fixed_array = np.array([rmg.BC_LINK_IS_FIXED, ] * 3)
     assert_array_equal(rmg.status_at_link[5:8], fixed_array)
 
 
-@with_setup(setup_grid)
 def test_bc_set_code_init():
-    assert_equal(rmg.bc_set_code, 0)
+    grid = RasterModelGrid((4, 4))
+    assert_equal(grid.bc_set_code, grid.BC_NODE_IS_CORE)
 
 
 @with_setup(setup_grid)
 def test_bc_set_code_change():
-    rmg.status_at_node[rmg.nodes_at_bottom_edge] = CLOSED_BOUNDARY
+    rmg.status_at_node[rmg.nodes_at_bottom_edge] = rmg.BC_NODE_IS_CLOSED
     assert_not_equal(rmg.bc_set_code, 0)
