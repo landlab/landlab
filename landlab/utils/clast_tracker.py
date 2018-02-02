Correct calculation of travel distance to take slope into account (not horizontal distance), correct calculation of detachment probability, delete duplicate names for deposition rate

landlab/utils/clast_tracker.py
@@ -33,7 +33,7 @@ class ClastSet(object):
 
         # Clast position arrays:
         self._clast__node = clast__node
-        self._clast__elev = clast__elev
+        self._clast__elev = clast__elev   # !!!ELEV OF CLAST BASE!!!
 
         # Clast size (radius):
         self._clast__radius = clast__radius  # in meters
@@ -69,8 +69,8 @@ class ClastSet(object):
             self._clast__distance = np.zeros(self._set__size)
 
         for i in range(self._set__size):
-            # If initial elevation is above topographic elev, set clast elev
-            # to topo elev:
+            # If initial clast elevation is above topographic elev, 
+            # set clast elev to topo elev:
             if self._clast__elev[i] > self._elev[self._clast__node[i]]:
                 self._clast__elev[i] = self._elev[self._clast__node[i]]
 
@@ -102,7 +102,7 @@ class ClastSet(object):
         self._dt = dt
         self._erosion__depth = self._erosion_rate * self._dt
         self._deposition__thickness = self._deposition_rate * self._dt
-        self._deposition__flux = self._deposition_rate * self._grid.dx
+# TO DELETE???        self._deposition__flux = self._deposition_rate # * self._grid.dx
 
 
         if uplift is not None:
@@ -120,19 +120,23 @@ class ClastSet(object):
         for i in range(self._set__size):   # Treat clasts one after the other
             # Test if clast in grid core (=not phantom)
             if ClastSet.phantom(self, i) == False:
-                print('not phantom')
+                #print('not phantom')
                 # Clast is in grid core
                 # Test if clast is detached:
                 if ClastSet.clast_detach_proba(self, i) == True:
-                    print('detached')
+                    #print('detached')
                     # Clast is detached -> Test if moves (leaves node):
                     if ClastSet.clast_move_proba(self, i) == True:
-                        print('move')
+                        # print('move')
                         # Clast leaves node:
                         # Clast is moved to next node downstream:
+                        node_i = self._clast__node[i]
+                        receiver_i = self._grid.at_node['flow__receiver_node'][self._clast__node[i]]
+                        
                         # Update travelled distance:
-                        self._clast__distance[i] += (
-                                self._distances[self._clast__node[i], self._grid.at_node['flow__receiver_node'][self._clast__node[i]]])
+                        self._clast__distance[i] += np.sqrt(np.power(
+                                self._distances[node_i, receiver_i],2)+(np.power(
+                                        self._grid.at_node['topographic__elevation'][node_i]-self._grid.at_node['topographic__elevation'][receiver_i],2)))
                         # Move clast to next downstream:
                         self._clast__node[i] = (
                                 self._receiver[self._clast__node[i]])
@@ -143,13 +147,18 @@ class ClastSet(object):
                         # deposited:
                         # While clast is not deposited, keeps changing node:
                         while ClastSet.clast_depo_proba(self, i) == False:
-                            print('not depositing')
-                            # Clast is moved to next node downstream
-                            self._clast__node[i] = (
-                                    self._receiver[self._clast__node[i]])
-                            # Update travelled distance:
-                            self._clast__distance[i] += (
-                                    self._distances[self._clast__node[i], self._grid.at_node['flow__receiver_node'][self._clast__node[i]]])
+                            if ClastSet.phantom(self, i) == False:
+                                print(i)
+                                print('not depositing')
+                                # Update travelled distance:
+                                self._clast__distance[i] += np.sqrt(np.power(
+                                        self._distances[node_i, receiver_i],2)+(np.power(
+                                                self._grid.at_node['topographic__elevation'][node_i]-self._grid.at_node['topographic__elevation'][receiver_i],2)))
+                                # Clast is moved to next node downstream
+                                self._clast__node[i] = (
+                                        self._receiver[self._clast__node[i]])
+                            else:
+                                break
                     
                     # next section instead of following 2 parag, to update clast elev at the end of this timestep as somewhere in the deposited layer?
                     else:
@@ -179,15 +188,18 @@ class ClastSet(object):
                     # Clast is not detached -> go to next clast
                     pass
 
+            if hasattr(self, '_uplift') is True:
+                self._clast__elev[i] += self._uplift[self._clast__node[i]] * self._dt
+
             else:
                 # Clast is phantom -> go to next clast
                 # for display purpose: phantom clast has a radius of 0
                 # self._clast__radius(i) = 0.
                 pass
-
-        for i in range(self._set__size):
-            if hasattr(ClastSet, '_uplift') is True:
-                self._clast__elev[i] += 0   # self._uplift[self._clast__node[i]] * (np.ones(self._set__size) * self._dt)
+#
+#        for i in range(self._set__size):
+#            if hasattr(ClastSet, '_uplift') is True:
+#                self._clast__elev[i] += 0   # self._uplift[self._clast__node[i]] * (np.ones(self._set__size) * self._dt)
 
 #        for i in range(self._set__size):
 #                # Test if clast has now exited core nodes (is phantom):
@@ -212,16 +224,16 @@ class ClastSet(object):
     # @classmethod   # ? or @staticmethod?
     def clast_detach_proba(self, i):
         # Test if clast is detached:
-        # clast is detached if its elevation (its base) is above topography
+        # clast is detached if erosion is sufficient to expose its base
         clast__elev = self._clast__elev[i]
         topo__elev = self._elev[self._clast__node[i]]
-        # erosion = _erosion__depth[self._clast__node[i]]
+        erosion = self._erosion__depth[self._clast__node[i]]
 
         _det = np.zeros(1, dtype=bool)
 
-        # if erosion >= topo__elev - clast__elev:
+        if erosion >= topo__elev - clast__elev:
         # Previously: 
-        if clast__elev >= topo__elev:
+        # if clast__elev >= topo__elev:
             _det = True
 
         return _det
@@ -246,7 +258,14 @@ class ClastSet(object):
                 proba_move = (1 / np.sqrt(2)) * 1.25 * ((erosion) / (
                         2 * radius)) * (1-(clast__depth / erosion))
 
-            # proba_move can be >1 -> set to 1
+#                # if clast will potentially move in row or column
+#                proba_move = ((erosion) / (
+#                        2 * radius)) * (1-(clast__depth / erosion))
+#            else:   # clast will potentially move in diagonal
+#                proba_move = (1 / np.sqrt(2)) * ((erosion) / (
+#                        2 * radius)) * (1-(clast__depth / erosion))
+
+            # proba_move can be >1 (if clast was at surface) -> set to 1 
             if proba_move > 1:
                 proba_move = 1
         else:
@@ -266,7 +285,7 @@ class ClastSet(object):
 
         # Clast is deposited if its probability to be deposited is
         # higher than a random number.
-        depo = self._deposition__flux[self._clast__node[i]]
+        depo = self._deposition_rate[self._clast__node[i]]
         fluxin = self._sediment__flux_in[self._clast__node[i]]
         R = np.random.rand(1)
 
@@ -282,6 +301,7 @@ class ClastSet(object):
         if proba_depo < R:
             # Clast  is not deposited
             _depo = False
+            print(proba_depo)
         else:
             _depo = True
 