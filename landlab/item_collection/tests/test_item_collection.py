7545a5d4dbf4843c02017fd5efd07f24aa334e87
kbarnhart <katy.barnhart@gmail.com>
2018-03-21 14:51:34 -0600

    adding the first of a big test.

--- landlab/item_collection/tests/test_item_collection.py
+++ landlab/item_collection/tests/test_item_collection.py
@@ -18,39 +18,87 @@ except ImportError:
 
 import landlab
 from landlab import RasterModelGrid, HexModelGrid, FieldError # NetworkModelGrid,
+from landlab.item_collection import ItemCollection
 
 from landlab import CLOSED_BOUNDARY
 from landlab import BAD_INDEX_VALUE as XX
 
-# def test_NetworkModelGrid():
-#     """ """
-#     pass
+_LOCATIONS = ['node', 'patch', 'link', 'corner', 'face', 'cell']
 
+def test_RasterAndHexModelGrids():
+    """Test compatability with RasterModelGrid and HexModelGrid"""
 
-def test_RasterModelGrid():
-    """ """
-    pass
-
-
-def test_HexModelGrid():
-    """ """
-    pass
-
-
-def test_different_length_data():
-    """ """
-    pass
-
-
-def test_bad_location_index():
-    """ """
-    pass
-
-
-def test_bad_grid_element_name():
-    """ """
-    pass
-
+    element_id = [0, 0, 1, 1, 2, 3, 5]
+    volume = [1, 2, 3, 4, 5, 6, 7]
+    age = [10, 11, 12, 13, 14, 15, 16] 
+    
+    rgrid = RasterModelGrid(6,6)
+    hgrid = HexModelGrid(4,5)
+    # ngrid = NetworkModelGrid(...)
+    
+    # for each grid type
+    for grid in [rgrid, hgrid]: # [rgrid, hgrid, ngrid]
+        # for each location, test that you can make an item collection there:
+        
+#        if isinstance(grid, NetworkModelGrid):
+#            locations = ['node', 'link']
+#        else:
+#            locations = _LOCATIONS
+            
+        locations = _LOCATIONS
+        for loc in locations:
+            # recreate data
+            data = {'age': age,
+                    'volume': volume}
+        
+            # test that an item collection can be created
+            ic = ItemCollection(grid, 
+                                data = data, 
+                                grid_element = loc, 
+                                element_id = element_id)
+        
+            # test that a bad grid element name raises the right error
+            data = {'age': age,
+                    'volume': volume}
+            assert_raises(ValueError, 
+                          ItemCollection, 
+                          grid, 
+                          data = data, 
+                          grid_element = 'foo', 
+                          element_id = element_id)
+            
+            # test that an too big element raises a warning
+            data = {'age': age,
+                    'volume': volume}
+            bad_element_id = [0, 0, 1, 1, 2, 3, 500]
+            assert_raises(ValueError, 
+                          ItemCollection, 
+                          grid, 
+                          data = data, 
+                          grid_element = loc, 
+                          element_id = bad_element_id)
+            
+            # test that a too small element raises a warming
+            data = {'age': age,
+                    'volume': volume}
+            bad_element_id = [0, 0, 1, 1, 2, 3, -1]
+            assert_raises(ValueError, 
+                          ItemCollection, 
+                          grid, 
+                          data = data, 
+                          grid_element = loc, 
+                          element_id = bad_element_id)      
+            
+            # test wrong length data
+            data = {'age': age[:-1],
+                    'volume': volume}
+            assert_raises(ValueError, 
+                          ItemCollection, 
+                          grid, 
+                          data = data, 
+                          grid_element = loc, 
+                          element_id = bad_element_id) 
+            
 
 def test_grid_element_size():
     """ """
