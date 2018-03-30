cf3c74ffdba8a1ff4892b8f8c0e1ec602b47a6c8
kbarnhart <katy.barnhart@gmail.com>
2018-03-30 10:45:04 -0600

    adding route to multiple functionality

--- landlab/utils/stream_length.py
+++ landlab/utils/stream_length.py
@@ -12,8 +12,7 @@ def calculate_stream_length(grid, add_to_grid=False, noclobber=True):
 
     This utility calculates the along stream distance based on the results of
     running flow accumulation on the grid. It will use the connectivity
-    used by the FlowAccumulator (e.g. D4 or D8). It requires that a route to
-    one method was used.
+    used by the FlowAccumulator (e.g. D4, D8, Dinf).
 
     Parameters
     ----------
@@ -44,10 +43,6 @@ def calculate_stream_length(grid, add_to_grid=False, noclobber=True):
     # get the reciever nodes
     flow__receiver_node = grid.at_node['flow__receiver_node']
 
-    if grid_nodes.size != flow__receiver_node.size:
-        raise ValueError('A route to one method must be used with the calculate '
-                         'stream distance method.')
-
     # get the upstream node order
     flow__upstream_node_order = grid.at_node['flow__upstream_node_order']
 
@@ -64,15 +59,62 @@ def calculate_stream_length(grid, add_to_grid=False, noclobber=True):
     # identified the locations of the outlet nodes and have
     for node in flow__upstream_node_order:
 
+        # get flow recievers
+        reciever = flow__receiver_node[node]
+
+        # assess if this is a to one (D8/D4) or to multiple (Dinf, MFD)
+        # flow directing method.
+        if len(reciever) == 1:
+            to_one = True
+            potential_outlet = reciever
+        else:
+            to_one = False
+            # if this is an outlet, the first element of the recievers will be
+            # the nodes ID.
+            potential_outlet = reciever[0]
+
+        # assess if this is an outlet or not.
+        if potential_outlet == node:
+            not_outlet = False
+        else:
+            not_outlet = True
+
         # if not an outlet
-        if flow__receiver_node[node] != node:
+        if not_outlet:
 
-            # get the stream length of the downstream node
-            downstream_stream_length = stream__length[flow__receiver_node[node]]
+            # deal with the two cases of route to one and route to multiple.
+            if to_one:
+                # get the stream length of the downstream node
+                downstream_stream_length = stream__length[reciever]
 
-            # get the stream segment length from this node to its downstream
-            # neigbor
-            stream_increment_length = flow_link_lengths[node]
+                # get the stream segment length from this node to its downstream
+                # neigbor
+                stream_increment_length = flow_link_lengths[node]
+
+            else:
+                # non-existant links are coded with -1
+                useable_recievers = np.where(reciever != BAD_INDEX_VALUE)[0]
+
+                # we will have the stream flow to the downstream node with the
+                # shortest distance to the outlet.
+                # in the event of a tie, we will choose the shorter link length.
+
+                # get the stream lengths of the downstream nodes
+                potential_downstream_stream_lengths = stream__length[flow__receiver_node[node]][useable_recievers]
+
+                # get the stream segment lengths from this node to its downstream
+                # neighbor
+                potential_stream_increment_lengths = flow_link_lengths[node][useable_recievers]
+
+                # get the lowest downstream stream length.
+                downstream_stream_length = np.min(downstream_stream_lengths)
+
+                # determine which of the stream increments flowed to this
+                # downstream neighbor.
+                which_link = np.where(potential_downstream_stream_lengths == downstream_stream_length)[0]
+
+                # and choose the smallest of these links.
+                stream_increment_length = np.min(potential_stream_increment_lengths[which_link])
 
             # set the total stream length of this node
             stream__length[node] = downstream_stream_length + stream_increment_length
