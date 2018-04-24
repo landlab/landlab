36b79b8326fdb27ae19c11fd5f41403dea7c5b38
Greg Tucker <gtucker@colorado.edu>
2018-04-24 08:16:42 -0600

    update 2 docstrings

--- landlab/ca/celllab_cts.py
+++ landlab/ca/celllab_cts.py
@@ -626,7 +626,7 @@ class CellLabCTSModel(object):
 
         **creates**:
 
-        * ``self.active_link_orientation`` : 1D numpy array
+        * ``self.link_orientation`` : 1D numpy array
 
         Notes
         -----
@@ -716,7 +716,7 @@ class CellLabCTSModel(object):
         >>> cts.trn_to
         array([2, 1, 6, 7])
         >>> cts.trn_rate
-        array([ 1.,  2.,  3.,  4.])
+        array([1., 2., 3., 4.])
         """
 
         # First, create an array that stores the number of possible transitions
