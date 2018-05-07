54e0763d14df81bada1c92d7dcec703b0a0078c6
nathanlyons <nathan.j.lyons@gmail.com>
2018-05-06 19:00:28 -0500

    removed remaining habitat files

--- landlab/components/biota_macroevolution/__init__.py
+++ landlab/components/biota_macroevolution/__init__.py
@@ -3,7 +3,5 @@ from .seamless import BiotaEvolver
 from .zone import Zone
 from .plot import *
 from .species import Species
-from .stream_habitat_patch import StreamHabitatPatch
-from .stream_species import StreamSpecies
 
 __all__ = ['BiotaEvolver']
