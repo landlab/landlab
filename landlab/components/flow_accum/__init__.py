7ae77b74af7e6fda4ddcc0db8a10099a5c4666e2
kbarnhart <katy.barnhart@gmail.com>
2018-05-20 16:05:40 -0600

    changing flow accum submodule init

--- landlab/components/flow_accum/__init__.py
+++ landlab/components/flow_accum/__init__.py
@@ -3,7 +3,7 @@ from .flow_accum_bw import (make_ordered_node_array,
                             flow_accumulation)
 
 from .flow_accumulator import FlowAccumulator
-
-__all__ = ['FlowAccumulator',
+from .richdem_flow_accumulator import RichDemFlowAccumulator
+__all__ = ['FlowAccumulator', 'RichDemFlowAccumulator',
            'make_ordered_node_array', 'find_drainage_area_and_discharge',
            'flow_accumulation', ]
