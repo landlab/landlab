312bfc67097225e74c41e53768051527ccd49cdd
kbarnhart <katy.barnhart@gmail.com>
2018-05-21 11:39:19 -0600

    fixing line to change to return array

--- landlab/components/flow_accum/flow_accumulator.py
+++ landlab/components/flow_accum/flow_accumulator.py
@@ -16,7 +16,7 @@ import warnings
 
 from landlab import FieldError, Component
 from landlab import RasterModelGrid, VoronoiDelaunayGrid, NetworkModelGrid  # for type testing
-from landlab.utils.decorators import use_field_name_or_array
+from landlab.utils.return_array import return_array_at_node
 from landlab.core.messages import warning_message
 
 from landlab.components.flow_accum import flow_accum_bw
