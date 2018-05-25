b8facee8a3a8a57932577bb51c087ba0f51a59b9
kbarnhart <katy.barnhart@gmail.com>
2018-05-25 11:17:05 -0600

    A test for correct shape from function

--- landlab/components/lithology/tests/test_litholayers.py
+++ landlab/components/lithology/tests/test_litholayers.py
@@ -14,7 +14,7 @@ Created on Mon Apr 30 09:17:36 2018
 @author: barnhark
 """
 
-#import numpy as np
+import numpy as np
 #from numpy.testing import assert_array_equal, assert_array_almost_equal
 from nose.tools import assert_raises#, assert_almost_equal, assert_equal
 
@@ -49,4 +49,14 @@ def test_bad_function():
     ids = [1, 2, 1, 2, 1, 2, 1, 2, 1]
     attrs = {'K_sp': {1: 0.001, 2: 0.0001}}
     func = lambda x, y, z: 0*x + 0*y + z
+    assert_raises(ValueError, LithoLayers, mg, z0s, ids, attrs, function=func)
+
+def test_function_returns_wrong_number_of_values():
+    """Test that providing a function that returns one value raises error."""
+    mg = RasterModelGrid(3, 3)
+    z = mg.add_zeros('node', 'topographic__elevation')
+    z0s = [-4, -3, -2, -1, 0, 1, 2, 4, 6]
+    ids = [1, 2, 1, 2, 1, 2, 1, 2, 1]
+    attrs = {'K_sp': {1: 0.001, 2: 0.0001}}
+    func = lambda x, y, z: np.mean(x+y)
     assert_raises(ValueError, LithoLayers, mg, z0s, ids, attrs, function=func)
