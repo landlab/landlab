b3fa732856247df63152532b92cef5cf50215876
kbarnhart <katy.barnhart@gmail.com>
2018-07-18 21:29:40 -0600

    no more nose

--- landlab/components/stream_power/tests/test_smooth_thresh.py
+++ landlab/components/stream_power/tests/test_smooth_thresh.py
@@ -1,5 +1,4 @@
-from nose.tools import assert_raises
-
+import pytest
 import numpy as np
 
 from numpy.testing import assert_array_almost_equal
@@ -12,7 +11,8 @@ def test_bad_nsp():
 
     mg = RasterModelGrid(4, 4, 1)
     mg.set_closed_boundaries_at_grid_edges(False, False, True, True)
-    assert_raises(ValueError, Spst, mg, K_sp = 1.0, n_sp = 1.01)
+    with pytest.raises(ValueError):
+        spst = Spst(mg, K_sp = 1.0, n_sp = 1.01)
 
 def test_no_thresh():
     K = 0.001
