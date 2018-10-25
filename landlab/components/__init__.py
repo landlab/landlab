a80da5d9fe2ff8967cb34524c8a7a653e31ef87a
Dan Hobley <daniel@dhmac.geol.cf.ac.uk>
2018-10-25 13:41:18 +0100

    add to component listing
    in __init__.py files

--- landlab/components/__init__.py
+++ landlab/components/__init__.py
@@ -10,6 +10,7 @@ from .flow_director import FlowDirectorSteepest
 from .flow_director import FlowDirectorMFD
 from .flow_director import FlowDirectorDINF
 from .flow_accum import FlowAccumulator
+from .flow_accum import LossyFlowAccumulator
 from .overland_flow import OverlandFlowBates, OverlandFlow
 from .overland_flow import KinwaveImplicitOverlandFlow, KinwaveOverlandFlowModel
 from .potentiality_flowrouting import PotentialityFlowRouter
@@ -52,7 +53,8 @@ COMPONENTS = [ChiFinder, LinearDiffuser,
               ExponentialWeatherer, DepthDependentDiffuser,
               TaylorNonLinearDiffuser, DepthSlopeProductErosion,
               FlowDirectorD8, FlowDirectorSteepest, FlowDirectorMFD,
-              FlowDirectorDINF, FlowAccumulator, Space, ErosionDeposition,
+              FlowDirectorDINF, FlowAccumulator, LossyFlowAccumulator,
+              Space, ErosionDeposition,
               LandslideProbability, DepthDependentTaylorDiffuser,
               NormalFault, Lithology, LithoLayers]
 
