f49edfc16814f6f2a9a19aef6d267a5201fb4d97
mcflugen <mcflugen@gmail.com>
2018-11-04 15:50:34 -0700

    Add more tests for Flexure component.

--- landlab/components/flexure/tests/test_flexure.py
+++ landlab/components/flexure/tests/test_flexure.py
@@ -2,13 +2,48 @@
 """
 Unit tests for landlab.components.flexure.flexure
 """
-import pytest
+from pytest import raises, approx
 
 import numpy as np
+from landlab import RasterModelGrid
+from landlab.components.flexure import Flexure
 
 
 (_SHAPE, _SPACING, _ORIGIN) = ((20, 20), (10e3, 10e3), (0., 0.))
-_ARGS = (_SHAPE, _SPACING, _ORIGIN)
+
+
+def test_method_names():
+    grid = RasterModelGrid((20, 20), spacing=10e3)
+    assert Flexure(grid, method="airy").method == "airy"
+    assert Flexure(grid, method="flexure").method == "flexure"
+    with raises(ValueError):
+        Flexure(grid, method="bad-name")
+
+
+def test_eet_attribute():
+    grid = RasterModelGrid((20, 20), spacing=10e3)
+    for val in (10e3, 1e3):
+        assert Flexure(grid, eet=val).eet == approx(val)
+    with raises(ValueError):
+        assert Flexure(grid, eet=-10e3)
+
+
+def test_youngs_attribute():
+    grid = RasterModelGrid((20, 20), spacing=10e3)
+    for val in (10e3, 1e3):
+        assert Flexure(grid, youngs=val).youngs == approx(val)
+
+
+def test_gravity_attribute():
+    grid = RasterModelGrid((20, 20), spacing=10e3)
+    for val in (10e3, 1e3):
+        assert Flexure(grid, gravity=val).gravity == approx(val)
+
+
+def test_rho_mantle_attribute():
+    grid = RasterModelGrid((20, 20), spacing=10e3)
+    for val in (10e3, 1e3):
+        assert Flexure(grid, rho_mantle=val).rho_mantle == approx(val)
 
 
 def test_name(flex):
@@ -53,7 +88,7 @@ def test_field_getters(flex):
             flex.grid.number_of_node_rows * flex.grid.number_of_node_columns,
         )
 
-    with pytest.raises(KeyError):
+    with raises(KeyError):
         flex.grid["not_a_var_name"]
 
 
@@ -61,3 +96,45 @@ def test_field_initialized_to_zero(flex):
     for name in flex.grid["node"]:
         field = flex.grid["node"][name]
         assert np.all(field == 0.)
+
+
+def test_update():
+    n = 11
+    n_mid = (n - 1) // 2
+    i_mid = np.ravel_multi_index((n_mid, n_mid), (n, n))
+    load_0 = 1e9
+
+    grid = RasterModelGrid((n, n), spacing=1e3)
+    flex = Flexure(grid, method="flexure")
+
+    load = grid.at_node["lithosphere__overlying_pressure_increment"]
+    load[i_mid] = load_0
+
+    flex.update()
+    dz = flex.grid.at_node["lithosphere_surface__elevation_increment"].reshape((n, n))
+
+    assert np.argmax(dz) == i_mid
+    assert dz[n_mid, n_mid] > 0.
+    assert np.all(dz[:, n_mid::-1] == dz[:, n_mid:])
+    assert np.all(dz[n_mid::-1, :] == dz[n_mid:, :])
+
+
+def test_subside_loads():
+    n, load_0 = 11, 1e9
+
+    grid = RasterModelGrid((n, n), spacing=1e3)
+    flex = Flexure(grid, method="flexure")
+
+    grid.at_node["lithosphere__overlying_pressure_increment"][0] = load_0
+    flex.update()
+    dz_expected = flex.grid.at_node["lithosphere_surface__elevation_increment"]
+
+    load = np.zeros((n, n))
+    load[0, 0] = load_0
+
+    dz = flex.subside_loads(load)
+    assert np.all(dz.flatten() == approx(dz_expected))
+
+    out = np.zeros((n, n))
+    dz = flex.subside_loads(load, out=out)
+    assert dz is out
