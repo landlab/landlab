fd95cb1dadea3d598c417b5c29dc06a39d321125
kbarnhart <katy.barnhart@gmail.com>
2018-11-05 14:20:36 -0700

    using faster function

--- landlab/utils/watershed.py
+++ landlab/utils/watershed.py
@@ -224,35 +224,15 @@ def get_watershed_masks_with_area_threshold(grid, critical_area):
     >>> np.all(below_critical_area_nodes)
     True
     """
-
     nodes = grid.nodes.flatten()
-    core_nodes = grid.node_is_core(nodes)
-    boundary_nodes = grid.node_is_boundary(nodes)
+    area = grid.at_node['drainage_area']
 
-    # Find outlets along grid boundary.
-    A = grid.at_node['drainage_area']
-    stream_nodes = A >= critical_area
-    outlet_nodes = np.all([stream_nodes, boundary_nodes], 0)
-    outlet_nodes = np.where(outlet_nodes)[0]
+    watershed_masks = assign_watershed_mask_to_all_nodes(grid)
 
-    # Get watersheds of boundary outlets.
-    watershed_masks = np.repeat(-1, grid.number_of_nodes)
-    for outlet in outlet_nodes:
-        _assign_outlet_id_to_watershed(grid, outlet, watershed_masks)
+    area_of_watersheds = area[watershed_masks]
+    too_small = area_of_watersheds < critical_area
 
-    # Get internal watersheds.
-    node_is_unresolved = np.all([A >= critical_area, watershed_masks == -1,
-                                 core_nodes], 0)
-    while np.any(node_is_unresolved):
-        outlet = np.all([A == max(A[node_is_unresolved]), node_is_unresolved],
-                         0)
-
-        # Get the outlet with the greatest drainage area of the unresolved
-        # nodes.
-        outlet = np.where(outlet)[0][0]
-        _assign_outlet_id_to_watershed(grid, outlet, watershed_masks)
-        node_is_unresolved = np.all([A >= critical_area, watershed_masks == -1,
-                                     core_nodes], 0)
+    watershed_masks[too_small] = -1
 
     return watershed_masks
 
