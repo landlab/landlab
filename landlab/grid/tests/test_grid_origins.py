e317eeca09e7f538cd52e058ae92e2ca2b01bccf
kbarnhart <katy.barnhart@gmail.com>
2018-11-07 19:02:56 -0700

    tests that I though would pass... but I was wrong.

---
+++ landlab/grid/tests/test_grid_origins.py
@@ -0,0 +1,54 @@
+
+
+from numpy.random import rand
+from landlab import RasterModelGrid, HexModelGrid, RadialModelGrid, VoronoiDelaunayGrid
+
+
+def test_move_origin_raster():
+    mg = RasterModelGrid(9, 5, dx=2.0, origin=(10., 20.))
+    assert mg._origin == (20., 10.)
+    assert mg.x_of_node.min() == 20.
+    assert mg.y_of_node.min() == 10.
+
+    mg.origin == (0., 0.)
+    assert mg._origin == (0., 0.)
+    assert mg.x_of_node.min() == 0.
+    assert mg.y_of_node.min() == 0.
+
+
+def test_move_origin_hex():
+    shapes = ['rect', 'hex']
+    orientations = ['horizontal', 'vertical']
+
+    for shape in shapes:
+        for orientation in orientations:
+            mg = HexModelGrid(9, 5, dx=2.0, origin=(10., 20.), orientation=orientation, shape=shape)
+            assert mg._origin == (20., 10.)
+            assert mg.x_of_node.min() == 20.
+            assert mg.y_of_node.min() == 10.
+
+            mg.origin == (0., 0.)
+            assert mg._origin == (0., 0.)
+            assert mg.x_of_node.min() == 0.
+            assert mg.y_of_node.min() == 0.
+
+
+def test_move_origin_radial():
+    mg = RadialModelGrid(num_shells=9, dr=10., origin_x=20., origin_y=10.)
+    assert mg._origin == (20., 10.)
+    assert mg.x_of_node.min() == 20.
+    assert mg.y_of_node.min() == 10.
+
+    mg.origin = (0., 0.)
+    assert mg._origin == (0., 0.)
+    assert mg.x_of_node.min() == 0.
+    assert mg.y_of_node.min() == 0.
+
+
+def test_move_origin_voronoi():
+    x, y = rand(25), rand(25)
+    mg = VoronoiDelaunayGrid(x, y)
+
+    assert mg._origin == (y.min(), x.min())
+    mg.origin = (0., 0.)
+    assert mg._origin == (0., 0.)
