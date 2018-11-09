4c3727b77d91fc43e0409b2dac9c531877918fdd
kbarnhart <katy.barnhart@gmail.com>
2018-11-08 20:07:17 -0700

    expanding hex tests to size.

--- landlab/grid/tests/test_grid_origins.py
+++ landlab/grid/tests/test_grid_origins.py
@@ -5,6 +5,7 @@ from landlab import RasterModelGrid, HexModelGrid, RadialModelGrid, VoronoiDelau
 
 _START_ORIGIN = (10., 20.)
 _MOVE_ORIGIN = (30., 45.)
+
 def test_move_origin_raster():
     mg = RasterModelGrid(9, 5, dx=2.0, origin=_START_ORIGIN)
     assert mg._origin == _START_ORIGIN
@@ -20,20 +21,24 @@ def test_move_origin_raster():
 def test_move_origin_hex():
     shapes = ["rect", "hex"]
     orientations = ["horizontal", "vertical"]
-
+    sizes = ( (8, 7),
+              (7, 8),
+              (7, 11),
+              (8, 12))
     for shape in shapes:
         for orientation in orientations:
-            mg = HexModelGrid(
-                9, 5, dx=2.0, origin=_START_ORIGIN, orientation=orientation, shape=shape
-            )
-            assert mg._origin == _START_ORIGIN
-            assert mg.x_of_node[0] == _START_ORIGIN[1]
-            assert mg.y_of_node[0] == _START_ORIGIN[0]
+            for size in sizes:
+                mg = HexModelGrid(
+                    size[0], size[1], dx=2.0, origin=_START_ORIGIN, orientation=orientation, shape=shape
+                )
+                assert mg._origin == _START_ORIGIN
+                assert mg.x_of_node[0] == _START_ORIGIN[1]
+                assert mg.y_of_node[0] == _START_ORIGIN[0]
 
-            mg.origin = _MOVE_ORIGIN
-            assert mg._origin == _MOVE_ORIGIN
-            assert mg.x_of_node[0] == _MOVE_ORIGIN[1]
-            assert mg.y_of_node[0] == _MOVE_ORIGIN[0]
+                mg.origin = _MOVE_ORIGIN
+                assert mg._origin == _MOVE_ORIGIN
+                assert mg.x_of_node[0] == _MOVE_ORIGIN[1]
+                assert mg.y_of_node[0] == _MOVE_ORIGIN[0]
 
 
 def test_move_origin_radial():
