832b26e113add4d8f2168a8968ccc6e0c2a672cf
mcflugen <mcflugen@gmail.com>
2018-11-11 15:42:54 -0700

    Use print function.

--- examples/ca/turbulent_suspension_with_settling.py
+++ examples/ca/turbulent_suspension_with_settling.py
@@ -9,6 +9,7 @@ turbulent fluid.
 
 Written by Greg Tucker, February 2015
 """
+from __future__ import print_function
 
 import time
 
@@ -55,10 +56,10 @@ def setup_transition_list():
     7 (1-1)         (none)              -                   -
     
     """
-    
+
     # Create an empty transition list
     xn_list = []
-    
+
     # Append four transitions to the list.
     # Note that the arguments to the Transition() object constructor are:
     #  - Tuple representing starting pair state
@@ -67,25 +68,25 @@ def setup_transition_list():
     #    (bottom cell, top cell, orientation [1=vertical])
     #  - Transition rate (cells per time step, in this case 1 sec)
     #  - Name for transition
-    xn_list.append( Transition((0,1,0), (1,0,0), 10., 'left motion') )
-    xn_list.append( Transition((1,0,0), (0,1,0), 10., 'right motion') )
-    xn_list.append( Transition((0,1,1), (1,0,1), 10.55, 'down motion') )
-    xn_list.append( Transition((1,0,1), (0,1,1), 9.45, 'up motion') )
-    
+    xn_list.append(Transition((0, 1, 0), (1, 0, 0), 10., "left motion"))
+    xn_list.append(Transition((1, 0, 0), (0, 1, 0), 10., "right motion"))
+    xn_list.append(Transition((0, 1, 1), (1, 0, 1), 10.55, "down motion"))
+    xn_list.append(Transition((1, 0, 1), (0, 1, 1), 9.45, "up motion"))
+
     return xn_list
-    
-    
+
+
 def main():
-    
+
     # INITIALIZE
 
     # User-defined parameters
     nr = 100  # number of rows in grid
     nc = 64  # number of columns in grid
-    plot_interval = 0.5   # time interval for plotting, sec
-    run_duration = 20.0   # duration of run, sec
+    plot_interval = 0.5  # time interval for plotting, sec
+    run_duration = 20.0  # duration of run, sec
     report_interval = 10.0  # report interval, in real-time seconds
-    
+
     # Remember the clock time, and calculate when we next want to report
     # progress.
     current_real_time = time.time()
@@ -93,72 +94,77 @@ def main():
 
     # Create grid
     mg = RasterModelGrid(nr, nc, 1.0)
-    
+
     # Make the boundaries be walls
     mg.set_closed_boundaries_at_grid_edges(True, True, True, True)
-    
+
     # Set up the states and pair transitions.
-    ns_dict = { 0 : 'fluid', 1 : 'particle' }
+    ns_dict = {0: "fluid", 1: "particle"}
     xn_list = setup_transition_list()
 
     # Create the node-state array and attach it to the grid
-    node_state_grid = mg.add_zeros('node', 'node_state_map', dtype=int)
-    
+    node_state_grid = mg.add_zeros("node", "node_state_map", dtype=int)
+
     # Initialize the node-state array: here, the initial condition is a pile of
     # resting grains at the bottom of a container.
-    bottom_rows = where(mg.node_y<0.1*nr)[0]
+    bottom_rows = where(mg.node_y < 0.1 * nr)[0]
     node_state_grid[bottom_rows] = 1
-    
+
     # For visual display purposes, set all boundary nodes to fluid
     node_state_grid[mg.closed_boundary_nodes] = 0
-    
+
     # Create the CA model
     ca = OrientedRasterCTS(mg, ns_dict, xn_list, node_state_grid)
-    
-    grain = '#5F594D'
-    fluid = '#D0E4F2'
-    clist = [fluid,grain]
+
+    grain = "#5F594D"
+    fluid = "#D0E4F2"
+    clist = [fluid, grain]
     my_cmap = matplotlib.colors.ListedColormap(clist)
 
     # Create a CAPlotter object for handling screen display
     ca_plotter = CAPlotter(ca, cmap=my_cmap)
-    
+
     # Plot the initial grid
     ca_plotter.update_plot()
 
     # RUN
     current_time = 0.0
     while current_time < run_duration:
-        
+
         # Once in a while, print out simulation and real time to let the user
         # know that the sim is running ok
         current_real_time = time.time()
         if current_real_time >= next_report:
-            print 'Current sim time',current_time,'(',100*current_time/run_duration,'%)'
+            print(
+                "Current sim time",
+                current_time,
+                "(",
+                100 * current_time / run_duration,
+                "%)",
+            )
             next_report = current_real_time + report_interval
-        
+
         # Run the model forward in time until the next output step
-        ca.run(current_time+plot_interval, ca.node_state, 
-               plot_each_transition=False)
+        ca.run(current_time + plot_interval, ca.node_state, plot_each_transition=False)
         current_time += plot_interval
-        
+
         # Plot the current grid
         ca_plotter.update_plot()
-        
 
     # FINALIZE
 
     # Plot
     ca_plotter.finalize()
-    
+
     # Calculate concentration profile
     c = zeros(nr)
     for r in range(nr):
-        c[r] = mean(node_state_grid[r*nc:(r+1)*nc])
-        
+        c[r] = mean(node_state_grid[r * nc : (r + 1) * nc])
+
     figure(2)
-    plot(c, range(nr), 'o')
+    plot(c, range(nr), "o")
     show()
+
 
 # If user runs this file, activate the main() function
 if __name__ == "__main__":
