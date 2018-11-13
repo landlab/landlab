22e9b74a8df8a68e8092a23936e72239232efcc6
mcflugen <mcflugen@gmail.com>
2018-11-13 12:31:23 -0700

    Fix merge errors.

--- landlab/grid/tests/test_raster_grid/test_bc_updates.py
+++ landlab/grid/tests/test_raster_grid/test_bc_updates.py
@@ -51,11 +51,10 @@ def test_link_update_with_nodes_fixed_grad():
 
 def test_bc_set_code_init():
     grid = RasterModelGrid((4, 4))
-    assert rmg.bc_set_code == grid.BC_NODE_IS_CORE
+    assert grid.bc_set_code == grid.BC_NODE_IS_CORE
 
 
 def test_bc_set_code_change():
     rmg = RasterModelGrid((4, 5))
     rmg.status_at_node[rmg.nodes_at_bottom_edge] = rmg.BC_NODE_IS_CLOSED
-    assert_not_equal(rmg.bc_set_code, 0)
     assert rmg.bc_set_code != rmg.BC_NODE_IS_CORE
