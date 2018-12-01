8fe040a3637da4d8d3bb62af32b275ffe5883567
kbarnhart <katy.barnhart@gmail.com>
2018-11-30 17:21:06 -0700

    tests!

--- landlab/io/shapefile/tests/test_read_shapefile.py
+++ landlab/io/shapefile/tests/test_read_shapefile.py
@@ -1,12 +1,14 @@
 
 import os
+import pytest
+
 from landlab.io.shapefile import read_shapefile
 
 _TEST_DATA_DIR = os.path.join(os.path.dirname(__file__), "data")
 
 
 def test_read_methow():
-    file = os.path.join(_TEST_DATA_DIR, "methow", "Methow_Network.shp")
+    file = os.path.join(_TEST_DATA_DIR, "Methow_Network.shp")
     grid = read_shapefile(file)
     assert grid.number_of_nodes == 721
     assert grid.number_of_links == 720
@@ -15,3 +17,18 @@ def test_read_methow():
     assert grid.y_of_node[0] == 1160800.240247
     assert 'x_of_polyline' in grid.at_link
     assert 'y_of_polyline' in grid.at_link
+
+def test_bad_file():
+    file = os.path.join(_TEST_DATA_DIR, "bad_file.shp")
+    with pytest.raises(ValueError):
+        read_shapefile(file)
+
+def test_points():
+    file = os.path.join(_TEST_DATA_DIR, "points.shp")
+    with pytest.raises(ValueError):
+        read_shapefile(file)
+
+def test_multipart():
+    file = os.path.join(_TEST_DATA_DIR, "multipartpolyline.shp")
+    with pytest.raises(ValueError):
+        read_shapefile(file)
