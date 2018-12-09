ed22a2b96612c84c3fc961b14c27cc151caba570
kbarnhart <katy.barnhart@gmail.com>
2018-12-09 12:25:02 -0700

    test wip

--- landlab/grid/tests/test_grid_reference.py
+++ landlab/grid/tests/test_grid_reference.py
@@ -1,5 +1,5 @@
 
-
+import pytest
 from numpy.random import rand
 from landlab import RasterModelGrid, HexModelGrid, RadialModelGrid
 
@@ -72,3 +72,89 @@ def test_move_reference_radial():
                   _MOVE_REFERENCE[1] - _START_REFERENCE[1])
 
     assert known_dydx == actual_dydx
+
+
+def test_radial_deprecate_origin_x():
+    with pytest.warns(DeprecationWarning):
+        mg = RadialModelGrid(num_shells=1,
+                             dr=1.,
+                             origin_x=10)
+    assert mg._xy_of_center == (10., 0.)
+    pts, npts = mg._create_radial_points(1,
+                                         1,
+                                         xy_of_center=mg._xy_of_center)
+    assert pts[0, 0] == mg._xy_of_center[0]
+    assert pts[0, 1] == mg._xy_of_center[1]
+
+
+def test_radial_deprecate_origin_y():
+    with pytest.warns(DeprecationWarning):
+        mg = RadialModelGrid(num_shells=1,
+                             dr=1.,
+                             origin_y=10)
+    assert mg._xy_of_center == (0., 10.)
+    pts, npts = mg._create_radial_points(1,
+                                         1,
+                                         xy_of_center=mg._xy_of_center)
+    assert pts[0, 0] == mg._xy_of_center[0]
+    assert pts[0, 1] == mg._xy_of_center[1]
+
+
+def test_raster_with_args_and_shape():
+    with pytest.raises(ValueError):
+        RasterModelGrid(3, 3, num_cols=3)
+
+
+def test_raster_with_negativee_shape():
+    with pytest.raises(ValueError):
+        RasterModelGrid(-2, 3)
+
+
+def test_raise_deprecation_dx():
+    with pytest.warns(DeprecationWarning):
+        mg = RasterModelGrid(3, 3, dx=3)
+    assert 1 == 1
+
+
+def test_raise_deprecation_dxdx():
+    with pytest.warns(DeprecationWarning):
+        mg = RasterModelGrid(3, 3, dx=(4, 5))
+    assert 1 == 1
+
+
+def test_raise_deprecation_spacing():
+    with pytest.warns(DeprecationWarning):
+        mg = RasterModelGrid(3, 3, spacing=(4, 5))
+    assert 1 == 1
+
+
+def test_raise_deprecation_spacing_as_arg():
+    with pytest.warns(DeprecationWarning):
+        mg = RasterModelGrid(3, 3, 5)
+    assert 1 == 1
+
+
+def test_raise_deprecation_spacing2_as_arg():
+    with pytest.warns(DeprecationWarning):
+        mg = RasterModelGrid(3, 3, (4, 5))
+    assert 1 == 1
+
+
+def test_bad_shape_xy_spacing():
+    with pytest.raises(ValueError):
+        RasterModelGrid(3, 3, xy_spacing=(4, 5, 5))
+
+
+def test_bad_type_xy_spacing():
+    with pytest.raises(ValueError):
+        RasterModelGrid(3, 3, xy_spacing='spam and eggs')
+
+
+def test_deprecate_origin():
+    with pytest.warns(DeprecationWarning):
+        mg = RasterModelGrid(3, 3, origin=(10, 13))
+
+
+def test_bad_origin():
+    with pytest.raises(ValueError):
+        RasterModelGrid(3, 3, xy_of_lower_left=(10, 13, 12))
