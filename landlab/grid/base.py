e5adb64fb854dfd4a64bd8466435a9f8aedf2aab
kbarnhart <katy.barnhart@gmail.com>
2019-01-21 08:35:44 -0700

    adding boundary neighbor to base

--- landlab/grid/base.py
+++ landlab/grid/base.py
@@ -4253,6 +4253,63 @@ class ModelGrid(ModelDataFieldsMixIn, EventLayersMixIn, MaterialLayersMixIn):
         """
         self._xy_of_node += origin
 
+    def node_has_boundary_neighbor(self, ids):
+        """Check if ModelGrid nodes have neighbors that are boundary nodes.
+
+        Parameters
+        ----------
+        mg : HexModelGrid
+            Source grid
+        node_id : int
+            ID of node to test.
+
+        Returns
+        -------
+        boolean
+            ``True`` if node has a neighbor with a boundary ID,
+            ``False`` otherwise.
+
+
+        Checks to see if one of the eight neighbor nodes of node(s) with
+        *id* has a boundary node.  Returns True if a node has a boundary node,
+        False if all neighbors are interior.
+
+                0,  1,  2,  3,
+              4,  5,  6,  7,  8,
+            9, 10,  11, 12, 13, 14,
+              15, 16, 17, 18, 19,
+                20, 21, 22, 23
+
+        Examples
+        --------
+        >>> from landlab import HexModelGrid
+        >>> hmg = HexModelGrid(5, 4)
+        >>> hmg.node_has_boundary_neighbor(6)
+        True
+        >>> hmg.node_has_boundary_neighbor(12)
+        False
+        >>> hmg.node_has_boundary_neighbor([12, 0])
+        [False, True]
+
+        LLCATS: NINF CONN BC
+        """
+        ans = []
+        for i in np.atleast_1d(np.asarray(ids)):
+            neighbors = self.adjacent_nodes_at_node[i]
+            real_neighbors = neighbors[neighbors != BAD_INDEX_VALUE]
+            if real_neighbors.size == 0:
+                ans.append(True)
+            else:
+                neighbor_status = self.status_at_node[real_neighbors].astype(bool)
+                if np.any(neighbor_status != CORE_NODE):
+                    ans.append(True)
+                else:
+                    ans.append(False)
+
+        if len(ans) == 1:
+            return ans[0]
+        else:
+            return ans
 
 add_module_functions_to_class(ModelGrid, "mappers.py", pattern="map_*")
 # add_module_functions_to_class(ModelGrid, 'gradients.py',
