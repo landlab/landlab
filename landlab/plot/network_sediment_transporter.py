943ba453152c98159ed19e566c0ee102fb397886
kbarnhart <katy.barnhart@gmail.com>
2019-04-12 09:54:18 -0600

    pseudocode for plotting.

---
+++ landlab/plot/network_sediment_transporter.py
@@ -0,0 +1,17 @@
+
+
+def plot_network_average(grid, parcels, time, color=attribute_name, line_width=xxx, **kwargs):
+  this would plot lines from the saved shapefile attrite on grid and make
+  color based on the given attribute.
+  kwargs would have to do with color map, etc
+
+  have multiple options to save or return plot instance.
+
+
+def plot_network_parcels(grid, parcels, time, color=xxx, shape=xxx, size=xxs):
+  this would plot lines from saved shapefile,
+  then calculate distance down each grid segment,
+  then place each parcel along the squiggly shapefile line correctly with the
+  correct color, size, etc.
+
+  have multiple options to save or return plot instance.
