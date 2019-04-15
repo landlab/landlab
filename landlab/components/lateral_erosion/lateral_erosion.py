114e2f0bbbbf01815bd043259c902c4fcb6fb28d
alangston <alangston@ksu.edu>
2019-04-15 15:42:09 -0500

    end of day 4/15
    Inlet flags with qsin, area, etc. looking good.

--- landlab/components/lateral_erosion/lateral_erosion.py
+++ landlab/components/lateral_erosion/lateral_erosion.py
@@ -47,7 +47,7 @@ class LateralEroder(Component):
 #***from how to make a component: every component must start with init
 # first parameter is grid and **kwds is last parameter (allows to pass dictionary)
 # in between, need individual parameters.
-    def __init__(self, grid, vol_lat, latero_mech="UC", alph=0.8, Kv=None, Kl_ratio=1.0, inlet_node=None): #input_stream,
+    def __init__(self, grid, vol_lat, latero_mech="UC", alph=0.8, Kv=None, Kl_ratio=1.0, inlet_node=None, inlet_area=None, qsinlet=None): #input_stream,
         #**4/4/2019: come back to this: why did I put the underscore in from of grid? because diffusion said so.
         self._grid = grid
 #        self.initialize(grid, input_stream)
@@ -73,18 +73,33 @@ class LateralEroder(Component):
             self._TB = False
 
         self.alph=alph
-        self.Kv=Kv    #can be overwritten with spatially variable (enventually)
+        self.Kv=Kv    #can be overwritten with spatially variable 
+        self.inlet_on=False    #will be overwritten below if inlet area is provided
         self.Klr=float(Kl_ratio)    #default ratio of Kv/Kl is 1. Can be overwritten
         self.frac = 0.3 #for time step calculations
+        
 ##optional inputs
         if inlet_node is not None:
             self.inlet_node = inlet_node
-
+        if qsinlet is not None:
+            self.qsinlet = qsinlet
+        if inlet_area is not None:
+            self.inlet_area = inlet_area
+            # 4/24/2017 add inlet to change drainage area with spatially variable runoff rate
+            #runoff is an array with values of the area of each node (dx**2)
+            runoffinlet=np.ones(grid.number_of_nodes)*grid.dx**2
+            #Change the runoff at the inlet node to node area + inlet node
+            runoffinlet[inlet_node]=+inlet_area
+            _=grid.add_field('node', 'water__unit_flux_in', runoffinlet,
+                             noclobber=False)
+#            print("waterflux", reshape(grid['node'][ 'water__unit_flux_in'],(grid.number_of_node_rows,grid.number_of_node_columns)))
+            self.inlet_on=True
         #initialize qsin for each interior node, all zero initially.
 #        self.qsin = grid.zeros(centering='node')    # qsin (M^3/Y)
         self.dzdt = grid.zeros(centering='node')    # elevation change rate (M/Y)
 
 
+#        print(delt)
 #below, adding flag calling for Kv to be specified. as of April 15, this only works for
         #arrays and floats, NOT field names.
         if self.Kv is None:
@@ -110,15 +125,13 @@ class LateralEroder(Component):
         #anything defined in initialize, you have to redefine here.
         UC=self._UC
         TB=self._TB
-
+        inlet_on=self.inlet_on    #this is a true/false flag
         Kv=self.Kv
         frac = self.frac
         qsin=self.qsin
         dzdt=self.dzdt
         alph=self.alph
-        print("alpha=", alph)
-        print("KLr=", Klr)
-        print(delt)
+
         self.dt=dt
         vol_lat=self.grid.at_node['vol_lat']
 
@@ -148,10 +161,22 @@ class LateralEroder(Component):
         dzver=np.zeros(grid.number_of_nodes)
         vol_lat_dt=np.zeros(grid.number_of_nodes)
 
-
+        if inlet_on:
+            #define inlet_node
+            inlet_node=self.inlet_node
+            qsinlet=self.qsinlet
+            q=grid.at_node['surface_water__discharge']
+            da=q/dx**2    #this is the drainage area that I need for code below with an inlet set by spatially varible runoff.
+            #also change qsinlet
+            qsin[inlet_node]=qsinlet
+#            print("inlet on")
+        else:
+            da=grid.at_node['drainage_area']    #renamed this drainage area set by flow router
+#        print("da", da.reshape(nr,nc))
+#        print("qsin", qsin.reshape(nr,nc))
         #flow__upstream_node_order is node array contianing downstream to upstream order list of node ids
         s=grid.at_node['flow__upstream_node_order']
-        da=grid.at_node['drainage_area']    #renamed this drainage area set by flow router
+
         max_slopes=grid.at_node['topographic__steepest_slope']
         flowdirs=grid.at_node['flow__receiver_node']
         if(0):
@@ -169,7 +194,6 @@ class LateralEroder(Component):
 
         #make a list l, where node status is interior (signified by label 0) in s
         l=s[np.where((grid.status_at_node[s] == 0))[0]]
-
         dwnst_nodes=l
         #reverse list so we go from upstream to down stream
         dwnst_nodes=dwnst_nodes[::-1]
@@ -271,7 +295,7 @@ class LateralEroder(Component):
                 #***** CHECK ON THIS! what about time step? what units are qsin?
                 # april 10. this has to be here so that deposition can happen. dep takes qsin
                 qsin[flowdirs[i]]+=qsin[i]-(dzver[i]*dx**2)-(petlat*dx*wd)   #qsin to next node
-
+#            print("qsinafter", qsin.reshape(nr,nc))
             dzdt=dzver
 #            print("original erosion")
 #            print(dzver)
@@ -417,7 +441,8 @@ class LateralEroder(Component):
                 dt = globdt - time
                 print("small time steps. dt=",dt )
                 
-#                print(delt)
+                #clear qsin for next loop
+                qsin = grid.zeros(centering='node')
                 #recalculate flow directions
                 fa = FlowAccumulator(grid, 
                                      surface='topographic__elevation',
@@ -425,6 +450,19 @@ class LateralEroder(Component):
                                      runoff_rate=None,
                                      depression_finder=None, routing='D8')
                 (da, q) = fa.accumulate_flow()
+#                print("da", da.reshape(nr,nc))
+#                print("q2", q.reshape(nr,nc))
+                if inlet_on:
+#                    q=grid.at_node['surface_water__discharge']
+                    da=q/dx**2    #this is the drainage area that I need for code below with an inlet set by spatially varible runoff.
+                    qsin[inlet_node]=qsinlet
+#                    print("inlet on")
+#                    print("da2", da.reshape(nr,nc))
+#                    print("q2", q.reshape(nr,nc))
+#                    print(delt)
+                else:
+                    da=grid.at_node['drainage_area']    #renamed this drainage area set by flow router
+#                    print("da", da.reshape(nr,nc))
                 da=grid.at_node['drainage_area']    #renamed this drainage area set by flow router
                 s=grid.at_node['flow__upstream_node_order']
                 max_slopes=grid.at_node['topographic__steepest_slope']
@@ -432,7 +470,6 @@ class LateralEroder(Component):
                 flowdirs=grid.at_node['flow__receiver_node']
 #                drain_area=q/dx**2    #this is the drainage area that I need for code below with an inlet set by spatially varible runoff.
                 #recalculate downstream order
-                dsind = np.where((s >= min(interior_nodes)) & (s <= max(interior_nodes)))
                 l=s[np.where((grid.status_at_node[s] == 0))[0]]
                 dwnst_nodes=l
                 dwnst_nodes=dwnst_nodes[::-1]
@@ -440,9 +477,7 @@ class LateralEroder(Component):
 #                max_slopes=grid.calc_grad_at_active_link(z)
                 #temporary hack for drainage area
 #                drain_area[inlet_node]=inlet_area
-                #clear qsin for next loop
-                qsin = grid.zeros(centering='node')
-                qt = grid.zeros(centering='node')
+#               clear other things too.....
                 lat_nodes=np.zeros(grid.number_of_nodes, dtype=int)
                 dzlat=np.zeros(grid.number_of_nodes)
                 vol_lat_dt=np.zeros(grid.number_of_nodes)
