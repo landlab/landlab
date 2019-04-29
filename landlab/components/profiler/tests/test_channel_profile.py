7b60f096f8c58a0f550ba965f3f5ae1e21d180a4
kbarnhart <katy.barnhart@gmail.com>
2019-04-29 14:09:18 -0600

    moving note

--- landlab/components/profiler/tests/test_channel_profile.py
+++ landlab/components/profiler/tests/test_channel_profile.py
@@ -376,7 +376,8 @@ def test_re_calculating_profile_structure_and_distance():
     profiler.run_one_step()
     assert len(profiler._distance_along_profile) == 1  # result: 1
     profiler.run_one_step()
-    assert len(profiler._distance_along_profile) == 1  # here nathan originally found result: 2, a bug!
+    # here nathan originally found result: 2, a bug!
+    assert len(profiler._distance_along_profile) == 1
 
     # make the most complicated profile structure
     profiler = ChannelProfiler(
