755e5c124b75d0f357bc298c6da0f347197f7530
cmshobe <charles.shobe@colorado.edu>
2019-06-20 15:01:24 -0600

    fix TL analytical soln for erodep tests

--- landlab/components/erosion_deposition/tests/test_erodep.py
+++ landlab/components/erosion_deposition/tests/test_erodep.py
@@ -144,7 +144,7 @@ def test_steady_state_with_basic_solver_option():
     num_slope = mg.at_node["topographic__steepest_slope"][mg.core_nodes]
     analytical_slope = np.power(
         ((U * v_s * (1 - phi)) / (K * np.power(mg.at_node["drainage_area"][mg.core_nodes], m_sp)))
-        + (U / (K * np.power(mg.at_node["drainage_area"][mg.core_nodes], m_sp))),
+        + ((U * (1-phi)) / (K * np.power(mg.at_node["drainage_area"][mg.core_nodes], m_sp))),
         1.0 / n_sp,
     )
 
