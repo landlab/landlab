e6f7d14f7fe6e8b1a3959f69b11a84f5dbc16418
cmshobe <cmshobe@email.wm.edu>
2019-06-21 14:11:57 -0600

    fix mistake in space dt calc

--- landlab/components/space/space.py
+++ landlab/components/space/space.py
@@ -568,7 +568,8 @@ class Space(_GeneralizedErosionDeposition):
             dt_max2 = np.amin(time_to_zero_alluv)
 
             # Take the smaller of the limits
-            dt_max = max(self.dt_min, dt_max1, dt_max2)
+            dt_max = min(dt_max1, dt_max2)
+            dt_max = max(self.dt_min, dt_max)
 
             # Now a vector operation: apply dzdt and dhdt to all nodes
             br[cores] -= self.Er[cores] * dt_max
