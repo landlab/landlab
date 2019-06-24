7851245dca58e3cd240e1e27dc54d177313b1e60
cmshobe <charles.shobe@colorado.edu>
2019-06-24 13:55:21 -0600

    undo test for full coverage of dynamic solver

--- landlab/components/erosion_deposition/erosion_deposition.py
+++ landlab/components/erosion_deposition/erosion_deposition.py
@@ -323,7 +323,7 @@ class ErosionDeposition(_GeneralizedErosionDeposition):
             # For the first iteration, we assume this has already been done
             # outside the component (e.g., by flow router), but we need to do
             # it ourselves on subsequent iterations.
-            if first_iteration == False:
+            if not first_iteration:
                 # update the link slopes
                 self._update_flow_link_slopes()
                 # update where nodes are flooded. This shouuldn't happen bc
