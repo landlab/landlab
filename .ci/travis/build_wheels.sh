34d8c0d2f0b37f6da9490a7ea0870391409e4c33
mcflugen <mcflugen@gmail.com>
2019-07-04 10:37:53 -0600

    build with python 3.6 and 3.7

--- .ci/travis/build_wheels.sh
+++ .ci/travis/build_wheels.sh
@@ -4,10 +4,15 @@ set -e -x
 # Install a system package required by our library
 # yum install -y atlas-devel
 
-for PYBIN in /opt/python/*3*/bin/; do
-  ${PYBIN}/pip install numpy
-  ${PYBIN}/pip wheel /io/ -w wheelhouse
-done
+# for PYBIN in /opt/python/*3*/bin/; do
+#   ${PYBIN}/pip install numpy
+#   ${PYBIN}/pip wheel /io/ -w wheelhouse
+# done
+
+/opt/python/cp36-cp36m/bin/pip install numpy
+/opt/python/cp36-cp36m/bin/pip wheel /io/ -w wheelhouse
+/opt/python/cp37-cp37m/bin/pip install numpy
+/opt/python/cp37-cp37m/bin/pip wheel /io/ -w wheelhouse
 
 # Bundle external shared libraries into the wheels
 # for whl in wheelhouse/*.whl; do
