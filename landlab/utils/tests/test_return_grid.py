c4f56ad318f7fdc7abb5a46e1bd011af0176ffed
Greg Tucker <gtucker@colorado.edu>
2019-07-10 11:45:57 -0600

    add test for return_array_at...

--- landlab/utils/tests/test_return_grid.py
+++ landlab/utils/tests/test_return_grid.py
@@ -1,10 +1,21 @@
 import pytest
 
+import numpy as np
 from landlab import FieldError, RasterModelGrid
-from landlab.utils.return_array import return_array_at_node
+from landlab.utils.return_array import (return_array_at_node,
+                                        return_array_at_link)
 
 
 def test_no_field():
     mg = RasterModelGrid((10, 10))
     with pytest.raises(FieldError):
         return_array_at_node(mg, "spam")
+    with pytest.raises(FieldError):
+        return_array_at_link(mg, "spam")
+
+def test_return_array():
+    mg = RasterModelGrid((10, 10))
+    node_vals = np.arange(mg.number_of_nodes)
+    return_array_at_node(mg, node_vals)
+    link_vals = np.arange(mg.number_of_links)
+    return_array_at_link(mg, link_vals)
