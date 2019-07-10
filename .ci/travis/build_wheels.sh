3cd9c3f4871a086a452f896d38be2903afeb4a7b
mcflugen <mcflugen@gmail.com>
2019-07-10 12:11:37 -0600

    build wheels for 27, 36, and 37

--- .ci/travis/build_wheels.sh
+++ .ci/travis/build_wheels.sh
@@ -1,29 +1,21 @@
 #!/bin/bash
 set -e -x
 
-# Install a system package required by our library
-# yum install -y atlas-devel
+PYBIN=" \
+  /opt/python/cp27-cp27m/bin \
+  /opt/python/cp27-cp27mu/bin \
+  /opt/python/cp36-cp36m/bin \
+  /opt/python/cp37-cp37m/bin \
+"
 
-# for PYBIN in /opt/python/*3*/bin/; do
-#   ${PYBIN}/pip install numpy
-#   ${PYBIN}/pip wheel /io/ -w wheelhouse
-# done
-
-/opt/python/cp36-cp36m/bin/pip install numpy
-/opt/python/cp36-cp36m/bin/pip wheel /io/ -w /io/wheelhouse
-
-/opt/python/cp37-cp37m/bin/pip install numpy
-/opt/python/cp37-cp37m/bin/pip wheel /io/ -w /io/wheelhouse
+for bindir in $PYBIN; do
+  ${bindir}/pip install numpy
+  ${bindir}/pip wheel /io/ -w /io/wheelhouse
+done
 
 # Bundle external shared libraries into the wheels
 for whl in /io/wheelhouse/*.whl; do
     auditwheel repair "$whl" --plat $PLAT -w /io/wheelhouse/
 done
-
-# Install packages and test
-# for PYBIN in /opt/python/*/bin/; do
-#     "${PYBIN}/pip" install python-manylinux-demo --no-index -f /io/wheelhouse
-#     (cd "$HOME"; "${PYBIN}/nosetests" pymanylinuxdemo)
-# done
 
 ls /io/wheelhouse
