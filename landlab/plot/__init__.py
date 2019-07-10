dd1a7be3b13b7092c2abb50876960df88e8cfbad
Katherine Barnhart <katy.barnhart@gmail.com>
2019-07-09 18:20:19 -0600

    remove plot.profiler from inits

--- landlab/plot/__init__.py
+++ landlab/plot/__init__.py
@@ -1,4 +1,3 @@
-from landlab.plot.channel_profile import analyze_channel_network_and_plot
 from landlab.plot.imshow import imshow_grid, imshow_grid_at_node
 
-__all__ = ["imshow_grid", "imshow_grid_at_node", "analyze_channel_network_and_plot"]
+__all__ = ["imshow_grid", "imshow_grid_at_node"]
