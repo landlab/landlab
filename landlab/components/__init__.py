959ae09be664443ab02fb08802ae091296b8093a
Katy Barnhart <katy.barnhart@gmail.com>
2019-08-08 19:14:35 -0600

    add dupuit. (#986)

--- landlab/components/__init__.py
+++ landlab/components/__init__.py
@@ -18,6 +18,7 @@ from .flow_routing import DepressionFinderAndRouter, FlowRouter
 from .fracture_grid import FractureGridGenerator
 from .gflex import gFlex
 from .hack_calculator import HackCalculator
+from .groundwater import GroundwaterDupuitPercolator
 from .lake_fill import LakeMapperBarnes
 from .landslides import LandslideProbability
 from .lithology import LithoLayers, Lithology
@@ -53,6 +54,7 @@ from .weathering import ExponentialWeatherer
 
 COMPONENTS = [
     ChiFinder,
+    GroundwaterDupuitPercolator,
     ChannelProfiler,
     LinearDiffuser,
     Flexure,
