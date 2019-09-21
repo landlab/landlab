334478dfe7df59f61f9fded055ef971619814b25
mcflugen <mcflugen@gmail.com>
2019-09-21 17:06:46 -0600

    move skipped netcdf doctests into tests folder

--- tests/io/netcdf/test_read_netcdf.py
+++ tests/io/netcdf/test_read_netcdf.py
@@ -4,6 +4,7 @@
 import os
 
 import pytest
+from numpy.testing import assert_array_equal
 
 from landlab import RasterModelGrid
 from landlab.io import (
@@ -31,50 +32,78 @@ grid_mapping_keys = [
 ]
 
 
+def test_read_netcdf(datadir):
+    grid = read_netcdf(datadir / "test-netcdf4.nc")
+    assert grid.shape == (4, 3)
+    assert grid.dy, grid.dx == (1.0, 1.0)
+    assert list(grid.at_node.keys()) == ["surface__elevation"]
+    assert_array_equal(
+        grid.at_node["surface__elevation"],
+        [0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 11.0],
+    )
+
+
+def test_read_netcdf_64bit(datadir):
+    grid = read_netcdf(datadir / "test-netcdf3-64bit.nc")
+    assert grid.shape == (4, 3)
+    assert grid.dy, grid.dx == (1.0, 1.0)
+
+    grid = RasterModelGrid((6, 5), xy_of_lower_left=(-1.0, -1.0))
+    grid = read_netcdf(datadir / "test-netcdf4.nc", grid=grid, halo=1, nodata_value=-1)
+    assert_array_equal(
+        grid.at_node["surface__elevation"].reshape(grid.shape),
+        [
+            [-1.0, -1.0, -1.0, -1.0, -1.0],
+            [-1.0, 0.0, 1.0, 2.0, -1.0],
+            [-1.0, 3.0, 4.0, 5.0, -1.0],
+            [-1.0, 6.0, 7.0, 8.0, -1.0],
+            [-1.0, 9.0, 10.0, 11.0, -1.0],
+            [-1.0, -1.0, -1.0, -1.0, -1.0],
+        ],
+    )
+
+
 @pytest.mark.skipif(not WITH_NETCDF4, reason="netCDF4 package not installed")
-def test_read_netcdf4_bad_field_name():
+def test_read_netcdf4_bad_field_name(datadir):
     with pytest.raises(ValueError):
-        read_netcdf(
-            os.path.join(_TEST_DATA_DIR, "test-netcdf4.nc"),
-            name="not_surface__elevation",
-        )
+        read_netcdf(datadir / "test-netcdf4.nc", name="not_surface__elevation")
 
 
-def test_read_netcdf3_64bit():
+def test_read_netcdf3_64bit(datadir):
     """Test read_netcdf for with 64-bit netcdf3 format."""
-    grid = read_netcdf(os.path.join(_TEST_DATA_DIR, "test-netcdf3-64bit.nc"))
+    grid = read_netcdf(datadir / "test-netcdf3-64bit.nc")
     assert grid.shape == (4, 3)
 
 
 @pytest.mark.skipif(not WITH_NETCDF4, reason="netCDF4 package not installed")
-def test_read_netcdf4():
+def test_read_netcdf4(datadir):
     """Test read_netcdf with netcdf4 format."""
-    grid = read_netcdf(os.path.join(_TEST_DATA_DIR, "test-netcdf4.nc"))
+    grid = read_netcdf(datadir / "test-netcdf4.nc")
     assert grid.shape == (4, 3)
 
-    grid = read_netcdf(os.path.join(_TEST_DATA_DIR, "test-netcdf4.nc"))
+    grid = read_netcdf(datadir / "test-netcdf4.nc")
     assert grid.shape == (4, 3)
 
 
 @pytest.mark.skipif(not WITH_NETCDF4, reason="netCDF4 package not installed")
-def test_bad_data_size():
+def test_bad_data_size(datadir):
     """Test read_netcdf with netcdf4 format."""
     grid = RasterModelGrid((10, 10))
     with pytest.raises(MismatchGridDataSizeError):
-        read_netcdf(os.path.join(_TEST_DATA_DIR, "test-netcdf4.nc"), grid=grid)
+        read_netcdf(datadir / "test-netcdf4.nc", grid=grid)
 
 
 @pytest.mark.skipif(not WITH_NETCDF4, reason="netCDF4 package not installed")
-def test_bad_dx():
+def test_bad_dx(datadir):
     """Test read_netcdf with netcdf4 format."""
     grid = RasterModelGrid((4, 3), xy_spacing=10)
     with pytest.raises(MismatchGridXYSpacing):
-        read_netcdf(os.path.join(_TEST_DATA_DIR, "test-netcdf4.nc"), grid=grid)
+        read_netcdf(datadir / "test-netcdf4.nc", grid=grid)
 
 
 @pytest.mark.skipif(not WITH_NETCDF4, reason="netCDF4 package not installed")
-def test_bad_llc():
+def test_bad_llc(datadir):
     """Test read_netcdf with netcdf4 format."""
     grid = RasterModelGrid((4, 3), xy_of_lower_left=(-1, -2))
     with pytest.raises(MismatchGridXYLowerLeft):
-        read_netcdf(os.path.join(_TEST_DATA_DIR, "test-netcdf4.nc"), grid=grid)
+        read_netcdf(datadir / "test-netcdf4.nc", grid=grid)
