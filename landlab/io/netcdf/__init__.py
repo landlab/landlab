ccb7a4c03bc199a598efcb0278ab3ac7998465f5
mcflugen <mcflugen@gmail.com>
2019-09-21 17:06:00 -0600

    skip doctest using data files

--- landlab/io/netcdf/__init__.py
+++ landlab/io/netcdf/__init__.py
@@ -14,19 +14,10 @@ except ImportError:
 else:
     WITH_NETCDF4 = True
 
-NETCDF4_EXAMPLE_FILE = os.path.join(
-    os.path.dirname(__file__), "tests", "data", "test-netcdf4.nc"
-)
-NETCDF3_64BIT_EXAMPLE_FILE = os.path.join(
-    os.path.dirname(__file__), "tests", "data", "test-netcdf3-64bit.nc"
-)
-
 __all__ = [
     "read_netcdf",
     "write_netcdf",
     "write_raster_netcdf",
     "NotRasterGridError",
     "WITH_NETCDF4",
-    "NETCDF4_EXAMPLE_FILE",
-    "NETCDF3_64BIT_EXAMPLE_FILE",
 ]
