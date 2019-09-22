1fb2dd256c0cdbe4a82404306d61b5ec77c37fa9
mcflugen <mcflugen@gmail.com>
2019-09-21 22:08:17 -0600

    remove checks to see if netcdf4 is installed

--- landlab/io/netcdf/__init__.py
+++ landlab/io/netcdf/__init__.py
@@ -4,15 +4,6 @@ from .errors import NotRasterGridError
 from .read import read_netcdf
 from .write import write_netcdf, write_raster_netcdf
 
-try:
-    import netCDF4  # noqa: F401
-except ImportError:
-    import warnings
-
-    warnings.warn("Unable to import netCDF4.", ImportWarning)
-    WITH_NETCDF4 = False
-else:
-    WITH_NETCDF4 = True
 
 NETCDF4_EXAMPLE_FILE = os.path.join(
     os.path.dirname(__file__), "tests", "data", "test-netcdf4.nc"
@@ -26,7 +17,6 @@ __all__ = [
     "write_netcdf",
     "write_raster_netcdf",
     "NotRasterGridError",
-    "WITH_NETCDF4",
     "NETCDF4_EXAMPLE_FILE",
     "NETCDF3_64BIT_EXAMPLE_FILE",
 ]
