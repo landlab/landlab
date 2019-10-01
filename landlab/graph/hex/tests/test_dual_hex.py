aef7d409b5e2bd61aaf754bc5cdad7e845559e5c
Katy Barnhart <katy.barnhart@gmail.com>
2019-10-01 14:00:27 -0600

    remove lint now that tests have been removed

--- landlab/graph/hex/tests/test_dual_hex.py
+++ landlab/graph/hex/tests/test_dual_hex.py
@@ -1,7 +1,7 @@
 """Test HexGraph and DualHexGraph."""
 import numpy as np
 from numpy.testing import assert_array_almost_equal, assert_array_equal
-from pytest import approx, mark
+from pytest import approx
 
 from landlab.graph import DualHexGraph
 
