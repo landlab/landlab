8b1cd47adf7ecc065b49e81d360f39ba4a83e9e3
Katy Barnhart <katy.barnhart@gmail.com>
2019-11-01 14:56:57 -0600

    formatting

--- landlab/components/taylor_nonlinear_hillslope_flux/taylor_nonlinear_hillslope_flux.py
+++ landlab/components/taylor_nonlinear_hillslope_flux/taylor_nonlinear_hillslope_flux.py
@@ -200,12 +200,7 @@ class TaylorNonLinearDiffuser(Component):
         else:
             self._flux = self._grid.add_zeros("link", "soil__flux")
 
-    def soilflux(
-            self,
-            dt,
-            dynamic_dt=False,
-            if_unstable="pass",
-            courant_factor=0.2):
+    def soilflux(self, dt, dynamic_dt=False, if_unstable="pass", courant_factor=0.2):
         """Calculate soil flux for a time period 'dt'.
 
         Parameters
