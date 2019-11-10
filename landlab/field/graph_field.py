45318a10460c75eeea41caf61c7c00ee5e98d282
mcflugen <mcflugen@gmail.com>
2019-11-10 09:38:22 -0700

    use new-style add_ones signature in landlab

--- landlab/field/graph_field.py
+++ landlab/field/graph_field.py
@@ -574,7 +574,7 @@ class GraphFields(object):
         >>> from landlab.field import GraphFields
         >>> fields = GraphFields()
         >>> fields.new_field_location('node', 12)
-        >>> _ = fields.add_ones('node', 'topographic__elevation')
+        >>> _ = fields.add_ones("topographic__elevation", at="node")
         >>> fields.has_field('node', 'topographic__elevation')
         True
         >>> fields.has_field('cell', 'topographic__elevation')
@@ -679,7 +679,7 @@ class GraphFields(object):
         to the *node* group. The *field_values* method returns a reference
         to the field's data.
 
-        >>> _ = fields.add_ones('node', 'topographic__elevation')
+        >>> _ = fields.add_ones("topographic__elevation", at="node")
         >>> fields.field_values('node', 'topographic__elevation')
         array([ 1.,  1.,  1.,  1.])
 
@@ -750,7 +750,7 @@ class GraphFields(object):
         to the *node* group. The *field_values* method returns a reference
         to the field's data.
 
-        >>> _ = fields.add_ones('node', 'topographic__elevation')
+        >>> _ = fields.add_ones("topographic__elevation", at="node")
         >>> fields.field_values('node', 'topographic__elevation')
         array([ 1.,  1.,  1.,  1.])
 
@@ -1194,7 +1194,7 @@ class GraphFields(object):
         >>> from landlab.field import GraphFields
         >>> field = GraphFields()
         >>> field.new_field_location('node', 4)
-        >>> field.add_ones('node', 'topographic__elevation')
+        >>> field.add_ones("topographic__elevation", at="node")
         array([ 1.,  1.,  1.,  1.])
         >>> list(field.keys('node'))
         ['topographic__elevation']
