af5f4ed11c65ee36586d530039a81b6d6e9d2618
Katy Barnhart <katy.barnhart@gmail.com>
2020-04-01 16:47:36 -0600

    work on making plotting tool.

--- landlab/plot/__init__.py
+++ landlab/plot/__init__.py
@@ -1,3 +1,4 @@
 from landlab.plot.imshow import imshow_grid, imshow_grid_at_node
+from landlab.plot.network_sediment_transporter import plot_network_and_parcels
 
-__all__ = ["imshow_grid", "imshow_grid_at_node"]
+__all__ = ["imshow_grid", "imshow_grid_at_node", "plot_network_and_parcels"]
--- landlab/plot/network_sediment_transporter/__init__.py
+++ landlab/plot/network_sediment_transporter/__init__.py
@@ -4,17 +4,20 @@ Created on Tue Jun 18 14:22:27 2019
 
 @author: pfeif
 """
-from .locate_parcel_xy import locate_parcel_xy
-from .plot_network_links import plot_network_links
-from .plot_network_parcels import plot_network_parcels
-from .plot_pathway_values import plot_pathway_values
+# from .locate_parcel_xy import locate_parcel_xy
+# from .plot_network_links import plot_network_links
+# from .plot_network_parcels import plot_network_parcels
+# from .plot_pathway_values import plot_pathway_values
 
+from .plot_network_and_parcels import plot_network_and_parcels
+
+__all__ = ["plot_network_and_parcels"]
 # from .other_example_plot import other_example_plot
-
-# __all__ = ["plot_network_links","other_example_plot"]
-__all__ = [
-    "plot_network_links",
-    "locate_parcel_xy",
-    "plot_network_parcels",
-    "plot_pathway_values",
-]
+#
+# # __all__ = ["plot_network_links","other_example_plot"]
+# __all__ = [
+#     "plot_network_links",
+#     "locate_parcel_xy",
+#     "plot_network_parcels",
+#     "plot_pathway_values",
+# ]
