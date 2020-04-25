d8a8e674b24bae4ae9bee23f2dc722003772ba94
Katy Barnhart <katy.barnhart@gmail.com>
2020-04-25 08:20:11 -0600

    expand tests to include both adaptive and basic timesteppers.

--- tests/components/space/test_mass_conserve_pits.py
+++ tests/components/space/test_mass_conserve_pits.py
@@ -1,34 +1,38 @@
+import pytest
 from numpy.testing import assert_array_almost_equal
 
 from landlab import RasterModelGrid
-from landlab.components import Space, FlowAccumulator
+from landlab.components import FlowAccumulator, Space
 
 
-def test_mass_conserve_all_closed_no_depression_finder():
-
-    grid = RasterModelGrid((10,10), xy_spacing=10.0)
+@pytest.fixture
+def grid():
+    grid = RasterModelGrid((10, 10), xy_spacing=10.0)
     grid.set_closed_boundaries_at_grid_edges(True, True, True, True)
     z = grid.add_zeros("node", "topographic__elevation")
-    H = grid.add_zeros("node", "soil__depth")
-    z+=grid.x_of_node.copy() + grid.y_of_node.copy()
-    z[25]-=40
-    z[35]-=40
-    z[26]-=40
-    z[36]-=40
-    z[24]-=40
-    z[34]-=40
+    grid.add_zeros("node", "soil__depth")
+    z += grid.x_of_node.copy() + grid.y_of_node.copy()
+    z[25] -= 40
+    z[35] -= 40
+    z[26] -= 40
+    z[36] -= 40
+    z[24] -= 40
+    z[34] -= 40
+    return grid
 
-    z_init = z.copy()
 
+@pytest.mark.parametrize("solver", ["basic", "adaptive"])
+def test_mass_conserve_all_closed_no_depression_finder(grid, solver):
+    z_init = grid.at_node["topographic__elevation"].copy()
     fa = FlowAccumulator(grid)
     fa.run_one_step()
 
-    sp = Space(grid, phi=0.)
+    sp = Space(grid, phi=0.0, solver=solver)
     sp.run_one_step(1.0)
 
-    dz = z - z_init
+    dz = grid.at_node["topographic__elevation"] - z_init
 
-    assert_array_almost_equal(dz.mean(), 0.)
+    assert_array_almost_equal(dz.mean(), 0.0)
 
 
 # Note that we can't make an equivalent test for with a depression finder yet
