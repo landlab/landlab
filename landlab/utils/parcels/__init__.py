bb48cd15e221b14dadb5a1123cfdf29c6b7841d3
mcflugen <mcflugen@gmail.com>
2020-05-18 08:18:03 -0600

    import BedParcelInitializer with landlab.utils.parcels

--- landlab/utils/parcels/__init__.py
+++ landlab/utils/parcels/__init__.py
@@ -1,3 +1,6 @@
-from .bed_parcel_initializer import synthetic_bed_parcel_initializer
+from .bed_parcel_initializer import (
+    synthetic_bed_parcel_initializer, BedParcelInitializer
+)
 
-__all__ = ["synthetic_bed_parcel_initializer"]
+
+__all__ = ["synthetic_bed_parcel_initializer", "BedParcelInitializer"]
