c5c79b2379d51a8b7fc7f429c6a9704dc5b437fb
mcflugen <mcflugen@gmail.com>
2020-08-18 08:57:04 -0600

    use enum for link directions

--- landlab/grid/hex_mappers.py
+++ landlab/grid/hex_mappers.py
@@ -9,22 +9,25 @@ Mapping functions unique to hex grids
 
     ~landlab.grid.hex_mappers.map_link_vector_components_to_node_hex
 """
+import enum
 
 import numpy as np
 
-LINK_TO_EAST = 0  # array column of link to east/right of node, horizontal grid
-LINK_TO_NNE = 1  # array column of link to nne/upper right of node, horizontal grid
-LINK_TO_NNW = 2  # array column of link to nnw/upper left of node, horizontal grid
-LINK_TO_WEST = 3  # array column of link to west/left of node, horizontal grid
-LINK_TO_SSW = 4  # array column of link to ssw/lower left of node, horizontal grid
-LINK_TO_SSE = 5  # array column of link to sse/lower right of node, horizontal grid
 
-LINK_TO_ENE = 0  # array column of link to ene/upper right of node, vertical grid
-LINK_TO_NORTH = 1  # array column of link to north/top of node, vertical grid
-LINK_TO_WNW = 2  # array column of link to wnw/upper left of node, vertical grid
-LINK_TO_WSW = 3  # array column of link to wsw/lower left of node, vertical grid
-LINK_TO_SOUTH = 4  # array column of link to south/bottom of node, vertical grid
-LINK_TO_ESE = 5  # array column of link to ese/lower right of node, vertical grid
+class LinkAtNode(enum.IntEnum):
+    EAST = 0  # array column of link to east/right of node, horizontal grid
+    ENE = 0  # array column of link to ene/upper right of node, vertical grid
+    NNE = 1  # array column of link to nne/upper right of node, horizontal grid
+    NORTH = 1  # array column of link to north/top of node, vertical grid
+    NNW = 2  # array column of link to nnw/upper left of node, horizontal grid
+    WNW = 2  # array column of link to wnw/upper left of node, vertical grid
+    WEST = 3  # array column of link to west/left of node, horizontal grid
+    WSW = 3  # array column of link to wsw/lower left of node, vertical grid
+    SSW = 4  # array column of link to ssw/lower left of node, horizontal grid
+    SOUTH = 4  # array column of link to south/bottom of node, vertical grid
+    SSE = 5  # array column of link to sse/lower right of node, horizontal grid
+    ESE = 5  # array column of link to ese/lower right of node, vertical grid
+
 
 SIN60 = np.sin(np.deg2rad(60.0))
 
@@ -41,11 +44,13 @@ def map_link_vector_components_to_node_hex(grid, data_at_link):
 
     Examples
     --------
+    >>> import numpy as np
     >>> from landlab import HexModelGrid
     >>> from landlab.grid.mappers import map_link_vector_components_to_node
     >>> grid = HexModelGrid((3, 3))
     >>> link_data = np.zeros(grid.number_of_links) + 0.5 * 3.0**0.5
-    >>> link_data[grid.link_with_angle(0.0)] = 0.0
+    >>> link_data[np.isclose(grid.angle_of_link, 0.0)] = 0.0
+    >>> # link_data[grid.link_with_angle(0.0)] = 0.0
     >>> vx, vy = map_link_vector_components_to_node(grid, link_data)
     >>> vx
     array([ 0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.])
@@ -56,7 +61,8 @@ def map_link_vector_components_to_node_hex(grid, data_at_link):
     >>> vx
     array([ 0. ,  0. ,  0. ,  0. ,  8.5,  9.5,  0. ,  0. ,  0. ,  0. ])
     >>> link_data = np.zeros(grid.number_of_links) + 0.5 * 3.0**0.5
-    >>> link_data[grid.link_with_angle(120.0)] = 0.0
+    >>> link_data[np.isclose(grid.angle_of_link, 2.0 / 3.0 * np.pi)] = 0.0
+    >>> # link_data[grid.link_with_angle(120.0)] = 0.0
     >>> vx, vy = map_link_vector_components_to_node(grid, link_data)
     >>> np.round(vx, 3)
     array([ 0. ,  0. ,  0. ,  0. ,  0.866,  0.866,  0. ,  0. ,  0. ,  0. ])
@@ -69,7 +75,8 @@ def map_link_vector_components_to_node_hex(grid, data_at_link):
     >>> vy
     array([ 0. , 0. ,  0. ,  5.5,  0. ,  0. , 12.5,  0. ,  0. ,  0. ])
     >>> link_data = np.zeros(grid.number_of_links) + 0.5 * 3.0**0.5
-    >>> link_data[grid.link_with_angle(90.0)] = 0.0
+    >>> link_data[np.isclose(grid.angle_of_link, np.pi / 2.0)] = 0.0
+    >>> # link_data[grid.link_with_angle(90.0)] = 0.0
     >>> vx, vy = map_link_vector_components_to_node(grid, link_data)
     >>> vx
     array([ 0.,  0.,  0.,  1.,  0.,  0.,  1.,  0.,  0.,  0.])
@@ -115,23 +122,24 @@ def map_link_vector_components_to_node_hex(grid, data_at_link):
     y_component = np.zeros(grid.number_of_nodes)
 
     if grid.orientation[0] == "h":
-        east = grid.links_at_node[cores, LINK_TO_EAST]
-        nne = grid.links_at_node[cores, LINK_TO_NNE]
-        nnw = grid.links_at_node[cores, LINK_TO_NNW]
-        west = grid.links_at_node[cores, LINK_TO_WEST]
-        sse = grid.links_at_node[cores, LINK_TO_SSE]
-        ssw = grid.links_at_node[cores, LINK_TO_SSW]
+        east = grid.links_at_node[cores, LinkAtNode.EAST]
+        nne = grid.links_at_node[cores, LinkAtNode.NNE]
+        nnw = grid.links_at_node[cores, LinkAtNode.NNW]
+        west = grid.links_at_node[cores, LinkAtNode.WEST]
+        sse = grid.links_at_node[cores, LinkAtNode.SSE]
+        ssw = grid.links_at_node[cores, LinkAtNode.SSW]
+
         x_component[cores] = (data_at_link[west] + data_at_link[east]) / 2
         vyn = (3.0 * data_at_link[nnw] - data_at_link[nne]) / (2 * SIN60)
         vys = (3.0 * data_at_link[ssw] - data_at_link[sse]) / (2 * SIN60)
         y_component[cores] = (vyn + vys) / 2
     else:
-        ene = grid.links_at_node[cores, LINK_TO_ENE]
-        north = grid.links_at_node[cores, LINK_TO_NORTH]
-        wnw = grid.links_at_node[cores, LINK_TO_WNW]
-        wsw = grid.links_at_node[cores, LINK_TO_WSW]
-        south = grid.links_at_node[cores, LINK_TO_SOUTH]
-        ese = grid.links_at_node[cores, LINK_TO_ESE]
+        ene = grid.links_at_node[cores, LinkAtNode.ENE]
+        north = grid.links_at_node[cores, LinkAtNode.NORTH]
+        wnw = grid.links_at_node[cores, LinkAtNode.WNW]
+        wsw = grid.links_at_node[cores, LinkAtNode.WSW]
+        south = grid.links_at_node[cores, LinkAtNode.SOUTH]
+        ese = grid.links_at_node[cores, LinkAtNode.ESE]
         y_component[cores] = (data_at_link[north] + data_at_link[south]) / 2
         vxe = (3.0 * data_at_link[ese] - data_at_link[ene]) / (2 * SIN60)
         vxw = (3.0 * data_at_link[wsw] - data_at_link[wnw]) / (2 * SIN60)
