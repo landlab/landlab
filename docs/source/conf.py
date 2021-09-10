7341d197624750fc0e3776cf05223ea373c51980
mcflugen <mcflugen@gmail.com>
2021-09-10 12:14:08 -0600

    add landlab newsletter to ignore list

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -57,17 +57,17 @@ source_suffix = ".rst"
 
 # Regex for links that we know work in browser, but do not work in sphinx/CI (BE VERY CAREFUL ADDING LINKS TO THIS LIST)
 if os.getenv("GITHUB_ACTIONS"):
-    # linkcheck_ignore = [
-    #     r"https://pubs.geoscienceworld.org/gsa/geology.*",  # Added by KRB Dec 2019, at this point two links match this pattern
-    #     r"https://dx.doi.org/10.1130/*",  # Added by KRB Jan 2019. Four links match this pattern
-    #     re.escape(
-    #         r"https://github.us18.list-manage.com/subscribe?u=2db7cea82e3ea40fcf4c91247&id=b9bad233c7"
-    #     ),  # Added by EWHH Feb 2020
-    #     r"https://dx.doi.org/10.1029/2011jf002181",  # Added by EWHH April 2020
-    #     r"https://doi.org/10.1029/2019JB018596",  # Added by EWHH April 2020
-    #     r"https://doi.org/10.3133/pp294B",  # Added by EWHH September 2021
-    #     r"https://yaml.org/start.html",  # Added by EWHH September 2021
-    # ]
+    linkcheck_ignore = [
+        #     r"https://pubs.geoscienceworld.org/gsa/geology.*",  # Added by KRB Dec 2019, at this point two links match this pattern
+        #     r"https://dx.doi.org/10.1130/*",  # Added by KRB Jan 2019. Four links match this pattern
+        re.escape(
+            r"https://github.us18.list-manage.com/subscribe?u=2db7cea82e3ea40fcf4c91247&id=b9bad233c7"
+        ),  # Added by EWHH Feb 2020
+        #     r"https://dx.doi.org/10.1029/2011jf002181",  # Added by EWHH April 2020
+        #     r"https://doi.org/10.1029/2019JB018596",  # Added by EWHH April 2020
+        #     r"https://doi.org/10.3133/pp294B",  # Added by EWHH September 2021
+        #     r"https://yaml.org/start.html",  # Added by EWHH September 2021
+    ]
     linkcheck_retries = 5
 
 # The master toctree document.
