08257264328ee016572128740be019aef95c32eb
mcflugen <mcflugen@gmail.com>
2021-10-12 10:48:24 -0600

    use int instead of np.int in cython

--- landlab/components/space_v2/cfuncs.pyx
+++ landlab/components/space_v2/cfuncs.pyx
@@ -9,7 +9,7 @@ from libc.math cimport log,exp
 DTYPE_FLOAT = np.double
 ctypedef np.double_t DTYPE_FLOAT_t
 
-DTYPE_INT = np.int
+DTYPE_INT = int
 ctypedef np.int_t DTYPE_INT_t
 
 DTYPE_UINT8 = np.uint8
