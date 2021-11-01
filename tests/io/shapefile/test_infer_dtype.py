754cc25077824a13e6ea45b08a803a5ba41c36e0
mcflugen <mcflugen@gmail.com>
2021-11-01 13:32:19 -0600

    test infer from object array

--- tests/io/shapefile/test_infer_dtype.py
+++ tests/io/shapefile/test_infer_dtype.py
@@ -1,5 +1,3 @@
-from functools import partial
-
 import numpy as np
 import pytest
 from numpy.testing import assert_array_equal
@@ -8,24 +6,32 @@ from numpy.testing import assert_array_equal
 from landlab.io.shapefile.read_shapefile import _infer_data_type
 
 
-@pytest.mark.parametrize("in_type", [np.int32, np.int64, float])
-@pytest.mark.parametrize(
-    "astype", [list, tuple, np.asarray, partial(np.asarray, dtype=object)]
-)
-def test_infer_dtype(astype, in_type):
-    values = np.asarray([1, 2, 3], dtype=in_type)
-    array = _infer_data_type(astype(values))
-    assert array.dtype == in_type
+@pytest.mark.parametrize("src_type", [np.int32, np.int64, float, bool])
+@pytest.mark.parametrize("as_iterable", [list, tuple, np.asarray])
+def test_infer_dtype(as_iterable, src_type):
+    values = np.asarray([1, 2, 3], dtype=src_type)
+    array = _infer_data_type(as_iterable(values))
+    assert array.dtype == src_type
     assert_array_equal(array, values)
 
 
-@pytest.mark.parametrize("dtype", [int, float, np.int32, np.int64])
-@pytest.mark.parametrize("in_type", [int, float, object])
+@pytest.mark.parametrize("as_iterable", [list, tuple, np.asarray])
 @pytest.mark.parametrize(
-    "astype", [list, tuple, np.asarray, partial(np.asarray, dtype=object)]
+    "values,dst_type",
+    [([1, 2, 3], np.int_), ([1.0, 2.0, 3.0], float), ([True, True, False], bool)],
 )
-def test_dtype_keyword(astype, in_type, dtype):
-    values = np.asarray([1, 2, 3], dtype=in_type)
-    array = _infer_data_type(astype(values), dtype=dtype)
-    assert array.dtype == dtype
+def test_infer_dtype_from_object(as_iterable, values, dst_type):
+    values = np.asarray(values, dtype=object)
+    array = _infer_data_type(as_iterable(values))
+    assert array.dtype == dst_type
+    assert_array_equal(array, values)
+
+
+@pytest.mark.parametrize("dst_type", [int, float, np.int32, np.int64])
+@pytest.mark.parametrize("src_type", [int, float, object])
+@pytest.mark.parametrize("as_iterable", [list, tuple, np.asarray])
+def test_dtype_keyword(as_iterable, src_type, dst_type):
+    values = np.asarray([1, 2, 3], dtype=src_type)
+    array = _infer_data_type(as_iterable(values), dtype=dst_type)
+    assert array.dtype == dst_type
     assert_array_equal(array, [1, 2, 3])
