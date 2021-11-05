f22bf211114b3d0aa76955dbcf868d0874de82b4
Benjamin Campforts <beca4397@cu-biot-13-10.203.176.159.int.colorado.edu>
2021-11-05 15:00:20 -0600

    adjust imshowhs docstrings of methods

--- landlab/plot/imshow.py
+++ landlab/plot/imshow.py
@@ -230,70 +230,65 @@ def imshowhs_grid_at_node(grid, values, **kwds):
                                 shaded relief provided in the 
                                 'topographic__elevation' field
         The default is "DEM".
-    drape1 : TYPE, optional
-        DESCRIPTION. The default is None.
-    drape2 : TYPE, optional
-        DESCRIPTION. The default is None.
-    cmap2 : TYPE, optional
-        DESCRIPTION. The default is None.
-    vertical_exa : TYPE, optional
-        DESCRIPTION. The default is None.
-    azdeg : TYPE, optional
-        DESCRIPTION. The default is None.
-    altdeg : TYPE, optional
-        DESCRIPTION. The default is None.
-    thres_drape1 : TYPE, optional
-        DESCRIPTION. The default is None.
-    alpha : TYPE, optional
-        DESCRIPTION. The default is None.
-    thres_drape2 : TYPE, optional
-        DESCRIPTION. The default is None.
-    alpha2 : TYPE, optional
-        DESCRIPTION. The default is None.
-    add_double_colorbar : TYPE, optional
-        DESCRIPTION. The default is False.
-    plt_contour : TYPE, optional
-        DESCRIPTION. The default is False.
-    contour_nb : TYPE, optional
-        DESCRIPTION. The default is 50.
-    default_fontsize : TYPE, optional
-        DESCRIPTION. The default is 10.
-    cbar_height : TYPE, optional
-        DESCRIPTION. The default is "5%".
-    cbar_width : TYPE, optional
-        DESCRIPTION. The default is "30%".
-    cbar_or : TYPE, optional
-        DESCRIPTION. The default is "horizontal".
-    cbar_loc : TYPE, optional
-        DESCRIPTION. The default is "lower right".
-    bbox_to_anchor : TYPE, optional
-        DESCRIPTION. The default is (0, 0, 1, 1).
-    cbar_ticks_position : TYPE, optional
-        DESCRIPTION. The default is "top".
-    cbar_ticks_position2 : TYPE, optional
-        DESCRIPTION. The default is "bottom".
-    colorbar_label_y : TYPE, optional
-        DESCRIPTION. The default is -40.
-    colorbar_label_x : TYPE, optional
-        DESCRIPTION. The default is 0.5.
-    cbar_tick_size : TYPE, optional
-        DESCRIPTION. The default is 10.
-    cbar_label_color : TYPE, optional
-        DESCRIPTION. The default is 'black'.
-    cbar_label_fontweight : TYPE, optional
-        DESCRIPTION. The default is 'bold'.
-    add_label_bbox : TYPE, optional
-        DESCRIPTION. The default is False.
-
-    Raises
-    ------
-    ValueError
-        DESCRIPTION.
+    drape1 : array_like, masked_array
+        Node values to plot on top of a hillshade map. The default is None.
+    drape2 : array_like, masked_array
+        Node values to plot on top of drape1 and a hillshade map. The default is None.
+    cmap2 : str
+        Name of a colormap for drape 2. The default is None.
+    vertical_exa : float, optional
+        vertical exageration of hillshade map. The default is None.
+    azdeg : float, optional
+        azimuth of light source. The default is 315.
+    altdeg : float, optional
+        elevation of light source. The default is 65.
+    thres_drape1 : float, optional
+        threshold below which drape1 is made transparant. The default is None.
+    alpha : float (0-1), optional
+        transparency of DEM/Drape1 . The default is None.
+    thres_drape2 : float, optional
+        threshold below which drape2 is made transparant. The default is None.
+    alpha2 : float (0-1), optional
+        transparency of Drape2 . The default is None.
+    add_double_colorbar : bool, optional
+        add a double colorbar when two drapes are plotted. The default is False.
+    plt_contour : bool, optional
+        Add contour lines to elevation plot . The default is False.
+    contour_nb : int, optional
+        number of contour lines. The default is 50.
+    default_fontsize : float, optional
+        Default font size of plot labels. The default is 10.
+    cbar_height : percentage, optional
+        height of colorbar in percentage of figure. The default is "5%".
+    cbar_width : percentage, optional
+        width of colorbar in percentage of figure. The default is "30%".
+    cbar_or : str, optional
+        orientation of colorbar. The default is "horizontal".
+    cbar_loc : str, optional
+        location of colorbar. The default is "lower right".
+    bbox_to_anchor : vector, optional
+        bbox to anchor. The default is (0, 0, 1, 1).
+    cbar_ticks_position : str, optional
+        location of colorbar ticks (below or on top of the colorbar). The default is "top".
+    cbar_ticks_position2 : str, optional
+        location of colorbar ticks for colorbar of Drape2 (below or on top of the colorbar). The default is "bottom".
+    colorbar_label_y : float, optional
+        location of colorbar label with respect to the colorbar in y direction. The default is -40.
+    colorbar_label_x : float , optional
+        location of colorbar label with respect to the colorbar in x direction. The default is 0.5.
+    cbar_tick_size : float, optional
+        colorbar tick size. The default is 10.
+    cbar_label_color : str, optional
+        colorbar tick color. The default is 'black'.
+    cbar_label_fontweight : str, optional
+        colorbar font weight. The default is 'bold'.
+    add_label_bbox : bool, optional
+        Add a bbox surrounding the colorbar label. The default is False.
 
     Returns
     -------
-    ax : TYPE
-        DESCRIPTION.
+    ax : figure ax
+        return ax if output == True.
 
     """
     if isinstance(values, str):
@@ -620,8 +615,8 @@ def _imshowhs_grid_values(
     drape2=None,
     cmap2=None,
     vertical_exa=None,
-    azdeg=None,
-    altdeg=None,
+    azdeg=315,
+    altdeg=65,
     thres_drape1=None,
     alpha=None,
     thres_drape2=None,
@@ -686,10 +681,7 @@ def _imshowhs_grid_values(
             - grid.dx * 0.5
             + grid.xy_of_lower_left[0]
         )
-        if azdeg is None:
-            azdeg = 315
-        if altdeg is None:
-            altdeg = 65
+        
 
         ls = LightSource(azdeg=azdeg, altdeg=altdeg)
         if cmap is None:
@@ -1220,7 +1212,157 @@ def imshow_grid(grid, values, **kwds):
 
 def imshowhs_grid(grid, values, **kwds):
     """
-    see imshow_grid
+    Prepare a map view of data over all nodes in the grid using a hillshade 
+    topography map in the background.
+
+    Data is plotted as cells shaded with the value at the node at its center.
+    Outer edges of perimeter cells are extrapolated. Closed elements are
+    colored uniformly (default black, overridden with kwd 'color_for_closed');
+    other open boundary nodes get their actual values.
+
+    *values* can be a field name, a regular array, or a masked array. If a
+    masked array is provided, masked entries will be treated as if they were
+    Landlab BC_NODE_IS_CLOSED. Used together with the color_at_closed=None
+    keyword (i.e., "transparent"), this can allow for construction of overlay
+    layers in a figure (e.g., only defining values in a river network, and
+    overlaying it on another landscape).
+
+    Use matplotlib functions like xlim, ylim to modify your plot after calling
+    :func:`imshowhs_grid`, as desired.
+
+    Node coordinates are printed when a mouse button is pressed on a cell in
+    the plot.
+
+    For now, this function only works with regular grids.
+
+    Parameters
+    ----------
+    grid : ModelGrid
+        Grid containing the field to plot, or describing the geometry of the
+        provided array.
+    values : array_like, masked_array, or str
+        Node values, or a field name as a string from which to draw the data.
+    plot_name : str, optional
+        String to put as the plot title.
+    var_name : str, optional
+        Variable name, to use as a colorbar label.
+    var_name_two : str, optional
+        Variable name of second layer, to use as a colorbar label.
+    var_units : str, optional
+        Units for the variable being plotted, for the colorbar.
+    grid_units : tuple of str, optional
+        Units for y, and x dimensions. If None, component will look to the
+        gri property `axis_units` for this information. If no units are
+        specified there, no entry is made.
+    symmetric_cbar : bool
+        Make the colormap symetric about 0.
+    cmap : str
+        Name of a colormap
+    limits : tuple of float
+        Minimum and maximum of the colorbar.
+    vmin, vmax: floats
+        Alternatives to limits.
+    norm : matplotlib.colors.Normalize
+        The normalizing object which scales data, typically into the interval
+        [0, 1]. Ignore in most cases.
+    thicks_km : bool, optional
+        Display thicks in km instead of m
+    allow_colorbar : bool
+        If True, include the colorbar.
+    shrink : float
+        Fraction by which to shrink the colorbar.
+    color_for_closed : str or None
+        Color to use for closed nodes (default 'black'). If None, closed
+        (or masked) nodes will be transparent.
+    color_for_background : color str or other color declaration, or None
+        Color to use for closed elements (default None). If None, the
+        background will be transparent, and appear white.
+    output : None, string, or bool
+        If None (or False), the image is sent to the imaging buffer to await
+        an explicit call to show() or savefig() from outside this function.
+        If a string, the string should be the path to a save location, and the
+        filename (with file extension). The function will then call
+        plt.savefig([string]) itself. If True, the function will call
+        plt.show() itself once plotting is complete.   
+    fontweight_xlabel : str, optional
+        weight of x label. The default is 'bold'.
+    fontweight_ylabel : str, optional
+        weight of y label. The default is 'bold'.   
+    plot_type : str, optional
+        The type of plot that will be plotted. 
+        There are four options:
+            * 'DEM'         : Display a digital elevation map underlain by a shaded 
+                                relief, based on the same DEM 
+                                ('topographic__elevation')
+            * 'Hillshade'   : Display the shaded relief, of the provided DEM 
+                                ('topographic__elevation')
+            * 'Drape1'      : Display any kind of provided layer on top of a 
+                                shaded relief provided in the 
+                                'topographic__elevation' field
+            * 'Drape2'      : Display two layers on top of a 
+                                shaded relief provided in the 
+                                'topographic__elevation' field
+        The default is "DEM".
+    drape1 : array_like, masked_array
+        Node values to plot on top of a hillshade map. The default is None.
+    drape2 : array_like, masked_array
+        Node values to plot on top of drape1 and a hillshade map. The default is None.
+    cmap2 : str
+        Name of a colormap for drape 2. The default is None.
+    vertical_exa : float, optional
+        vertical exageration of hillshade map. The default is None.
+    azdeg : float, optional
+        azimuth of light source. The default is 315.
+    altdeg : float, optional
+        elevation of light source. The default is 65.
+    thres_drape1 : float, optional
+        threshold below which drape1 is made transparant. The default is None.
+    alpha : float (0-1), optional
+        transparency of DEM/Drape1 . The default is None.
+    thres_drape2 : float, optional
+        threshold below which drape2 is made transparant. The default is None.
+    alpha2 : float (0-1), optional
+        transparency of Drape2 . The default is None.
+    add_double_colorbar : bool, optional
+        add a double colorbar when two drapes are plotted. The default is False.
+    plt_contour : bool, optional
+        Add contour lines to elevation plot . The default is False.
+    contour_nb : int, optional
+        number of contour lines. The default is 50.
+    default_fontsize : float, optional
+        Default font size of plot labels. The default is 10.
+    cbar_height : percentage, optional
+        height of colorbar in percentage of figure. The default is "5%".
+    cbar_width : percentage, optional
+        width of colorbar in percentage of figure. The default is "30%".
+    cbar_or : str, optional
+        orientation of colorbar. The default is "horizontal".
+    cbar_loc : str, optional
+        location of colorbar. The default is "lower right".
+    bbox_to_anchor : vector, optional
+        bbox to anchor. The default is (0, 0, 1, 1).
+    cbar_ticks_position : str, optional
+        location of colorbar ticks (below or on top of the colorbar). The default is "top".
+    cbar_ticks_position2 : str, optional
+        location of colorbar ticks for colorbar of Drape2 (below or on top of the colorbar). The default is "bottom".
+    colorbar_label_y : float, optional
+        location of colorbar label with respect to the colorbar in y direction. The default is -40.
+    colorbar_label_x : float , optional
+        location of colorbar label with respect to the colorbar in x direction. The default is 0.5.
+    cbar_tick_size : float, optional
+        colorbar tick size. The default is 10.
+    cbar_label_color : str, optional
+        colorbar tick color. The default is 'black'.
+    cbar_label_fontweight : str, optional
+        colorbar font weight. The default is 'bold'.
+    add_label_bbox : bool, optional
+        Add a bbox surrounding the colorbar label. The default is False.
+
+    Returns
+    -------
+    ax : figure ax
+        return ax if output == True.
+
     """
     values_at = kwds.pop("values_at", "node")
     values_at = kwds.pop("at", values_at)
