19dae2263073fd20bc4100c64e7e31055cfbcc7b
Benjamin Campforts <beca4397@cu-biotedu-1-10.235.134.115.int.colorado.edu>
2021-11-11 12:10:16 -0700

    Changed name of space large scale eroder cython function to calc_sequential_ero_depo

--- landlab/components/space/space_large_scale_eroder.py
+++ landlab/components/space/space_large_scale_eroder.py
@@ -5,7 +5,7 @@ from landlab.grid.nodestatus import NodeStatus
 from landlab.utils.return_array import return_array_at_node
 
 from ..depression_finder.lake_mapper import _FLOODED
-from .ext.calc_qs_large_scale import calculate_qs_in
+from .ext.calc_sequential_ero_depo import _sequential_ero_depo
 
 ROOT2 = np.sqrt(2.0)  # syntactic sugar for precalculated square root of 2
 TIME_STEP_FACTOR = 0.5  # factor used in simple subdivision solver
@@ -371,7 +371,7 @@ class SpaceLargeScaleEroder(Component):
 
         self._qs_in[:] = 0
 
-        vol_SSY_riv = calculate_qs_in(
+        vol_SSY_riv = _sequential_ero_depo(
             stack_flip_ud_sel,
             r,
             area,
