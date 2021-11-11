8f4ef80582e84868aed561a32e7a38033bd75017
Benjamin Campforts <beca4397@cu-biotedu-1-10.235.134.115.int.colorado.edu>
2021-11-11 11:44:59 -0700

    Improved performance of cython code of priority_flood_flow_router

--- landlab/components/priority_flood_flow_router/cfuncs.pyx
+++ landlab/components/priority_flood_flow_router/cfuncs.pyx
@@ -10,17 +10,17 @@ DTYPE_FLOAT = np.double
 ctypedef np.double_t DTYPE_FLOAT_t
 
 
-cpdef D8_flowDir(np.ndarray[DTYPE_INT_t, ndim=1] receivers,
+cpdef _D8_flowDir(np.ndarray[DTYPE_INT_t, ndim=1] receivers,
              np.ndarray[DTYPE_FLOAT_t, ndim=1] distance_receiver,
              np.ndarray[DTYPE_FLOAT_t, ndim=1] steepest_slope,
-             np.ndarray[DTYPE_FLOAT_t, ndim=1] el,
+             np.ndarray[DTYPE_FLOAT_t, ndim=1] el_dep_free,
              np.ndarray[DTYPE_FLOAT_t, ndim=1] el_ori,
              np.ndarray[DTYPE_FLOAT_t, ndim=1] dist,
              np.ndarray[DTYPE_INT_t, ndim=1] ngb,
              np.ndarray[DTYPE_INT_t, ndim=1] activeCores,
              np.ndarray[DTYPE_INT_t, ndim=1] activeCells,
              np.ndarray[DTYPE_FLOAT_t, ndim=1] el_d,
-             DTYPE_INT_t r, DTYPE_INT_t c,
+             DTYPE_INT_t c,
              DTYPE_FLOAT_t dx,
              np.ndarray[DTYPE_INT_t, ndim=2] adj_link ,
              np.ndarray[DTYPE_INT_t, ndim=1] rec_link):
@@ -42,15 +42,16 @@ cpdef D8_flowDir(np.ndarray[DTYPE_INT_t, ndim=1] receivers,
         ngb[7] = i -c + 1
             
         # Differences after filling can be very small, *1e3 to exaggerate those
-        # Set to -1 at boundaries (active cell ==0)
-        el_d[0] = (el[i] - el[i + 1])*1e3*activeCells[i + 1]-1+activeCells[i + 1] 
-        el_d[1] = (el[i] - el[i + c])*1e3 *activeCells[i + c]-1+activeCells[i + c]
-        el_d[2] = (el[i] - el[i -1])*1e3  *activeCells[i -1]-1+activeCells[i -1]
-        el_d[3] = (el[i] - el[i - c])*1e3 *activeCells[i - c]-1+activeCells[i - c]
-        el_d[4] = (el[i] - el[i + c + 1])*1e3/(np.sqrt(2)) *activeCells[i + c + 1]-1+activeCells[i + c + 1]
-        el_d[5] = (el[i] - el[i + c - 1])*1e3/(np.sqrt(2)) *activeCells[i + c - 1]-1+activeCells[i + c - 1]
-        el_d[6] = (el[i] - el[i -c - 1])*1e3/(np.sqrt(2)) *activeCells[i -c - 1]-1+activeCells[i -c - 1]
-        el_d[7] = (el[i] - el[i -c + 1])*1e3/(np.sqrt(2)) *activeCells[i -c + 1]-1+activeCells[i -c + 1]
+        # Set to -1 at boundaries (active cell ==0)        
+        # If cells have equal slopes, the flow will be directed following Landlab rotational ordening going first to cardial, then to diagonal cells
+        el_d[0] = (el_dep_free[i] - el_dep_free[i + 1])*1e3*activeCells[i + 1]-1+activeCells[i + 1] 
+        el_d[1] = (el_dep_free[i] - el_dep_free[i + c])*1e3 *activeCells[i + c]-1+activeCells[i + c]
+        el_d[2] = (el_dep_free[i] - el_dep_free[i -1])*1e3  *activeCells[i -1]-1+activeCells[i -1]
+        el_d[3] = (el_dep_free[i] - el_dep_free[i - c])*1e3 *activeCells[i - c]-1+activeCells[i - c]
+        el_d[4] = (el_dep_free[i] - el_dep_free[i + c + 1])*1e3/(np.sqrt(2)) *activeCells[i + c + 1]-1+activeCells[i + c + 1]
+        el_d[5] = (el_dep_free[i] - el_dep_free[i + c - 1])*1e3/(np.sqrt(2)) *activeCells[i + c - 1]-1+activeCells[i + c - 1]
+        el_d[6] = (el_dep_free[i] - el_dep_free[i -c - 1])*1e3/(np.sqrt(2)) *activeCells[i -c - 1]-1+activeCells[i -c - 1]
+        el_d[7] = (el_dep_free[i] - el_dep_free[i -c + 1])*1e3/(np.sqrt(2)) *activeCells[i -c + 1]-1+activeCells[i -c + 1]
       
      
         # check to see if pixel is not a lake, if a lake, drain to itself
@@ -65,29 +66,19 @@ cpdef D8_flowDir(np.ndarray[DTYPE_INT_t, ndim=1] receivers,
             receivers[i] = i
             rec_link[i]= -1
             steepest_slope[i] = 0
-            
-            
-          
-    
 
-
-
-cpdef _FA_D8(DTYPE_INT_t np,
-             np.ndarray[DTYPE_FLOAT_t, ndim=1] a,
+cpdef _D8_FlowAcc(np.ndarray[DTYPE_FLOAT_t, ndim=1] a,
              np.ndarray[DTYPE_FLOAT_t, ndim=1] q,
-             np.ndarray[DTYPE_INT_t, ndim=1] stack,
+             np.ndarray[DTYPE_INT_t, ndim=1] stack_flip,
              np.ndarray[DTYPE_INT_t, ndim=1] receivers):
     """
     Accumulates drainage area and discharge, permitting transmission losses.
     """
     cdef int donor, recvr, i
 
-    # Iterate backward through the list, which means we work from upstream to
-    # downstream.
-    for i in range (np-1,-1,-1):
-            donor = stack[i]
-            if receivers[donor] != -1:
-                rcvr = receivers[donor]
-                a[rcvr] += a[donor]
-                q[rcvr] += q[donor]            
+    # Work from upstream to downstream.
+    for donor in stack_flip:
+        rcvr = receivers[donor]
+        a[rcvr] += a[donor]
+        q[rcvr] += q[donor]            
     
