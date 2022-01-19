a8535550d09bed00558a73a9894716d02dfed8e1
Greg Tucker <gtucker@colorado.edu>
2022-01-18 18:21:22 -0700

    add initial test file

---
+++ tests/components/overland_flow/test_linear_diffusion_overland_flow.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Unit tests for KinwaveImplicitOverlandFlowModel.
+
+Created on Sat Apr  1 10:49:33 2017
+
+@author: gtucker
+"""
+
+import numpy as np
+
+from landlab import RasterModelGrid
+from landlab.components import LinearDiffusionOverlandFlowRouter
+
+
+def test_steady_one_node():
+    """Run to steady state with a single node"""
+    pass
