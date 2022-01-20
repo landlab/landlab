a09e8ab28a0417bb403c210570aaf56565085ff7
Eric Hutton <mcflugen@users.noreply.github.com>
2022-01-20 14:09:20 -0700

    fix: install with richdem even if it is not available (#1379)
    * optionally require richdem
    
    * use a mocked version of richdem if it's not installed
    
    * skip priority_flood_flow_router tests if richdem isn't installed
    
    * install dependencies with pip
    
    * add WITH_RICHDEM attribute to PriorityFloodFlowRouter
    
    * use pip to install requirements
    
    * bmi-topography is not a requirement of landlab
    
    * add bmi-topography to notebooks requirements
    
    * exclude richdem notebooks from testing
    
    * drop Python 3.7 from notebook testing
    
    * programmatically add system check to requirements
    
    * use pip to install requirements
    
    * install rasterio from conda-forge
    
    * update classifiers
    
    * use setuptools environment markers for excluding notebooks
    
    * exclude priority flood notebooks if no richdem
    
    * use conda to install requirements for notebook testing
    
    * remove python 3.10 from notebook testing
    
    * update installation commands
    
    * don't install mamba for pip installations
    
    * add instructions about installing dev requirements
    
    [skip ci]

--- landlab/components/priority_flood_flow_router/priority_flood_flow_router.py
+++ landlab/components/priority_flood_flow_router/priority_flood_flow_router.py
@@ -17,7 +17,6 @@ from functools import partial
 
 import numpy as np
 import numpy.matlib as npm
-import richdem as rd
 
 from landlab import Component, FieldError, RasterModelGrid
 from landlab.grid.nodestatus import NodeStatus
@@ -25,6 +24,22 @@ from landlab.utils.return_array import return_array_at_node
 
 from ...utils.suppress_output import suppress_output
 from .cfuncs import _D8_FlowAcc, _D8_flowDir
+
+try:
+    import richdem as rd
+except ModuleNotFoundError:
+
+    class richdem:
+        def __getattribute__(self, name):
+            raise RuntimeError(
+                "PriorityFloodFlowRouter requires richdem but richdem is not installed"
+            )
+
+    rd = richdem()
+    WITH_RICHDEM = False
+else:
+    WITH_RICHDEM = True
+
 
 # Codes for depression status
 _UNFLOODED = 0
@@ -326,6 +341,8 @@ class PriorityFloodFlowRouter(Component):
             "doc": "Node array of proportion of flow sent to each receiver.",
         },
     }
+
+    WITH_RICHDEM = WITH_RICHDEM
 
     def __init__(
         self,
