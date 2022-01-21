fa2bf301dd2c32c0c8a6a8ecdeeaae18797dc01b
Greg Tucker <gtucker@colorado.edu>
2022-01-21 12:57:00 -0700

    add test

--- tests/components/overland_flow/test_linear_diffusion_overland_flow.py
+++ tests/components/overland_flow/test_linear_diffusion_overland_flow.py
@@ -8,7 +8,6 @@ Created on Sat Apr  1 10:49:33 2017
 @author: gtucker
 """
 
-import numpy as np
 from numpy.testing import assert_equal
 
 from landlab import RasterModelGrid
@@ -19,7 +18,7 @@ def test_steady_one_node():
     """Run to steady state with a single node"""
     grid = RasterModelGrid((3, 3), xy_spacing=2.0)
     grid.set_closed_boundaries_at_grid_edges(False, True, True, True)
-    elev = grid.add_zeros("topographic__elevation", at="node")
+    grid.add_zeros("topographic__elevation", at="node")
     olflow = LinearDiffusionOverlandFlowRouter(
         grid, rain_rate=72.0 / (3600.0 * 1000.0), roughness=0.01, velocity_scale=1.0
     )
