4320d2a0c506794811993de414047d12863eed37
mcflugen <mcflugen@gmail.com>
2022-01-27 14:26:53 -0700

    calculate expected value of water depth

--- tests/components/overland_flow/test_linear_diffusion_overland_flow.py
+++ tests/components/overland_flow/test_linear_diffusion_overland_flow.py
@@ -8,7 +8,7 @@ Created on Sat Apr  1 10:49:33 2017
 @author: gtucker
 """
 
-from numpy.testing import assert_equal
+from numpy.testing import assert_allclose
 
 from landlab import RasterModelGrid
 from landlab.components import LinearDiffusionOverlandFlowRouter
@@ -16,12 +16,20 @@ from landlab.components import LinearDiffusionOverlandFlowRouter
 
 def test_steady_one_node():
     """Run to steady state with a single node"""
-    grid = RasterModelGrid((3, 3), xy_spacing=2.0)
+    xy_spacing = 2.0
+    roughness = 0.01
+    velocity_scale = 1.0
+    rain_rate = 72.0 / (3600.0 * 1000.0)
+
+    grid = RasterModelGrid((3, 3), xy_spacing=xy_spacing)
     grid.set_closed_boundaries_at_grid_edges(False, True, True, True)
     grid.add_zeros("topographic__elevation", at="node")
     olflow = LinearDiffusionOverlandFlowRouter(
-        grid, rain_rate=72.0 / (3600.0 * 1000.0), roughness=0.01, velocity_scale=1.0
+        grid, rain_rate=rain_rate, roughness=roughness, velocity_scale=velocity_scale
     )
     for _ in range(18):
         olflow.run_one_step(20.0)
-    assert_equal(round(grid.at_node["surface_water__depth"][4], 4), 0.0037)
+    actual = grid.at_node["surface_water__depth"][4]
+    expected = (grid.dx * grid.dy * olflow.rain_rate / olflow.vel_coef) ** 0.3
+
+    assert_allclose(actual, expected, atol=1e-4)
