9e560191ceab3b448d904800eb281cc7891e545d
Greg Tucker <gtucker@colorado.edu>
2022-02-10 17:31:08 -0700

    add doctest examples

--- landlab/components/carbonate/carbonate_producer.py
+++ landlab/components/carbonate/carbonate_producer.py
@@ -42,7 +42,52 @@ def smooth_heaviside(x, width=0.5, out=None):
 
 
 class CarbonateProducer(Component):
-    """Calculate marine carbonate production and deposition."""
+    """Calculate marine carbonate production and deposition.
+
+    Uses the growth-rate of Bosscher and Schlager (1992), which represents the
+    vertical growth rate :math:`G` as:
+
+    ..math::
+
+        G = G_m \tanh (I_0 e^{-kz} / I_k)
+
+    where :math:`G_m` is the maximum growth rate, :math:`I_0` is the surface
+    light intensity, :math:`I_k` is the saturating light intensity, :math:`z` is
+    water depth, and :math:`k` is the light extinction coefficient.
+
+    Bosscher and Schlager (1992) suggest plausible values or ranges for these
+    parameters as follows: :math:`G_m` = 10 to 15 mm/y, :math:`I_0` = 2,000 to
+    2,250 micro Einsteins per square meter per second in the tropics, :math:`I_k`
+    = 50 to 450 micro Einsteins per square meter per second, and :math:`k` =
+    0.04 to 0.16 m:math:`^{-1}` (corresponding to a decay depth of 6.25 to 16 m).
+    The default values used in this component are based on these estimates.
+
+    Examples
+    --------
+    >>> from landlab import RasterModelGrid
+    >>> from landlab.components import CarbonateProducer
+    >>> grid = RasterModelGrid((3, 3))
+    >>> elev = grid.add_zeros('topographic__elevation', at='node')
+    >>> sealevel = grid.add_field('sea_level__elevation', 0.0, at='grid')
+    >>> elev[:] = -1.0
+    >>> cp = CarbonateProducer(grid)
+    >>> np.round(cp.calc_carbonate_production_rate()[4], 2)
+    0.01
+    >>> elev[:] = -40.0
+    >>> np.round(cp.calc_carbonate_production_rate()[4], 5)
+    0.00091
+    >>> cp.sea_level = -39.0
+    >>> np.round(cp.calc_carbonate_production_rate()[4], 2)
+    0.01
+    >>> thickness = cp.produce_carbonate(10.0)
+    >>> np.round(10 * grid.at_node['carbonate_thickness'][4])
+    1.0
+    >>> thickness is grid.at_node['carbonate_thickness']
+    True
+    >>> cp.run_one_step(10.0)
+    >>> np.round(10 * thickness[4])
+    2.0
+    """
 
     _name = "CarbonateProducer"
 
@@ -213,10 +258,10 @@ class CarbonateProducer(Component):
         self._depth.clip(min=-2.0 * self._tidal_range, out=self._depth)
         self._carb_prod_rate[:] = self._max_carbonate_production_rate * np.tanh(
             self.surface_light
-            * np.exp(-self.extinction_coefficient * water_depth[under_water])
+            * np.exp(-self.extinction_coefficient * self._depth)
             / self.saturating_light
         )
-        self._carb_prod_rate *= smooth_heaviside(water_depth, width=self.tidal_range)
+        self._carb_prod_rate *= smooth_heaviside(self._depth, width=self.tidal_range)
         return self._carb_prod_rate
 
     def produce_carbonate(self, dt):
@@ -233,7 +278,7 @@ class CarbonateProducer(Component):
         if self._carbonate_thickness is None:
             self._carbonate_thickness = self._create_carbonate_thickness_field()
 
-        calc_carbonate_production_rate()
+        self.calc_carbonate_production_rate()
         self._carbonate_thickness += self._carb_prod_rate * dt
 
         return self._carbonate_thickness
@@ -248,4 +293,4 @@ class CarbonateProducer(Component):
         dt : float
             Time step duration (normally in years)
         """
-        produce_carbonate(dt)
+        self.produce_carbonate(dt)
