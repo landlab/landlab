7d36c5f67868a97137ea4c09d32dd27be77f87e1
mcflugen <mcflugen@gmail.com>
2022-03-11 10:26:49 -0700

    add a few more unit tests

--- tests/plot/test_layers.py
+++ tests/plot/test_layers.py
@@ -1,4 +1,4 @@
-from landlab.plot.layers import _search_shorelines
+from landlab.plot.layers import _interp_shorelines, _search_shorelines
 from hypothesis.strategies import integers, floats
 from hypothesis import given
 from numpy.testing import assert_array_equal
@@ -52,3 +52,30 @@ def test_search_shorelines_all_zeros():
 )
 def test_search_shorelines(y, shoreline):
     assert_array_equal(_search_shorelines(y), shoreline)
+
+
+def test_interp_shorelines():
+    assert_array_equal(_interp_shorelines([0, 1, 2], [1, -1, -1], [0]), [0.5])
+    assert_array_equal(
+        _interp_shorelines([0, 1, 2, 3], [1, -1, -1, 4], [0, 2]), [0.5, 2.2]
+    )
+
+
+def test_interp_shorelines_on_end_point():
+    assert_array_equal(_interp_shorelines([1.0, 3.0, 5.0], [1, 0, -1], [1]), [3.0])
+    assert_array_equal(_interp_shorelines([1.0, 3.0, 5.0], [1, 0, -1], [0]), [3.0])
+
+
+@pytest.mark.parametrize("shoreline", [0, 1])
+def test_interp_shorelines_bounds_error(shoreline):
+    with pytest.raises(ValueError):
+        _interp_shorelines([1.0, 3.0, 5.0], [1, 2, 3], [shoreline])
+
+
+def test_interp_shorelines_last_element():
+    with pytest.raises(ValueError):
+        _interp_shorelines([1.0, 3.0, 5.0], [1, 1, 0], [2])
+
+
+def test_interp_shoreline_all_shoreline():
+    assert_array_equal(_interp_shorelines([1.0, 3.0, 5.0], [0, 0, 0], [1]), [5.0])
