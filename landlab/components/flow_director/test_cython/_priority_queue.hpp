ec67a21559e649af371cab2e25faf6ed98538f37
sebastien-lenard <sebastien.lenard@gmail.com>
2022-08-10 20:44:00 +0000

    erase of the cython test files

--- landlab/components/flow_director/test_cython/_priority_queue.hpp
+++
@@ -1,9 +0,0 @@
-// priority_queue
-#include <queue>
-// pair
-#include <utility>
-// function
-#include <functional>
-using namespace std;
-
-using _priority_queue = std::priority_queue<std::pair<long,double>,std::vector<std::pair<long,double>>,std::function<bool(std::pair<long,double>,std::pair<long,double>)>>;
