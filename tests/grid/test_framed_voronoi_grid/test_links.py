78e6aa8f66ebb7ff7739731fb79ac2c6ff848a09
sebastien-lenard <sebastien.lenard@gmail.com>
2022-09-07 16:36:18 +0000

    simplification of unit tests

--- tests/grid/test_framed_voronoi_grid/test_links.py
+++ tests/grid/test_framed_voronoi_grid/test_links.py
@@ -15,26 +15,9 @@ def test_rect_patches_at_link():
 
 def test_rect_link_angle():
     grid = FramedVoronoiGrid((3, 3), node_layout="rect", random_seed=False)
-    assert grid.angle_of_link / np.pi == approx(
+    assert grid.angle_of_link[0:2] / np.pi == approx(
         np.array(
-            [
-                0.0,
-                0.0,
-                0.5,
-                1.79537105,
-                0.47742325,
-                0.73267387,
-                0.5,
-                0.08243367,
-                0.07343099,
-                0.5,
-                1.76664719,
-                0.52412986,
-                1.79537105,
-                0.5,
-                0.0,
-                0.0,
-            ]
+            [0.0, 0.0, 0.5]
         ),
         rel=1.0e-5,
     )
