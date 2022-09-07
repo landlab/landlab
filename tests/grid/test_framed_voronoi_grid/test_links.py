4ca69507fbce736ee5e1386a6c66c327a1bfebb8
sebastien-lenard <sebastien.lenard@gmail.com>
2022-09-07 16:58:20 +0000

    min. corr test_links

--- tests/grid/test_framed_voronoi_grid/test_links.py
+++ tests/grid/test_framed_voronoi_grid/test_links.py
@@ -15,7 +15,7 @@ def test_rect_patches_at_link():
 
 def test_rect_link_angle():
     grid = FramedVoronoiGrid((3, 3), node_layout="rect", random_seed=False)
-    assert grid.angle_of_link[0:2] / np.pi == approx(
+    assert grid.angle_of_link[0:3] / np.pi == approx(
         np.array([0.0, 0.0, 0.5]),
         rel=1.0e-5,
     )
