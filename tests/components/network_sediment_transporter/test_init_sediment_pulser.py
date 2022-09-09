b6cf5a523d7690d11f7471650ec15984709eba1a
keckje <keckje@gmail.com>
2022-09-08 21:40:28 -0700

    lint

--- tests/components/network_sediment_transporter/test_init_sediment_pulser.py
+++ tests/components/network_sediment_transporter/test_init_sediment_pulser.py
@@ -5,8 +5,9 @@ from landlab.components import SedimentPulserAtLinks, SedimentPulserEachParcel
 from landlab import RasterModelGrid
 
 
-variable_list = ['_grid', '_parcels', '_D50', '_D84_D50', '_rho_sediment', 
-            '_parcel_volume', '_abrasion_rate']
+variable_list = ['_grid', '_parcels', '_D50', '_D84_D50', '_rho_sediment',
+                 '_parcel_volume', '_abrasion_rate']
+
 
 def test_basic_init_each_parcel(example_nmg, example_parcels):
     """test all class variables are present after SedimentPulserEachParcel initializes"""
@@ -20,6 +21,7 @@ def test_basic_init_at_link(example_nmg, example_parcels):
     pulser = SedimentPulserAtLinks(example_nmg, parcels=example_parcels)
     TF = [True if val in list(vars(pulser).keys()) else False for val in variable_list]
     assert all(TF)
+
 
 def test_grid_is_nmg():
     """test ValueError exception is raised when nmg is a mg"""
