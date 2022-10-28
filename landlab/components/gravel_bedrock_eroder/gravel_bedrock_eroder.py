ac065c2a4dfad1fbbf7c37f81a0dcd05eecc1dc8
Greg Tucker <gtucker@colorado.edu>
2022-10-28 16:26:19 -0600

    basic draft without bedrock erosion

--- landlab/components/gravel_bedrock_eroder/gravel_bedrock_eroder.py
+++ landlab/components/gravel_bedrock_eroder/gravel_bedrock_eroder.py
@@ -1,41 +1,21 @@
 import numpy as np
-from scipy.sparse.linalg import spsolve
-
 from landlab import Component, HexModelGrid
 from landlab.grid.diagonals import DiagonalsMixIn
 
 
-def make_empty_matrix_and_rhs(grid):
-    from scipy.sparse import csc_matrix
+class GravelBedrockEroder(Component):
+    """Model drainage network evolution for a network of rivers that have
+    a layer of gravel alluvium overlying bedrock.
 
-    mat = csc_matrix((grid.number_of_core_nodes, grid.number_of_core_nodes))
-    rhs = np.zeros(grid.number_of_core_nodes)
-    return mat, rhs
-
-
-def zero_out_matrix(grid, mat, rcvr, mat_id):
-    for i in grid.core_nodes:
-        j = mat_id[i]
-        mat[j, j] = 0.0
-        r = rcvr[i]
-        if grid.status_at_node[r] == grid.BC_NODE_IS_CORE:
-            k = mat_id[r]
-            mat[j, k] = 0.0
-            mat[k, k] = 0.0
-            mat[k, j] = 0.0
-
-
-class GravelRiverTransporter(Component):
-    """Model drainage network evolution for a network of transport-limited
-    gravel-bed rivers with downstream abrasion.
-
-    GravelRiverTransporter is designed to operate together with a flow-routing
-    component such as PriorityFloodFlowRouter, so that each grid node has
+    GravelBedrockEroder is designed to operate together with a flow-routing
+    component such as FlowAccumulator, so that each grid node has
     a defined flow direction toward one of its neighbor nodes. Each core node
     is assumed to contain one outgoing fluvial channel, and (depending on
     the drainage structure) zero, one, or more incoming channels. These channels are
     treated as effectively sub-grid-scale features that are embedded in valleys
-    that have a width of one grid cell. The rate of gravel transport out of
+    that have a width of one grid cell.
+
+    As with the GravelRiverTransporter component, the rate of gravel transport out of
     a given node is calculated as the product of bankfull discharge, channel
     gradient (to the 7/6 power), a dimensionless transport coefficient, and
     an intermittency factor that represents the fraction of time that bankfull
@@ -51,10 +31,20 @@ class GravelRiverTransporter(Component):
     distance (for example, if a grid node is carrying a gravel load Qs to a
     neighboring node dx meters downstream, the rate of gravel loss in volume per
     time per area at the node will be beta Qs dx, where beta is the abrasion
-    coefficient). Sediment mass conservation is calculated across each entire
+    coefficient).
+
+    Sediment mass conservation is calculated across each entire
     grid cell. For example, if a cell has surface area A, a total volume influx
     Qin, and downstream transport rate Qs, the resulting rate of change of
-    elevation will be (Qin - Qs / (A (1 - phi)), where phi is porosity.
+    alluvium thickness will be (Qin - Qs / (A (1 - phi)), plus gravel produced by
+    plucking erosion of bedrock (phi is porosity).
+
+    Bedrock is eroded by a combination of abrasion and plucking. Abrasion per unit
+    channel length is calculated as the product of volumetric sediment discharge
+    and an abrasion coefficient. Sediment produced by abrasion is assumed to
+    go into wash load that is removed from the model domain. Plucking is calculated
+    using a discharge-slope expression, and a user-defined fraction of plucked
+    material is added to the coarse alluvium.
 
     Parameters
     ----------
@@ -68,8 +58,8 @@ class GravelRiverTransporter(Component):
         Abrasion coefficient with units of inverse length
     sediment_porosity : float (default 0.35)
         Bulk porosity of bed sediment
-    solver : string (default "explicit")
-        Solver type (currently only "explicit" is tested and operational)
+    depth_decay_scale : float (default 1.0)
+        Scale for depth decay in bedrock exposure function
 
     Examples
     --------
@@ -77,22 +67,24 @@ class GravelRiverTransporter(Component):
     >>> from landlab.components import FlowAccumulator
     >>> grid = RasterModelGrid((3, 3), xy_spacing=1000.0)
     >>> elev = grid.add_zeros("topographic__elevation", at="node")
+    >>> sed = grid.add_zeros("soil__depth", at="node")
+    >>> sed[4] = 300.0
     >>> grid.status_at_node[grid.perimeter_nodes] = grid.BC_NODE_IS_CLOSED
     >>> grid.status_at_node[5] = grid.BC_NODE_IS_FIXED_VALUE
     >>> fa = FlowAccumulator(grid, runoff_rate=10.0)
     >>> fa.run_one_step()
-    >>> transporter = GravelRiverTransporter(grid, abrasion_coefficient=0.0005)
+    >>> eroder = GravelBedrockEroder(grid, abrasion_coefficient=0.0005)
     >>> for _ in range(200):
     ...     fa.run_one_step()
     ...     elev[grid.core_nodes] += 1.0
-    ...     transporter.run_one_step(10000.0)
+    ...     eroder.run_one_step(10000.0)
     >>> int(elev[4] * 100)
     2366
     """
 
     _ONE_SIXTH = 1.0 / 6.0
 
-    _name = "GravelRiverTransporter"
+    _name = "GravelBedrockEroder"
 
     _unit_agnostic = True
 
@@ -120,6 +112,14 @@ class GravelRiverTransporter(Component):
             "units": "m**2/y",
             "mapping": "node",
             "doc": "Volumetric outgoing streamwise bedload sediment transport rate",
+        },
+        "bedrock__elevation": {
+            "dtype": float,
+            "intent": "out",
+            "optional": False,
+            "units": "m",
+            "mapping": "node",
+            "doc": "elevation of the bedrock surface",
         },
         "flow__link_to_receiver_node": {
             "dtype": int,
@@ -152,6 +152,14 @@ class GravelRiverTransporter(Component):
             "units": "m/y",
             "mapping": "node",
             "doc": "Time rate of change of sediment thickness",
+        },
+        "soil__depth": {
+            "dtype": float,
+            "intent": "in",
+            "optional": False,
+            "units": "m",
+            "mapping": "node",
+            "doc": "Depth of soil or weathered bedrock",
         },
         "surface_water__discharge": {
             "dtype": float,
@@ -186,7 +194,7 @@ class GravelRiverTransporter(Component):
         transport_coefficient=0.041,
         abrasion_coefficient=0.0,
         sediment_porosity=0.35,
-        solver="explicit",
+        depth_decay_scale=1.0,
     ):
         """Initialize GravelRiverTransporter."""
 
@@ -197,9 +205,18 @@ class GravelRiverTransporter(Component):
         self._intermittency_factor = intermittency_factor
         self._abrasion_coef = abrasion_coefficient
         self._porosity_factor = 1.0 / (1.0 - sediment_porosity)
+        self._depth_decay_scale = depth_decay_scale
 
         # Fields and arrays
         self._elev = grid.at_node["topographic__elevation"]
+        self._sed = grid.at_node["soil__depth"]
+        if "bedrock__elevation" in grid.at_node:
+            self._bedrock__elevation = grid.at_node["bedrock__elevation"]
+        else:
+            self._bedrock__elevation = grid.add_zeros(
+                "bedrock__elevation", at="node", dtype=float
+            )
+            self._bedrock__elevation[:] = self._elev - self._sed
         self._discharge = grid.at_node["surface_water__discharge"]
         self._slope = grid.at_node["topographic__steepest_slope"]
         self._receiver_node = grid.at_node["flow__receiver_node"]
@@ -212,22 +229,6 @@ class GravelRiverTransporter(Component):
 
         # Constants
         self._SEVEN_SIXTHS = 7.0 / 6.0
-
-        # Solver type
-        if solver == "explicit":
-            self.run_one_step = self.run_one_step_simple_explicit
-        elif solver == "matrix":
-            import warnings
-
-            from landlab.utils.matrix import get_core_node_at_node
-
-            warnings.warn("Matrix-based solver is experimental & not fully tested")
-            self.run_one_step = self.run_one_step_matrix_inversion
-            self._mat, self._rhs = make_empty_matrix_and_rhs(grid)
-            self._mat_id = np.zeros(grid.number_of_nodes, dtype=int)
-            self._mat_id = get_core_node_at_node(grid)
-        else:
-            raise ValueError("Solver type not recognized")
 
         self._setup_length_of_flow_link()
 
@@ -282,18 +283,20 @@ class GravelRiverTransporter(Component):
         >>> grid = RasterModelGrid((3, 3), xy_spacing=1000.0)
         >>> elev = grid.add_zeros("topographic__elevation", at="node")
         >>> elev[3:] = 10.0
+        >>> sed = grid.add_zeros("soil__depth", at="node")
+        >>> sed[3:] = 100.0
         >>> fa = FlowAccumulator(grid)
         >>> fa.run_one_step()
-        >>> transporter = GravelRiverTransporter(grid)
-        >>> depth = transporter.calc_implied_depth()
-        >>> int(depth[4] * 1000)
+        >>> eroder = GravelBedrockEroder(grid)
+        >>> water_depth = eroder.calc_implied_depth()
+        >>> int(water_depth[4] * 1000)
         98
         """
-        DEPTH_FACTOR = 0.09801
+        depth_factor = 0.09801
         depth = np.zeros(self._grid.number_of_nodes)
         nonzero_slope = self._slope > 0.0
         depth[nonzero_slope] = (
-            DEPTH_FACTOR * grain_diameter / self._slope[nonzero_slope]
+            depth_factor * grain_diameter / self._slope[nonzero_slope]
         )
         return depth
 
@@ -323,22 +326,23 @@ class GravelRiverTransporter(Component):
         >>> grid = RasterModelGrid((3, 3), xy_spacing=10000.0)
         >>> elev = grid.add_zeros("topographic__elevation", at="node")
         >>> elev[3:] = 100.0
+        >>> sed = grid.add_zeros("soil__depth", at="node")
+        >>> sed[3:] = 100.0
         >>> fa = FlowAccumulator(grid)
         >>> fa.run_one_step()
-        >>> transporter = GravelRiverTransporter(grid)
-        >>> width = transporter.calc_implied_width()
-        >>> int(width[4] * 100)
+        >>> eroder = GravelBedrockEroder(grid)
+        >>> chan_width = eroder.calc_implied_width()
+        >>> int(chan_width[4] * 100)
         3833
         >>> grid.at_node["surface_water__discharge"] *= 1. / (3600 * 24 * 365.25)
-        >>> width = transporter.calc_implied_width(time_unit="s")
-        >>> int(width[4] * 100)
+        >>> chan_width = eroder.calc_implied_width(time_unit="s")
+        >>> int(chan_width[4] * 100)
         3838
         """
         if time_unit[0] == "y":
             width_fac = 8.26e-8
         else:
             width_fac = 2.61  # assume seconds if not years
-        width = np.zeros(self._grid.number_of_nodes)
         width = (
             width_fac
             * self._discharge
@@ -351,7 +355,10 @@ class GravelRiverTransporter(Component):
         """Calculate and return bed-load transport capacity.
 
         Calculation uses Wickert-Schildgen approach, and provides
-        volume per time rate.
+        volume per time rate. Transport rate is modulated by available
+        sediment, using the exponential function (1 - exp(-H / Hs)),
+        so that transport rate approaches zero as sediment thickness
+        approaches zero.
 
         Examples
         --------
@@ -360,11 +367,13 @@ class GravelRiverTransporter(Component):
         >>> grid = RasterModelGrid((3, 3), xy_spacing=100.0)
         >>> elev = grid.add_zeros("topographic__elevation", at="node")
         >>> elev[3:] = 1.0
+        >>> sed = grid.add_zeros("soil__depth", at="node")
+        >>> sed[3:] = 100.0
         >>> fa = FlowAccumulator(grid)
         >>> fa.run_one_step()
-        >>> transporter = GravelRiverTransporter(grid)
-        >>> transporter.calc_transport_capacity()
-        >>> round(transporter._sediment_outflux[4], 4)
+        >>> eroder = GravelBedrockEroder(grid)
+        >>> eroder.calc_transport_capacity()
+        >>> round(eroder._sediment_outflux[4], 4)
         0.019
         """
         self._sediment_outflux[:] = (
@@ -372,6 +381,7 @@ class GravelRiverTransporter(Component):
             * self._intermittency_factor
             * self._discharge
             * self._slope**self._SEVEN_SIXTHS
+            * (1.0 - np.exp(-self._sed / self._depth_decay_scale))
         )
 
     def calc_abrasion_rate(self):
@@ -392,12 +402,14 @@ class GravelRiverTransporter(Component):
         >>> grid = RasterModelGrid((3, 3), xy_spacing=1000.0)
         >>> elev = grid.add_zeros("topographic__elevation", at="node")
         >>> elev[3:] = 10.0
+        >>> sed = grid.add_zeros("soil__depth", at="node")
+        >>> sed[3:] = 100.0
         >>> fa = FlowAccumulator(grid)
         >>> fa.run_one_step()
-        >>> transporter = GravelRiverTransporter(grid, abrasion_coefficient=0.0002)
-        >>> transporter.calc_transport_capacity()
-        >>> transporter.calc_abrasion_rate()
-        >>> int(transporter._abrasion[4] * 1e8)
+        >>> eroder = GravelBedrockEroder(grid, abrasion_coefficient=0.0002)
+        >>> eroder.calc_transport_capacity()
+        >>> eroder.calc_abrasion_rate()
+        >>> int(eroder._abrasion[4] * 1e8)
         19
         """
         cores = self._grid.core_nodes
@@ -421,17 +433,19 @@ class GravelRiverTransporter(Component):
         >>> grid = RasterModelGrid((3, 4), xy_spacing=100.0)
         >>> elev = grid.add_zeros("topographic__elevation", at="node")
         >>> elev[:] = 0.01 * grid.x_of_node
+        >>> sed = grid.add_zeros("soil__depth", at="node")
+        >>> sed[:] = 100.0
         >>> grid.status_at_node[grid.perimeter_nodes] = grid.BC_NODE_IS_CLOSED
         >>> grid.status_at_node[4] = grid.BC_NODE_IS_FIXED_VALUE
         >>> fa = FlowAccumulator(grid)
         >>> fa.run_one_step()
-        >>> transporter = GravelRiverTransporter(grid)
-        >>> transporter.calc_sediment_rate_of_change()
-        >>> np.round(transporter._sediment_outflux[4:7], 3)
+        >>> eroder = GravelBedrockEroder(grid)
+        >>> eroder.calc_sediment_rate_of_change()
+        >>> np.round(eroder._sediment_outflux[4:7], 3)
         array([ 0.   ,  0.038,  0.019])
-        >>> np.round(transporter._sediment_influx[4:7], 3)
+        >>> np.round(eroder._sediment_influx[4:7], 3)
         array([ 0.038,  0.019,  0.   ])
-        >>> np.round(transporter._dzdt[5:7], 8)
+        >>> np.round(eroder._dzdt[5:7], 8)
         array([ -2.93000000e-06,  -2.93000000e-06])
         """
         self.calc_transport_capacity()
@@ -448,7 +462,7 @@ class GravelRiverTransporter(Component):
             - self._abrasion[cores]
         )
 
-    def run_one_step_simple_explicit(self, dt):
+    def run_one_step(self, dt):
         """Advance solution by time interval dt.
 
         Examples
@@ -459,89 +473,16 @@ class GravelRiverTransporter(Component):
         >>> grid = RasterModelGrid((3, 4), xy_spacing=100.0)
         >>> elev = grid.add_zeros("topographic__elevation", at="node")
         >>> elev[:] = 0.01 * grid.x_of_node
+        >>> sed = grid.add_zeros("soil__depth", at="node")
+        >>> sed[:] = 1000.0
         >>> grid.status_at_node[grid.perimeter_nodes] = grid.BC_NODE_IS_CLOSED
         >>> grid.status_at_node[4] = grid.BC_NODE_IS_FIXED_VALUE
         >>> fa = FlowAccumulator(grid)
         >>> fa.run_one_step()
-        >>> transporter = GravelRiverTransporter(grid, solver="explicit")
-        >>> transporter.run_one_step(1000.0)
+        >>> eroder = GravelBedrockEroder(grid)
+        >>> eroder.run_one_step(1000.0)
         >>> np.round(elev[4:7], 4)
         array([ 0.    ,  0.9971,  1.9971])
         """
         self.calc_sediment_rate_of_change()
         self._elev += self._dzdt * dt
-
-    def _fill_matrix_and_rhs(self, dt):
-        """Fill out entries in a sparse matrix and corresponding right-hand side
-        vector.
-
-        Examples
-        --------
-        >>> from landlab import RasterModelGrid
-        >>> from landlab.components import FlowAccumulator
-        >>> grid = RasterModelGrid((3, 4), xy_spacing=100.0)
-        >>> elev = grid.add_zeros("topographic__elevation", at="node")
-        >>> elev[:] = 0.01 * grid.x_of_node
-        >>> grid.status_at_node[grid.perimeter_nodes] = grid.BC_NODE_IS_CLOSED
-        >>> grid.status_at_node[4] = grid.BC_NODE_IS_FIXED_VALUE
-        >>> fa = FlowAccumulator(grid)
-        >>> transporter = GravelRiverTransporter(grid, solver="matrix")
-        >>> transporter._mat.toarray()
-        array([[ 0.,  0.],
-               [ 0.,  0.]])
-        >>> fa.run_one_step()
-        >>> transporter._receiver_node[5:7]
-        array([4, 5])
-        >>> transporter._fill_matrix_and_rhs(1000.0)
-        >>> transporter._rhs
-        array([ 1.,  2.])
-        """
-        prefac = (
-            self._trans_coef * self._intermittency_factor * self._porosity_factor * dt
-        ) / self.grid.dx**2
-        a = prefac * (1.0 / self.grid.dx + self._abrasion_coef / 2)
-        b = prefac * (1.0 / self.grid.dx - self._abrasion_coef / 2)
-        f = self._discharge * (self._slope ** (self._ONE_SIXTH))
-
-        zero_out_matrix(self.grid, self._mat, self._receiver_node, self._mat_id)
-        for i in self.grid.core_nodes:
-            j = self._mat_id[i]
-            self._rhs[j] = self._elev[i]
-            self._mat[j, j] += 1 + a * f[i]
-            r = self._receiver_node[i]
-            if self.grid.status_at_node[r] == self.grid.BC_NODE_IS_CORE:
-                k = self._mat_id[r]
-                self._mat[j, k] -= a * f[i]
-                self._mat[k, k] += b * f[i]
-                self._mat[k, j] -= b * f[i]
-            else:
-                self._rhs[j] += a * f[i] * self._elev[r]
-
-    def run_one_step_matrix_inversion(self, dt):
-        """Advance solution by time interval dt.
-
-        WARNING: EXPERIMENTAL AND NOT FULLY TESTED - USE AT OWN RISK!
-
-        Notes
-        -----
-        Does not update abrasion rate or sediment outflux fields.
-
-        Examples
-        --------
-        >>> import numpy as np
-        >>> from landlab import RasterModelGrid
-        >>> from landlab.components import FlowAccumulator
-        >>> grid = RasterModelGrid((3, 4), xy_spacing=100.0)
-        >>> elev = grid.add_zeros("topographic__elevation", at="node")
-        >>> elev[:] = 0.01 * grid.x_of_node
-        >>> grid.status_at_node[grid.perimeter_nodes] = grid.BC_NODE_IS_CLOSED
-        >>> grid.status_at_node[4] = grid.BC_NODE_IS_FIXED_VALUE
-        >>> fa = FlowAccumulator(grid)
-        >>> fa.run_one_step()
-        >>> transporter = GravelRiverTransporter(grid, solver="matrix")
-        >>> transporter.run_one_step == transporter.run_one_step_matrix_inversion
-        True
-        >>> transporter.run_one_step(1000.0)
-        """
-        self._fill_matrix_and_rhs(dt)
-        self._elev[self.grid.core_nodes] = spsolve(self._mat, self._rhs)
