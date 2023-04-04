1a5ec1ffae6c886b91f8b29eb4fa39864c218644
mcflugen <mcflugen@gmail.com>
2023-04-03 21:24:26 -0600

    add more tests for orientation_of_link

--- tests/grid/test_hex_grid/test_links.py
+++ tests/grid/test_hex_grid/test_links.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pytest
 from numpy.testing import assert_array_equal
 from pytest import approx
 
@@ -34,7 +35,38 @@ def test_link_angle():
     )
 
 
-def test_link_orientation():
+ORIENTATION_ANGLE = {
+    LinkOrientation.E: 0.0,
+    LinkOrientation.ENE: 30.0,
+    LinkOrientation.NNE: 60.0,
+    LinkOrientation.N: 90.0,
+    LinkOrientation.NNW: 120.0,
+    LinkOrientation.ESE: 330.0,
+}
+
+
+@pytest.mark.parametrize("orientation", ("horizontal", "vertical"))
+@pytest.mark.parametrize("node_layout", ("hex", "rect"))
+@pytest.mark.parametrize("link_orientation", list(ORIENTATION_ANGLE))
+def test_link_orientation(orientation, node_layout, link_orientation):
+    grid = HexModelGrid((5, 5), orientation=orientation, node_layout=node_layout)
+
+    links = grid.orientation_of_link == link_orientation
+    assert np.all(
+        np.isclose(
+            grid.angle_of_link[links],
+            ORIENTATION_ANGLE[link_orientation] * np.pi / 180.0,
+        )
+    )
+
+
+def test_link_orientation_is_cached():
+    """Check successive calls returns cached array."""
+    grid = HexModelGrid((3, 3))
+    assert grid.orientation_of_link is grid.orientation_of_link
+
+
+def test_link_orientation_default_hex():
     grid = HexModelGrid((3, 3))
     assert_array_equal(
         grid.orientation_of_link,
