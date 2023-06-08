38256cf7eb3db82cb74f2b9569f9102fc447692e
Greg Tucker <gtucker@colorado.edu>
2023-06-08 16:02:11 -0600

    remove old ver from init

--- landlab/components/tectonics/__init__.py
+++ landlab/components/tectonics/__init__.py
@@ -6,6 +6,5 @@
 """
 
 from .listric_kinematic_extender import ListricKinematicExtender
-from .listric_kinematic_stepwise_extender import ListricKinematicStepwiseExtender
 
-__all__ = ["ListricKinematicExtender", "ListricKinematicStepwiseExtender"]
+__all__ = ["ListricKinematicExtender"]
