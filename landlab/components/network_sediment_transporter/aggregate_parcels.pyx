732f08f481dca464fc129c026bd776247bb9877e
mcflugen <mcflugen@gmail.com>
2023-06-23 09:56:14 -0600

    add aggregator that counts parcels; more aggregator tests

--- landlab/components/network_sediment_transporter/aggregate_parcels.pyx
+++ landlab/components/network_sediment_transporter/aggregate_parcels.pyx
@@ -5,19 +5,43 @@ cimport numpy as cnp
 from libc.stdlib cimport free, malloc
 
 
+ctypedef fused float_or_int:
+    cython.integral
+    cython.floating
+
+
 @cython.boundscheck(False)
 @cython.wraparound(False)
-def aggregate_parcels_at_link_sum(
-    double [:] out,
+def aggregate_parcels_at_link_count(
+    cython.integral [:] out,
     const long number_of_links,
-    const double [:] value_of_parcel,
-    const long [:] link_of_parcel,
+    const cython.integral [:] link_of_parcel,
     const long number_of_parcels,
 ):
     cdef int parcel, link
 
     for link in range(number_of_links):
-        out[link] = 0.0
+        out[link] = 0
+
+    for parcel in range(number_of_parcels):
+        link = link_of_parcel[parcel]
+        if link >= 0:
+            out[link] = out[link] + 1
+
+
+@cython.boundscheck(False)
+@cython.wraparound(False)
+def aggregate_parcels_at_link_sum(
+    cython.floating [:] out,
+    const long number_of_links,
+    const cython.integral [:] link_of_parcel,
+    const long number_of_parcels,
+    const float_or_int [:] value_of_parcel,
+):
+    cdef int parcel, link
+
+    for link in range(number_of_links):
+        out[link] = 0
 
     for parcel in range(number_of_parcels):
         link = link_of_parcel[parcel]
@@ -28,12 +52,12 @@ def aggregate_parcels_at_link_sum(
 @cython.boundscheck(False)
 @cython.wraparound(False)
 def aggregate_parcels_at_link_mean(
-    double [:] out,
+    cython.floating [:] out,
     const long number_of_links,
-    const double [:] value_of_parcel,
-    const double [:] weight_of_parcel,
-    const long [:] link_of_parcel,
+    const cython.integral [:] link_of_parcel,
     const long number_of_parcels,
+    const float_or_int [:] value_of_parcel,
+    const cython.floating [:] weight_of_parcel,
 ):
     cdef int parcel, link
     cdef double * total_weight_at_link = <double *>malloc(number_of_links * sizeof(double))
