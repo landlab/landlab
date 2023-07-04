0bd9fcec075cd1c2af6ce8c2a0d16f459e98ba64
angelmons <angelm@uidaho.edu>
2023-07-04 15:21:49 -0600

    add River Bed Dynamics
    Added the River Bed Component

---
+++ tests/components/river_bed_dynamics/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from landlab import RasterModelGrid
+from landlab.components import RiverBedDynamics
+
+@pytest.fixture
+def r_b_d():
+    grid = RasterModelGrid((32, 240), xy_spacing=25)
+    grid.add_zeros("bed_surface__grain_size_distribution_location", at="node")
+    grid.add_zeros("surface_water__depth", at="link")
+    grid.add_zeros("surface_water__velocity", at="link")
+    grid.add_zeros("surface_water__velocity_previous_time", at="link")   
+    grid.add_zeros("topographic__elevation", at="node")
+    return RiverBedDynamics(grid)    
