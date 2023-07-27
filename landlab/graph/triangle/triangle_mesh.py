89a849da759af665f8aa8e4e9030be7114906334
Ethan Pierce <ethan.pierce@colorado.edu>
2023-07-27 13:35:53 -0600

    Fix broken doctest

--- landlab/graph/triangle/triangle_mesh.py
+++ landlab/graph/triangle/triangle_mesh.py
@@ -70,7 +70,7 @@ class TriangleMesh:
     10    10  0.25  0.75   0
     11    11  0.75  0.75   0
     12    12  0.25  0.25   0
-    >>> tri.voronoi['edges']
+    >>> tri.voronoi['faces']
         Link  head  tail
     0      0     0    13
     1      1     0     7
