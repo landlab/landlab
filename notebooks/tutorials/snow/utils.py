df4786d53087e40ab89f36c6abdb461adc17ecb1
gantian127 <jamy127@foxmail.com>
2023-09-19 17:39:58 -0600

    update code for lint

--- notebooks/tutorials/snow/utils.py
+++ notebooks/tutorials/snow/utils.py
@@ -6,15 +6,15 @@ import os
 
 # installation function
 def install_api_key():
-    home_dir = os.path.expanduser('~')
+    home_dir = os.path.expanduser("~")
 
     # create CDS API key file
-    cds_key = input('Enter Your CDS API Key: ')
-    cds_url = 'https://cds.climate.copernicus.eu/api/v2'
-    cds_config_content = 'url: {} \nkey: {}'.format(cds_url, cds_key)
-    cds_config_path = os.path.join(home_dir, '.cdsapirc')
+    cds_key = input("Enter Your CDS API Key: ")
+    cds_url = "https://cds.climate.copernicus.eu/api/v2"
+    cds_config_content = f"url: {cds_url} \nkey: {cds_key}"
+    cds_config_path = os.path.join(home_dir, ".cdsapirc")
 
-    with open(cds_config_path, 'w') as cds_config_file:
+    with open(cds_config_path, "w") as cds_config_file:
         cds_config_file.write(cds_config_content)
 
-    print('CDS API Key file is created at {}'.format(cds_config_path))
+    print(f"CDS API Key file is created at {cds_config_path}")
