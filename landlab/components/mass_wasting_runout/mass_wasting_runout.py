ae3a8fbaa68c174c45b3984f86310be49f100875
Jeffrey Keck <31672064+keckje@users.noreply.github.com>
2023-11-27 08:53:02 -0800

    draft update to include saver

--- landlab/components/mass_wasting_runout/mass_wasting_runout.py
+++ landlab/components/mass_wasting_runout/mass_wasting_runout.py
@@ -1,8 +1,8 @@
 import numpy as np
 import pandas as pd
-from landlab import RasterModelGrid
 from landlab import Component
 from landlab.components import FlowDirectorMFD
+from landlab.components.mass_wasting_runout.MWR_saver import MWR_Saver
 
 class MassWastingRunout(Component):
     
@@ -89,7 +89,7 @@ class MassWastingRunout(Component):
 
     Run MassWastingRunout
     
-    >>> example_square_MWR.run_one_step(run_id = 0)
+    >>> example_square_MWR.run_one_step()
     
     By subtracting the initial DEM from the final DEM, which has evolvod as 
     a consequence of the runout, we can see areas of aggradation (positive values) 
@@ -241,11 +241,10 @@ class MassWastingRunout(Component):
     run_id = 0
     ):
         
-        """
-        
+        """        
         Parameters
         ----------
-        grid : landlab raster model grid
+        grid: landlab raster model grid
 
         critical_slope: list of floats
             critical slope (angle of repose if no cohesion) of mass 
@@ -366,7 +365,7 @@ class MassWastingRunout(Component):
 
         super().__init__(grid)
         
-        if len(critical_slope)>1: # change this to check for list input if defined as func of CA
+        if len(critical_slope)>1: 
             self.variable_slpc = True    
             self.a = critical_slope[0]
             self.b = critical_slope[1]
@@ -428,22 +427,8 @@ class MassWastingRunout(Component):
         self._grid.at_node['topographic__initial_elevation'] = self._grid.at_node['topographic__elevation'].copy()
         
        
-    def run_one_step(self, run_id):
-        """for each mass wasting event that occurs during time/instance ""run_id",
-        implement Algorithm 1 and Algorithm 2.
-        
-        
-        Parameters
-        ----------
-        run_id : label for landslide run, can be a time stamp or some other identifier
-
-        Returns
-        -------
-        None.
-
-        """
-        # set run_id
-        self.run_id = run_id
+    def run_one_step(self):
+        """run MWR"""
                    
         # get all nodes that define the mass wasting events
         mask = self._grid.at_node['mass__wasting_id'] > 0
@@ -456,30 +441,9 @@ class MassWastingRunout(Component):
             innL.append(np.hstack(self._grid.nodes)[ls_mask])
                
         # prepare data containers for saving model images and behavior statistics    
-        self.arndn_r = {}
-        if self.save:
-            # lists and dictionaries for tracking model behavior 
-            self.EL = [] # entrainment depth / regolith depth
-            self.AL = [] # aggradation (deposition) depth
-            self.qsiL = [] # incoming flux (qsi)
-            self.TauL = [] # basal shear stress 
-            self.slopeL = [] # slope
-            self.velocityL = [] # velocity (if any)
-            self.arqso_r = {} # flux out
-            self.arn_r = {} # receiver nodes
-            self.aratt_r = {} #  arriving attributes
-            self.flowing_volume = {} # the total volume [m3] of the mobilized runout material
-            # dictionaries, that save the entire model grid field each model iteration
-            # for all fields listed below
-            # usefull for creating movies of how the flow and terrain evolve
-            self.runout_evo_maps = {} # runout material + topographic__elevation
-            self.topo_evo_maps = {}# topographic__elevation
-            self.att_r = {} # attribute value
-            self.st_r = {} # soil__thickness
-            self.tss_r ={} # topographic__steepest_slope
-            self.frn_r = {} # flow__receiver_node
-            self.frp_r = {} # 'flow__receiver_proportions'
-            self.arqso_r = {} # flux out
+        if self.save:           
+            self.saver = MWR_Saver(self)
+            self.saver.prep_data_containers()
             
         # For each mass wasting event in list:
         for mw_i,inn in enumerate(innL):
@@ -491,56 +455,29 @@ class MassWastingRunout(Component):
                 self.qsi_max = self._grid.at_node['soil__thickness'][inn].max()
             
             # prepare temporary data containers for each mass wasting event mw_i
-            self.arndn_r[mw_id] = []
             if self.save:
-                self.runout_evo_maps[mw_i] = {}
-                self.topo_evo_maps[mw_i] = {}
-                self.flowing_volume[mw_id] = []
-                self.st_r[mw_id] = []
-                self.tss_r[mw_id] = []
-                self.frn_r[mw_id] = []
-                self.frp_r[mw_id] = []            
-                self.arqso_r[mw_id] = []
-                self.arn_r[mw_id] = []
-                if self.track_attributes:
-                    self.att_r[mw_id] = dict.fromkeys(self._tracked_attributes, []) # this becomes the data container for each attribute
-                    self.aratt_r[mw_id] = dict.fromkeys(self._tracked_attributes, [])  
-                    
+                self.saver.prep_mw_data_containers(mw_i, mw_id)
             
             # Algorithm 1, prepare initial mass wasting material (debritons) for release 
             self._prep_initial_mass_wasting_material(inn, mw_i)
 
-            self.arndn_r[mw_id].append(self.arndn)
+            # self.arndn_r[mw_id].append(self.arndn)
             if self.save:
                 # save first set of data to reflect scar created by landslide
-                self.runout_evo_maps[mw_i][0] = self._grid.at_node['energy__elevation'].copy()
-                self.topo_evo_maps[mw_i][0] = self._grid.at_node['topographic__elevation'].copy()
-                self.flowing_volume[mw_id].append(0)            
-                if self.track_attributes:
-                    for key in self._tracked_attributes:
-                        self.att_r[mw_id][key].append(self._grid.at_node[key].copy()) # for each attribute, a copy of entire grid
-                        self.aratt_r[mw_id][key].append(self.aratt) 
-                self.st_r[mw_id].append(self._grid.at_node['soil__thickness'].copy())
-                self.tss_r[mw_id].append(self._grid.at_node['topographic__steepest_slope'].copy())
-                self.frn_r[mw_id].append(self._grid.at_node['flow__receiver_node'].copy())
-                self.frp_r[mw_id].append(self._grid.at_node['flow__receiver_proportions'].copy())
-                self.arqso_r[mw_id].append(self.arqso)
-                self.arn_r[mw_id].append(self.arn)
-                   
+                self.saver.save_conditions_before_runout(mw_i, mw_id)
 
             # Algorith 2, now loop through each receiving nodes, 
             # determine next set of recieving nodes,
             # repeat until no more receiving nodes (material deposits)             
-            c = 0 # model iteration counter
-            while len(self.arn)>0 and c < self.itL:
-
-                self.c = c
+            self.c = 0 # model iteration counter
+            while len(self.arn)>0 and self.c < self.itL:
+                
                 # set qsc: the qsc constraint does not fully apply until runout 
                 # has traveled dist_to_full_qsc_constraint
                 if self.d_it == 0:
                     self.qsc_v = self.qsc
                 else:
-                    self.qsc_v = self.qsc*(min(c/self.d_it,1))
+                    self.qsc_v = self.qsc*(min(self.c/self.d_it,1))
 
                 # temporary data containers for each iteration of the while loop, 
                 # that store receiving node, flux and attributes to become the 
@@ -603,26 +540,10 @@ class MassWastingRunout(Component):
                     self.aratt = self.aratt_ns
 
                 if self.save:
-                    DEMf = self._grid.at_node['topographic__elevation'].copy()                    
-                    DEMdf_r = DEMf-self._grid.at_node['topographic__initial_elevation']            
-                    self.flowing_volume[mw_id].append(DEMdf_r.sum()*self._grid.dx*self._grid.dy)     
-                    self.runout_evo_maps[mw_i][c+1] = self._grid.at_node['energy__elevation'].copy()
-                    self.runout_evo_maps[mw_i][c+1] = self._grid.at_node['energy__elevation'].copy()
-                    self.topo_evo_maps[mw_i][c+1] = self._grid.at_node['topographic__elevation'].copy()
-                    if self.track_attributes:
-                        for key in self._tracked_attributes:
-                            self.att_r[mw_id][key].append(self._grid.at_node[key].copy())
-                            self.aratt_r[mw_id][key].append(self.arattL) 
-                    self.st_r[mw_id].append(self._grid.at_node['soil__thickness'].copy())
-                    self.tss_r[mw_id].append(self._grid.at_node['topographic__steepest_slope'].copy())
-                    self.frn_r[mw_id].append(self._grid.at_node['flow__receiver_node'].copy())
-                    self.frp_r[mw_id].append(self._grid.at_node['flow__receiver_proportions'].copy())
-                    self.arqso_r[mw_id].append(self.arqsoL)
-                    self.arn_r[mw_id].append(self.arnL)                          
-                    self.arndn_r[mw_id].append(self.arndn)
+                    self.saver.save_conditions_after_one_iteration(mw_i, mw_id)
 
                 # update iteration counter
-                c+=1
+                self.c+=1
                 
                                             
     def _prep_initial_mass_wasting_material(self, inn, mw_i):
@@ -702,7 +623,7 @@ class MassWastingRunout(Component):
             rni = np.concatenate((rni,rn), axis = 0) 
             rqsoi = np.concatenate((rqsoi,rqso), axis = 0) 
         
-        self.arndn = np.ones([len(rni)])*np.nan # TODO: set this to node id
+        self.arndn = np.ones([len(rni)])*np.nan
         self.arn = rni
         self.arqso = rqsoi
         if self._tracked_attributes:
@@ -737,13 +658,10 @@ class MassWastingRunout(Component):
             # maximum slope at node n
             slpn = self._grid.at_node['topographic__steepest_slope'][n].max()
             
-            # proportion of flow in steepest direction
-            pn = 1#self._grid.at_node['flow__receiver_proportions'][n].max() 
-            
             if self.effecitve_qsi:
-                qsi_ = min(qsi*pn,self.qsi_max)
+                qsi_ = min(qsi,self.qsi_max)
             else:
-                qsi_ = qsi*pn
+                qsi_ = qsi
 
             dn = self.arndn[self.arn == n]
 
@@ -802,12 +720,7 @@ class MassWastingRunout(Component):
             
             # model behavior tracking
             if self.save:
-                self.EL.append(E)
-                self.AL.append(A)
-                self.qsiL.append(qsi)
-                self.slopeL.append(slpn) # slope
-                self.TauL.append(Tau)
-                self.velocityL.append(u) # velocity (if any)
+                self.saver.save_flow_stats(E,A,qsi,slpn,Tau,u)
 
             # updated attribute values at node
             if self._tracked_attributes:
@@ -1075,17 +988,7 @@ class MassWastingRunout(Component):
             Dp = att_in['particle__diameter']
             if depth < Dp: # grain size dependent erosion breaks if depth<Dp
                 Dp = depth*.99
-            
-            # phi = np.arctan(0.32)
-            
-            # # inertial stresses
-            # us = (self.g*depth*slpn)**0.5
-            # u = us*5.75*np.log10(depth/Dp)
             u = flow_velocity(Dp,depth,slpn,self.g)
-            
-            # dudz = u/depth
-            # Tcn = np.cos(theta)*self.vs*self.ros*(Dp**2)*(dudz**2)
-            # Tau = Tcn*np.tan(phi)
             
             Tau = shear_stress_grains(self.vs,
                                       self.ros,
@@ -1094,7 +997,6 @@ class MassWastingRunout(Component):
                                       slpn,
                                       self.g)
 
-            # Ec = (self.k*Tau**self.eta)
             Ec = self._grid.dx*erosion_rate(self.k,
                                             Tau,
                                             self.eta,
