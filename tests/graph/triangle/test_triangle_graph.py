07a550d6ea846d51f78a70f487dccd42de4ee6c6
mcflugen <mcflugen@gmail.com>
2023-12-02 11:51:47 -0700

    test DualTriGraph instead of TriangleGraph

--- tests/graph/triangle/test_triangle_graph.py
+++ tests/graph/triangle/test_triangle_graph.py
@@ -4,18 +4,15 @@ import numpy as np
 import pytest
 from numpy.testing import assert_array_equal
 
-from landlab.graph.triangle import TriangleGraph, TriangleMesh
+from landlab.graph.triangle.dual_triangle import DualTriGraph
+from landlab.graph.triangle.triangle_mesh import TriangleMesh
 
 if not TriangleMesh.validate_triangle():
     pytestmark = pytest.mark.skip(reason="triangle is not installed")
 
 
-ys = [0, 0, 10, 10]
-xs = [0, 10, 10, 0]
-
-
 def test_graph_init():
-    graph = TriangleGraph(np.array([ys, xs]), triangle_opts="pqa1Djevz")
+    graph = DualTriGraph(([0, 0, 10, 10], [0, 10, 10, 0]), triangle_opts="pqa1Djevz")
 
     assert graph.number_of_nodes == 89
     assert len(graph.x_of_node) == graph.number_of_nodes
@@ -26,8 +23,13 @@ def test_graph_init():
     assert graph.number_of_faces == 200
     assert graph.number_of_cells == 57
 
+    assert np.all(graph.area_of_cell > 0.0)
+    assert np.all(graph.area_of_patch > 0.0)
+    assert np.all(graph.length_of_link > 0.0)
+    assert np.all(graph.length_of_face > 0.0)
+
     assert_array_equal(
-        graph.nodes_at_patch[:3], [[41, 43, 15], [23, 28, 31], [4, 51, 50]]
+        graph.nodes_at_patch[:3], [[41, 43, 15], [28, 31, 23], [51, 50, 4]]
     )
 
     assert_array_equal(graph.nodes_at_link[:3], [[41, 43], [43, 15], [15, 41]])
@@ -39,13 +41,13 @@ def test_graph_init():
     assert_array_equal(
         graph.corners_at_cell[:3],
         [
-            [2, 62, 68, 70, 71, 77, 79, -1, -1],
-            [22, 23, 27, 28, 63, 65, -1, -1, -1],
-            [88, 89, 95, 101, 115, 136, 138, -1, -1],
+            [70, 79, 2, 77, 71, 68, 62, -1, -1],
+            [23, 65, 63, 27, 22, 28, -1, -1, -1],
+            [88, 115, 136, 101, 138, 89, 95, -1, -1],
         ],
     )
 
-    assert_array_equal(graph.n_corners_at_cell[:3], [9, 9, 9])
+    # assert_array_equal(graph.n_corners_at_cell[:3], [9, 9, 9])
 
     assert_array_equal(graph.cell_at_node[:5], [-1, -1, -1, -1, 0])
 
@@ -56,9 +58,9 @@ def test_graph_init():
     assert_array_equal(
         graph.faces_at_cell[:3],
         [
-            [6, 8, 109, 110, 116, 117, 119, -1, -1],
-            [54, 55, 56, 62, 63, 112, -1, -1, -1],
-            [145, 146, 148, 149, 164, 166, 180, -1, -1],
+            [117, 109, 110, 116, 119, 8, 6, -1, -1],
+            [56, 55, 54, 62, 63, 112, -1, -1, -1],
+            [180, 145, 146, 148, 149, 166, 164, -1, -1],
         ],
     )
 
@@ -104,14 +106,14 @@ def test_graph_init():
 def test_raise_error_if_no_interior_nodes(datadir):
     """If no cells are generated, raise a ValueError."""
     with pytest.raises(ValueError):
-        TriangleGraph.from_shapefile(
+        DualTriGraph.from_shapefile(
             datadir / "polygon_concave.geojson", triangle_opts="pqa100Djevz"
         )
 
 
 def test_generate_graph_from_geojson(datadir):
     """Test the graph constructor from a geojson file."""
-    graph = TriangleGraph.from_shapefile(
+    graph = DualTriGraph.from_shapefile(
         datadir / "polygon_concave.geojson", triangle_opts="pqa10Djevz"
     )
 
@@ -124,7 +126,12 @@ def test_generate_graph_from_geojson(datadir):
     assert graph.number_of_faces == 27
     assert graph.number_of_cells == 1
 
-    assert_array_equal(graph.nodes_at_patch[:3], [[8, 18, 20], [13, 1, 6], [4, 8, 22]])
+    assert np.all(graph.area_of_cell > 0.0)
+    assert np.all(graph.area_of_patch > 0.0)
+    assert np.all(graph.length_of_link > 0.0)
+    assert np.all(graph.length_of_face > 0.0)
+
+    assert_array_equal(graph.nodes_at_patch[:3], [[8, 18, 20], [6, 13, 1], [22, 4, 8]])
 
     assert_array_equal(graph.nodes_at_link[:3], [[8, 18], [18, 20], [20, 8]])
 
@@ -132,9 +139,9 @@ def test_generate_graph_from_geojson(datadir):
 
     assert_array_equal(graph.corners_at_face[:3], [[0, 18], [0, 5], [1, 16]])
 
-    assert_array_equal(graph.corners_at_cell[0], [8, 9, 21, 22, 24, 25])
+    assert_array_equal(graph.corners_at_cell[0], [22, 8, 24, 21, 25, 9])
 
-    assert_array_equal(graph.n_corners_at_cell[0], [6])
+    # assert_array_equal(graph.n_corners_at_cell[0], [6])
 
     assert_array_equal(graph.cell_at_node[24], [0])
 
@@ -142,7 +149,7 @@ def test_generate_graph_from_geojson(datadir):
 
     assert_array_equal(graph.node_at_cell[0], [24])
 
-    assert_array_equal(graph.faces_at_cell[0], [15, 16, 18, 19, 25, 26])
+    assert_array_equal(graph.faces_at_cell[0], [16, 18, 19, 26, 25, 15])
 
 
 def test_invalid_polygon_error():
@@ -151,4 +158,4 @@ def test_invalid_polygon_error():
     xs = [0, 0, 3, 3, 2, 2, 1, 1, 2, 2, 0]
 
     with pytest.raises(ValueError):
-        TriangleGraph([ys, xs], triangle_opts="pqa0.1Devjz")
+        DualTriGraph([ys, xs], triangle_opts="pqa0.1Devjz")
