970b842e4d260d52ffd1dce1feb8ef6905dee52d
mcflugen <mcflugen@gmail.com>
2023-12-11 09:22:43 -0700

    addjust triangle option to produce a nice grid

--- tests/grid/test_triangle_grid/test_triangle_grid.py
+++ tests/grid/test_triangle_grid/test_triangle_grid.py
@@ -11,18 +11,23 @@ if not TriangleMesh.validate_triangle():
     pytestmark = pytest.mark.skip(reason="triangle is not installed")
 
 
-ys = np.array([0, 0, 10, 10])
-xs = np.array([0, 10, 10, 0])
-
-
 def test_grid_init():
-    grid = TriangleModelGrid((ys, xs), triangle_opts="pqa1Devjz")
+    grid = TriangleModelGrid(
+        ([-1.0, -1.0, 11.0, 11.0], [0.0, 10.0, 10.0, 0.0]), triangle_opts="pqa1Devjz"
+    )
 
-    assert grid.number_of_nodes == 89
+    assert np.all(grid.x_of_node >= 0.0) and np.all(grid.x_of_node <= 10.0)
+    assert np.all(grid.y_of_node >= -1.0) and np.all(grid.y_of_node <= 11.0)
+    assert np.all(grid.length_of_link >= 0.0)
+    assert np.all(grid.length_of_face >= 0.0)
+    assert np.all(grid.area_of_cell >= 0.0)
+    assert np.all(grid.area_of_patch >= 0.0)
 
 
 def test_plot_nodes_and_links():
-    grid = TriangleModelGrid((ys, xs), triangle_opts="pqa1Devjz")
+    grid = TriangleModelGrid(
+        ([0.0, 0.0, 11.0, 11.0], [0.0, 10.0, 10.0, 0.0]), triangle_opts="pqa1Devjz"
+    )
     plot = grid.plot_nodes_and_links()
 
     assert len(plot.axes) == 1
@@ -67,16 +72,15 @@ def test_concave_polygon(datadir):
 
 def test_multiple_interior_rings(datadir):
     grid = TriangleModelGrid.from_shapefile(
-        datadir / "polygon_two_interior_rings.geojson", triangle_opts="pqa10Djevz"
+        datadir / "polygon_two_interior_rings.geojson", triangle_opts="pq10a10Djevz"
     )
 
-    assert grid.number_of_nodes == 26
-    assert len(grid.boundary_nodes) == 20
+    assert np.all(grid.x_of_node >= 0.0) and np.all(grid.x_of_node <= 10.0)
+    assert np.all(grid.y_of_node >= -1.0) and np.all(grid.y_of_node <= 11.0)
+    assert np.all(grid.length_of_link >= 0.0)
+    assert np.all(grid.length_of_face >= 0.0)
+    assert np.all(grid.area_of_cell >= 0.0)
+    assert np.all(grid.area_of_patch >= 0.0)
+
     assert grid.number_of_cells == grid.number_of_nodes - len(grid.boundary_nodes)
-    assert len(grid.x_of_node) == grid.number_of_nodes
-    assert len(grid.y_of_node) == grid.number_of_nodes
-    assert grid.number_of_links == 61
-    assert grid.number_of_patches == 34
     assert grid.number_of_corners == grid.number_of_patches
-    assert grid.number_of_faces == 41
-    assert grid.number_of_cells == 6
