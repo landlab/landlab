543ed3f73ad627c2c6cd0e7f8442f6cc08ab937b
gantian127 <jamy127@foxmail.com>
2024-01-11 21:53:52 -0700

    install xesmf

--- noxfile.py
+++ noxfile.py
@@ -88,6 +88,7 @@ def test_notebooks(session: nox.Session) -> None:
         PATH["requirements"] / "notebooks.txt",
     )
     session.conda_install("richdem", channel=["nodefaults", "conda-forge"])
+    session.conda_install("xesmf", channel=["nodefaults", "conda-forge"])
     session.install("git+https://github.com/mcflugen/nbmake.git@mcflugen/add-markers")
     session.install("-e", ".", "--no-deps")
 
