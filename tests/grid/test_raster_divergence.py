1142d1c9918ca34323c799a9a97a1a88c5b4d729
mcflugen <mcflugen@gmail.com>
2024-03-21 12:40:46 -0600

    add one test for calc_net_face_flux_at_cell

--- tests/grid/test_raster_divergence.py
+++ tests/grid/test_raster_divergence.py
@@ -5,6 +5,28 @@ from numpy.testing import assert_array_almost_equal
 from landlab import RasterModelGrid
 from landlab.grid.divergence import calc_flux_div_at_node as calc_flux_div_at_node_slow
 from landlab.grid.raster_divergence import calc_flux_div_at_node
+from landlab.grid.raster_divergence import calc_net_face_flux_at_cell
+
+
+def test_calc_net_face_flux_at_cell():
+    grid = RasterModelGrid((4, 5), xy_spacing=(1.0, 2.0))
+
+    unit_flux_at_face = (
+        *(0.0, 0.0, 0.0),
+        *(1.0, 0.0, 0.0, 0.0),
+        *(0.0, 0.0, 0.0),
+        *(1.0, 0.0, 0.0, 0.0),
+        *(0.0, 0.0, 0.0),
+    )
+
+    out = calc_net_face_flux_at_cell(grid, unit_flux_at_face)
+
+    expected = (
+        *(2.0, 0.0, 0.0),
+        *(2.0, 0.0, 0.0),
+    )
+
+    assert_array_almost_equal(out, expected)
 
 
 @pytest.mark.benchmark(group="small")
