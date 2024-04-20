9dc3716f8cc3f62ef30d0f6ec9ef2c72f47d8648
sebastien-lenard <sebastien.lenard@gmail.com>
2024-04-20 22:07:51 +0000

    FlowRouter: tests assert to int64/float64

--- tests/components/flow_router/test_flow_router_run_accumulations.py
+++ tests/components/flow_router/test_flow_router_run_accumulations.py
@@ -13,7 +13,7 @@ def test_run_flow_accumulations_raster():
     g = RasterModelGrid((5, 5), (spacing, spacing))
     g.status_at_node[g.perimeter_nodes] = g.BC_NODE_IS_CLOSED
     self = FlowRouter(g)
-    g.at_node["topographic__elevation"] = np.array(
+    g.at_node["topographic__elevation"] = np.float64(
         [10, 10, 10, 10, 10]
         + [20, 20, 0, 20, 20]
         + [30, 0, 10, 20, 10]
@@ -25,14 +25,14 @@ def test_run_flow_accumulations_raster():
 
     assert_array_equal(
         g.at_node["flow__upstream_node_order"],
-        np.array(
+        np.int64(
             [4, 8, 13, 12, 18, 7, 6, 11, 16, 17, 0, 1, 2, 3, 5, 9, 10]
             + [14, 15, 19, 20, 21, 22, 23, 24]
         ),
     )
     assert_array_almost_equal(
         g.at_node["drainage_area"],
-        np.array(
+        np.float64(
             [0.0, 0.0, 0.0, 0.0, 90000.0]
             + [0.0, 10000.0, 50000.0, 90000.0, 0.0]
             + [0.0, 30000.0, 20000.0, 10000.0, 0.0]
@@ -42,7 +42,7 @@ def test_run_flow_accumulations_raster():
     )
     assert_array_almost_equal(
         g.at_node["surface_water__discharge"],
-        np.array(
+        np.float64(
             [0.0, 0.0, 0.0, 0.0, 90000.0]
             + [0.0, 10000.0, 50000.0, 90000.0, 0.0]
             + [0.0, 30000.0, 20000.0, 10000.0, 0.0]
@@ -60,7 +60,7 @@ def test_run_flow_accumulations_hex():
     g.status_at_node[0] = g.BC_NODE_IS_CLOSED
 
     self = FlowRouter(g, surface="soil__elevation", diagonals=True, runoff_rate=2.0)
-    g.at_node["soil__elevation"] = np.array(
+    g.at_node["soil__elevation"] = np.float64(
         [10.0, 20.0, 10.0]
         + [10.0, 0.0, 5.0, 10.0]
         + [20.0, 10.0, 5.0, 10.0, 20.0]
@@ -72,11 +72,11 @@ def test_run_flow_accumulations_hex():
 
     assert_array_equal(
         g.at_node["flow__upstream_node_order"],
-        np.array([1, 2, 3, 4, 9, 5, 10, 8, 6, 7, 11, 12, 15, 16, 17, 13, 14] + [18, 0]),
+        np.int64([1, 2, 3, 4, 9, 5, 10, 8, 6, 7, 11, 12, 15, 16, 17, 13, 14] + [18, 0]),
     )
     assert_array_almost_equal(
         g.at_node["drainage_area"],
-        np.array(
+        np.float64(
             [0.0, 0.0, 0.0]
             + [433.0127, 346.41016, 173.20508, 0.0]
             + [0.0, 86.60254, 86.60254, 86.60254, 0.0]
@@ -86,7 +86,7 @@ def test_run_flow_accumulations_hex():
     )
     assert_array_almost_equal(
         g.at_node["surface_water__discharge"],
-        np.array(
+        np.float64(
             [0.0, 0.0, 0.0]
             + [866.0254, 692.82032, 346.41016, 0.0]
             + [0.0, 173.20508, 173.20508, 173.20508, 0.0]
@@ -114,12 +114,12 @@ def test_run_flow_accumulations_network():
     self.run_flow_accumulations()
 
     assert_array_equal(
-        g.at_node["flow__upstream_node_order"], np.array([0, 1, 2, 3, 5, 6, 7, 4])
+        g.at_node["flow__upstream_node_order"], np.int64([0, 1, 2, 3, 5, 6, 7, 4])
     )
     assert_array_almost_equal(
-        g.at_node["drainage_area"], np.array([8.0, 7.0, 1.0, 4.0, 1.0, 3.0, 1.0, 1.0])
+        g.at_node["drainage_area"], np.float64([8.0, 7.0, 1.0, 4.0, 1.0, 3.0, 1.0, 1.0])
     )
     assert_array_almost_equal(
         g.at_node["surface_water__discharge"],
-        np.array([8.0, 14.0, 3.0, 16.0, 3.0, 9.6, 4.5, 1.0]),
+        np.float64([8.0, 14.0, 3.0, 16.0, 3.0, 9.6, 4.5, 1.0]),
     )
