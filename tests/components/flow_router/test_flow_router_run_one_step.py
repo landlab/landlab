6a4dcd70516f29d1d859e8d4bcdbb96cb5394fbb
sebastien-lenard <sebastien.lenard@gmail.com>
2024-04-20 18:03:08 +0000

    FlowRouter: conversion of all tests into int64/float64 compatible

--- tests/components/flow_router/test_flow_router_run_one_step.py
+++ tests/components/flow_router/test_flow_router_run_one_step.py
@@ -13,7 +13,7 @@ def test_run_one_step_raster():
     g = RasterModelGrid((4, 4), (spacing, spacing))
     g.status_at_node[g.perimeter_nodes] = g.BC_NODE_IS_CLOSED
     self = FlowRouter(g)
-    g.at_node["topographic__elevation"] = np.array(
+    g.at_node["topographic__elevation"] = np.float64(
         [10.0, 20.0, 10.0, 10.0]
         + [10.0, 0.0, 5.0, 10.0]
         + [20.0, 20.0, 5.0, 10.0]
@@ -23,11 +23,11 @@ def test_run_one_step_raster():
 
     assert_array_equal(
         g.at_node["flow__receiver_node"],
-        np.array([0, 1, 2, 3, 4, 6, 3, 7, 8, 6, 6, 11, 12, 13, 14, 15]),
+        np.int64([0, 1, 2, 3, 4, 6, 3, 7, 8, 6, 6, 11, 12, 13, 14, 15]),
     )
     assert_array_almost_equal(
         g.at_node["topographic__steepest_slope"],
-        np.array(
+        np.float64(
             [0.0, 0.0, 0.0, 0.0]
             + [0.0, 0.0, 0.0, 0.0]
             + [0.0, 1.06066017, 0.0, 0.0]
@@ -36,17 +36,17 @@ def test_run_one_step_raster():
     )
     assert_array_equal(
         g.at_node["flow__link_to_receiver_node"],
-        np.array(
+        np.int64(
             [-1, -1, -1, -1] + [-1, 8, 29, -1] + [-1, 33, 12, -1] + [-1, -1, -1, -1]
         ),
     )
     assert_array_equal(
         g.at_node["flood_status_code"],
-        np.array([0, 0, 0, 0] + [0, 3, 3, 0] + [0, 0, 3, 0] + [0, 0, 0, 0]),
+        np.int64([0, 0, 0, 0] + [0, 3, 3, 0] + [0, 0, 3, 0] + [0, 0, 0, 0]),
     )
     assert_array_almost_equal(
         g.at_node["depression__depth"],
-        np.array(
+        np.float64(
             [0.0, 0.0, 0.0, 0.0]
             + [0.0, 10.0, 5.0, 0.0]
             + [0.0, 0.0, 5.0, 0.0]
@@ -55,17 +55,17 @@ def test_run_one_step_raster():
     )
     assert_array_equal(
         g.at_node["outlet_node"],
-        np.array([0, 1, 2, 3] + [4, 3, 3, 7] + [8, 3, 3, 11] + [12, 13, 14, 15]),
+        np.int64([0, 1, 2, 3] + [4, 3, 3, 7] + [8, 3, 3, 11] + [12, 13, 14, 15]),
     )
     assert_array_equal(
         g.at_node["depression__outlet_node"],
-        np.array(
+        np.int64(
             [-1, -1, -1, -1] + [-1, 3, 3, -1] + [-1, -1, 3, -1] + [-1, -1, -1, -1]
         ),
     )
     assert_array_almost_equal(
         g.at_node["depression_free__elevation"],
-        np.array(
+        np.float64(
             [10.0, 20.0, 10.0, 10.0]
             + [10.0, 10.0000002, 10.0000001, 10.0]
             + [20.0, 20.0, 10.0000002, 10.0]
@@ -75,11 +75,11 @@ def test_run_one_step_raster():
 
     assert_array_equal(
         g.at_node["flow__upstream_node_order"],
-        np.array([3, 6, 5, 9] + [10, 0, 1, 2] + [4, 7, 8, 11] + [12, 13, 14, 15]),
+        np.int64([3, 6, 5, 9] + [10, 0, 1, 2] + [4, 7, 8, 11] + [12, 13, 14, 15]),
     )
     assert_array_almost_equal(
         g.at_node["drainage_area"],
-        np.array(
+        np.float64(
             [0.0, 0.0, 0.0, 400.0]
             + [0.0, 100.0, 400.0, 0.0]
             + [0.0, 100.0, 100.0, 0.0]
@@ -88,7 +88,7 @@ def test_run_one_step_raster():
     )
     assert_array_almost_equal(
         g.at_node["surface_water__discharge"],
-        np.array(
+        np.float64(
             [0.0, 0.0, 0.0, 400.0]
             + [0.0, 100.0, 400.0, 0.0]
             + [0.0, 100.0, 100.0, 0.0]
@@ -105,7 +105,7 @@ def test_run_one_step_hex():
     g.status_at_node[0] = g.BC_NODE_IS_CLOSED
 
     self = FlowRouter(g, surface="soil__elevation", diagonals=True, runoff_rate=2.0)
-    g.at_node["soil__elevation"] = np.array(
+    g.at_node["soil__elevation"] = np.float64(
         [10.0, 20.0, 10.0]
         + [10.0, 0.0, 5.0, 10.0]
         + [20.0, 10.0, 5.0, 10.0, 20.0]
@@ -116,7 +116,7 @@ def test_run_one_step_hex():
 
     assert_array_equal(
         g.at_node["flow__receiver_node"],
-        np.array(
+        np.int64(
             [0, 1, 2]
             + [3, 3, 4, 6]
             + [7, 3, 4, 5, 11]
@@ -126,7 +126,7 @@ def test_run_one_step_hex():
     )
     assert_array_almost_equal(
         g.at_node["topographic__steepest_slope"],
-        np.array(
+        np.float64(
             [0.0, 0.0, 0.0]
             + [0.0, 0.0, 0.5, 0.0]
             + [0.0, 0.0, 0.5, 0.5, 0.0]
@@ -136,7 +136,7 @@ def test_run_one_step_hex():
     )
     assert_array_equal(
         g.at_node["flow__link_to_receiver_node"],
-        np.array(
+        np.int64(
             [-1, -1, -1]
             + [-1, 8, 9, -1]
             + [-1, 12, 14, 16, -1]
@@ -146,11 +146,11 @@ def test_run_one_step_hex():
     )
     assert_array_equal(
         g.at_node["flood_status_code"],
-        np.array([0, 0, 0] + [0, 3, 3, 0] + [0, 0, 3, 0, 0] + [0, 0, 0, 0] + [0, 0, 0]),
+        np.int64([0, 0, 0] + [0, 3, 3, 0] + [0, 0, 3, 0, 0] + [0, 0, 0, 0] + [0, 0, 0]),
     )
     assert_array_almost_equal(
         g.at_node["depression__depth"],
-        np.array(
+        np.float64(
             [0.0, 0.0, 0.0]
             + [0.0, 10.0, 5.0, 0.0]
             + [0.0, 0.0, 5.0, 0.0, 0.0]
@@ -160,7 +160,7 @@ def test_run_one_step_hex():
     )
     assert_array_equal(
         g.at_node["outlet_node"],
-        np.array(
+        np.int64(
             [0, 1, 2]
             + [3, 3, 3, 6]
             + [7, 3, 3, 3, 11]
@@ -170,7 +170,7 @@ def test_run_one_step_hex():
     )
     assert_array_equal(
         g.at_node["depression__outlet_node"],
-        np.array(
+        np.int64(
             [-1, -1, -1]
             + [-1, 3, 3, -1]
             + [-1, -1, 3, -1, -1]
@@ -180,7 +180,7 @@ def test_run_one_step_hex():
     )
     assert_array_almost_equal(
         g.at_node["depression_free__elevation"],
-        np.array(
+        np.float64(
             [10.0, 20.0, 10.0]
             + [10.0, 10.0000001, 10.0000002, 10.0]
             + [20.0, 10.0, 10.0000002, 10.0, 20.0]
@@ -191,7 +191,7 @@ def test_run_one_step_hex():
 
     assert_array_equal(
         g.at_node["flow__upstream_node_order"],
-        np.array(
+        np.int64(
             [1, 2, 3]
             + [4, 9, 5, 10]
             + [8, 6, 7, 11, 12]
@@ -201,7 +201,7 @@ def test_run_one_step_hex():
     )
     assert_array_almost_equal(
         g.at_node["drainage_area"],
-        np.array(
+        np.float64(
             [0.0, 0.0, 0.0]
             + [433.0127, 346.41016, 173.20508, 0.0]
             + [0.0, 86.60254, 86.60254, 86.60254, 0.0]
@@ -211,7 +211,7 @@ def test_run_one_step_hex():
     )
     assert_array_almost_equal(
         g.at_node["surface_water__discharge"],
-        np.array(
+        np.float64(
             [0.0, 0.0, 0.0]
             + [866.0254, 692.82032, 346.41016, 0.0]
             + [0.0, 173.20508, 173.20508, 173.20508, 0.0]
@@ -239,41 +239,41 @@ def test_run_one_step_network():
     self.run_one_step()
 
     assert_array_equal(
-        g.at_node["flow__receiver_node"], np.array([0, 0, 1, 1, 1, 3, 5, 5])
+        g.at_node["flow__receiver_node"], np.int64([0, 0, 1, 1, 1, 3, 5, 5])
     )
     assert_array_almost_equal(
         g.at_node["topographic__steepest_slope"],
-        np.array(
+        np.float64(
             [0.0, 0.002, 0.00048507, 0.0, 0.00035355]
             + [0.00223607, 0.00357771, 0.0022188],
         ),
     )
     assert_array_equal(
-        g.at_node["flow__link_to_receiver_node"], np.array([-1, 0, 1, 2, 3, 4, 5, 6])
+        g.at_node["flow__link_to_receiver_node"], np.int64([-1, 0, 1, 2, 3, 4, 5, 6])
     )
     assert_array_equal(
-        g.at_node["flood_status_code"], np.array([0, 0, 0, 3, 0, 0, 0, 0])
+        g.at_node["flood_status_code"], np.int64([0, 0, 0, 3, 0, 0, 0, 0])
     )
     assert_array_almost_equal(
         g.at_node["depression__depth"],
-        np.array([0.0, 0.0, 0.0, 0.05, 0.0, 0.0, 0.0, 0.0]),
+        np.float64([0.0, 0.0, 0.0, 0.05, 0.0, 0.0, 0.0, 0.0]),
     )
-    assert_array_equal(g.at_node["outlet_node"], np.array([0, 0, 0, 0, 0, 0, 0, 0]))
+    assert_array_equal(g.at_node["outlet_node"], np.int64([0, 0, 0, 0, 0, 0, 0, 0]))
     assert_array_equal(
-        g.at_node["depression__outlet_node"], np.array([-1, -1, -1, 1, -1, -1, -1, -1])
+        g.at_node["depression__outlet_node"], np.int64([-1, -1, -1, 1, -1, -1, -1, -1])
     )
     assert_array_almost_equal(
         g.at_node["depression_free__elevation"],
-        np.array([0.0, 0.2, 0.25, 0.2, 0.25, 0.4, 0.8, 0.8]),
+        np.float64([0.0, 0.2, 0.25, 0.2, 0.25, 0.4, 0.8, 0.8]),
     )
 
     assert_array_equal(
-        g.at_node["flow__upstream_node_order"], np.array([0, 1, 2, 3, 5, 6, 7, 4])
+        g.at_node["flow__upstream_node_order"], np.int64([0, 1, 2, 3, 5, 6, 7, 4])
     )
     assert_array_almost_equal(
-        g.at_node["drainage_area"], np.array([8.0, 7.0, 1.0, 4.0, 1.0, 3.0, 1.0, 1.0])
+        g.at_node["drainage_area"], np.float64([8.0, 7.0, 1.0, 4.0, 1.0, 3.0, 1.0, 1.0])
     )
     assert_array_almost_equal(
         g.at_node["surface_water__discharge"],
-        np.array([8.0, 14.0, 3.0, 16.0, 3.0, 9.6, 4.5, 1.0]),
+        np.float64([8.0, 14.0, 3.0, 16.0, 3.0, 9.6, 4.5, 1.0]),
     )
