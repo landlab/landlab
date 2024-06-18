3c8fc8f78e26501406a81cdc4d7b8168650fac87
mcflugen <mcflugen@gmail.com>
2024-06-18 16:39:59 -0600

    update the raster_mapper extension

--- landlab/grid/ext/raster_mappers.pyx
+++ landlab/grid/ext/raster_mappers.pyx
@@ -1,10 +1,10 @@
 cimport cython
-
 from cython.parallel cimport prange
 
 ctypedef fused float_or_int:
     cython.integral
     cython.floating
+
 
 @cython.boundscheck(False)
 @cython.wraparound(False)
@@ -13,24 +13,42 @@ def map_max_of_link_nodes_to_link(
     const float_or_int[:] value_at_node,
     shape,
 ):
-    cdef int node, link
-    cdef int row, col
-    cdef int n_rows = shape[0]
-    cdef int n_cols = shape[1]
-    cdef int links_per_row = 2 * n_cols - 1
-
-    for row in prange(n_rows, nogil=True, schedule="static"):
-        link = row * links_per_row
-        node = row * n_cols
-        for col in range(n_cols - 1):
-            out[link] = max(value_at_node[node], value_at_node[node + 1])
-            link = link + 1
-            node = node + 1
+    cdef long n_rows = shape[0]
+    cdef long nodes_per_row = shape[1]
+    cdef long horizontal_links_per_row = nodes_per_row - 1
+    cdef long vertical_links_per_row = nodes_per_row
+    cdef long links_per_row = horizontal_links_per_row + vertical_links_per_row
+    cdef long link
+    cdef long node
+    cdef long row
+    cdef long first_link
+    cdef long first_node
 
     for row in prange(n_rows - 1, nogil=True, schedule="static"):
-        link = row * links_per_row + n_cols - 1
-        node = row * n_cols
-        for col in range(n_cols):
-            out[link] = max(value_at_node[node], value_at_node[node + n_cols])
-            link = link + 1
+        first_link = row * links_per_row
+        first_node = row * nodes_per_row
+
+        node = first_node
+        for link in range(first_link, first_link + horizontal_links_per_row):
+            out[link] = max(value_at_node[node], value_at_node[node + 1])
+
+            node = node + 1
+
+        first_link = first_link + horizontal_links_per_row
+        node = first_node
+        for link in range(first_link, first_link + vertical_links_per_row):
+            out[link] = max(
+                value_at_node[node], value_at_node[node + nodes_per_row]
+            )
+
+            node = node + 1
+
+    with nogil:
+        first_link = (n_rows - 1) * links_per_row
+        first_node = (n_rows - 1) * nodes_per_row
+
+        node = first_node
+        for link in range(first_link, first_link + horizontal_links_per_row):
+            out[link] = max(value_at_node[node], value_at_node[node + 1])
+
             node = node + 1
--- src/landlab/grid/raster_mappers.py
+++ src/landlab/grid/raster_mappers.py
@@ -189,11 +189,11 @@ def map_max_of_link_nodes_to_link(grid, value_at_node, out=None):
     ...     ],
     ...     at="node",
     ... )
-    >>> map_max_of_link_nodes_to_link(grid, 'z')
+    >>> map_max_of_link_nodes_to_link(grid, "z")
     array([  1.,   2.,   3.,   7.,   6.,   5.,   4.,   7.,   6.,   5.,   8.,
              9.,  10.,  11.,   9.,  10.,  11.])
 
-    >>> values_at_links = grid.empty(at='link')
+    >>> values_at_links = grid.empty(at="link")
     >>> rtn = map_max_of_link_nodes_to_link(grid, "z", out=values_at_links)
     >>> values_at_links
     array([  1.,   2.,   3.,   7.,   6.,   5.,   4.,   7.,   6.,   5.,   8.,
