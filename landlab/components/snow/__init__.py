9a8a01035bd3b263d353dd43dd1588848a772fa4
gantian127 <jamy127@foxmail.com>
2024-08-27 16:29:43 -0600

    update __init__.py file

--- landlab/components/snow/__init__.py
+++ landlab/components/snow/__init__.py
@@ -1,3 +1,7 @@
 from .snow_degree_day import SnowDegreeDay
+from .snow_energy_balance import SnowEnergyBalance
 
-__all__ = ["SnowDegreeDay"]
+__all__ = [
+    "SnowDegreeDay",
+    "SnowEnergyBalance"
+]
