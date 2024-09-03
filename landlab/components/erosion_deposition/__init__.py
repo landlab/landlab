4f756efdac0c2ce87e7e8906b0fa73a0133c1e23
annie <ann.anwei.thompson@gmail.com>
2024-09-03 12:22:48 -0700

    init file changes for shared stream power component

--- landlab/components/erosion_deposition/__init__.py
+++ landlab/components/erosion_deposition/__init__.py
@@ -1,3 +1,5 @@
 from .erosion_deposition import ErosionDeposition
+from .shared_stream_power import SharedStreamPower
 
-__all__ = ["ErosionDeposition"]
+__all__ = ["ErosionDeposition",
+           "SharedStreamPower",]
