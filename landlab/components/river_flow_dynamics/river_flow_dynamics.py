f08d791e0391076275c341a5268bbfd9b8956ebe
angelmons <amonsalve@uidaho.edu>
2024-09-03 20:41:21 -0400

    Update river_flow_dynamics.py
    scipy problems with sp.sparse.linalg.cg
    nox test require tol as argument to pass the test.
    
    pytest uses rtol.
    
    We removed that argument and left it with the default value of 1e-5.

--- landlab/components/river_flow_dynamics/river_flow_dynamics.py
+++ landlab/components/river_flow_dynamics/river_flow_dynamics.py
@@ -118,7 +118,7 @@ Examine the flow depth at the center of the channel after 10 seconds.
 
 >>> flow_depth = np.reshape(grid['node']["surface_water__depth"],(nRows,nCols))[10,:]
 
->>> np.round(flow_depth,2)
+>>> np.round(flow_depth,3)
 array([0.5  , 0.491, 0.48 , 0.473, 0.467, 0.464, 0.46 , 0.458, 0.455,
        0.454, 0.452, 0.45 , 0.449, 0.448, 0.446, 0.445, 0.443, 0.442,
        0.441, 0.439, 0.438, 0.437, 0.435, 0.434, 0.433, 0.431, 0.43 ,
@@ -1617,7 +1617,7 @@ class river_flow_dynamics(Component):
             left_hand_side,
             right_hand_side,
             M=Mi,
-            tol=self._pcg_tolerance,
+            #rtol=self._pcg_tolerance,
             maxiter=self._pcg_max_iterations,
             atol=0,
         )
