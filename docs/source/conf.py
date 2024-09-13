85e5523f76338f18ea1965508ac83d0969e5ea23
mcflugen <mcflugen@gmail.com>
2024-09-13 10:58:12 -0600

    don't include init docstring with class docstring

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -377,7 +377,7 @@ todo_include_todos = True
 
 napoleon_numpy_docstring = True
 napoleon_google_docstring = False
-napoleon_include_init_with_doc = True
+napoleon_include_init_with_doc = False
 napoleon_include_special_with_doc = True
 
 # -- Options for towncrier_draft extension --------------------------------------------
