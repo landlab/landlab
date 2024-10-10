8a4c95868fe53deb41d85b7a905712455ebd5162
mcflugen <mcflugen@gmail.com>
2024-10-10 17:18:48 -0600

    parametrize data types for unravel tests

--- tests/utils/test_jaggedarray.py
+++ tests/utils/test_jaggedarray.py
@@ -5,11 +5,12 @@ from numpy.testing import assert_array_equal
 from landlab.utils.jaggedarray import unravel
 
 
-@pytest.mark.parametrize("dtype", (int,))
-def test_unravel_jaggedarray(dtype):
-    actual = unravel(
-        np.arange(24, dtype=int),
-        np.asarray([0, 4, 8, 12, 16, 20, 24], dtype=int),
-    )
+@pytest.mark.parametrize("dtype0", (int, np.intp, np.long, np.longlong, float))
+@pytest.mark.parametrize("dtype1", (int, np.intp, np.long, np.longlong))
+def test_unravel_jaggedarray(dtype0, dtype1):
+    data = np.empty(24, dtype=dtype0)
+    data[:] = np.arange(24)
+    actual = unravel(data, np.asarray([0, 4, 8, 12, 16, 20, 24], dtype=dtype1))
+    expected = data.copy().reshape((6, 4))
 
-    assert_array_equal(actual, np.arange(24).reshape((6, 4)))
+    assert_array_equal(actual, expected)
