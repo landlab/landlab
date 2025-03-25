8fb1062a1fd1c78771be257cf0fa0316a4e97323
yuvalsmilo <36788569+yuvalsmilo@users.noreply.github.com>
2025-03-25 15:05:14 -0600

    Update __init__.py

--- src/landlab/components/gravel_bedrock_eroder/__init__.py
+++ src/landlab/components/gravel_bedrock_eroder/__init__.py
@@ -1,3 +1,4 @@
-from .gravel_bedrock_eroder import GravelBedrockEroder
+#from .gravel_bedrock_eroder import GravelBedrockEroder
+from .gravel_bedrock_eroder_extended import GravelBedrockEroder
 
 __all__ = ["GravelBedrockEroder"]
