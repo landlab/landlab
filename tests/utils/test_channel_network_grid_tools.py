82086c7fb1986bfa0f849f479f5230d629f73eeb
mcflugen <mcflugen@gmail.com>
2025-10-08 15:24:25 -0600

    add unit tests for choose_unique

--- tests/utils/test_channel_network_grid_tools.py
+++ tests/utils/test_channel_network_grid_tools.py
@@ -43,6 +43,48 @@ def test_choose_from_repeated_bad_keyword(choose):
         choose_from_repeated([0, 0, 1, 1], choose=choose)
 
 
+@pytest.mark.parametrize("choose", ("foo", "FIRST", " last", "last ", ""))
+def test_choose_unique_bad_choose(choose):
+    with pytest.raises(ValueError, match="choose must be"):
+        choose_unique([0, 1, 0], choose=choose)
+
+
+@pytest.mark.parametrize(
+    "order_by",
+    (
+        ([0, 1, 2, 3],),
+        ([0, 1],),
+        ([0, 1, 2], [0]),
+    ),
+)
+def test_choose_unique_bad_order_by(order_by):
+    with pytest.raises(ValueError, match="All `order_by` arrays must match"):
+        choose_unique([0, 1, 0], order_by=order_by)
+
+
+def test_choose_unique_normal():
+    actual = choose_unique([10, 11, 10], choose="last")
+    assert_array_equal(actual, [1, 2])
+
+    actual = choose_unique([10, 11, 10], choose="first")
+    assert_array_equal(actual, [0, 1])
+
+
+@pytest.mark.parametrize(
+    "values, order_by, choose, expected",
+    (
+        ([10, 11, 10], ([5.0, 6.0, 4.0],), "last", [0, 1]),
+        ([10, 11, 10], ([5.0, 6.0, 4.0],), "first", [1, 2]),
+        ([10, 11, 10], ([4.0, 6.0, 5.0],), "last", [1, 2]),
+        ([10, 11, 10], ([4.0, 6.0, 5.0],), "first", [0, 1]),
+        ([10, 11, 10], ([4.0, 6.0, 5.0], [5.0, 6.0, 4.0]), "first", [1, 2]),
+    ),
+)
+def test_choose_unique_with_order_by(values, order_by, choose, expected):
+    actual = choose_unique(values, order_by=order_by, choose=choose)
+    assert_array_equal(actual, expected)
+
+
 def test_create_lol():
     a_list = [1, 2, 3, 3, 2, 4, 5, 6]
     linkID = [0, 0, 0, 1, 1, 1, 2, 2]
