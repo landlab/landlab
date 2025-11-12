3239e1e85ce49e99f40399fcfcb5eb9ce7247fbc
mcflugen <mcflugen@gmail.com>
2025-11-12 09:46:36 -0700

    add unit test for calc_weighted_mean_of_parallel_links

---
+++ tests/components/overland_flow/test_calc.py
@@ -0,0 +1,57 @@
+import numpy as np
+import pytest
+from numpy.testing import assert_array_equal
+
+from landlab import RasterModelGrid
+from landlab.components.overland_flow._calc import calc_weighted_mean_of_parallel_links
+
+
+def reference(vals, parallels, status, where, theta):
+    out = np.array(vals)
+    for link in where:
+        l, r = parallels[link]
+        if l == -1 or r == -1 or status[l] == 4 or status[r] == 4:
+            pass
+        else:
+            out[link] = theta * vals[link] + 0.5 * (1 - theta) * (vals[l] + vals[r])
+
+    return out
+
+
+@pytest.mark.parametrize("values_dtype", (np.float32, np.float64))
+@pytest.mark.parametrize("where_dtype", (np.int32, np.int64))
+@pytest.mark.parametrize("theta", (0.0, 0.25, 0.5, 0.75, 1.0))
+def test_kernel(values_dtype, where_dtype, theta):
+    grid = RasterModelGrid((3, 4))
+
+    values_at_link = np.asarray(
+        [
+            *[0, 1, 3],
+            *[3, 5, 9, 11],
+            *[0, 1, 3],
+            *[3, 5, 9, 11],
+            *[0, 1, 3],
+        ],
+        dtype=values_dtype,
+    )
+    n_links = len(values_at_link)
+
+    actual = np.empty_like(values_at_link)
+    calc_weighted_mean_of_parallel_links(
+        values_at_link,
+        grid.parallel_links_at_link.astype(where_dtype),
+        np.zeros(n_links, dtype=np.uint8),
+        np.arange(n_links, dtype=where_dtype),
+        theta,
+        actual,
+    )
+
+    expected = reference(
+        values_at_link,
+        grid.parallel_links_at_link,
+        np.zeros(n_links),
+        np.arange(n_links),
+        theta,
+    )
+
+    assert_array_equal(actual, expected)
