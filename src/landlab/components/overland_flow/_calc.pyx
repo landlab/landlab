487f58b5132ae1f814117c26900c18391a4cf099
mcflugen <mcflugen@gmail.com>
2025-11-16 10:01:23 -0700

    add cython calc_discharge_at_links

--- src/landlab/components/overland_flow/_calc.pyx
+++ src/landlab/components/overland_flow/_calc.pyx
@@ -3,6 +3,7 @@ from cython.parallel cimport prange
 from libc.math cimport copysign
 from libc.math cimport fabs
 from libc.math cimport fmin
+from libc.math cimport pow
 from libc.math cimport sqrt
 from libc.stdint cimport int32_t
 from libc.stdint cimport int64_t
@@ -131,6 +132,55 @@ def adjust_supercritical_discharge(
         h = h_at_link[link]
 
         out[link] = copysign(fmin(fabs(q), factor * h * sqrt(h)), q)
+
+    return (<object>out).base
+
+
+@cython.boundscheck(False)
+@cython.wraparound(False)
+@cython.nonecheck(False)
+def calc_discharge_at_links(
+    const cython.floating [::1] q_at_link,
+    const cython.floating [::1] q_mean_at_link,
+    const cython.floating [::1] h_at_link,
+    const cython.floating [::1] water_slope_at_link,
+    const cython.floating [::1] mannings_at_link,
+    *,
+    const double g,
+    const double dt,
+    const id_t [::1] where,
+    cython.floating [::1] out,
+):
+    cdef Py_ssize_t n_links = where.shape[0]
+    cdef Py_ssize_t link
+    cdef Py_ssize_t i
+    cdef double h_to_seven_thirds
+    cdef double numerator
+    cdef double denominator
+    cdef double mannings
+    cdef double h
+    cdef double g_times_dt = g * dt
+    cdef double seven_thirds = 7.0 / 3.0
+
+    for i in prange(n_links, nogil=True, schedule="static"):
+        link = where[i]
+        h = h_at_link[link]
+
+        if h > 0.0:
+            mannings = mannings_at_link[link]
+            h_to_seven_thirds = pow(h, seven_thirds)
+
+            numerator = (
+                q_mean_at_link[link] - g_times_dt * h * water_slope_at_link[link]
+            )
+
+            denominator = (
+                1.0
+                + g_times_dt * mannings * mannings * fabs(q_at_link[link])
+                / h_to_seven_thirds
+            )
+
+            out[link] = numerator / denominator
 
     return (<object>out).base
 
