5880dbeca711e5afdd7a0b8c12beda39f0253968
Jeffrey Keck <31672064+keckje@users.noreply.github.com>
2026-02-13 12:30:13 -0800

    improved load_params.py error messages and tests

--- src/landlab/core/model_parameter_loader.py
+++ src/landlab/core/model_parameter_loader.py
@@ -37,10 +37,15 @@ def load_file_contents(file_like):
     try:
         contents = file_like.read()
     except AttributeError:  # was a str
+        root, ext = os.path.splitext(file_like)
         if os.path.isfile(file_like):
             with open(file_like) as fp:
                 contents = fp.read()
-        else:
+        # if file was not found but string looks like a file path string (includes an extension)
+        # throw a FileNotFoundError.
+        elif ext:
+            raise FileNotFoundError(f"The parameter file '{file_like}' was not found")
+        else:  # otherwise treat string as raw YAML
             contents = file_like
 
     return contents
@@ -75,10 +80,25 @@ def load_params(file_like):
     (0.0, 10.0, 2.0)
     """
     contents = load_file_contents(file_like)
+    try:
+        params = yaml.load(contents, Loader=_loader)
+    except yaml.YAMLError as exc:
+        raise ValueError(f"Error parsing YAML content: {exc}") from exc
 
-    params = yaml.load(contents, Loader=_loader)
+    if params is None:
+        if os.path.isfile(file_like):
+            raise ValueError("File found but is empty")
+        else:
+            raise ValueError("The parameter file is empty")
 
     if not isinstance(params, dict):
-        raise ValueError("parsing of parameter file did not produce a dict-like object")
+        if os.path.isfile(file_like):
+            raise ValueError(
+                "File found but parsing did not produce a dict-like object. Ensure that your file uses 'key: value' syntax"
+            )
+        else:
+            raise ValueError(
+                f"The YAML content was parsed successfully but produced a {type(params).__name__} instead of a dictionary. Ensure that your file uses 'key: value' syntax"
+            )
 
     return params
--- tests/core/test_load_params.py
+++ tests/core/test_load_params.py
@@ -1,6 +1,6 @@
 from io import StringIO
-
 from landlab.core import load_params
+import pytest
 
 YAML_PARAMS_STR = """
 x: 1e7
@@ -8,6 +8,18 @@ y: 1
 z: [1, 2]
 a: frog
 """
+
+NOT_A_YAML_PARAMS_STR = """
+x: 1e7
+y: 1
+z: [1, 2]
+a: frog
+a
+"""
+
+YAML_PARAMS_LIST_STR = """[2,3,2,1]"""
+
+YAML_PARAMS_EMPTY_STR = """ """
 
 YAML_PARAMS = {"x": 1e7, "y": 1, "z": [1, 2], "a": "frog"}
 
@@ -37,3 +49,67 @@ def test_from_yaml_path(tmpdir):
     assert params == YAML_PARAMS
     assert isinstance(params["x"], float)
     assert isinstance(params["y"], int)
+
+
+def test_read_text_file(tmpdir):
+    """load parameters from a text file"""
+    with tmpdir.as_cwd():
+        with open("params.txt", "w") as fp:
+            fp.write(YAML_PARAMS_STR)
+        params = load_params("./params.txt")
+    assert params == YAML_PARAMS
+    assert isinstance(params["x"], float)
+    assert isinstance(params["y"], int)
+
+
+def test_read_file_no_extension(tmpdir):
+    """load parameters from a text file that lacks an extension"""
+    with tmpdir.as_cwd():
+        with open("params", "w") as fp:
+            fp.write(YAML_PARAMS_STR)
+        params = load_params("./params")
+    assert params == YAML_PARAMS
+    assert isinstance(params["x"], float)
+    assert isinstance(params["y"], int)
+
+
+def test_file_found_but_faulty_yaml_syntax(tmpdir):
+    with tmpdir.as_cwd():
+        with open("params_not_yaml.txt", "w") as fp:
+            fp.write(NOT_A_YAML_PARAMS_STR)
+        with pytest.raises(ValueError):
+            load_params("./params_not_yaml.txt")
+
+
+def test_file_found_but_not_a_dict(tmpdir):
+    with tmpdir.as_cwd():
+        with open("params_list.txt", "w") as fp:
+            fp.write(YAML_PARAMS_LIST_STR)
+        with pytest.raises(
+            ValueError,
+            match="File found but parsing did not produce a dict-like object. Ensure that your file uses 'key: value' syntax",
+        ):
+            load_params("./params_list.txt")
+
+
+def test_yaml_string_but_is_not_a_dict():
+    yaml_string = YAML_PARAMS_LIST_STR
+    with pytest.raises(
+        ValueError,
+        match="The YAML content was parsed successfully but produced a list instead of a dictionary. Ensure that your file uses 'key: value' syntax",
+    ):
+        load_params(yaml_string)
+
+
+def test_yaml_string_is_empty():
+    yaml_string = """ """
+    with pytest.raises(ValueError, match="The parameter file is empty"):
+        load_params(yaml_string)
+
+
+def test_file_found_but_is_empty(tmpdir):
+    with tmpdir.as_cwd():
+        with open("params_empty.txt", "w") as fp:
+            fp.write(YAML_PARAMS_EMPTY_STR)
+        with pytest.raises(ValueError, match="File found but is empty"):
+            load_params("./params_empty.txt")
