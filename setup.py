ac51dbd7e3ab6aa48d27229dedb3964a54c2004e
mcflugen <mcflugen@gmail.com>
2026-03-09 19:45:53 -0600

    refactor setup.py for building with openmp

--- setup.py
+++ setup.py
@@ -8,10 +8,44 @@ from setuptools import Extension
 from setuptools import setup
 from setuptools.command.build_ext import build_ext
 
-if os.environ.get("LANDLAB_BUILD_RELEASE", "0") == "1":
-    extra_compile_args = ["-O3"] if sys.platform != "win32" else ["/O2"]
+is_windows = sys.platform.startswith("win")
+is_macos = sys.platform == "darwin"
+is_linux = sys.platform.startswith("linux")
+
+openmp_prefix = os.environ.get("OPENMP_PREFIX")
+if openmp_prefix:
+    with_openmp = True
 else:
-    extra_compile_args = ["-O0"] if sys.platform != "win32" else ["/Od"]
+    if is_macos:
+        with_openmp = os.environ.get("WITH_OPENMP", "0") == "1"
+    else:
+        with_openmp = os.environ.get("WITH_OPENMP", "1") == "1"
+
+include_dirs: list[str] = []
+library_dirs: list[str] = []
+libraries: list[str] = []
+extra_compile_args: list[str] = []
+extra_link_args: list[str] = []
+
+if os.environ.get("LANDLAB_BUILD_RELEASE", "0") == "1":
+    extra_compile_args += ["-O3"] if not is_windows else ["/O2"]
+else:
+    extra_compile_args += ["-O0"] if not is_windows else ["/Od"]
+
+if openmp_prefix:
+    include_dirs += [os.path.join(openmp_prefix, "include")]
+    library_dirs += [os.path.join(openmp_prefix, "lib")]
+
+if with_openmp and is_linux:
+    extra_compile_args += ["-fopenmp"]
+    extra_link_args += ["-fopenmp"]
+
+if with_openmp and is_macos:
+    extra_compile_args += ["-Xpreprocessor", "-fopenmp"]
+    libraries += ["omp"]
+
+if with_openmp and is_windows:
+    extra_compile_args += ["/openmp"]
 
 
 def _build_cpu_count():
@@ -31,6 +65,10 @@ ext_modules = cythonize(
             [path],
             define_macros=[("NPY_NO_DEPRECATED_API", "1")],
             extra_compile_args=extra_compile_args,
+            include_dirs=include_dirs,
+            library_dirs=library_dirs,
+            libraries=libraries,
+            extra_link_args=extra_link_args,
         )
         for path in cython_files
     ],
