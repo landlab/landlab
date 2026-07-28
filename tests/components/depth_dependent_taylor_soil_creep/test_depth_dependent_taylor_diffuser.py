211573ceae72b34484cf8610b1e00a629ffcd249
Matheus Almeida <matheusalmeida0906@gmail.com>
2026-07-28 13:53:32 +0200

    adding tests for variable K

--- tests/components/depth_dependent_taylor_soil_creep/test_depth_dependent_taylor_diffuser.py
+++ tests/components/depth_dependent_taylor_soil_creep/test_depth_dependent_taylor_diffuser.py
@@ -36,7 +36,66 @@ def test_4x7_grid_vs_analytical_solution():
     )
 
     diffuser = DepthDependentTaylorDiffuser(
-        mg, linear_diffusivity=0.01, slope_crit=0.8, soil_transport_decay_depth=0.5
+        mg, soil_transport_velocity=0.01, slope_crit=0.8, soil_transport_decay_depth=0.5
+    )
+
+    # Get a reference to bedrock elevation field
+    z_bedrock = mg.at_node["bedrock__elevation"]
+
+    # Estimate a reasonable time step. Here we take advantage of the fact that
+    # we know the final slope at the outer links will be about 1.33. Stability
+    # for the cubic term involves an "effective D" parameter, Deff, that should
+    # be Deff = D (S / Sc)^2. (see notebook calcs)
+    baselevel_rate = 0.0001
+    dt = 250.0
+
+    # Run for 750 ky
+    for _ in range(3000):
+        z[mg.core_nodes] += baselevel_rate * dt
+        z_bedrock[mg.core_nodes] += baselevel_rate * dt
+
+        weatherer.calc_soil_prod_rate()
+        diffuser.run_one_step(dt)
+
+    # Test: these numbers represent equilibrium. See Jupyter notebook for
+    # calculations.
+    my_nodes = mg.nodes[2, :]
+    assert_array_equal(
+        np.round(z[my_nodes], 1), np.array([0.0, 6.2, 10.7, 12.6, 10.7, 6.2, 0.0])
+    )
+    assert_array_equal(
+        np.round(mg.at_node["soil__depth"][8:13], 2),
+        np.array([0.35, 0.35, 0.35, 0.35, 0.35]),
+    )
+
+
+def test_4x7_grid_vs_analytical_solution_array_K():
+    """Test against known analytical solution using K as an array of float."""
+
+    # Create a 4-row by 7-column grid with 10 m spacing
+    mg = RasterModelGrid((4, 7), xy_spacing=10.0)
+
+    # Close off top and bottom (N and S) boundaries so it becomes a 1D problem
+    mg.set_closed_boundaries_at_grid_edges(False, True, False, True)
+
+    # Create an elevation field, initially zero
+    z = mg.add_zeros("topographic__elevation", at="node")
+    mg.add_zeros("soil__depth", at="node")
+
+    # Create an array of K
+    k_array = np.full(mg.number_of_links, 0.01)
+    # Instantiate components, and set their parameters. Note that traditional
+    # diffusivity, D, is D = SCE x H*, where SCE is soil-creep efficiency.
+    # Here we want D = 0.01 m2/yr and H* = 0,.5 m, so cwe set SCE = 0.02.
+    weatherer = ExponentialWeatherer(
+        mg, soil_production_maximum_rate=0.0002, soil_production_decay_depth=0.5
+    )
+
+    diffuser = DepthDependentTaylorDiffuser(
+        mg,
+        soil_transport_velocity=k_array,
+        slope_crit=0.8,
+        soil_transport_decay_depth=0.5,
     )
 
     # Get a reference to bedrock elevation field
@@ -109,6 +168,45 @@ def test_infinite_taylor_error():
     expweath.calc_soil_prod_rate()
     with pytest.raises(RuntimeError):
         DDdiff.soilflux(10)
+
+
+def test_variable_K_matches_uniform_scalar():
+    mg_scalar = RasterModelGrid((5, 5))
+
+    dt = 1
+    soilTh_scalar = mg_scalar.add_zeros("soil__depth", at="node")
+    z_scalar = mg_scalar.add_zeros("topographic__elevation", at="node")
+    BRz_scalar = mg_scalar.add_zeros("bedrock__elevation", at="node")
+    z_scalar += mg_scalar.node_x.copy() ** 2
+    BRz_scalar = z_scalar.copy() - 1.0
+    soilTh_scalar[:] = z_scalar - BRz_scalar
+    expweath_scalar = ExponentialWeatherer(mg_scalar)
+    expweath_scalar.calc_soil_prod_rate()
+    DDdiff_scalar = DepthDependentTaylorDiffuser(mg_scalar, soil_transport_velocity=2.0)
+    DDdiff_scalar.run_one_step(dt)
+
+    mg_array = RasterModelGrid((5, 5))
+    soilTh = mg_array.add_zeros("soil__depth", at="node")
+    z = mg_array.add_zeros("topographic__elevation", at="node")
+    BRz = mg_array.add_zeros("bedrock__elevation", at="node")
+    z += mg_array.node_x.copy() ** 2
+    BRz = z.copy() - 1.0
+    soilTh[:] = z - BRz
+    expweath = ExponentialWeatherer(mg_array)
+    expweath.calc_soil_prod_rate()
+    k_array = np.full(mg_array.number_of_links, 2.0)
+    # Initializing component with a float
+
+    # Initializing component with an array of float
+    DDdiff_array = DepthDependentTaylorDiffuser(
+        mg_array, soil_transport_velocity=k_array
+    )
+    DDdiff_array.run_one_step(dt)
+
+    np.testing.assert_allclose(
+        mg_scalar.at_node["topographic__elevation"],
+        mg_array.at_node["topographic__elevation"],
+    )
 
 
 # def test_warn():
