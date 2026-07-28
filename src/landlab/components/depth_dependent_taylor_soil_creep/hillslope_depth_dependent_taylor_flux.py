fixing time step calculation to handle variable K

src/landlab/components/depth_dependent_taylor_soil_creep/hillslope_depth_dependent_taylor_flux.py
@@ -317,7 +317,7 @@ class DepthDependentTaylorDiffuser(Component):
         ----------
         grid: ModelGrid
             Landlab ModelGrid object
-        linear_diffusivity: float, optional, DEPRECATED
+        linear_diffusivity: float, ndarray of float, or str, optional, DEPRECATED
             Hillslope diffusivity / decay depth, m/yr
             Default = 1.0
         slope_crit: float, optional
@@ -337,7 +337,7 @@ class DepthDependentTaylorDiffuser(Component):
             "raise", "warn")
         courant_factor : float, optional
             Courant factor for timestep calculation.
-        soil_transport_velocity : float, optional, default = 1.0
+        soil_transport_velocity : float, ndarray of float, or str, optional, default = 1.0
             Velocity parameter for soil transport, m/yr. Diffusivity is the
             product of this parameter and soil_transport_decay_depth.
         """
@@ -405,7 +405,7 @@ class DepthDependentTaylorDiffuser(Component):
             # Test for time stepping courant condition
             # Test for time stepping courant condition
             courant_slope_term = 0.0
-            courant_s_over_scrit = self._slope.max() / self._slope_crit
+            courant_s_over_scrit = self._slope / self._slope_crit
             for i in range(0, 2 * self._nterms, 2):
                 courant_slope_term += courant_s_over_scrit**i
                 if np.any(np.isinf(courant_slope_term)):
@@ -417,8 +417,10 @@ class DepthDependentTaylorDiffuser(Component):
                     raise RuntimeError(message)
             # Calculate De Max
             de_max = self._K * self._soil_transport_decay_depth * courant_slope_term
-            # Calculate longest stable timestep
-            self._dt_max = self._courant_factor * self._shortest_link**2 / de_max
+            # Calculate longest stable timestep per link
+            dt_max_per_link = self._courant_factor * self._grid.length_of_link**2 / de_max
+            # Uses the lowest dt (in case of variable K) 
+            self._dt_max = np.min(dt_max_per_link)
 
             # Test for the Courant condition and print warning if user intended
             # for it to be printed.